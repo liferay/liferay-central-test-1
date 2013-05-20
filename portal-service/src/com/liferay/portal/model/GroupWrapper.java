sergiogonzalez/liferay-portal

LPS-28900 Autogenerated

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -694,6 +694,20 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getPrivateLayoutsPageCount();
 	}
 
+	public java.lang.String getScopeName(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _group.getScopeName(themeDisplay);
+	}
+
+	public java.lang.String getScopeType(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _group.getScopeType(themeDisplay);
+	}
+
 	public com.liferay.portal.model.LayoutSet getPublicLayoutSet() {
 		return _group.getPublicLayoutSet();
 	}

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/model/GroupWrapper.java
@@ -702,6 +702,18 @@ public class GroupWrapper implements Group, ModelWrapper<Group> {
 		return _group.getPublicLayoutsPageCount();
 	}
 
+	public java.lang.String getScopeDescriptiveName(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _group.getScopeDescriptiveName(themeDisplay);
+	}
+
+	public java.lang.String getScopeLabel(
+		com.liferay.portal.theme.ThemeDisplay themeDisplay) {
+		return _group.getScopeLabel(themeDisplay);
+	}
+
 	public com.liferay.portal.model.Group getStagingGroup() {
 		return _group.getStagingGroup();
 	}