JorgeFerrer/liferay-portal

LPS-35438 getRedirect must specify whether we need to send redirect or doing set forward is enough. There are some cases where we need to force the send redirect because we need to redirect the user to the right layout friendly url in case the locale of the request and the locale of the friendly url don't match.
The code of PortalImpl.getLocalizedFriendlyURL() is derived from the one of I18nFilter, but we couldn't find a way to reuse it due to the differences.

portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -36,6 +37,7 @@ import com.liferay.portal.util.WebKeys;
 import java.io.IOException;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -93,9 +95,16 @@ public class FriendlyURLServlet extends HttpServlet {
 		request.setAttribute(
 			WebKeys.FRIENDLY_URL, _friendlyURLPathPrefix.concat(pathInfo));
 
+		Object[] redirectObj = null;
+
+		boolean forceRedirect = false;
+
 		try {
-			redirect = getRedirect(
+			redirectObj = getRedirect(
 				request, pathInfo, mainPath, request.getParameterMap());
+
+			redirect = (String)redirectObj[0];
+			forceRedirect = GetterUtil.getBoolean(redirectObj[1]);
 
 			if (request.getAttribute(WebKeys.LAST_PATH) == null) {
 				LastPath lastPath = new LastPath(
@@ -127,7 +136,7 @@ public class FriendlyURLServlet extends HttpServlet {
 			_log.debug("Redirect " + redirect);
 		}
 
-		if (redirect.charAt(0) == CharPool.SLASH) {
+		if ((redirect.charAt(0) == CharPool.SLASH) && !forceRedirect) {
 			ServletContext servletContext = getServletContext();
 
 			RequestDispatcher requestDispatcher =
@@ -142,13 +151,13 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 	}
 
-	protected String getRedirect(
+	protected Object[] getRedirect(
 			HttpServletRequest request, String path, String mainPath,
 			Map<String, String[]> params)
 		throws Exception {
 
 		if (Validator.isNull(path) || (path.charAt(0) != CharPool.SLASH)) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		// Group friendly URL
@@ -165,7 +174,7 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		if (Validator.isNull(friendlyURL)) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		long companyId = PortalInstances.getCompanyId(request);
@@ -215,7 +224,7 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		if (group == null) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		// Layout friendly URL
@@ -244,9 +253,40 @@ public class FriendlyURLServlet extends HttpServlet {
 			ServiceContextThreadLocal.pushServiceContext(serviceContext);
 		}
 
-		return PortalUtil.getActualURL(
+		String actualURL = PortalUtil.getActualURL(
 			group.getGroupId(), _private, mainPath, friendlyURL, params,
 			requestContext);
+
+		if (Validator.isNotNull(friendlyURL)) {
+			Locale locale = PortalUtil.getLocale(request);
+
+			Object[] actualLayout = PortalUtil.getActualLayout(
+				group.getGroupId(), _private, friendlyURL, params,
+				requestContext);
+
+			Layout layout = (Layout)actualLayout[0];
+
+			if (isInvalidLocalizedFriendlyURL(layout, friendlyURL, locale)) {
+				String redirect = PortalUtil.getLocalizedFriendlyURL(
+					request, layout, locale);
+
+				return new Object[] {redirect, true};
+			}
+		}
+
+		return new Object[] {actualURL, false};
+	}
+
+	protected boolean isInvalidLocalizedFriendlyURL(
+			Layout layout, String friendlyURL, Locale locale)
+		throws Exception {
+
+		if (!friendlyURL.startsWith(layout.getFriendlyURL(locale))) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(FriendlyURLServlet.class);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/servlet/FriendlyURLServlet.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.util.CharPool;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.User;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.ServiceContext;
@@ -36,6 +37,7 @@ import com.liferay.portal.util.WebKeys;
 import java.io.IOException;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -93,9 +95,16 @@ public class FriendlyURLServlet extends HttpServlet {
 		request.setAttribute(
 			WebKeys.FRIENDLY_URL, _friendlyURLPathPrefix.concat(pathInfo));
 
+		Object[] object = null;
+
+		boolean forceRedirect = false;
+
 		try {
-			redirect = getRedirect(
+			object = getRedirect(
 				request, pathInfo, mainPath, request.getParameterMap());
+
+			redirect = (String)object[0];
+			forceRedirect = GetterUtil.getBoolean(object[1]);
 
 			if (request.getAttribute(WebKeys.LAST_PATH) == null) {
 				LastPath lastPath = new LastPath(
@@ -127,7 +136,7 @@ public class FriendlyURLServlet extends HttpServlet {
 			_log.debug("Redirect " + redirect);
 		}
 
-		if (redirect.charAt(0) == CharPool.SLASH) {
+		if ((redirect.charAt(0) == CharPool.SLASH) && !forceRedirect) {
 			ServletContext servletContext = getServletContext();
 
 			RequestDispatcher requestDispatcher =
@@ -142,13 +151,13 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 	}
 
-	protected String getRedirect(
+	protected Object[] getRedirect(
 			HttpServletRequest request, String path, String mainPath,
 			Map<String, String[]> params)
 		throws Exception {
 
 		if (Validator.isNull(path) || (path.charAt(0) != CharPool.SLASH)) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		// Group friendly URL
@@ -165,7 +174,7 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		if (Validator.isNull(friendlyURL)) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		long companyId = PortalInstances.getCompanyId(request);
@@ -215,7 +224,7 @@ public class FriendlyURLServlet extends HttpServlet {
 		}
 
 		if (group == null) {
-			return mainPath;
+			return new Object[] {mainPath, false};
 		}
 
 		// Layout friendly URL
@@ -244,9 +253,41 @@ public class FriendlyURLServlet extends HttpServlet {
 			ServiceContextThreadLocal.pushServiceContext(serviceContext);
 		}
 
-		return PortalUtil.getActualURL(
+		String actualURL = PortalUtil.getActualURL(
 			group.getGroupId(), _private, mainPath, friendlyURL, params,
 			requestContext);
+
+		if (Validator.isNotNull(friendlyURL)) {
+			Locale locale = PortalUtil.getLocale(request);
+
+			Object[] object = PortalUtil.getLayout(
+				group.getGroupId(), _private, friendlyURL, params,
+				requestContext);
+
+			Layout layout = (Layout)object[0];
+			friendlyURL = (String)object[1];
+
+			if (isInvalidLocalizedFriendlyURL(layout, friendlyURL, locale)) {
+				String redirect = PortalUtil.getLocalizedFriendlyURL(
+					request, layout, locale);
+
+				return new Object[] {redirect, true};
+			}
+		}
+
+		return new Object[] {actualURL, false};
+	}
+
+	protected boolean isInvalidLocalizedFriendlyURL(
+			Layout layout, String friendlyURL, Locale locale)
+		throws Exception {
+
+		if (!friendlyURL.startsWith(layout.getFriendlyURL(locale))) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	private static Log _log = LogFactoryUtil.getLog(FriendlyURLServlet.class);