zberentey/liferay-portal

LPS-35256 Create SystemEvent annotation and advice that can automatically generate system event calls and handle hierarchy

portal-service/src/com/liferay/portal/kernel/systemevents/SystemEvent.java
@@ -1,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.systemevents;
+
+import com.liferay.portal.model.SystemEventConstants;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Zsolt Berentey
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SystemEvent {
+
+	public int action() default SystemEventConstants.ACTION_NONE;
+
+	public boolean sendEvent() default true;
+
+	public int type();
+
+}

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/kernel/systemevents/SystemEvent.java
@@ -1,0 +1,39 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.systemevents;
+
+import com.liferay.portal.model.SystemEventConstants;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Zsolt Berentey
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface SystemEvent {
+
+	public int childAction() default SystemEventConstants.ACTION_NONE;
+
+	public boolean sendEvent() default true;
+
+	public int type() default 0;
+
+}