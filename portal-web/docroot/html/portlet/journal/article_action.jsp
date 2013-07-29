juliocamarero/liferay-portal

LPS-35662 Change the request for the preview in saved articles and versions

portal-web/docroot/html/portlet/journal/article_action.jsp
@@ -81,18 +81,10 @@ else {
 		</c:if>
 
 		<c:if test="<%= JournalArticlePermission.contains(permissionChecker, article, ActionKeys.VIEW) %>">
-			<portlet:actionURL var="previewURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
-				<portlet:param name="struts_action" value="/journal/preview_article_content" />
-				<portlet:param name="<%= Constants.CMD %>" value="<%= Constants.VIEW %>" />
-				<portlet:param name="groupId" value="<%= String.valueOf(article.getGroupId()) %>" />
-				<portlet:param name="articleId" value="<%= article.getArticleId() %>" />
-				<portlet:param name="version" value="<%= String.valueOf(article.getVersion()) %>" />
-			</portlet:actionURL>
-
 			<liferay-ui:icon
+				id='<%= row.getPos() + "viewButton" %>'
 				image="view"
-				target="_blank"
-				url="<%= previewURL.toString() %>"
+				url="javascript:;"
 			/>
 
 			<c:if test="<%= JournalPermission.contains(permissionChecker, scopeGroupId, ActionKeys.ADD_ARTICLE) %>">
@@ -135,4 +127,30 @@ else {
 			<liferay-ui:icon-delete trash="<%= TrashUtil.isTrashEnabled(scopeGroupId) %>" url="<%= deleteURL %>" />
 		</c:if>
 	</liferay-ui:icon-menu>
+
+	<aui:script use="aui-base">
+		<portlet:renderURL var="previewArticleContentURL" windowState="<%= LiferayWindowState.POP_UP.toString() %>">
+			<portlet:param name="struts_action" value="/journal/preview_article_content" />
+			<portlet:param name="groupId" value="<%= String.valueOf(article.getGroupId()) %>" />
+			<portlet:param name="articleId" value="<%= article.getArticleId() %>" />
+			<portlet:param name="version" value="<%= String.valueOf(article.getVersion()) %>" />
+		</portlet:renderURL>
+
+		var viewButton = A.one('#<%= portletDisplay.getNamespace() + row.getPos() + "viewButton" %>');
+
+		viewButton.on(
+			'click',
+			function(event) {
+				event.preventDefault();
+
+				Liferay.Util.openWindow(
+					{
+						cache: false,
+						title: '<%= article.getTitle(locale) %>',
+						uri: '<%= previewArticleContentURL.toString() %>'
+					}
+				);
+			}
+		);
+	</aui:script>
 </span>

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/journal/article_action.jsp
@@ -80,19 +80,22 @@ else {
 			/>
 		</c:if>
 
-		<c:if test="<%= JournalArticlePermission.contains(permissionChecker, article, ActionKeys.VIEW) %>">
-			<portlet:actionURL var="previewURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
-				<portlet:param name="struts_action" value="/journal/preview_article_content" />
-				<portlet:param name="<%= Constants.CMD %>" value="<%= Constants.VIEW %>" />
-				<portlet:param name="groupId" value="<%= String.valueOf(article.getGroupId()) %>" />
-				<portlet:param name="articleId" value="<%= article.getArticleId() %>" />
-				<portlet:param name="version" value="<%= String.valueOf(article.getVersion()) %>" />
-			</portlet:actionURL>
+		<portlet:renderURL var="previewArticleContentURL" windowState="<%= LiferayWindowState.EXCLUSIVE.toString() %>">
+			<portlet:param name="struts_action" value="/journal/preview_article_content" />
+			<portlet:param name="groupId" value="<%= String.valueOf(article.getGroupId()) %>" />
+			<portlet:param name="articleId" value="<%= article.getArticleId() %>" />
+			<portlet:param name="version" value="<%= String.valueOf(article.getVersion()) %>" />
+		</portlet:renderURL>
 
+		<%
+		String previewOnClick = "Liferay.fire('previewArticle', {title: '" + article.getTitle(locale) + "', uri: '" + previewArticleContentURL.toString() + "'});";
+		%>
+
+		<c:if test="<%= JournalArticlePermission.contains(permissionChecker, article, ActionKeys.VIEW) %>">
 			<liferay-ui:icon
-				image="view"
-				target="_blank"
-				url="<%= previewURL.toString() %>"
+				image="preview"
+				onClick="<%= previewOnClick %>"
+				url="javascript:;"
 			/>
 
 			<c:if test="<%= JournalPermission.contains(permissionChecker, scopeGroupId, ActionKeys.ADD_ARTICLE) %>">