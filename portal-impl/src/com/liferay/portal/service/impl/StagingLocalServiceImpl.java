mhan810/liferay-portal

LPS-38176 Adding new validate method to StagingService with additional helper methods and logic changes

portal-impl/src/com/liferay/portal/service/impl/StagingLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.lar.MissingReferences;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
@@ -32,6 +33,7 @@ import com.liferay.portal.service.base.StagingLocalServiceBaseImpl;
 import com.liferay.portal.util.PortletKeys;
 import com.liferay.portlet.documentlibrary.NoSuchFolderException;
 import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.documentlibrary.store.DLStoreUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -43,6 +45,7 @@ import java.util.Map;
 
 /**
  * @author Michael C. Han
+ * @author Mate Thurzo
  */
 public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 
@@ -81,54 +84,14 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 
 	@Override
 	public void publishStagingRequest(
-			long userId, long stagingRequestId, boolean privateLayout,
-			Map<String, String[]> parameterMap)
+			long userId, long groupId, long stagingRequestId,
+			boolean privateLayout, Map<String, String[]> parameterMap)
 		throws PortalException, SystemException {
 
-		File file = null;
+		File file = checkStagingRequestFile(userId, stagingRequestId);
 
-		FileOutputStream fileOutputStream = null;
-
-		try {
-			file = FileUtil.createTempFile("lar");
-
-			fileOutputStream = new FileOutputStream(file);
-
-			Folder folder = PortletFileRepositoryUtil.getPortletFolder(
-				stagingRequestId);
-
-			List<FileEntry> fileEntries =
-				PortletFileRepositoryUtil.getPortletFileEntries(
-					folder.getGroupId(), folder.getFolderId());
-
-			for (FileEntry fileEntry : fileEntries) {
-				InputStream inputStream = fileEntry.getContentStream();
-
-				try {
-					StreamUtil.transfer(inputStream, fileOutputStream, false);
-				}
-				finally {
-					StreamUtil.cleanUp(inputStream);
-				}
-			}
-
-			String checksum = FileUtil.getMD5Checksum(file);
-
-			if (!checksum.equals(folder.getName())) {
-				throw new SystemException("Invalid checksum for LAR file");
-			}
-
-			layoutLocalService.importLayouts(
-				userId, folder.getGroupId(), privateLayout, parameterMap, file);
-		}
-		catch (IOException ioe) {
-			throw new SystemException("Unable to reassemble LAR file", ioe);
-		}
-		finally {
-			StreamUtil.cleanUp(fileOutputStream);
-
-			FileUtil.delete(file);
-		}
+		layoutLocalService.importLayouts(
+			userId, groupId, privateLayout, parameterMap, file);
 	}
 
 	@Override
@@ -148,6 +111,106 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 			new UnsyncByteArrayInputStream(bytes), fileName,
 			ContentTypes.APPLICATION_ZIP, false);
 	}
+
+	@Override
+	public MissingReferences validateStagingRequest(
+			long userId, long groupId, long stagingRequestId,
+			boolean privateLayout, Map<String, String[]> parameterMap)
+		throws PortalException, SystemException {
+
+		File file = checkStagingRequestFile(userId, stagingRequestId);
+
+		return layoutLocalService.validateImportLayoutsFile(
+			userId, groupId, privateLayout, parameterMap, file);
+	}
+
+	protected File checkStagingRequestFile(long userId, long stagingRequestId)
+		throws PortalException, SystemException {
+
+		Folder folder = PortletFileRepositoryUtil.getPortletFolder(
+			stagingRequestId);
+
+		File file = getStagingRequestFile(stagingRequestId, folder);
+
+		if (file != null) {
+			return file;
+		}
+
+		FileOutputStream fileOutputStream = null;
+
+		try {
+			file = FileUtil.createTempFile("lar");
+
+			fileOutputStream = new FileOutputStream(file);
+
+			List<FileEntry> fileEntries =
+				PortletFileRepositoryUtil.getPortletFileEntries(
+					folder.getGroupId(), folder.getFolderId());
+
+			for (FileEntry fileEntry : fileEntries) {
+				InputStream inputStream = fileEntry.getContentStream();
+
+				try {
+					StreamUtil.transfer(inputStream, fileOutputStream, false);
+				}
+				finally {
+					StreamUtil.cleanUp(inputStream);
+
+					PortletFileRepositoryUtil.deletePortletFileEntry(
+						fileEntry.getFileEntryId());
+				}
+			}
+
+			String checksum = FileUtil.getMD5Checksum(file);
+
+			if (!checksum.equals(folder.getName())) {
+				throw new SystemException("Invalid checksum for LAR file");
+			}
+
+			storeStagingRequestFile(userId, stagingRequestId, folder, file);
+
+			return getStagingRequestFile(stagingRequestId, folder);
+		}
+		catch (IOException ioe) {
+			throw new SystemException("Unable to reassemble LAR file", ioe);
+		}
+		finally {
+			StreamUtil.cleanUp(fileOutputStream);
+
+			FileUtil.delete(file);
+		}
+	}
+
+	protected File getStagingRequestFile(
+		long stagingRequestId, Folder parentFolder) {
+
+		try {
+			FileEntry fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+				parentFolder.getGroupId(), parentFolder.getFolderId(),
+				_ASSEMBLED_LAR_PREFIX + String.valueOf(stagingRequestId));
+
+			return DLStoreUtil.getFile(
+				fileEntry.getCompanyId(), fileEntry.getRepositoryId(),
+				fileEntry.getTitle());
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	protected void storeStagingRequestFile(
+			long userId, long stagingRequestId, Folder parentFolder, File file)
+		throws PortalException, SystemException {
+
+		PortletFileRepositoryUtil.addPortletFileEntry(
+			parentFolder.getGroupId(), userId, Group.class.getName(),
+			parentFolder.getGroupId(), PortletKeys.SITES_ADMIN,
+			parentFolder.getFolderId(), file,
+			_ASSEMBLED_LAR_PREFIX + String.valueOf(stagingRequestId),
+			ContentTypes.APPLICATION_ZIP, false);
+	}
+
+	private static final String _ASSEMBLED_LAR_PREFIX = "assembled_";
 
 	private static Log _log = LogFactoryUtil.getLog(
 		StagingLocalServiceImpl.class);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/service/impl/StagingLocalServiceImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.service.impl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
+import com.liferay.portal.kernel.lar.MissingReferences;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
@@ -24,6 +25,7 @@ import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Repository;
 import com.liferay.portal.portletfilerepository.PortletFileRepositoryUtil;
@@ -43,6 +45,7 @@ import java.util.Map;
 
 /**
  * @author Michael C. Han
+ * @author Mate Thurzo
  */
 public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 
@@ -85,50 +88,15 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 			Map<String, String[]> parameterMap)
 		throws PortalException, SystemException {
 
-		File file = null;
+		Folder folder = PortletFileRepositoryUtil.getPortletFolder(
+			stagingRequestId);
 
-		FileOutputStream fileOutputStream = null;
+		FileEntry stagingRequestFileEntry = getStagingRequestFileEntry(
+			userId, stagingRequestId, folder);
 
-		try {
-			file = FileUtil.createTempFile("lar");
-
-			fileOutputStream = new FileOutputStream(file);
-
-			Folder folder = PortletFileRepositoryUtil.getPortletFolder(
-				stagingRequestId);
-
-			List<FileEntry> fileEntries =
-				PortletFileRepositoryUtil.getPortletFileEntries(
-					folder.getGroupId(), folder.getFolderId());
-
-			for (FileEntry fileEntry : fileEntries) {
-				InputStream inputStream = fileEntry.getContentStream();
-
-				try {
-					StreamUtil.transfer(inputStream, fileOutputStream, false);
-				}
-				finally {
-					StreamUtil.cleanUp(inputStream);
-				}
-			}
-
-			String checksum = FileUtil.getMD5Checksum(file);
-
-			if (!checksum.equals(folder.getName())) {
-				throw new SystemException("Invalid checksum for LAR file");
-			}
-
-			layoutLocalService.importLayouts(
-				userId, folder.getGroupId(), privateLayout, parameterMap, file);
-		}
-		catch (IOException ioe) {
-			throw new SystemException("Unable to reassemble LAR file", ioe);
-		}
-		finally {
-			StreamUtil.cleanUp(fileOutputStream);
-
-			FileUtil.delete(file);
-		}
+		layoutLocalService.importLayouts(
+			userId, folder.getGroupId(), privateLayout, parameterMap,
+			stagingRequestFileEntry.getContentStream());
 	}
 
 	@Override
@@ -148,6 +116,110 @@ public class StagingLocalServiceImpl extends StagingLocalServiceBaseImpl {
 			new UnsyncByteArrayInputStream(bytes), fileName,
 			ContentTypes.APPLICATION_ZIP, false);
 	}
+
+	@Override
+	public MissingReferences validateStagingRequest(
+			long userId, long stagingRequestId, boolean privateLayout,
+			Map<String, String[]> parameterMap)
+		throws PortalException, SystemException {
+
+		Folder folder = PortletFileRepositoryUtil.getPortletFolder(
+			stagingRequestId);
+
+		FileEntry fileEntry = getStagingRequestFileEntry(
+			userId, stagingRequestId, folder);
+
+		return layoutLocalService.validateImportLayoutsFile(
+			userId, folder.getGroupId(), privateLayout, parameterMap,
+			fileEntry.getContentStream());
+	}
+
+	protected FileEntry fetchStagingRequestFileEntry(
+			long stagingRequestId, Folder folder)
+		throws PortalException, SystemException {
+
+		try {
+			return PortletFileRepositoryUtil.getPortletFileEntry(
+				folder.getGroupId(), folder.getFolderId(),
+				_ASSEMBLED_LAR_PREFIX + String.valueOf(stagingRequestId));
+		}
+		catch (Exception e) {
+			return null;
+		}
+	}
+
+	protected FileEntry getStagingRequestFileEntry(
+			long userId, long stagingRequestId, Folder folder)
+		throws PortalException, SystemException {
+
+		FileEntry stagingRequestFileEntry = fetchStagingRequestFileEntry(
+			stagingRequestId, folder);
+
+		if (stagingRequestFileEntry != null) {
+			return stagingRequestFileEntry;
+		}
+
+		FileOutputStream fileOutputStream = null;
+
+		File tempFile = null;
+
+		try {
+			tempFile = FileUtil.createTempFile("lar");
+
+			fileOutputStream = new FileOutputStream(tempFile);
+
+			List<FileEntry> fileEntries =
+				PortletFileRepositoryUtil.getPortletFileEntries(
+					folder.getGroupId(), folder.getFolderId());
+
+			for (FileEntry fileEntry : fileEntries) {
+				InputStream inputStream = fileEntry.getContentStream();
+
+				try {
+					StreamUtil.transfer(inputStream, fileOutputStream, false);
+				}
+				finally {
+					StreamUtil.cleanUp(inputStream);
+
+					PortletFileRepositoryUtil.deletePortletFileEntry(
+						fileEntry.getFileEntryId());
+				}
+			}
+
+			String checksum = FileUtil.getMD5Checksum(tempFile);
+
+			if (!checksum.equals(folder.getName())) {
+				throw new SystemException("Invalid checksum for LAR file");
+			}
+
+			PortletFileRepositoryUtil.addPortletFileEntry(
+				folder.getGroupId(), userId, Group.class.getName(),
+				folder.getGroupId(), PortletKeys.SITES_ADMIN,
+				folder.getFolderId(), tempFile,
+				_ASSEMBLED_LAR_PREFIX + StringPool.UNDERLINE +
+					String.valueOf(stagingRequestId) + ".lar",
+				ContentTypes.APPLICATION_ZIP, false);
+
+			stagingRequestFileEntry = fetchStagingRequestFileEntry(
+				stagingRequestId, folder);
+
+			if (stagingRequestFileEntry == null) {
+				throw new SystemException("Unable to assemble LAR file");
+			}
+
+			return stagingRequestFileEntry;
+		}
+		catch (IOException ioe) {
+			throw new SystemException("Unable to reassemble LAR file", ioe);
+		}
+		finally {
+			StreamUtil.cleanUp(fileOutputStream);
+
+			FileUtil.delete(tempFile);
+		}
+	}
+
+	private static final String _ASSEMBLED_LAR_PREFIX = "assembled_";
 
 	private static Log _log = LogFactoryUtil.getLog(
 		StagingLocalServiceImpl.class);