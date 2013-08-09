moltam89/liferay-portal

LPS-37302 Autogenerated

portal-service/src/com/liferay/portal/model/LayoutRevision.java
@@ -69,6 +69,8 @@ public interface LayoutRevision extends LayoutRevisionModel, PersistedModel {
 	public boolean hasChildren()
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public boolean isContentDisplayPage();
+
 	public boolean isInheritLookAndFeel();
 
 	public boolean isInheritWapLookAndFeel();

portal-service/src/com/liferay/portal/model/LayoutRevisionWrapper.java
@@ -1888,6 +1888,11 @@ public class LayoutRevisionWrapper implements LayoutRevision,
 	}
 
 	@Override
+	public boolean isContentDisplayPage() {
+		return _layoutRevision.isContentDisplayPage();
+	}
+
+	@Override
 	public boolean isInheritLookAndFeel() {
 		return _layoutRevision.isInheritLookAndFeel();
 	}

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/model/LayoutRevision.java
@@ -69,6 +69,8 @@ public interface LayoutRevision extends LayoutRevisionModel, PersistedModel {
 	public boolean hasChildren()
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public boolean hasDefaultAssetPublisherPortletId();
+
 	public boolean isInheritLookAndFeel();
 
 	public boolean isInheritWapLookAndFeel();

portal-service/src/com/liferay/portal/model/LayoutRevisionWrapper.java
@@ -1888,6 +1888,11 @@ public class LayoutRevisionWrapper implements LayoutRevision,
 	}
 
 	@Override
+	public boolean hasDefaultAssetPublisherPortletId() {
+		return _layoutRevision.hasDefaultAssetPublisherPortletId();
+	}
+
+	@Override
 	public boolean isInheritLookAndFeel() {
 		return _layoutRevision.isInheritLookAndFeel();
 	}