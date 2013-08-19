brianchandotcom/liferay-portal

LPS-39133 Regenerate

portal-impl/src/com/liferay/portal/service/persistence/ResourceTypePermissionPersistenceImpl.java
@@ -1508,6 +1508,10 @@ public class ResourceTypePermissionPersistenceImpl extends BasePersistenceImpl<R
 	private static final String _FINDER_COLUMN_C_G_N_R_NAME_3 = "(resourceTypePermission.name IS NULL OR resourceTypePermission.name = '') AND ";
 	private static final String _FINDER_COLUMN_C_G_N_R_ROLEID_2 = "resourceTypePermission.roleId = ?";
 
+	public ResourceTypePermissionPersistenceImpl() {
+		super(ResourceTypePermission.class);
+	}
+
 	/**
 	 * Caches the resource type permission in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/ServiceComponentPersistenceImpl.java
@@ -904,6 +904,10 @@ public class ServiceComponentPersistenceImpl extends BasePersistenceImpl<Service
 	private static final String _FINDER_COLUMN_BNS_BNU_BUILDNAMESPACE_3 = "(serviceComponent.buildNamespace IS NULL OR serviceComponent.buildNamespace = '') AND ";
 	private static final String _FINDER_COLUMN_BNS_BNU_BUILDNUMBER_2 = "serviceComponent.buildNumber = ?";
 
+	public ServiceComponentPersistenceImpl() {
+		super(ServiceComponent.class);
+	}
+
 	/**
 	 * Caches the service component in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/SystemEventPersistenceImpl.java
@@ -1741,6 +1741,10 @@ public class SystemEventPersistenceImpl extends BasePersistenceImpl<SystemEvent>
 	private static final String _FINDER_COLUMN_G_C_C_T_CLASSPK_2 = "systemEvent.classPK = ? AND ";
 	private static final String _FINDER_COLUMN_G_C_C_T_TYPE_2 = "systemEvent.type = ?";
 
+	public SystemEventPersistenceImpl() {
+		super(SystemEvent.class);
+	}
+
 	/**
 	 * Caches the system event in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/UserGroupRolePersistenceImpl.java
@@ -2614,6 +2614,10 @@ public class UserGroupRolePersistenceImpl extends BasePersistenceImpl<UserGroupR
 	private static final String _FINDER_COLUMN_G_R_GROUPID_2 = "userGroupRole.id.groupId = ? AND ";
 	private static final String _FINDER_COLUMN_G_R_ROLEID_2 = "userGroupRole.id.roleId = ?";
 
+	public UserGroupRolePersistenceImpl() {
+		super(UserGroupRole.class);
+	}
+
 	/**
 	 * Caches the user group role in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/asset/service/persistence/AssetLinkPersistenceImpl.java
@@ -2891,6 +2891,10 @@ public class AssetLinkPersistenceImpl extends BasePersistenceImpl<AssetLink>
 	private static final String _FINDER_COLUMN_E_E_T_ENTRYID2_2 = "assetLink.entryId2 = ? AND ";
 	private static final String _FINDER_COLUMN_E_E_T_TYPE_2 = "assetLink.type = ?";
 
+	public AssetLinkPersistenceImpl() {
+		super(AssetLink.class);
+	}
+
 	/**
 	 * Caches the asset link in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMStructureLinkPersistenceImpl.java
@@ -1286,6 +1286,10 @@ public class DDMStructureLinkPersistenceImpl extends BasePersistenceImpl<DDMStru
 
 	private static final String _FINDER_COLUMN_STRUCTUREID_STRUCTUREID_2 = "ddmStructureLink.structureId = ?";
 
+	public DDMStructureLinkPersistenceImpl() {
+		super(DDMStructureLink.class);
+	}
+
 	/**
 	 * Caches the d d m structure link in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMTemplatePersistenceImpl.java
@@ -11193,6 +11193,10 @@ public class DDMTemplatePersistenceImpl extends BasePersistenceImpl<DDMTemplate>
 	private static final String _FINDER_COLUMN_G_C_C_T_M_MODE_2_SQL = "ddmTemplate.mode_ = ?";
 	private static final String _FINDER_COLUMN_G_C_C_T_M_MODE_3_SQL = "(ddmTemplate.mode_ IS NULL OR ddmTemplate.mode_ = '')";
 
+	public DDMTemplatePersistenceImpl() {
+		super(DDMTemplate.class);
+	}
+
 	/**
 	 * Caches the d d m template in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/social/service/persistence/SocialActivityAchievementPersistenceImpl.java
@@ -3095,6 +3095,10 @@ public class SocialActivityAchievementPersistenceImpl
 	private static final String _FINDER_COLUMN_G_U_F_USERID_2 = "socialActivityAchievement.userId = ? AND ";
 	private static final String _FINDER_COLUMN_G_U_F_FIRSTINGROUP_2 = "socialActivityAchievement.firstInGroup = ?";
 
+	public SocialActivityAchievementPersistenceImpl() {
+		super(SocialActivityAchievement.class);
+	}
+
 	/**
 	 * Caches the social activity achievement in the entity cache if it is enabled.
 	 *

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/service/persistence/ResourceTypePermissionPersistenceImpl.java
@@ -1508,6 +1508,10 @@ public class ResourceTypePermissionPersistenceImpl extends BasePersistenceImpl<R
 	private static final String _FINDER_COLUMN_C_G_N_R_NAME_3 = "(resourceTypePermission.name IS NULL OR resourceTypePermission.name = '') AND ";
 	private static final String _FINDER_COLUMN_C_G_N_R_ROLEID_2 = "resourceTypePermission.roleId = ?";
 
+	public ResourceTypePermissionPersistenceImpl() {
+		setModelClass(ResourceTypePermission.class);
+	}
+
 	/**
 	 * Caches the resource type permission in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/ServiceComponentPersistenceImpl.java
@@ -904,6 +904,10 @@ public class ServiceComponentPersistenceImpl extends BasePersistenceImpl<Service
 	private static final String _FINDER_COLUMN_BNS_BNU_BUILDNAMESPACE_3 = "(serviceComponent.buildNamespace IS NULL OR serviceComponent.buildNamespace = '') AND ";
 	private static final String _FINDER_COLUMN_BNS_BNU_BUILDNUMBER_2 = "serviceComponent.buildNumber = ?";
 
+	public ServiceComponentPersistenceImpl() {
+		setModelClass(ServiceComponent.class);
+	}
+
 	/**
 	 * Caches the service component in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/SystemEventPersistenceImpl.java
@@ -1741,6 +1741,10 @@ public class SystemEventPersistenceImpl extends BasePersistenceImpl<SystemEvent>
 	private static final String _FINDER_COLUMN_G_C_C_T_CLASSPK_2 = "systemEvent.classPK = ? AND ";
 	private static final String _FINDER_COLUMN_G_C_C_T_TYPE_2 = "systemEvent.type = ?";
 
+	public SystemEventPersistenceImpl() {
+		setModelClass(SystemEvent.class);
+	}
+
 	/**
 	 * Caches the system event in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portal/service/persistence/UserGroupRolePersistenceImpl.java
@@ -2614,6 +2614,10 @@ public class UserGroupRolePersistenceImpl extends BasePersistenceImpl<UserGroupR
 	private static final String _FINDER_COLUMN_G_R_GROUPID_2 = "userGroupRole.id.groupId = ? AND ";
 	private static final String _FINDER_COLUMN_G_R_ROLEID_2 = "userGroupRole.id.roleId = ?";
 
+	public UserGroupRolePersistenceImpl() {
+		setModelClass(UserGroupRole.class);
+	}
+
 	/**
 	 * Caches the user group role in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/asset/service/persistence/AssetLinkPersistenceImpl.java
@@ -2891,6 +2891,10 @@ public class AssetLinkPersistenceImpl extends BasePersistenceImpl<AssetLink>
 	private static final String _FINDER_COLUMN_E_E_T_ENTRYID2_2 = "assetLink.entryId2 = ? AND ";
 	private static final String _FINDER_COLUMN_E_E_T_TYPE_2 = "assetLink.type = ?";
 
+	public AssetLinkPersistenceImpl() {
+		setModelClass(AssetLink.class);
+	}
+
 	/**
 	 * Caches the asset link in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMStructureLinkPersistenceImpl.java
@@ -1286,6 +1286,10 @@ public class DDMStructureLinkPersistenceImpl extends BasePersistenceImpl<DDMStru
 
 	private static final String _FINDER_COLUMN_STRUCTUREID_STRUCTUREID_2 = "ddmStructureLink.structureId = ?";
 
+	public DDMStructureLinkPersistenceImpl() {
+		setModelClass(DDMStructureLink.class);
+	}
+
 	/**
 	 * Caches the d d m structure link in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/dynamicdatamapping/service/persistence/DDMTemplatePersistenceImpl.java
@@ -11193,6 +11193,10 @@ public class DDMTemplatePersistenceImpl extends BasePersistenceImpl<DDMTemplate>
 	private static final String _FINDER_COLUMN_G_C_C_T_M_MODE_2_SQL = "ddmTemplate.mode_ = ?";
 	private static final String _FINDER_COLUMN_G_C_C_T_M_MODE_3_SQL = "(ddmTemplate.mode_ IS NULL OR ddmTemplate.mode_ = '')";
 
+	public DDMTemplatePersistenceImpl() {
+		setModelClass(DDMTemplate.class);
+	}
+
 	/**
 	 * Caches the d d m template in the entity cache if it is enabled.
 	 *

portal-impl/src/com/liferay/portlet/social/service/persistence/SocialActivityAchievementPersistenceImpl.java
@@ -3095,6 +3095,10 @@ public class SocialActivityAchievementPersistenceImpl
 	private static final String _FINDER_COLUMN_G_U_F_USERID_2 = "socialActivityAchievement.userId = ? AND ";
 	private static final String _FINDER_COLUMN_G_U_F_FIRSTINGROUP_2 = "socialActivityAchievement.firstInGroup = ?";
 
+	public SocialActivityAchievementPersistenceImpl() {
+		setModelClass(SocialActivityAchievement.class);
+	}
+
 	/**
 	 * Caches the social activity achievement in the entity cache if it is enabled.
 	 *