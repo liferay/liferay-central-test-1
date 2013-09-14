caorongjin/liferay-portal

LPS-32718 There is an issue that prevents "Basic Document" because of the permissions. As "Basic Document" is an entity that doesn't have any permission (because it's a 'fake' entity) we need to extract it from the query affected by the permission and do a UNION ALL to make sure that it's always returned.

portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryTypeFinderImpl.java
@@ -169,8 +169,6 @@ public class DLFileEntryTypeFinderImpl
 			}
 
 			sql = StringUtil.replace(
-				sql, "[$WHERE$]", getWhere(includeBasicFileEntryType));
-			sql = StringUtil.replace(
 				sql, "[$GROUP_ID$]", getGroupIds(groupIds));
 			sql = CustomSQLUtil.replaceKeywords(
 				sql, "lower(name)", StringPool.LIKE, false, names);
@@ -184,15 +182,16 @@ public class DLFileEntryTypeFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
-			if (includeBasicFileEntryType) {
-				qPos.add(names, 2);
-				qPos.add(descriptions, 2);
-			}
-
 			qPos.add(companyId);
 			qPos.add(groupIds);
 			qPos.add(names, 2);
 			qPos.add(descriptions, 2);
+
+			int countValue = 0;
+
+			if (includeBasicFileEntryType) {
+				countValue = 1;
+			}
 
 			Iterator<Long> itr = q.iterate();
 
@@ -200,11 +199,11 @@ public class DLFileEntryTypeFinderImpl
 				Long count = itr.next();
 
 				if (count != null) {
-					return count.intValue();
+					return countValue + count.intValue();
 				}
 			}
 
-			return 0;
+			return countValue;
 		}
 		catch (Exception e) {
 			throw new SystemException(e);
@@ -238,7 +237,8 @@ public class DLFileEntryTypeFinderImpl
 			}
 
 			sql = StringUtil.replace(
-				sql, "[$WHERE$]", getWhere(includeBasicFileEntryType));
+				sql, "[$BASIC_DOCUMENT$]",
+				includeBasicDocument(includeBasicFileEntryType));
 			sql = StringUtil.replace(
 				sql, "[$GROUP_ID$]", getGroupIds(groupIds));
 			sql = CustomSQLUtil.replaceKeywords(
@@ -253,6 +253,10 @@ public class DLFileEntryTypeFinderImpl
 
 				sql = StringUtil.replace(
 					sql, "name ASC", orderByFields.concat(" DESC"));
+			}
+
+			if (includeBasicFileEntryType) {
+				sql = sql.concat(StringPool.CLOSE_BRACKET);
 			}
 
 			SQLQuery q = session.createSQLQuery(sql);
@@ -304,18 +308,19 @@ public class DLFileEntryTypeFinderImpl
 		return sb.toString();
 	}
 
-	protected String getWhere(boolean includeBasicFileEntryType) {
+	protected String includeBasicDocument(boolean includeBasicFileEntryType) {
 		if (!includeBasicFileEntryType) {
 			return StringPool.BLANK;
 		}
 
 		StringBundler sb = new StringBundler(5);
 
+		sb.append("(SELECT {DLFileEntryType.*} From DLFileEntryType WHERE ");
 		sb.append("((companyId = 0) AND (groupId = 0) AND (");
 		sb.append("(lower(name) LIKE ? [$AND_OR_NULL_CHECK$]) ");
 		sb.append("[$AND_OR_CONNECTOR$] ");
 		sb.append("(description LIKE ? [$AND_OR_NULL_CHECK$]) ");
-		sb.append(")) OR ");
+		sb.append("))) UNION ALL (");
 
 		return sb.toString();
 	}

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portlet/documentlibrary/service/persistence/DLFileEntryTypeFinderImpl.java
@@ -169,8 +169,6 @@ public class DLFileEntryTypeFinderImpl
 			}
 
 			sql = StringUtil.replace(
-				sql, "[$WHERE$]", getWhere(includeBasicFileEntryType));
-			sql = StringUtil.replace(
 				sql, "[$GROUP_ID$]", getGroupIds(groupIds));
 			sql = CustomSQLUtil.replaceKeywords(
 				sql, "lower(name)", StringPool.LIKE, false, names);
@@ -184,15 +182,16 @@ public class DLFileEntryTypeFinderImpl
 
 			QueryPos qPos = QueryPos.getInstance(q);
 
-			if (includeBasicFileEntryType) {
-				qPos.add(names, 2);
-				qPos.add(descriptions, 2);
-			}
-
 			qPos.add(companyId);
 			qPos.add(groupIds);
 			qPos.add(names, 2);
 			qPos.add(descriptions, 2);
+
+			int countValue = 0;
+
+			if (includeBasicFileEntryType) {
+				countValue = 1;
+			}
 
 			Iterator<Long> itr = q.iterate();
 
@@ -200,11 +199,11 @@ public class DLFileEntryTypeFinderImpl
 				Long count = itr.next();
 
 				if (count != null) {
-					return count.intValue();
+					return countValue + count.intValue();
 				}
 			}
 
-			return 0;
+			return countValue;
 		}
 		catch (Exception e) {
 			throw new SystemException(e);
@@ -238,7 +237,8 @@ public class DLFileEntryTypeFinderImpl
 			}
 
 			sql = StringUtil.replace(
-				sql, "[$WHERE$]", getWhere(includeBasicFileEntryType));
+				sql, "[$BASIC_DOCUMENT$]",
+				includeBasicDocument(includeBasicFileEntryType));
 			sql = StringUtil.replace(
 				sql, "[$GROUP_ID$]", getGroupIds(groupIds));
 			sql = CustomSQLUtil.replaceKeywords(
@@ -253,6 +253,10 @@ public class DLFileEntryTypeFinderImpl
 
 				sql = StringUtil.replace(
 					sql, "name ASC", orderByFields.concat(" DESC"));
+			}
+
+			if (includeBasicFileEntryType) {
+				sql = sql.concat(StringPool.CLOSE_PARENTHESIS);
 			}
 
 			SQLQuery q = session.createSQLQuery(sql);
@@ -304,18 +308,19 @@ public class DLFileEntryTypeFinderImpl
 		return sb.toString();
 	}
 
-	protected String getWhere(boolean includeBasicFileEntryType) {
+	protected String includeBasicDocument(boolean includeBasicFileEntryType) {
 		if (!includeBasicFileEntryType) {
 			return StringPool.BLANK;
 		}
 
 		StringBundler sb = new StringBundler(5);
 
+		sb.append("(SELECT {DLFileEntryType.*} From DLFileEntryType WHERE ");
 		sb.append("((companyId = 0) AND (groupId = 0) AND (");
 		sb.append("(lower(name) LIKE ? [$AND_OR_NULL_CHECK$]) ");
 		sb.append("[$AND_OR_CONNECTOR$] ");
 		sb.append("(description LIKE ? [$AND_OR_NULL_CHECK$]) ");
-		sb.append(")) OR ");
+		sb.append("))) UNION ALL (");
 
 		return sb.toString();
 	}