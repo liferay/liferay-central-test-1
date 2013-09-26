juliocamarero/liferay-portal

LPS-39594 Whereever we call the updateLastPublishDate from outside the scope of Staging/StagingImpl it used for setting the last publish date and not clearing. The method could be misused and accidentally clearing the date causing collosion in staging. Removing clear code part to make it more robust and easy to call from the outside.

portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -1742,8 +1742,14 @@ public class StagingImpl implements Staging {
 
 	@Override
 	public void updateLastPublishDate(
-			long groupId, boolean privateLayout, long lastPublishDate)
+			long groupId, boolean privateLayout, Date endDate)
 		throws Exception {
+
+		long lastPublishDate = System.currentTimeMillis();
+
+		if (endDate != null) {
+			lastPublishDate = endDate.getTime();
+		}
 
 		LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
 			groupId, privateLayout);
@@ -1751,13 +1757,8 @@ public class StagingImpl implements Staging {
 		UnicodeProperties settingsProperties =
 			layoutSet.getSettingsProperties();
 
-		if (lastPublishDate <= 0) {
-			settingsProperties.remove("last-publish-date");
-		}
-		else {
-			settingsProperties.setProperty(
-				"last-publish-date", String.valueOf(lastPublishDate));
-		}
+		settingsProperties.setProperty(
+			"last-publish-date", String.valueOf(lastPublishDate));
 
 		LayoutSetLocalServiceUtil.updateSettings(
 			layoutSet.getGroupId(), layoutSet.isPrivateLayout(),

portal-service/src/com/liferay/portal/kernel/staging/Staging.java
@@ -252,7 +252,7 @@ public interface Staging {
 		throws Exception;
 
 	void updateLastPublishDate(
-			long sourceGroupId, boolean privateLayout, long lastPublishDate)
+			long sourceGroupId, boolean privateLayout, Date endDate)
 		throws Exception;
 
 	public void updateStaging(PortletRequest PortletRequest, Group liveGroup)

portal-service/src/com/liferay/portal/kernel/staging/StagingUtil.java
@@ -456,11 +456,11 @@ public class StagingUtil {
 	}
 
 	public static void updateLastPublishDate(
-		long sourceGroupId, boolean privateLayout, long lastPublishDate)
+		long sourceGroupId, boolean privateLayout, Date endDate)
 	throws Exception {
 
 		getStaging().updateLastPublishDate(
-			sourceGroupId, privateLayout, lastPublishDate);
+			sourceGroupId, privateLayout, endDate);
 	}
 
 	public static void updateStaging(

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/staging/StagingImpl.java
@@ -1744,7 +1744,7 @@ public class StagingImpl implements Staging {
 
 	@Override
 	public void updateLastPublishDate(
-			long groupId, boolean privateLayout, long lastPublishDate)
+			long groupId, boolean privateLayout, Date lastPublishDate)
 		throws Exception {
 
 		LayoutSet layoutSet = LayoutSetLocalServiceUtil.getLayoutSet(
@@ -1753,13 +1753,8 @@ public class StagingImpl implements Staging {
 		UnicodeProperties settingsProperties =
 			layoutSet.getSettingsProperties();
 
-		if (lastPublishDate <= 0) {
-			settingsProperties.remove("last-publish-date");
-		}
-		else {
-			settingsProperties.setProperty(
-				"last-publish-date", String.valueOf(lastPublishDate));
-		}
+		settingsProperties.setProperty(
+			"last-publish-date", String.valueOf(lastPublishDate.getTime()));
 
 		LayoutSetLocalServiceUtil.updateSettings(
 			layoutSet.getGroupId(), layoutSet.isPrivateLayout(),

portal-service/src/com/liferay/portal/kernel/staging/Staging.java
@@ -252,7 +252,7 @@ public interface Staging {
 		throws Exception;
 
 	public void updateLastPublishDate(
-			long sourceGroupId, boolean privateLayout, long lastPublishDate)
+			long sourceGroupId, boolean privateLayout, Date lastPublishDate)
 		throws Exception;
 
 	public void updateStaging(PortletRequest PortletRequest, Group liveGroup)

portal-service/src/com/liferay/portal/kernel/staging/StagingUtil.java
@@ -456,7 +456,7 @@ public class StagingUtil {
 	}
 
 	public static void updateLastPublishDate(
-		long sourceGroupId, boolean privateLayout, long lastPublishDate)
+		long sourceGroupId, boolean privateLayout, Date lastPublishDate)
 	throws Exception {
 
 		getStaging().updateLastPublishDate(