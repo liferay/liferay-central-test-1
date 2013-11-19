hhuijser/liferay-portal

LPS-42268 Regenerated Files

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalService.java
@@ -332,6 +332,9 @@ public interface SocialRequestLocalService extends BaseLocalService,
 		throws com.liferay.portal.kernel.exception.PortalException,
 			com.liferay.portal.kernel.exception.SystemException;
 
+	public void deleteMembershipRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	/**
 	* Removes all the social requests for the receiving user.
 	*

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalServiceUtil.java
@@ -365,6 +365,11 @@ public class SocialRequestLocalServiceUtil {
 			extraData, receiverUserId);
 	}
 
+	public static void deleteMembershipRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteMembershipRequests(className, classPK);
+	}
+
 	/**
 	* Removes all the social requests for the receiving user.
 	*

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalServiceWrapper.java
@@ -385,6 +385,12 @@ public class SocialRequestLocalServiceWrapper
 			className, classPK, type, extraData, receiverUserId);
 	}
 
+	@Override
+	public void deleteMembershipRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		_socialRequestLocalService.deleteMembershipRequests(className, classPK);
+	}
+
 	/**
 	* Removes all the social requests for the receiving user.
 	*

liferay/liferay-central-test-1

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalService.java
@@ -363,6 +363,9 @@ public interface SocialRequestLocalService extends BaseLocalService,
 		com.liferay.portlet.social.model.SocialRequest request)
 		throws com.liferay.portal.kernel.exception.SystemException;
 
+	public void deleteRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException;
+
 	/**
 	* Removes all the social requests for the requesting user.
 	*

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalServiceUtil.java
@@ -402,6 +402,11 @@ public class SocialRequestLocalServiceUtil {
 		getService().deleteRequest(request);
 	}
 
+	public static void deleteRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		getService().deleteRequests(className, classPK);
+	}
+
 	/**
 	* Removes all the social requests for the requesting user.
 	*

portal-service/src/com/liferay/portlet/social/service/SocialRequestLocalServiceWrapper.java
@@ -425,6 +425,12 @@ public class SocialRequestLocalServiceWrapper
 		_socialRequestLocalService.deleteRequest(request);
 	}
 
+	@Override
+	public void deleteRequests(long className, long classPK)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		_socialRequestLocalService.deleteRequests(className, classPK);
+	}
+
 	/**
 	* Removes all the social requests for the requesting user.
 	*