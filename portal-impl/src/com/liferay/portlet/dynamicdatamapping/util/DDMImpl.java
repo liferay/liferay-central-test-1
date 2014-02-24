brianchandotcom/liferay-portal

LPS-34490 Add alt value as part of the DDM file data

portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -95,6 +95,8 @@ public class DDMImpl implements DDM {
 
 	public static final String TYPE_SELECT = "select";
 
+	public static final String TYPE_WCM_IMAGE = "wcm-image";
+
 	@Override
 	public DDMDisplay getDDMDisplay(ServiceContext serviceContext) {
 		String refererPortletName = (String)serviceContext.getAttribute(
@@ -492,23 +494,8 @@ public class DDMImpl implements DDM {
 					return null;
 				}
 
-				UploadRequest uploadRequest = (UploadRequest)request;
-
-				File file = uploadRequest.getFile(fieldNameValue);
-
-				try {
-					byte[] bytes = FileUtil.getBytes(file);
-
-					if (ArrayUtil.isNotEmpty(bytes)) {
-						fieldValue = UnicodeFormatter.bytesToHex(bytes);
-					}
-					else {
-						fieldValue = "update";
-					}
-				}
-				catch (IOException ioe) {
-					return null;
-				}
+				fieldValue = getImageFieldValue(
+					(UploadRequest)request, fieldNameValue);
 			}
 
 			if (fieldValue == null) {
@@ -533,6 +520,34 @@ public class DDMImpl implements DDM {
 		}
 
 		return fieldValues;
+	}
+
+	protected String getImageFieldValue(
+		UploadRequest uploadRequest, String fieldNameValue) {
+
+		File file = uploadRequest.getFile(fieldNameValue);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			byte[] bytes = FileUtil.getBytes(file);
+
+			if (ArrayUtil.isNotEmpty(bytes)) {
+				jsonObject.put("data", UnicodeFormatter.bytesToHex(bytes));
+			}
+			else {
+				jsonObject.put("data", "update");
+			}
+		}
+		catch (IOException ioe) {
+			return null;
+		}
+
+		String alt = uploadRequest.getParameter(fieldNameValue + "Alt");
+
+		jsonObject.put("alt", alt);
+
+		return jsonObject.toString();
 	}
 
 }

portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -454,6 +454,14 @@ public class JournalConverterImpl implements JournalConverter {
 
 			serializable = jsonArray.toString();
 		}
+		else if (DDMImpl.TYPE_WCM_IMAGE.equals(type)) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			jsonObject.put("alt", dynamicContentElement.attributeValue("alt"));
+			jsonObject.put("data", dynamicContentElement.getText());
+
+			serializable = jsonObject.toString();
+		}
 		else {
 			serializable = FieldConstants.getSerializable(
 				dataType, dynamicContentElement.getText());
@@ -747,6 +755,22 @@ public class JournalConverterImpl implements JournalConverter {
 			else {
 				dynamicContentElement.addCDATA(jsonArray.getString(0));
 			}
+		}
+		else if (DDMImpl.TYPE_WCM_IMAGE.equals(fieldType) &&
+				 Validator.isNotNull(fieldValue)) {
+
+			if (fieldValue.equals("delete")) {
+				dynamicContentElement.addCDATA(fieldValue);
+
+				return;
+			}
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				fieldValue);
+
+			dynamicContentElement.addAttribute(
+				"alt", jsonObject.getString("alt"));
+			dynamicContentElement.addCDATA(jsonObject.getString("data"));
 		}
 		else {
 			dynamicContentElement.addCDATA(fieldValue);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portlet/dynamicdatamapping/util/DDMImpl.java
@@ -89,6 +89,8 @@ public class DDMImpl implements DDM {
 
 	public static final String TYPE_DDM_DOCUMENTLIBRARY = "ddm-documentlibrary";
 
+	public static final String TYPE_DDM_IMAGE = "ddm-image";
+
 	public static final String TYPE_DDM_LINK_TO_PAGE = "ddm-link-to-page";
 
 	public static final String TYPE_RADIO = "radio";
@@ -492,23 +494,8 @@ public class DDMImpl implements DDM {
 					return null;
 				}
 
-				UploadRequest uploadRequest = (UploadRequest)request;
-
-				File file = uploadRequest.getFile(fieldNameValue);
-
-				try {
-					byte[] bytes = FileUtil.getBytes(file);
-
-					if (ArrayUtil.isNotEmpty(bytes)) {
-						fieldValue = UnicodeFormatter.bytesToHex(bytes);
-					}
-					else {
-						fieldValue = "update";
-					}
-				}
-				catch (IOException ioe) {
-					return null;
-				}
+				fieldValue = getImageFieldValue(
+					(UploadRequest)request, fieldNameValue);
 			}
 
 			if (fieldValue == null) {
@@ -533,6 +520,34 @@ public class DDMImpl implements DDM {
 		}
 
 		return fieldValues;
+	}
+
+	protected String getImageFieldValue(
+		UploadRequest uploadRequest, String fieldNameValue) {
+
+		File file = uploadRequest.getFile(fieldNameValue);
+
+		JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+		try {
+			byte[] bytes = FileUtil.getBytes(file);
+
+			if (ArrayUtil.isNotEmpty(bytes)) {
+				jsonObject.put("data", UnicodeFormatter.bytesToHex(bytes));
+			}
+			else {
+				jsonObject.put("data", "update");
+			}
+		}
+		catch (IOException ioe) {
+			return null;
+		}
+
+		String alt = uploadRequest.getParameter(fieldNameValue + "Alt");
+
+		jsonObject.put("alt", alt);
+
+		return jsonObject.toString();
 	}
 
 }

portal-impl/src/com/liferay/portlet/journal/util/JournalConverterImpl.java
@@ -416,6 +416,14 @@ public class JournalConverterImpl implements JournalConverter {
 			serializable = getDocumentLibraryValue(
 				dynamicContentElement.getText());
 		}
+		else if (DDMImpl.TYPE_DDM_IMAGE.equals(type)) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			jsonObject.put("alt", dynamicContentElement.attributeValue("alt"));
+			jsonObject.put("data", dynamicContentElement.getText());
+
+			serializable = jsonObject.toString();
+		}
 		else if (DDMImpl.TYPE_DDM_LINK_TO_PAGE.equals(type)) {
 			String[] values = StringUtil.split(
 				dynamicContentElement.getText(), CharPool.AT);
@@ -699,6 +707,21 @@ public class JournalConverterImpl implements JournalConverter {
 				false, true);
 
 			dynamicContentElement.addCDATA(fieldValue);
+		}
+		else if (DDMImpl.TYPE_DDM_IMAGE.equals(fieldType) &&
+				 Validator.isNotNull(fieldValue)) {
+
+			if (fieldValue.equals("delete")) {
+				dynamicContentElement.addCDATA(fieldValue);
+			}
+			else {
+				JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+					fieldValue);
+
+				dynamicContentElement.addAttribute(
+					"alt", jsonObject.getString("alt"));
+				dynamicContentElement.addCDATA(jsonObject.getString("data"));
+			}
 		}
 		else if (DDMImpl.TYPE_DDM_LINK_TO_PAGE.equals(fieldType) &&
 				 Validator.isNotNull(fieldValue)) {