liferay/liferay-central-test-1

commit 39a1f3723ee165b8578575dff537c38db9b4f3ee

LPS-44122 SF - Uses class var _hightlight and removes escape var, because when we do a hightlight of a text we should do the operations of escape and hightlight in the right order

portal-service/src/com/liferay/portal/kernel/search/Summary.java
@@ -50,8 +50,12 @@ public class Summary {
 		return _content;
 	}
 
-	public String getContent(boolean escape, boolean highlight) {
-		return _escapeAndHighlight(_content, escape, highlight);
+	public String getHighlightedContent() {
+		return _escapeAndHighlight(_content);
+	}
+
+	public String getHighlightedTitle() {
+		return _escapeAndHighlight(_title);
 	}
 
 	public Locale getLocale() {
@@ -72,10 +76,6 @@ public class Summary {
 
 	public String getTitle() {
 		return _title;
-	}
-
-	public String getTitle(boolean escape, boolean highlight) {
-		return _escapeAndHighlight(_title, escape, highlight);
 	}
 
 	public boolean isHighlight() {
@@ -122,35 +122,28 @@ public class Summary {
 		_title = title;
 	}
 
-	private String _escapeAndHighlight(
-		String s, boolean escape, boolean highlight) {
-
-		if (Validator.isNull(s) || ArrayUtil.isEmpty(_queryTerms)) {
-			return s;
+	private String _escapeAndHighlight(String text) {
+		if (Validator.isNull(text) || ArrayUtil.isEmpty(_queryTerms)) {
+			return text;
 		}
 
-		String result = s;
-
-		if (highlight) {
-			result = SearchUtil.highlight(
-				result, _queryTerms, ESCAPE_SAFE_HIGHLIGHT_1,
+		if (_highlight) {
+			text = SearchUtil.highlight(
+				text, _queryTerms, ESCAPE_SAFE_HIGHLIGHT_1,
 				ESCAPE_SAFE_HIGHLIGHT_2);
-		}
 
-		if (escape) {
-			result = HtmlUtil.escape(result);
-		}
+			text = HtmlUtil.escape(text);
 
-		if (highlight) {
-			result = StringUtil.replace(
-				result, new String[] {
-					ESCAPE_SAFE_HIGHLIGHT_1, ESCAPE_SAFE_HIGHLIGHT_2},
+			text = StringUtil.replace(
+				text,
+				new String[] {ESCAPE_SAFE_HIGHLIGHT_1, ESCAPE_SAFE_HIGHLIGHT_2},
 				new String[] {
 					SearchUtil.DEFAULT_HIGHLIGHT_1,
-					SearchUtil.DEFAULT_HIGHLIGHT_2});
+					SearchUtil.DEFAULT_HIGHLIGHT_2
+				});
 		}
 
-		return result;
+		return text;
 	}
 
 	private static final String ESCAPE_SAFE_HIGHLIGHT_1 = "[@HIGHLIGHT1@]";

commit 920f473b9ea848f508a24e702693d4f1c04a02dd

LPS-44511 Fix XSS in Summary: we always have to do escape, even if highlight is disabled Caused by:
39a1f3723ee165b8578575dff537c38db9b4f3ee

- LPS-44122 SF - Uses class var _hightlight and removes escape var, because when we do a hightlight of a text we should do the operations of escape and hightlight in the right order -

portal-service/src/com/liferay/portal/kernel/search/Summary.java
@@ -131,9 +131,11 @@ public class Summary {
 			text = SearchUtil.highlight(
 				text, _queryTerms, ESCAPE_SAFE_HIGHLIGHT_1,
 				ESCAPE_SAFE_HIGHLIGHT_2);
+		}
 
-			text = HtmlUtil.escape(text);
+		text = HtmlUtil.escape(text);
 
+		if (_highlight) {
 			text = StringUtil.replace(
 				text,
 				new String[] {ESCAPE_SAFE_HIGHLIGHT_1, ESCAPE_SAFE_HIGHLIGHT_2},