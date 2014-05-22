sergiogonzalez/liferay-portal

LPS-46822 autogenerated

portal-service/src/com/liferay/portal/service/RepositoryLocalService.java
@@ -362,6 +362,12 @@ public interface RepositoryLocalService extends BaseLocalService,
 			com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getLocalRepositoryImplByGroupId(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Repository getRepository(long groupId,
 		java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,

portal-service/src/com/liferay/portal/service/RepositoryLocalServiceUtil.java
@@ -413,6 +413,13 @@ public class RepositoryLocalServiceUtil {
 				   .getLocalRepositoryImpl(folderId, fileEntryId, fileVersionId);
 	}
 
+	public static java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getLocalRepositoryImplByGroupId(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().getLocalRepositoryImplByGroupId(groupId);
+	}
+
 	public static com.liferay.portal.model.Repository getRepository(
 		long groupId, java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,

portal-service/src/com/liferay/portal/service/RepositoryLocalServiceWrapper.java
@@ -439,6 +439,14 @@ public class RepositoryLocalServiceWrapper implements RepositoryLocalService,
 	}
 
 	@Override
+	public java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getLocalRepositoryImplByGroupId(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _repositoryLocalService.getLocalRepositoryImplByGroupId(groupId);
+	}
+
+	@Override
 	public com.liferay.portal.model.Repository getRepository(long groupId,
 		java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/service/RepositoryLocalService.java
@@ -362,6 +362,12 @@ public interface RepositoryLocalService extends BaseLocalService,
 			com.liferay.portal.kernel.exception.SystemException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getGroupLocalRepositoryImpl(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException;
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.Repository getRepository(long groupId,
 		java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,

portal-service/src/com/liferay/portal/service/RepositoryLocalServiceUtil.java
@@ -413,6 +413,13 @@ public class RepositoryLocalServiceUtil {
 				   .getLocalRepositoryImpl(folderId, fileEntryId, fileVersionId);
 	}
 
+	public static java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getLocalRepositoryImplByGroupId(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return getService().getGroupLocalRepositoryImpl(groupId);
+	}
+
 	public static com.liferay.portal.model.Repository getRepository(
 		long groupId, java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,

portal-service/src/com/liferay/portal/service/RepositoryLocalServiceWrapper.java
@@ -439,6 +439,14 @@ public class RepositoryLocalServiceWrapper implements RepositoryLocalService,
 	}
 
 	@Override
+	public java.util.List<com.liferay.portal.kernel.repository.LocalRepository> getGroupLocalRepositoryImpl(
+		long groupId)
+		throws com.liferay.portal.kernel.exception.PortalException,
+			com.liferay.portal.kernel.exception.SystemException {
+		return _repositoryLocalService.getGroupLocalRepositoryImpl(groupId);
+	}
+
+	@Override
 	public com.liferay.portal.model.Repository getRepository(long groupId,
 		java.lang.String portletId)
 		throws com.liferay.portal.kernel.exception.PortalException,