jonathanmccann/liferay-portal

LPS-46900 Improve performance when getting mapped IDs from users and use ID's when that's all we need

portal-web/docroot/html/portlet/users_admin/select_organization.jsp
@@ -144,8 +144,8 @@ if (Validator.isNotNull(target)) {
 					boolean disabled = false;
 
 					if (selUser != null) {
-						for (Organization curOrganization : selUser.getOrganizations()) {
-							if (curOrganization.getOrganizationId() == organization.getOrganizationId()) {
+						for (long curOrganizationId : selUser.getOrganizations()) {
+							if (curOrganizationId == organization.getOrganizationId()) {
 								disabled = true;
 
 								break;

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/users_admin/select_organization.jsp
@@ -144,8 +144,8 @@ if (Validator.isNotNull(target)) {
 					boolean disabled = false;
 
 					if (selUser != null) {
-						for (Organization curOrganization : selUser.getOrganizations()) {
-							if (curOrganization.getOrganizationId() == organization.getOrganizationId()) {
+						for (long curOrganizationId : selUser.getOrganizationIds()) {
+							if (curOrganizationId == organization.getOrganizationId()) {
 								disabled = true;
 
 								break;