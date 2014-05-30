marcellustavares/liferay-portal

LPS-46436 Comments API: absorb "deleteComment" from LinkbackConsumerUtil
https://issues.liferay.com/browse/LPS-46436

portal-impl/src/com/liferay/portal/comments/CommentsImpl.java
@@ -58,4 +58,11 @@ public class CommentsImpl implements Comments {
 		return message.getMessageId();
 	}
 
+	@Override
+	public void deleteComment(long messageId)
+		throws PortalException, SystemException {
+
+		MBMessageLocalServiceUtil.deleteDiscussionMessage(messageId);
+	}
+
 }

portal-service/src/com/liferay/portal/kernel/comments/Comments.java
@@ -30,4 +30,7 @@ public interface Comments {
 		Function<String, ServiceContext> serviceContextFunction)
 	throws PortalException, SystemException;
 
+	public void deleteComment(long messageId)
+		throws PortalException, SystemException;
+
 }

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/comments/CommentsImpl.java
@@ -58,4 +58,11 @@ public class CommentsImpl implements Comments {
 		return message.getMessageId();
 	}
 
+	@Override
+	public void deleteComment(long commentId)
+		throws PortalException, SystemException {
+
+		MBMessageLocalServiceUtil.deleteDiscussionMessage(commentId);
+	}
+
 }

portal-service/src/com/liferay/portal/kernel/comments/Comments.java
@@ -30,4 +30,7 @@ public interface Comments {
 			Function<String, ServiceContext> serviceContextFunction)
 		throws PortalException, SystemException;
 
+	public void deleteComment(long commentId)
+		throws PortalException, SystemException;
+
 }