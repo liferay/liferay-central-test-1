jonmak08/liferay-portal

LPS-46966 Update <img> Attribute - Document Library Portlet

portal-web/docroot/html/portlet/document_library/asset/file_entry_abstract.jsp
@@ -36,7 +36,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 		<c:choose>
 			<c:when test="<%= showThumbnail && ImageProcessorUtil.hasImages(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="image" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&imageThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="image" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&imageThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">
@@ -45,7 +45,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 			</c:when>
 			<c:when test="<%= showThumbnail && PDFProcessorUtil.hasImages(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="document" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&documentThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="document" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&documentThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">
@@ -54,7 +54,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 			</c:when>
 			<c:when test="<%= showThumbnail && VideoProcessorUtil.hasVideo(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="video" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&videoThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="video" />" class="img-polaroid" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&videoThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/document_library/asset/file_entry_abstract.jsp
@@ -36,7 +36,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 		<c:choose>
 			<c:when test="<%= showThumbnail && ImageProcessorUtil.hasImages(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="image" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&imageThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="image" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&imageThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">
@@ -45,7 +45,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 			</c:when>
 			<c:when test="<%= showThumbnail && PDFProcessorUtil.hasImages(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="document" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&documentThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="document" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&documentThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">
@@ -54,7 +54,7 @@ if (fileEntry.getVersion().equals(fileVersion.getVersion())) {
 			</c:when>
 			<c:when test="<%= showThumbnail && VideoProcessorUtil.hasVideo(fileVersion) %>">
 				<div class="asset-thumbnail">
-					<img alt="<liferay-ui:message key="video" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&videoThumbnail=1") %>" />
+					<img alt="<liferay-ui:message escapeAttribute="<%= true %>" key="video" />" class="img-thumbnail" src="<%= DLUtil.getPreviewURL(fileEntry, fileVersion, themeDisplay, "&videoThumbnail=1") %>" />
 				</div>
 
 				<p class="asset-description">