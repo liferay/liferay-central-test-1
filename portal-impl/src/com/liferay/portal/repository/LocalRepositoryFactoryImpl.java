sergiogonzalez/liferay-portal

LPS-48728 replace hardcoded code by events

portal-impl/src/com/liferay/portal/repository/LocalRepositoryFactoryImpl.java
@@ -52,7 +52,8 @@ public class LocalRepositoryFactoryImpl
 
 		return new CapabilityLocalRepository(
 			localRepository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getPublicCapabilities());
+			repositoryConfiguration.getPublicCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override
@@ -81,7 +82,8 @@ public class LocalRepositoryFactoryImpl
 
 		return new CapabilityLocalRepository(
 			localRepository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getPublicCapabilities());
+			repositoryConfiguration.getPublicCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/repository/RepositoryFactoryImpl.java
@@ -48,7 +48,8 @@ public class RepositoryFactoryImpl extends BaseRepositoryFactory<Repository>
 		return new CapabilityRepository(
 			repositoryCreator.createRepository(repositoryId),
 			repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getPublicCapabilities());
+			repositoryConfiguration.getPublicCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override
@@ -77,7 +78,8 @@ public class RepositoryFactoryImpl extends BaseRepositoryFactory<Repository>
 
 		return new CapabilityRepository(
 			repository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getPublicCapabilities());
+			repositoryConfiguration.getPublicCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryRegistryPlugin.java
@@ -15,11 +15,17 @@
 package com.liferay.portal.repository.liferayrepository;
 
 import com.liferay.portal.kernel.bean.BeanReference;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.capabilities.TrashCapability;
+import com.liferay.portal.kernel.repository.event.RepositoryEventListener;
+import com.liferay.portal.kernel.repository.event.RepositoryEventType;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryRegistryPlugin;
 import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
 import com.liferay.portal.repository.capabilities.LiferayTrashCapability;
 import com.liferay.portal.service.ClassNameLocalService;
 
@@ -41,7 +47,7 @@ public class LiferayRepositoryRegistryPlugin
 	@Override
 	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
 		capabilityRegistry.addPublicCapability(
-			TrashCapability.class, new LiferayTrashCapability());
+			TrashCapability.class, _trashCapability);
 	}
 
 	@Override
@@ -49,6 +55,19 @@ public class LiferayRepositoryRegistryPlugin
 		RepositoryCreatorRegistry repositoryCreatorRegistry) {
 
 		repositoryCreatorRegistry.setRepositoryCreator(_repositoryCreator);
+	}
+
+	@Override
+	public void registerRepositoryEventListeners(
+		RepositoryEventRegistry repositoryEventRegistry) {
+
+		repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Delete.class, FileEntry.class,
+			new DeleteFileEntryRepositoryEventListener());
+
+		repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Delete.class, Folder.class,
+			new DeleteFolderRepositoryEventListener());
 	}
 
 	public void setRepositoryCreator(RepositoryCreator repositoryCreator) {
@@ -61,5 +80,27 @@ public class LiferayRepositoryRegistryPlugin
 	private ClassNameLocalService _classNameLocalService;
 
 	private RepositoryCreator _repositoryCreator;
+	private TrashCapability _trashCapability = new LiferayTrashCapability();
+
+	private class DeleteFileEntryRepositoryEventListener
+		implements RepositoryEventListener
+			<RepositoryEventType.Delete, FileEntry> {
+
+		@Override
+		public void execute(FileEntry fileEntry) throws PortalException {
+			_trashCapability.deleteTrashEntry(fileEntry);
+		}
+
+	}
+
+	private class DeleteFolderRepositoryEventListener
+		implements RepositoryEventListener<RepositoryEventType.Delete, Folder> {
+
+		@Override
+		public void execute(Folder folder) throws PortalException {
+			_trashCapability.deleteTrashEntry(folder);
+		}
+
+	}
 
 }

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/repository/LocalRepositoryFactoryImpl.java
@@ -52,7 +52,8 @@ public class LocalRepositoryFactoryImpl
 
 		return new CapabilityLocalRepository(
 			localRepository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getExportedCapabilities());
+			repositoryConfiguration.getExportedCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override
@@ -81,7 +82,8 @@ public class LocalRepositoryFactoryImpl
 
 		return new CapabilityLocalRepository(
 			localRepository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getExportedCapabilities());
+			repositoryConfiguration.getExportedCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override

portal-impl/src/com/liferay/portal/repository/RepositoryFactoryImpl.java
@@ -74,7 +74,8 @@ public class RepositoryFactoryImpl extends BaseRepositoryFactory<Repository>
 
 		return new CapabilityRepository(
 			repository, externalSupportedCapabilities,
-			externalExportedCapabilityClasses);
+			externalExportedCapabilityClasses,
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	@Override
@@ -103,7 +104,8 @@ public class RepositoryFactoryImpl extends BaseRepositoryFactory<Repository>
 
 		return new CapabilityRepository(
 			repository, repositoryConfiguration.getSupportedCapabilities(),
-			repositoryConfiguration.getExportedCapabilities());
+			repositoryConfiguration.getExportedCapabilities(),
+			repositoryConfiguration.getRepositoryEventHandler());
 	}
 
 	protected CMISRepositoryHandler getCMISRepositoryHandler(

portal-impl/src/com/liferay/portal/repository/liferayrepository/LiferayRepositoryRegistryPlugin.java
@@ -14,11 +14,17 @@
 
 package com.liferay.portal.repository.liferayrepository;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.repository.capabilities.TrashCapability;
+import com.liferay.portal.kernel.repository.event.RepositoryEventListener;
+import com.liferay.portal.kernel.repository.event.RepositoryEventType;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.repository.registry.BaseRepositoryRegistryPlugin;
 import com.liferay.portal.kernel.repository.registry.CapabilityRegistry;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreator;
 import com.liferay.portal.kernel.repository.registry.RepositoryCreatorRegistry;
+import com.liferay.portal.kernel.repository.registry.RepositoryEventRegistry;
 import com.liferay.portal.repository.capabilities.LiferayTrashCapability;
 
 /**
@@ -35,7 +41,7 @@ public class LiferayRepositoryRegistryPlugin
 	@Override
 	public void registerCapabilities(CapabilityRegistry capabilityRegistry) {
 		capabilityRegistry.addExportedCapability(
-			TrashCapability.class, new LiferayTrashCapability());
+			TrashCapability.class, _liferayTrashCapability);
 	}
 
 	@Override
@@ -45,10 +51,45 @@ public class LiferayRepositoryRegistryPlugin
 		repositoryCreatorRegistry.setRepositoryCreator(_repositoryCreator);
 	}
 
+	@Override
+	public void registerRepositoryEventListeners(
+		RepositoryEventRegistry repositoryEventRegistry) {
+
+		repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Delete.class, FileEntry.class,
+			new DeleteFileEntryRepositoryEventListener());
+		repositoryEventRegistry.registerRepositoryEventListener(
+			RepositoryEventType.Delete.class, Folder.class,
+			new DeleteFolderRepositoryEventListener());
+	}
+
 	public void setRepositoryCreator(RepositoryCreator repositoryCreator) {
 		_repositoryCreator = repositoryCreator;
 	}
 
+	private TrashCapability _liferayTrashCapability =
+		new LiferayTrashCapability();
 	private RepositoryCreator _repositoryCreator;
+
+	private class DeleteFileEntryRepositoryEventListener
+		implements RepositoryEventListener
+			<RepositoryEventType.Delete, FileEntry> {
+
+		@Override
+		public void execute(FileEntry fileEntry) throws PortalException {
+			_liferayTrashCapability.deleteTrashEntry(fileEntry);
+		}
+
+	}
+
+	private class DeleteFolderRepositoryEventListener
+		implements RepositoryEventListener<RepositoryEventType.Delete, Folder> {
+
+		@Override
+		public void execute(Folder folder) throws PortalException {
+			_liferayTrashCapability.deleteTrashEntry(folder);
+		}
+
+	}
 
 }