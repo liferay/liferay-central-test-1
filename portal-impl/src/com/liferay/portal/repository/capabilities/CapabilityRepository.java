sergiogonzalez/liferay-portal

LPS-48895 don't execute listeners for root folder

portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityRepository.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.model.Lock;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 
 import java.io.File;
 import java.io.InputStream;
@@ -697,8 +698,10 @@ public class CapabilityRepository
 		Folder folder = repository.updateFolder(
 			folderId, title, description, serviceContext);
 
-		_repositoryEventHandler.trigger(
-			RepositoryEventType.Update.class, Folder.class, folder);
+		if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			_repositoryEventHandler.trigger(
+				RepositoryEventType.Update.class, Folder.class, folder);
+		}
 
 		return folder;
 	}

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityRepository.java
@@ -29,6 +29,7 @@ import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.model.Lock;
 import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 
 import java.io.File;
 import java.io.InputStream;
@@ -697,8 +698,10 @@ public class CapabilityRepository
 		Folder folder = repository.updateFolder(
 			folderId, title, description, serviceContext);
 
-		_repositoryEventTrigger.trigger(
-			RepositoryEventType.Update.class, Folder.class, folder);
+		if (folderId != DLFolderConstants.DEFAULT_PARENT_FOLDER_ID) {
+			_repositoryEventTrigger.trigger(
+				RepositoryEventType.Update.class, Folder.class, folder);
+		}
 
 		return folder;
 	}