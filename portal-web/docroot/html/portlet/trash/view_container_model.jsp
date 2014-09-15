sergiogonzalez/liferay-portal

LPS-49317 Do not show the choose button when showing the rootContainerModels

portal-web/docroot/html/portlet/trash/view_container_model.jsp
@@ -72,21 +72,23 @@ if (hasRootContainerModel) {
 
 	<liferay-ui:breadcrumb showGuestGroup="<%= false %>" showLayout="<%= false %>" showParentGroups="<%= false %>" />
 
-	<aui:button-row>
+	<c:if test="<%= !hasRootContainerModel %>">
+		<aui:button-row>
 
-		<%
-		Map<String, Object> data = new HashMap<String, Object>();
+			<%
+				Map<String, Object> data = new HashMap<String, Object>();
 
-		data.put("classname", className);
-		data.put("classpk", classPK);
-		data.put("containermodelid", containerModelId);
-		data.put("redirect", redirect);
-		%>
+				data.put("classname", className);
+				data.put("classpk", classPK);
+				data.put("containermodelid", containerModelId);
+				data.put("redirect", redirect);
+			%>
 
-		<aui:button cssClass="selector-button" data="<%= data %>" value='<%= LanguageUtil.format(request, "choose-this-x", containerModelName) %>' />
-	</aui:button-row>
+			<aui:button cssClass="selector-button" data="<%= data %>" value='<%= LanguageUtil.format(request, "choose-this-x", containerModelName) %>' />
+		</aui:button-row>
 
-	<br />
+		<br />
+	</c:if>
 
 	<%
 	containerURL.setParameter("containerModelId", String.valueOf(containerModelId));

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/trash/view_container_model.jsp
@@ -72,21 +72,23 @@ if (showRootContainerModel) {
 
 	<liferay-ui:breadcrumb showGuestGroup="<%= false %>" showLayout="<%= false %>" showParentGroups="<%= false %>" />
 
-	<aui:button-row>
+	<c:if test="<%= !showRootContainerModel %>">
+		<aui:button-row>
 
-		<%
-		Map<String, Object> data = new HashMap<String, Object>();
+			<%
+				Map<String, Object> data = new HashMap<String, Object>();
 
-		data.put("classname", className);
-		data.put("classpk", classPK);
-		data.put("containermodelid", containerModelId);
-		data.put("redirect", redirect);
-		%>
+				data.put("classname", className);
+				data.put("classpk", classPK);
+				data.put("containermodelid", containerModelId);
+				data.put("redirect", redirect);
+			%>
 
 		<aui:button cssClass="selector-button" data="<%= data %>" value='<%= LanguageUtil.format(request, "choose-this-x", containerModelName) %>' />
 	</aui:button-row>
 
-	<br />
+		<br />
+	</c:if>
 
 	<%
 	containerURL.setParameter("containerModelId", String.valueOf(containerModelId));