sergiogonzalez/liferay-portal

LPS-49778 Implement TemporaryFilesCapability (except the deleteExpiredTemporaryFiles() method)

portal-impl/src/com/liferay/portal/repository/capabilities/TemporaryFilesCapabilityImpl.java
@@ -14,11 +14,21 @@
 
 package com.liferay.portal.repository.capabilities;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.capabilities.ConfigurationCapability;
 import com.liferay.portal.kernel.repository.capabilities.TemporaryFilesCapability;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.documentlibrary.NoSuchFolderException;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
 import java.util.List;
@@ -27,6 +37,14 @@ import java.util.List;
  * @author Iván Zaera
  */
 public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
+
+	public static final long DEFAULT_TEMPORARY_FILES_TIMEOUT =
+		12 * 60 * 60 * 1000;
+
+	public static final String FOLDER_NAME_TMP = "tmp";
+
+	public static final String PROPERTY_TEMPORARY_FILES_TIMEOUT =
+		"temporaryFilesTimeout";
 
 	public TemporaryFilesCapabilityImpl(LocalRepository localRepository) {
 		_localRepository = localRepository;
@@ -38,7 +56,28 @@ public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
 			InputStream inputStream)
 		throws PortalException {
 
-		return null;
+		Folder folder = getTempFolder(userId, folderName);
+
+		File file = null;
+
+		try {
+			file = FileUtil.createTempFile(inputStream);
+
+			ServiceContext serviceContext = new ServiceContext();
+
+			serviceContext.setAddGroupPermissions(true);
+			serviceContext.setAddGuestPermissions(true);
+
+			return _localRepository.addFileEntry(
+				userId, folder.getFolderId(), fileName, mimeType, fileName, "",
+				"", file, serviceContext);
+		}
+		catch (IOException ioe) {
+			throw new SystemException("Unable to write temporary file", ioe);
+		}
+		finally {
+			FileUtil.delete(file);
+		}
 	}
 
 	@Override
@@ -49,6 +88,10 @@ public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
 	public void deleteTemporaryFile(
 			long userId, String folderName, String fileName)
 		throws PortalException {
+
+		FileEntry fileEntry = getTemporaryFile(userId, folderName, fileName);
+
+		_localRepository.deleteFileEntry(fileEntry.getFileEntryId());
 	}
 
 	@Override
@@ -56,23 +99,79 @@ public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
 			long userId, String folderName, String fileName)
 		throws PortalException {
 
-		return null;
+		Folder folder = getTempFolder(userId, folderName);
+
+		return _localRepository.getFileEntry(folder.getFolderId(), fileName);
 	}
 
 	@Override
 	public List<FileEntry> getTemporaryFiles(long userId, String folderName)
 		throws PortalException {
 
-		return null;
+		Folder folder = getTempFolder(userId, folderName);
+
+		return _localRepository.getRepositoryFileEntries(
+			folder.getFolderId(), 0, QueryUtil.ALL_POS, null);
 	}
 
 	@Override
 	public long getTemporaryFilesTimeout() {
-		return 0;
+		ConfigurationCapability configurationCapability =
+			_localRepository.getCapability(ConfigurationCapability.class);
+
+		String minimumLifespanMilliseconds =
+			configurationCapability.getProperty(
+				getClass(), PROPERTY_TEMPORARY_FILES_TIMEOUT);
+
+		if (minimumLifespanMilliseconds == null) {
+			return DEFAULT_TEMPORARY_FILES_TIMEOUT;
+		}
+
+		return Long.valueOf(minimumLifespanMilliseconds);
 	}
 
 	@Override
 	public void setTemporaryFilesTimeout(long temporaryFilesTimeout) {
+		ConfigurationCapability configurationCapability =
+			_localRepository.getCapability(ConfigurationCapability.class);
+
+		configurationCapability.setProperty(
+			getClass(), PROPERTY_TEMPORARY_FILES_TIMEOUT,
+			String.valueOf(temporaryFilesTimeout));
+	}
+
+	protected Folder getTempFolder(long userId, String folderName)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		Folder tmpFolder = _getFolder(
+			userId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, FOLDER_NAME_TMP,
+			serviceContext);
+
+		Folder userFolder = _getFolder(
+			userId, tmpFolder.getFolderId(), String.valueOf(userId),
+			serviceContext);
+
+		return _getFolder(
+			userId, userFolder.getFolderId(), folderName, serviceContext);
+	}
+
+	private Folder _getFolder(
+			long userId, long parentFolderId, String folderName,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		try {
+			return _localRepository.getFolder(parentFolderId, folderName);
+		}
+		catch (NoSuchFolderException nsfe) {
+			return _localRepository.addFolder(
+				userId, parentFolderId, folderName, "", serviceContext);
+		}
 	}
 
 	private LocalRepository _localRepository;

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/repository/capabilities/TemporaryFilesCapabilityImpl.java
@@ -14,20 +14,41 @@
 
 package com.liferay.portal.repository.capabilities;
 
+import com.liferay.portal.NoSuchModelException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.repository.LocalRepository;
+import com.liferay.portal.kernel.repository.capabilities.ConfigurationCapability;
 import com.liferay.portal.kernel.repository.capabilities.TemporaryFilesCapability;
 import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.documentlibrary.NoSuchFolderException;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 /**
  * @author Iván Zaera
  */
 public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
+
+	public static final long DEFAULT_TEMPORARY_FILES_TIMEOUT =
+		12 * 60 * 60 * 1000;
+
+	public static final String FOLDER_NAME_TMP = "tmp";
+
+	public static final String PROPERTY_TEMPORARY_FILES_TIMEOUT =
+		"temporaryFilesTimeout";
 
 	public TemporaryFilesCapabilityImpl(LocalRepository localRepository) {
 		_localRepository = localRepository;
@@ -35,11 +56,33 @@ public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
 
 	@Override
 	public FileEntry addTemporaryFile(
-			long userId, UUID callerUuid, String folderPath, String fileName,
+			UUID callerUuid, String folderPath, String fileName, long userId,
 			String mimeType, InputStream inputStream)
 		throws PortalException {
 
-		return null;
+		Folder folder = addTempFolder(userId, callerUuid, folderPath, fileName);
+
+		File file = null;
+
+		try {
+			file = FileUtil.createTempFile(inputStream);
+
+			ServiceContext serviceContext = new ServiceContext();
+
+			serviceContext.setAddGroupPermissions(true);
+			serviceContext.setAddGuestPermissions(true);
+			serviceContext.setHeaders(
+
+			return _localRepository.addFileEntry(
+				userId, folder.getFolderId(), fileName, mimeType, fileName, "",
+				"", file, serviceContext);
+		}
+		catch (IOException ioe) {
+			throw new SystemException("Unable to write temporary file", ioe);
+		}
+		finally {
+			FileUtil.delete(file);
+		}
 	}
 
 	@Override
@@ -48,34 +91,166 @@ public class TemporaryFilesCapabilityImpl implements TemporaryFilesCapability {
 
 	@Override
 	public void deleteTemporaryFile(
-			long userId, UUID callerUuid, String folderPath, String fileName)
+			UUID callerUuid, String folderPath, String fileName)
 		throws PortalException {
+
+		try {
+			FileEntry fileEntry = getTemporaryFile(
+				callerUuid, folderPath, fileName);
+
+			_localRepository.deleteFileEntry(fileEntry.getFileEntryId());
+		}
+		catch (NoSuchModelException nsme) {
+
+			// ignore
+
+		}
 	}
 
 	@Override
 	public FileEntry getTemporaryFile(
-			long userId, UUID callerUuid, String folderPath, String fileName)
+			UUID callerUuid, String folderPath, String fileName)
 		throws PortalException {
 
-		return null;
+		Folder folder = getTempFolder(callerUuid, folderPath);
+
+		return _localRepository.getFileEntry(folder.getFolderId(), fileName);
 	}
 
 	@Override
-	public List<FileEntry> getTemporaryFiles(
-			long userId, UUID callerUuid, String folderPath)
+	public List<FileEntry> getTemporaryFiles(UUID callerUuid, String folderPath)
 		throws PortalException {
 
-		return null;
+		try {
+			Folder folder = getTempFolder(callerUuid, folderPath);
+
+			return _localRepository.getRepositoryFileEntries(
+				folder.getFolderId(), 0, QueryUtil.ALL_POS, null);
+		}
+		catch (NoSuchModelException nsme) {
+			return Collections.emptyList();
+		}
 	}
 
 	@Override
 	public long getTemporaryFilesTimeout() {
-		return 0;
+		ConfigurationCapability configurationCapability =
+			_localRepository.getCapability(ConfigurationCapability.class);
+
+		String minimumLifespanMilliseconds =
+			configurationCapability.getProperty(
+				getClass(), PROPERTY_TEMPORARY_FILES_TIMEOUT);
+
+		if (minimumLifespanMilliseconds == null) {
+			return DEFAULT_TEMPORARY_FILES_TIMEOUT;
+		}
+
+		return Long.valueOf(minimumLifespanMilliseconds);
 	}
 
 	@Override
 	public void setTemporaryFilesTimeout(long temporaryFilesTimeout) {
+		ConfigurationCapability configurationCapability =
+			_localRepository.getCapability(ConfigurationCapability.class);
+
+		configurationCapability.setProperty(
+			getClass(), PROPERTY_TEMPORARY_FILES_TIMEOUT,
+			String.valueOf(temporaryFilesTimeout));
 	}
+
+	protected Folder addTempFolder(
+			long userId, UUID callerUuid, String folderPath, String fileName)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		Folder tmpFolder = _addFolder(
+			userId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, FOLDER_NAME_TMP,
+			serviceContext);
+
+		Folder callerFolder = _addFolder(
+			userId, tmpFolder.getFolderId(), callerUuid.toString(),
+			serviceContext);
+
+		return _addFolders(
+			userId, callerFolder.getFolderId(), folderPath, serviceContext);
+	}
+
+	protected Folder getTempFolder(UUID callerUuid, String folderPath)
+		throws PortalException {
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setAddGroupPermissions(true);
+		serviceContext.setAddGuestPermissions(true);
+
+		Folder tmpFolder = _getFolder(
+			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, FOLDER_NAME_TMP);
+
+		Folder callerFolder = _getFolder(
+			tmpFolder.getFolderId(), callerUuid.toString());
+
+		return _getFolders(callerFolder.getFolderId(), folderPath);
+	}
+
+	private Folder _addFolder(
+			long userId, long parentFolderId, String folderName,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		try {
+			return _getFolder(parentFolderId, folderName);
+		}
+		catch (NoSuchFolderException nsfe) {
+			return _localRepository.addFolder(
+				userId, parentFolderId, folderName, "", serviceContext);
+		}
+	}
+
+	private Folder _addFolders(
+			long userId, long folderId, String folderPath,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		String[] folderNames = folderPath.split(_PATTERN_SLASH);
+
+		Folder folder = null;
+
+		for (String folderName : folderNames) {
+			folder = _addFolder(userId, folderId, folderName, serviceContext);
+
+			folderId = folder.getFolderId();
+		}
+
+		return folder;
+	}
+
+	private Folder _getFolder(long parentFolderId, String folderName)
+		throws PortalException {
+
+		return _localRepository.getFolder(parentFolderId, folderName);
+	}
+
+	private Folder _getFolders(long parentFolderId, String folderPath)
+		throws PortalException {
+
+		String[] folderNames = folderPath.split(_PATTERN_SLASH);
+
+		Folder folder = null;
+
+		for (String folderName : folderNames) {
+			folder = _localRepository.getFolder(parentFolderId, folderName);
+
+			parentFolderId = folder.getFolderId();
+		}
+
+		return folder;
+	}
+
+	private static final String _PATTERN_SLASH = Pattern.quote("/");
 
 	private LocalRepository _localRepository;
 