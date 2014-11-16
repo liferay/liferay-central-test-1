sergiogonzalez/liferay-portal

LPS-51191 call WF capability when copying files

portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityRepository.java
@@ -255,6 +255,13 @@ public class CapabilityRepository
 		FileEntry fileEntry = repository.copyFileEntry(
 			userId, groupId, fileEntryId, destFolderId, serviceContext);
 
+		WorkflowCapability workflowCapability = getInternalCapability(
+			WorkflowCapability.class);
+
+		if (workflowCapability != null) {
+			workflowCapability.startWorkflow(userId, fileEntry, serviceContext);
+		}
+
 		_repositoryEventTrigger.trigger(
 			RepositoryEventType.Add.class, FileEntry.class, fileEntry);
 

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/repository/capabilities/CapabilityRepository.java
@@ -262,6 +262,13 @@ public class CapabilityRepository
 		FileEntry fileEntry = repository.copyFileEntry(
 			userId, groupId, fileEntryId, destFolderId, serviceContext);
 
+		WorkflowCapability workflowCapability = getInternalCapability(
+			WorkflowCapability.class);
+
+		if (workflowCapability != null) {
+			workflowCapability.addFileEntry(userId, fileEntry, serviceContext);
+		}
+
 		_repositoryEventTrigger.trigger(
 			RepositoryEventType.Add.class, FileEntry.class, fileEntry);
 