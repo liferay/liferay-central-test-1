danielkocsis/liferay-portal

LPS-51795 Add new interfaces for export import lifecycle events

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEvent.java
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * @author Daniel Kocsis
+ */
+public interface ExportImportLifecycleEvent extends Serializable {
+
+	public void addEventAttribute(Serializable serializable);
+
+	public List<Serializable> getEventAttributes();
+
+	public int getEventCode();
+
+	public void setEventCode(int eventCode);
+
+}

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/kernel/lar/lifecycle/ExportImportLifecycleEvent.java
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.lar.lifecycle;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * @author Daniel Kocsis
+ */
+public interface ExportImportLifecycleEvent extends Serializable {
+
+	public void addAttribute(Serializable serializable);
+
+	public List<Serializable> getAttributes();
+
+	public int getCode();
+
+	public void setCode(int eventCode);
+
+}