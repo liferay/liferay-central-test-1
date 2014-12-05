brianchandotcom/liferay-portal

LPS-51741 regen svcs

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalService.java
@@ -250,6 +250,12 @@ public interface DLFileEntryMetadataLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public long getFileVersionFileEntryMetadatasCount(long fileVersionId);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructureFileEntryMetadatas();
+
 	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.PersistedModel getPersistedModel(

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalServiceUtil.java
@@ -281,6 +281,14 @@ public class DLFileEntryMetadataLocalServiceUtil {
 		return getService().getFileVersionFileEntryMetadatasCount(fileVersionId);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas() {
+		return getService().getMismatchedCompanyIdFileEntryMetadatas();
+	}
+
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructureFileEntryMetadatas() {
+		return getService().getNoStructureFileEntryMetadatas();
+	}
+
 	public static com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalServiceWrapper.java
@@ -306,6 +306,16 @@ public class DLFileEntryMetadataLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas() {
+		return _dlFileEntryMetadataLocalService.getMismatchedCompanyIdFileEntryMetadatas();
+	}
+
+	@Override
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructureFileEntryMetadatas() {
+		return _dlFileEntryMetadataLocalService.getNoStructureFileEntryMetadatas();
+	}
+
+	@Override
 	public com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

liferay/liferay-central-test-1

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalService.java
@@ -250,6 +250,12 @@ public interface DLFileEntryMetadataLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public long getFileVersionFileEntryMetadatasCount(long fileVersionId);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas();
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructuresFileEntryMetadatas();
+
 	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.PersistedModel getPersistedModel(

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalServiceUtil.java
@@ -281,6 +281,14 @@ public class DLFileEntryMetadataLocalServiceUtil {
 		return getService().getFileVersionFileEntryMetadatasCount(fileVersionId);
 	}
 
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas() {
+		return getService().getMismatchedCompanyIdFileEntryMetadatas();
+	}
+
+	public static java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructuresFileEntryMetadatas() {
+		return getService().getNoStructuresFileEntryMetadatas();
+	}
+
 	public static com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileEntryMetadataLocalServiceWrapper.java
@@ -306,6 +306,16 @@ public class DLFileEntryMetadataLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getMismatchedCompanyIdFileEntryMetadatas() {
+		return _dlFileEntryMetadataLocalService.getMismatchedCompanyIdFileEntryMetadatas();
+	}
+
+	@Override
+	public java.util.List<com.liferay.portlet.documentlibrary.model.DLFileEntryMetadata> getNoStructuresFileEntryMetadatas() {
+		return _dlFileEntryMetadataLocalService.getNoStructuresFileEntryMetadatas();
+	}
+
+	@Override
 	public com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {