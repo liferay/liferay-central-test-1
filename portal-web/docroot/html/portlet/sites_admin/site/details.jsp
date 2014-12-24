brianchandotcom/liferay-portal

LPS-51903 SF - remove not used code

portal-web/docroot/html/portlet/sites_admin/site/details.jsp
@@ -154,15 +154,12 @@ else if (group != null) {
 
 <aui:fieldset>
 	<c:choose>
-		<c:when test="<%= ((liveGroup != null) && (liveGroup.isCompany() || PortalUtil.isSystemGroup(liveGroup.getGroupKey()))) %>">
-			<aui:input name="name" type="hidden" />
-		</c:when>
 		<c:when test="<%= (liveGroup != null) && liveGroup.isOrganization() %>">
 			<aui:input helpMessage="the-name-of-this-site-cannot-be-edited-because-it-belongs-to-an-organization" name="name" type="resource" value="<%= liveGroup.getDescriptiveName(locale) %>" />
 		</c:when>
-		<c:otherwise>
+		<c:when test="<%= (liveGroup == null) || (!liveGroup.isCompany() && !PortalUtil.isSystemGroup(liveGroup.getGroupKey())) %>">
 			<aui:input autoFocus="<%= windowState.equals(WindowState.MAXIMIZED) %>" name="name" />
-		</c:otherwise>
+		</c:when>
 	</c:choose>
 
 	<aui:input name="description" />

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/sites_admin/site/details.jsp
@@ -154,15 +154,12 @@ else if (group != null) {
 
 <aui:fieldset>
 	<c:choose>
-		<c:when test="<%= ((liveGroup != null) && (liveGroup.isCompany() || PortalUtil.isSystemGroup(liveGroup.getName()))) %>">
-			<aui:input name="name" type="hidden" />
-		</c:when>
 		<c:when test="<%= (liveGroup != null) && liveGroup.isOrganization() %>">
 			<aui:input helpMessage="the-name-of-this-site-cannot-be-edited-because-it-belongs-to-an-organization" name="name" type="resource" value="<%= liveGroup.getDescriptiveName(locale) %>" />
 		</c:when>
-		<c:otherwise>
+		<c:when test="<%= (liveGroup == null) || (!liveGroup.isCompany() && !PortalUtil.isSystemGroup(liveGroup.getName())) %>">
 			<aui:input autoFocus="<%= windowState.equals(WindowState.MAXIMIZED) %>" name="name" />
-		</c:otherwise>
+		</c:when>
 	</c:choose>
 
 	<aui:input name="description" />