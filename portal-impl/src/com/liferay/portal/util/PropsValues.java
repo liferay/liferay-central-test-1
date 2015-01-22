jonathanmccann/liferay-portal

LPS-27857 It is possible to enumerate the portal's users

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1628,6 +1628,8 @@ public class PropsValues {
 
 	public static final String SEARCH_FACET_CONFIGURATION = PropsUtil.get(PropsKeys.SEARCH_FACET_CONFIGURATION);
 
+	public static final boolean SECURE_FORGOT_PASSWORD = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.SECURE_FORGOT_PASSWORD));
+
 	public static final String[] SERVICE_BUILDER_SERVICE_READ_ONLY_PREFIXES = PropsUtil.getArray(PropsKeys.SERVICE_BUILDER_SERVICE_READ_ONLY_PREFIXES);
 
 	public static final String[] SERVLET_SERVICE_EVENTS_POST = PropsUtil.getArray(PropsKeys.SERVLET_SERVICE_EVENTS_POST);

portal-impl/src/com/liferay/portlet/login/action/ForgotPasswordAction.java
@@ -85,7 +85,6 @@ public class ForgotPasswordAction extends PortletAction {
 		}
 		catch (Exception e) {
 			if (e instanceof CaptchaTextException ||
-				e instanceof NoSuchUserException ||
 				e instanceof RequiredReminderQueryException ||
 				e instanceof SendPasswordException ||
 				e instanceof UserActiveException ||
@@ -94,6 +93,14 @@ public class ForgotPasswordAction extends PortletAction {
 				e instanceof UserReminderQueryException) {
 
 				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else if (e instanceof NoSuchUserException) {
+				if (PropsValues.SECURE_FORGOT_PASSWORD) {
+					sendRedirect(actionRequest, actionResponse);
+				}
+				else {
+					SessionErrors.add(actionRequest, e.getClass());
+				}
 			}
 			else {
 				PortalUtil.sendError(e, actionRequest, actionResponse);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1070,6 +1070,8 @@ public class PropsValues {
 
 	public static String[] LOGIN_FORM_NAVIGATION_PRE = PropsUtil.getArray(PropsKeys.LOGIN_FORM_NAVIGATION_PRE);
 
+	public static final boolean LOGIN_SECURE_FORGOT_PASSWORD = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LOGIN_SECURE_FORGOT_PASSWORD));
+
 	public static final String[] LOGOUT_EVENTS_POST = PropsUtil.getArray(PropsKeys.LOGOUT_EVENTS_POST);
 
 	public static final String[] LOGOUT_EVENTS_PRE = PropsUtil.getArray(PropsKeys.LOGOUT_EVENTS_PRE);

portal-impl/src/com/liferay/portlet/login/action/ForgotPasswordAction.java
@@ -85,7 +85,6 @@ public class ForgotPasswordAction extends PortletAction {
 		}
 		catch (Exception e) {
 			if (e instanceof CaptchaTextException ||
-				e instanceof NoSuchUserException ||
 				e instanceof RequiredReminderQueryException ||
 				e instanceof SendPasswordException ||
 				e instanceof UserActiveException ||
@@ -94,6 +93,14 @@ public class ForgotPasswordAction extends PortletAction {
 				e instanceof UserReminderQueryException) {
 
 				SessionErrors.add(actionRequest, e.getClass());
+			}
+			else if (e instanceof NoSuchUserException) {
+				if (PropsValues.LOGIN_SECURE_FORGOT_PASSWORD) {
+					sendRedirect(actionRequest, actionResponse);
+				}
+				else {
+					SessionErrors.add(actionRequest, e.getClass());
+				}
 			}
 			else {
 				PortalUtil.sendError(e, actionRequest, actionResponse);