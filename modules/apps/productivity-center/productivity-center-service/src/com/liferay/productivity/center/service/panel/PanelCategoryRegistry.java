sergiogonzalez/liferay-portal

LPS-53640 Add method to get a category by key

modules/apps/productivity-center/productivity-center-service/src/com/liferay/productivity/center/service/panel/PanelCategoryRegistry.java
@@ -16,6 +16,10 @@ package com.liferay.productivity.center.service.panel;
 
 import com.liferay.productivity.center.panel.PanelCategory;
 import com.liferay.productivity.center.service.util.ParentPanelCategoryServiceReferenceMapper;
+import com.liferay.registry.Registry;
+import com.liferay.registry.RegistryUtil;
+import com.liferay.registry.ServiceReference;
+import com.liferay.registry.collections.ServiceReferenceMapper;
 import com.liferay.registry.collections.ServiceTrackerCollections;
 import com.liferay.registry.collections.ServiceTrackerMap;
 
@@ -33,7 +37,12 @@ public class PanelCategoryRegistry {
 		return _instance._getPanelCategories(parentPanelCategory);
 	}
 
+	public static PanelCategory getPanelCategory(String panelCategoryKey) {
+		return _instance._getPanelCategory(panelCategoryKey);
+	}
+
 	private PanelCategoryRegistry() {
+		_panelCategoryServiceTrackerMap.open();
 		_parentCategoryServiceTrackerMap.open();
 	}
 
@@ -51,13 +60,50 @@ public class PanelCategoryRegistry {
 		return panelCategories;
 	}
 
+	private PanelCategory _getPanelCategory(String panelCategoryKey) {
+		PanelCategory panelCategory =
+			_panelCategoryServiceTrackerMap.getService(panelCategoryKey);
+
+		if (panelCategory == null) {
+			throw new IllegalArgumentException(
+				"category doesn't exist: " + panelCategoryKey);
+		}
+
+		return panelCategory;
+	}
+
 	private static final PanelCategoryRegistry _instance =
 		new PanelCategoryRegistry();
+
+	private final ServiceTrackerMap<String, PanelCategory>
+		_panelCategoryServiceTrackerMap =
+			ServiceTrackerCollections.singleValueMap(
+				PanelCategory.class, null,
+				new ServiceReferenceMapper<String, PanelCategory>() {
+					@Override
+					public void map(
+						ServiceReference<PanelCategory> serviceReference,
+						Emitter<String> emitter) {
+
+						Registry registry = RegistryUtil.getRegistry();
+
+						PanelCategory panelCategory = registry.getService(
+							serviceReference);
+
+						try {
+							emitter.emit(panelCategory.getKey());
+						}
+						finally {
+							registry.ungetService(serviceReference);
+						}
+					}
+				});
 
 	private final ServiceTrackerMap<String, List<PanelCategory>>
 		_parentCategoryServiceTrackerMap =
 			ServiceTrackerCollections.multiValueMap(
 				PanelCategory.class, "(panel.category=*)",
-				ParentPanelCategoryServiceReferenceMapper.<PanelCategory>create());
+				ParentPanelCategoryServiceReferenceMapper.
+					<PanelCategory>create());
 
 }

liferay/liferay-central-test-1

modules/apps/productivity-center/productivity-center-service/src/com/liferay/productivity/center/service/panel/PanelCategoryRegistry.java
@@ -14,6 +14,7 @@
 
 package com.liferay.productivity.center.service.panel;
 
+import com.liferay.osgi.service.tracker.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
 import com.liferay.productivity.center.panel.PanelCategory;
@@ -24,6 +25,7 @@ import java.util.List;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
@@ -47,8 +49,20 @@ public class PanelCategoryRegistry {
 		return panelCategories;
 	}
 
+	public PanelCategory getPanelCategory(String panelCategoryKey) {
+		PanelCategory panelCategory =
+			_panelCategoryServiceTrackerMap.getService(panelCategoryKey);
+
+		if (panelCategory == null) {
+			throw new IllegalArgumentException(
+				"No panel category found with key " + panelCategoryKey);
+		}
+
+		return panelCategory;
+	}
+
 	@Activate
-	protected void activate(BundleContext bundleContext)
+	protected void activate(final BundleContext bundleContext)
 		throws InvalidSyntaxException {
 
 		_serviceTrackerMap = ServiceTrackerMapFactory.multiValueMap(
@@ -56,13 +70,41 @@ public class PanelCategoryRegistry {
 			PanelEntryServiceReferenceMapper.<PanelCategory>create());
 
 		_serviceTrackerMap.open();
+
+		_panelCategoryServiceTrackerMap =
+			ServiceTrackerMapFactory.singleValueMap(
+				bundleContext, PanelCategory.class, null,
+				new ServiceReferenceMapper<String, PanelCategory>() {
+
+					@Override
+					public void map(
+						ServiceReference<PanelCategory> serviceReference,
+						Emitter<String> emitter) {
+
+						PanelCategory panelCategory = bundleContext.getService(
+							serviceReference);
+
+						try {
+							emitter.emit(panelCategory.getKey());
+						}
+						finally {
+							bundleContext.ungetService(serviceReference);
+						}
+					}
+
+				});
+
+		_panelCategoryServiceTrackerMap.open();
 	}
 
 	@Deactivate
 	protected void deactivate() {
 		_serviceTrackerMap.close();
+		_panelCategoryServiceTrackerMap.close();
 	}
 
+	private ServiceTrackerMap<String, PanelCategory>
+		_panelCategoryServiceTrackerMap;
 	private ServiceTrackerMap<String, List<PanelCategory>> _serviceTrackerMap;
 
 }