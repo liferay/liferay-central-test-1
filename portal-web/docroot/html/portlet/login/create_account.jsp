JorgeFerrer/liferay-portal

LPS-53409 Allow screen name input validator to be configurable for each validator

portal-web/docroot/html/portlet/login/create_account.jsp
@@ -138,7 +138,17 @@ birthdayCalendar.set(Calendar.YEAR, 1970);
 			%>
 
 			<c:if test="<%= !autoGenerateScreenName %>">
-				<aui:input autoFocus="<%= true %>" model="<%= User.class %>" name="screenName" />
+				<aui:input autoFocus="<%= true %>" model="<%= User.class %>" name="screenName">
+					<%
+					ScreenNameValidator screenNameValidator = ScreenNameValidatorFactory.getInstance();
+					%>
+
+					<c:if test="<%= !screenNameValidator.getJSValidation().isEmpty() %>">
+						<aui:validator errorMessage="<%= screenNameValidator.getJSValidationErrorMessage(locale) %>" name="custom">
+							<%= screenNameValidator.getJSValidation() %>
+						</aui:validator>
+					</c:if>
+				</aui:input>
 			</c:if>
 
 			<aui:input autoFocus="<%= autoGenerateScreenName %>" model="<%= User.class %>" name="emailAddress">

portal-web/docroot/html/portlet/users_admin/user/details.jsp
@@ -122,7 +122,15 @@ else {
 				</c:when>
 				<c:otherwise>
 					<aui:input name="screenName">
-						<aui:validator name="alphanum" />
+						<%
+						ScreenNameValidator screenNameValidator = ScreenNameValidatorFactory.getInstance();
+						%>
+
+						<c:if test="<%= !screenNameValidator.getJSValidation().isEmpty() %>">
+							<aui:validator errorMessage="<%= screenNameValidator.getJSValidationErrorMessage(locale) %>" name="custom">
+								<%= screenNameValidator.getJSValidation() %>
+							</aui:validator>
+						</c:if>
 					</aui:input>
 				</c:otherwise>
 			</c:choose>

liferay/liferay-central-test-1

portal-web/docroot/html/portlet/login/create_account.jsp
@@ -138,7 +138,17 @@ birthdayCalendar.set(Calendar.YEAR, 1970);
 			%>
 
 			<c:if test="<%= !autoGenerateScreenName %>">
-				<aui:input autoFocus="<%= true %>" model="<%= User.class %>" name="screenName" />
+				<aui:input autoFocus="<%= true %>" model="<%= User.class %>" name="screenName">
+					<%
+					ScreenNameValidator screenNameValidator = ScreenNameValidatorFactory.getInstance();
+					%>
+
+					<c:if test="<%= !screenNameValidator.getJSValidation().isEmpty() %>">
+						<aui:validator errorMessage="<%= screenNameValidator.getDescription(locale) %>" name="custom">
+							<%= screenNameValidator.getJSValidation() %>
+						</aui:validator>
+					</c:if>
+				</aui:input>
 			</c:if>
 
 			<aui:input autoFocus="<%= autoGenerateScreenName %>" model="<%= User.class %>" name="emailAddress">

portal-web/docroot/html/portlet/users_admin/user/details.jsp
@@ -122,7 +122,15 @@ else {
 				</c:when>
 				<c:otherwise>
 					<aui:input name="screenName">
-						<aui:validator name="alphanum" />
+						<%
+						ScreenNameValidator screenNameValidator = ScreenNameValidatorFactory.getInstance();
+						%>
+
+						<c:if test="<%= !screenNameValidator.getJSValidation().isEmpty() %>">
+							<aui:validator errorMessage="<%= screenNameValidator.getDescription(locale) %>" name="custom">
+								<%= screenNameValidator.getJSValidation() %>
+							</aui:validator>
+						</c:if>
 					</aui:input>
 				</c:otherwise>
 			</c:choose>