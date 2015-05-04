sergiogonzalez/liferay-portal

LPS-53136 Modify wiki module to allow contribution of external wiki engines

modules/apps/wiki/wiki-api/src/com/liferay/wiki/engine/BaseWikiEngine.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.engine;
+
+import com.liferay.wiki.exception.PageContentException;
+import com.liferay.wiki.model.WikiPage;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Iván Zaera
+ */
+public abstract class BaseWikiEngine implements WikiEngine {
+
+	public static WikiPage getWikiPage(ServletRequest servletRequest) {
+		return (WikiPage)servletRequest.getAttribute(_WIKI_PAGE);
+	}
+
+	@Override
+	public String convert(
+			WikiPage page, PortletURL viewPageURL, PortletURL editPageURL,
+			String attachmentURLPrefix)
+		throws PageContentException {
+
+		return page.getContent();
+	}
+
+	@Override
+	public String getFormatLabel(Locale locale) {
+		try {
+			Class<?> clazz = getClass();
+
+			ResourceBundle resourceBundle = ResourceBundle.getBundle(
+				"content/Language", locale, clazz.getClassLoader());
+
+			return resourceBundle.getString(getFormat());
+		}
+		catch (MissingResourceException mre) {
+			return getFormat();
+		}
+	}
+
+	@Override
+	public Map<String, Boolean> getOutgoingLinks(WikiPage page)
+		throws PageContentException {
+
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public void renderEditPage(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			WikiPage wikiPage)
+		throws IOException, ServletException {
+
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+		Dictionary<String, String> headers = bundle.getHeaders();
+
+		String webContextPath = headers.get("Web-ContextPath");
+
+		RequestDispatcher requestDispatcher =
+			servletRequest.getRequestDispatcher(
+				"/o" + webContextPath + "/edit_page.jsp");
+
+		servletRequest.setAttribute(_WIKI_PAGE, wikiPage);
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	@Override
+	public boolean validate(long nodeId, String newContent) {
+		return true;
+	}
+
+	private static final String _WIKI_PAGE =
+		BaseWikiEngine.class.getName() + "#wikiPage";
+
+}

modules/apps/wiki/wiki-web/docroot/html/portlet/wiki/edit_page.jsp
@@ -257,7 +257,7 @@ if (Validator.isNull(redirect)) {
 							for (String format : formats) {
 							%>
 
-								<aui:option label='<%= LanguageUtil.get(request, "wiki.formats." + format) %>' selected="<%= selectedFormat.equals(format) %>" value="<%= format %>" />
+								<aui:option label="<%= WikiUtil.getFormatLabel(format, request) %>" selected="<%= selectedFormat.equals(format) %>" value="<%= format %>" />
 
 							<%
 							}
@@ -276,9 +276,10 @@ if (Validator.isNull(redirect)) {
 
 				<%
 				request.setAttribute("edit_page.jsp-wikiPage", wikiPage);
+
+				WikiUtil.renderEditPageHTML(selectedFormat, pageContext, wikiPage);
 				%>
 
-				<liferay-util:include page="<%= WikiUtil.getEditPage(selectedFormat) %>" servletContext="<%= application %>" />
 			</div>
 
 			<c:if test="<%= wikiPage != null %>">

liferay/liferay-central-test-1

modules/apps/wiki/wiki-api/src/com/liferay/wiki/engine/BaseWikiEngine.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.engine;
+
+import com.liferay.wiki.exception.PageContentException;
+import com.liferay.wiki.model.WikiPage;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Iván Zaera
+ */
+public abstract class BaseWikiEngine implements WikiEngine {
+
+	public static WikiPage getWikiPage(ServletRequest servletRequest) {
+		return (WikiPage)servletRequest.getAttribute(_WIKI_PAGE);
+	}
+
+	@Override
+	public String convert(
+			WikiPage page, PortletURL viewPageURL, PortletURL editPageURL,
+			String attachmentURLPrefix)
+		throws PageContentException {
+
+		return page.getContent();
+	}
+
+	@Override
+	public String getFormatLabel(Locale locale) {
+		try {
+			Class<?> clazz = getClass();
+
+			ResourceBundle resourceBundle = ResourceBundle.getBundle(
+				"content/Language", locale, clazz.getClassLoader());
+
+			return resourceBundle.getString(getFormat());
+		}
+		catch (MissingResourceException mre) {
+			return getFormat();
+		}
+	}
+
+	@Override
+	public Map<String, Boolean> getOutgoingLinks(WikiPage page)
+		throws PageContentException {
+
+		return Collections.emptyMap();
+	}
+
+	@Override
+	public void renderEditPage(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			WikiPage page)
+		throws IOException, ServletException {
+
+		Bundle bundle = FrameworkUtil.getBundle(getClass());
+
+		Dictionary<String, String> headers = bundle.getHeaders();
+
+		String webContextPath = headers.get("Web-ContextPath");
+
+		RequestDispatcher requestDispatcher =
+			servletRequest.getRequestDispatcher(
+				"/o" + webContextPath + "/edit_page.jsp");
+
+		servletRequest.setAttribute(_WIKI_PAGE, page);
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	@Override
+	public boolean validate(long nodeId, String newContent) {
+		return true;
+	}
+
+	private static final String _WIKI_PAGE =
+		BaseWikiEngine.class.getName() + "#wikiPage";
+
+}

modules/apps/wiki/wiki-web/docroot/html/portlet/wiki/edit_page.jsp
@@ -257,7 +257,7 @@ if (Validator.isNull(redirect)) {
 							for (String format : formats) {
 							%>
 
-								<aui:option label='<%= LanguageUtil.get(request, "wiki.formats." + format) %>' selected="<%= selectedFormat.equals(format) %>" value="<%= format %>" />
+								<aui:option label="<%= WikiUtil.getFormatLabel(format, locale) %>" selected="<%= selectedFormat.equals(format) %>" value="<%= format %>" />
 
 							<%
 							}
@@ -276,9 +276,10 @@ if (Validator.isNull(redirect)) {
 
 				<%
 				request.setAttribute("edit_page.jsp-wikiPage", wikiPage);
+
+				WikiUtil.renderEditPageHTML(selectedFormat, pageContext, wikiPage);
 				%>
 
-				<liferay-util:include page="<%= WikiUtil.getEditPage(selectedFormat) %>" servletContext="<%= application %>" />
 			</div>
 
 			<c:if test="<%= wikiPage != null %>">