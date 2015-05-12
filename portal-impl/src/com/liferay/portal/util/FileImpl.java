rotty3000/liferay-portal

LPS-54558 Changing the method contract of mkdirs(String pathName) might break plugins/modules

portal-impl/src/com/liferay/portal/util/FileImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.util;
 
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayInputStream;
 import com.liferay.portal.kernel.io.unsync.UnsyncByteArrayOutputStream;
@@ -719,10 +720,15 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	}
 
 	@Override
-	public void mkdirs(String pathName) throws IOException {
+	public void mkdirs(String pathName) {
 		File file = new File(pathName);
 
-		mkdirs(file);
+		try {
+			mkdirs(file);
+		}
+		catch (IOException ioe) {
+			throw new SystemException(ioe);
+		}
 	}
 
 	@Override

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/util/FileImpl.java
@@ -32,6 +32,7 @@ import com.liferay.portal.kernel.util.Digester;
 import com.liferay.portal.kernel.util.DigesterUtil;
 import com.liferay.portal.kernel.util.FileComparator;
 import com.liferay.portal.kernel.util.PwdGenerator;
+import com.liferay.portal.kernel.util.ReflectionUtil;
 import com.liferay.portal.kernel.util.StreamUtil;
 import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringPool;
@@ -719,10 +720,15 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	}
 
 	@Override
-	public void mkdirs(String pathName) throws IOException {
+	public void mkdirs(String pathName) {
 		File file = new File(pathName);
 
-		mkdirs(file);
+		try {
+			mkdirs(file);
+		}
+		catch (IOException ioe) {
+			ReflectionUtil.throwException(ioe);
+		}
 	}
 
 	@Override