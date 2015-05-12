mjbradford89/liferay-portal

LPS-54489 - Create WYSIWYG editor registry impl to keep track of available editors

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/AlloyEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=alloyeditor"}, service = Object.class)
+public class AlloyEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/BBCodeEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=bbcode"}, service = Object.class)
+public class BBCodeEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/CKBBCodeEditor.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	property = {"editor.wysiwyg=ckeditor_bbcode"}, service = Object.class
+)
+public class CKBBCodeEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/CKCreoleEditor.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	property = {"editor.wysiwyg=ckeditor_creole"}, service = Object.class
+)
+public class CKCreoleEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/CKEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=ckeditor"}, service = Object.class)
+public class CKEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/TinyMCEEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=tinymce"}, service = Object.class)
+public class TinyMCEEditor {
+}

modules/frontend/frontend-js-web/src/com/liferay/frontend/js/web/TinyMCESimpleEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.js.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=tinymce_simple"}, service = Object.class)
+public class TinyMCESimpleEditor {
+}

liferay/liferay-central-test-1

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/AlloyEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=alloyeditor"}, service = Object.class)
+public class AlloyEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/BBCodeEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=bbcode"}, service = Object.class)
+public class BBCodeEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/CKBBCodeEditor.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	property = {"editor.wysiwyg=ckeditor_bbcode"}, service = Object.class
+)
+public class CKBBCodeEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/CKCreoleEditor.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	property = {"editor.wysiwyg=ckeditor_creole"}, service = Object.class
+)
+public class CKCreoleEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/CKEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=ckeditor"}, service = Object.class)
+public class CKEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/TinyMCEEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=tinymce"}, service = Object.class)
+public class TinyMCEEditor {
+}

modules/frontend/frontend-editors-web/src/com/liferay/frontend/editors/web/TinyMCESimpleEditor.java
@@ -1,0 +1,24 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.web;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(property = {"editor.wysiwyg=tinymce_simple"}, service = Object.class)
+public class TinyMCESimpleEditor {
+}