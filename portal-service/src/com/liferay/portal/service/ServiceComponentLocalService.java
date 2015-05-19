brianchandotcom/liferay-portal

LPS-55383 Build services

portal-service/src/com/liferay/portal/service/ServiceComponentLocalService.java
@@ -177,6 +177,9 @@ public interface ServiceComponentLocalService extends BaseLocalService,
 	*/
 	public java.lang.String getBeanIdentifier();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponentsByBuildNamespace();
+
 	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.PersistedModel getPersistedModel(

portal-service/src/com/liferay/portal/service/ServiceComponentLocalServiceUtil.java
@@ -199,6 +199,10 @@ public class ServiceComponentLocalServiceUtil {
 		return getService().getBeanIdentifier();
 	}
 
+	public static java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponentsByBuildNamespace() {
+		return getService().getLatestServiceComponentsByBuildNamespace();
+	}
+
 	public static com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portal/service/ServiceComponentLocalServiceWrapper.java
@@ -208,6 +208,11 @@ public class ServiceComponentLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponentsByBuildNamespace() {
+		return _serviceComponentLocalService.getLatestServiceComponentsByBuildNamespace();
+	}
+
+	@Override
 	public com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portal/service/persistence/ServiceComponentFinder.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.persistence;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@ProviderType
+public interface ServiceComponentFinder {
+	public java.util.List<com.liferay.portal.model.ServiceComponent> findLatestVersionByBuildNamespace();
+}

portal-service/src/com/liferay/portal/service/persistence/ServiceComponentFinderUtil.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.persistence;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@ProviderType
+public class ServiceComponentFinderUtil {
+	public static java.util.List<com.liferay.portal.model.ServiceComponent> findLatestVersionByBuildNamespace() {
+		return getFinder().findLatestVersionByBuildNamespace();
+	}
+
+	public static ServiceComponentFinder getFinder() {
+		if (_finder == null) {
+			_finder = (ServiceComponentFinder)PortalBeanLocatorUtil.locate(ServiceComponentFinder.class.getName());
+
+			ReferenceRegistry.registerReference(ServiceComponentFinderUtil.class,
+				"_finder");
+		}
+
+		return _finder;
+	}
+
+	public void setFinder(ServiceComponentFinder finder) {
+		_finder = finder;
+
+		ReferenceRegistry.registerReference(ServiceComponentFinderUtil.class,
+			"_finder");
+	}
+
+	private static ServiceComponentFinder _finder;
+}

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/service/ServiceComponentLocalService.java
@@ -177,6 +177,9 @@ public interface ServiceComponentLocalService extends BaseLocalService,
 	*/
 	public java.lang.String getBeanIdentifier();
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponents();
+
 	@Override
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portal.model.PersistedModel getPersistedModel(

portal-service/src/com/liferay/portal/service/ServiceComponentLocalServiceUtil.java
@@ -199,6 +199,10 @@ public class ServiceComponentLocalServiceUtil {
 		return getService().getBeanIdentifier();
 	}
 
+	public static java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponents() {
+		return getService().getLatestServiceComponents();
+	}
+
 	public static com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portal/service/ServiceComponentLocalServiceWrapper.java
@@ -208,6 +208,11 @@ public class ServiceComponentLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<com.liferay.portal.model.ServiceComponent> getLatestServiceComponents() {
+		return _serviceComponentLocalService.getLatestServiceComponents();
+	}
+
+	@Override
 	public com.liferay.portal.model.PersistedModel getPersistedModel(
 		java.io.Serializable primaryKeyObj)
 		throws com.liferay.portal.kernel.exception.PortalException {

portal-service/src/com/liferay/portal/service/persistence/ServiceComponentFinder.java
@@ -1,0 +1,26 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.persistence;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@ProviderType
+public interface ServiceComponentFinder {
+	public java.util.List<com.liferay.portal.model.ServiceComponent> findByMaxBuildNumber();
+}

portal-service/src/com/liferay/portal/service/persistence/ServiceComponentFinderUtil.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.service.persistence;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+/**
+ * @author Brian Wing Shun Chan
+ * @generated
+ */
+@ProviderType
+public class ServiceComponentFinderUtil {
+	public static java.util.List<com.liferay.portal.model.ServiceComponent> findByMaxBuildNumber() {
+		return getFinder().findByMaxBuildNumber();
+	}
+
+	public static ServiceComponentFinder getFinder() {
+		if (_finder == null) {
+			_finder = (ServiceComponentFinder)PortalBeanLocatorUtil.locate(ServiceComponentFinder.class.getName());
+
+			ReferenceRegistry.registerReference(ServiceComponentFinderUtil.class,
+				"_finder");
+		}
+
+		return _finder;
+	}
+
+	public void setFinder(ServiceComponentFinder finder) {
+		_finder = finder;
+
+		ReferenceRegistry.registerReference(ServiceComponentFinderUtil.class,
+			"_finder");
+	}
+
+	private static ServiceComponentFinder _finder;
+}