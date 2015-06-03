peterfellwock/liferay-portal

LPS-54505 - Update Consumers to select correct PortalWebResources

portal-impl/src/com/liferay/portal/model/impl/ThemeImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.ThemeHelper;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.web.PortalWebResourceConstants;
 import com.liferay.portal.kernel.web.PortalWebResourcesUtil;
 import com.liferay.portal.model.ColorScheme;
 import com.liferay.portal.model.Plugin;
@@ -306,7 +307,8 @@ public class ThemeImpl extends PluginBaseImpl implements Theme {
 			return proxyPath.concat(virtualPath);
 		}
 
-		String contextPath = PortalWebResourcesUtil.getContextPath();
+		String contextPath = PortalWebResourcesUtil.getContextPath(
+			PortalWebResourceConstants.RESOURCE_TYPE_CSS);
 
 		if (!isWARFile()) {
 			return contextPath;

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/model/impl/ThemeImpl.java
@@ -16,6 +16,8 @@ package com.liferay.portal.model.impl;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResourcesUtil;
 import com.liferay.portal.kernel.servlet.ServletContextPool;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.util.ContextPathUtil;
@@ -305,7 +307,8 @@ public class ThemeImpl extends PluginBaseImpl implements Theme {
 			return proxyPath.concat(virtualPath);
 		}
 
-		String contextPath = getContextPath();
+		String contextPath = PortalWebResourcesUtil.getContextPath(
+			PortalWebResourceConstants.RESOURCE_TYPE_CSS);
 
 		if (!isWARFile()) {
 			return contextPath;