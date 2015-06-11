holatuwol/liferay-portal

LPS-55098 ant build service portlet asset

portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalService.java
@@ -347,9 +347,9 @@ public interface AssetEntryLocalService extends BaseLocalService,
 		long[] groupIds, long[] classNameIds, java.lang.String keywords,
 		java.lang.String userName, java.lang.String title,
 		java.lang.String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, java.lang.String orderByCol1,
-		java.lang.String orderByCol2, java.lang.String orderByType1,
-		java.lang.String orderByType2);
+		boolean andOperator, boolean includeNonListable, int start, int end,
+		java.lang.String orderByCol1, java.lang.String orderByCol2,
+		java.lang.String orderByType1, java.lang.String orderByType2);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getEntriesCount(
@@ -359,7 +359,7 @@ public interface AssetEntryLocalService extends BaseLocalService,
 	public int getEntriesCount(long[] groupIds, long[] classNameIds,
 		java.lang.String keywords, java.lang.String userName,
 		java.lang.String title, java.lang.String description,
-		boolean advancedSearch, boolean andOperator);
+		boolean advancedSearch, boolean andOperator, boolean includeNonListable);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portlet.asset.model.AssetEntry getEntry(

liferay/liferay-central-test-1

portal-service/src/com/liferay/portlet/asset/service/AssetEntryLocalService.java
@@ -346,10 +346,10 @@ public interface AssetEntryLocalService extends BaseLocalService,
 	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> getEntries(
 		long[] groupIds, long[] classNameIds, java.lang.String keywords,
 		java.lang.String userName, java.lang.String title,
-		java.lang.String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, java.lang.String orderByCol1,
-		java.lang.String orderByCol2, java.lang.String orderByType1,
-		java.lang.String orderByType2);
+		java.lang.String description, java.lang.Boolean listable,
+		boolean advancedSearch, boolean andOperator, int start, int end,
+		java.lang.String orderByCol1, java.lang.String orderByCol2,
+		java.lang.String orderByType1, java.lang.String orderByType2);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getEntriesCount(
@@ -359,7 +359,7 @@ public interface AssetEntryLocalService extends BaseLocalService,
 	public int getEntriesCount(long[] groupIds, long[] classNameIds,
 		java.lang.String keywords, java.lang.String userName,
 		java.lang.String title, java.lang.String description,
-		boolean advancedSearch, boolean andOperator);
+		java.lang.Boolean listable, boolean advancedSearch, boolean andOperator);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public com.liferay.portlet.asset.model.AssetEntry getEntry(