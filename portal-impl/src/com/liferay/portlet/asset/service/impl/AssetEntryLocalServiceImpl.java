holatuwol/liferay-portal

LPS-55098 allow asset entry queries to find non-listable entries

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -195,13 +195,14 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	public List<AssetEntry> getEntries(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
 		String title, String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, String orderByCol1,
-		String orderByCol2, String orderByType1, String orderByType2) {
+		boolean andOperator, boolean includeNonListable, int start, int end,
+		String orderByCol1, String orderByCol2, String orderByType1,
+		String orderByType2) {
 
 		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
 			groupIds, classNameIds, keywords, userName, title, description,
-			advancedSearch, andOperator, start, end, orderByCol1, orderByCol2,
-			orderByType1, orderByType2);
+			includeNonListable, advancedSearch, andOperator, start, end, orderByCol1,
+			orderByCol2, orderByType1, orderByType2);
 
 		return getEntries(assetEntryQuery);
 	}
@@ -215,12 +216,12 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	public int getEntriesCount(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
 		String title, String description, boolean advancedSearch,
-		boolean andOperator) {
+		boolean andOperator, boolean includeNonListable) {
 
 		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
 			groupIds, classNameIds, keywords, userName, title, description,
-			advancedSearch, andOperator, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
-			null, null, null, null);
+			advancedSearch, andOperator, includeNonListable, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS, null, null, null, null);
 
 		return getEntriesCount(assetEntryQuery);
 	}
@@ -904,10 +905,15 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	protected AssetEntryQuery getAssetEntryQuery(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
 		String title, String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, String orderByCol1,
-		String orderByCol2, String orderByType1, String orderByType2) {
+		boolean andOperator, boolean includeNonListable, int start, int end,
+		String orderByCol1, String orderByCol2, String orderByType1,
+		String orderByType2) {
 
 		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
+
+		if (includeNonListable) {
+			assetEntryQuery.setListable(null);
+		}
 
 		if (advancedSearch) {
 			assetEntryQuery.setAndOperator(andOperator);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portlet/asset/service/impl/AssetEntryLocalServiceImpl.java
@@ -201,14 +201,15 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	@Override
 	public List<AssetEntry> getEntries(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
-		String title, String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, String orderByCol1,
-		String orderByCol2, String orderByType1, String orderByType2) {
+		String title, String description, Boolean listable,
+		boolean advancedSearch, boolean andOperator, int start, int end,
+		String orderByCol1, String orderByCol2, String orderByType1,
+		String orderByType2) {
 
 		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
 			groupIds, classNameIds, keywords, userName, title, description,
-			advancedSearch, andOperator, start, end, orderByCol1, orderByCol2,
-			orderByType1, orderByType2);
+			listable, advancedSearch, andOperator, start, end,
+			orderByCol1, orderByCol2, orderByType1, orderByType2);
 
 		return getEntries(assetEntryQuery);
 	}
@@ -221,13 +222,13 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 	@Override
 	public int getEntriesCount(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
-		String title, String description, boolean advancedSearch,
-		boolean andOperator) {
+		String title, String description, Boolean listable,
+		boolean advancedSearch, boolean andOperator) {
 
 		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
 			groupIds, classNameIds, keywords, userName, title, description,
-			advancedSearch, andOperator, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
-			null, null, null, null);
+			listable, advancedSearch, andOperator, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS, null, null, null, null);
 
 		return getEntriesCount(assetEntryQuery);
 	}
@@ -968,9 +969,10 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 
 	protected AssetEntryQuery getAssetEntryQuery(
 		long[] groupIds, long[] classNameIds, String keywords, String userName,
-		String title, String description, boolean advancedSearch,
-		boolean andOperator, int start, int end, String orderByCol1,
-		String orderByCol2, String orderByType1, String orderByType2) {
+		String title, String description, Boolean listable,
+		boolean advancedSearch, boolean andOperator, int start, int end,
+		String orderByCol1, String orderByCol2, String orderByType1,
+		String orderByType2) {
 
 		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
 
@@ -987,6 +989,7 @@ public class AssetEntryLocalServiceImpl extends AssetEntryLocalServiceBaseImpl {
 		assetEntryQuery.setClassNameIds(classNameIds);
 		assetEntryQuery.setEnd(end);
 		assetEntryQuery.setGroupIds(groupIds);
+		assetEntryQuery.setListable(listable);
 		assetEntryQuery.setOrderByCol1(orderByCol1);
 		assetEntryQuery.setOrderByCol2(orderByCol2);
 		assetEntryQuery.setOrderByType1(orderByType1);