liferay/liferay-central-test-1

commit 9c5070569e0b4802cbd7f2042a9023a4637027f4

LPS-56056 Create Render Command for Blogs Admin

portal-impl/src/com/liferay/portlet/blogsadmin/action/ViewMVCRenderCommand.java
@@ -1,0 +1,44 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portlet.blogsadmin.action;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * @author Sergio González
+ */
+@OSGiBeanProperties(
+	property = {
+		"javax.portlet.name=" + PortletKeys.BLOGS_ADMIN, "mvc.command.name=",
+		"mvc.command.name=/blogs_admin/view"
+	}
+)
+public class ViewMVCRenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		return "/html/portlet/blogs_admin/view.jsp";
+	}
+
+}

portal-impl/src/META-INF/blogs-spring.xml
@@ -8,4 +8,5 @@
 	xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans-3.0.xsd"
 >
 	<bean id="com.liferay.portlet.blogs.action.ViewMVCRenderCommand" class="com.liferay.portlet.blogs.action.ViewMVCRenderCommand" />
+	<bean id="com.liferay.portlet.blogsadmin.action.ViewMVCRenderCommand" class="com.liferay.portlet.blogsadmin.action.ViewMVCRenderCommand" />
 </beans>

portal-web/docroot/WEB-INF/portlet-custom.xml
@@ -942,10 +942,6 @@
 			<name>template-path</name>
 			<value>/html/portlet/blogs_admin/</value>
 		</init-param>
-		<init-param>
-			<name>view-action</name>
-			<value>/blogs_admin/view</value>
-		</init-param>
 		<expiration-cache>0</expiration-cache>
 		<supports>
 			<mime-type>text/html</mime-type>

portal-web/docroot/WEB-INF/struts-config.xml
@@ -255,8 +255,6 @@
 
 		<action path="/blogs_admin/upload_editor_image" type="com.liferay.portlet.blogs.action.UploadEditorImageAction" />
 
-		<action path="/blogs_admin/view" forward="portlet.blogs_admin.view" />
-
 		<action path="/blogs_admin/view_entry" type="com.liferay.portlet.blogs.action.EditEntryAction">
 			<forward name="portlet.blogs.edit_entry" path="portlet.blogs.view_entry" />
 			<forward name="portlet.blogs.error" path="portlet.blogs.error" />

portal-web/docroot/WEB-INF/tiles-defs.xml
@@ -176,12 +176,6 @@
 		<put name="portlet_content" value="/portlet/blogs/view_entry.jsp" />
 	</definition>
 
-	<!-- Blogs Admin -->
-
-	<definition name="portlet.blogs_admin.view" extends="portlet">
-			<put name="portlet_content" value="/portlet/blogs_admin/view.jsp" />
-	</definition>
-
 	<!-- Blogs Aggregator -->
 
 	<definition name="portlet.blogs_aggregator" extends="portlet" />

portal-web/docroot/html/portlet/blogs/view_entry.jsp
@@ -25,7 +25,7 @@ if (Validator.isNull(redirect) || (strutsAction.equals("/blogs/view_entry") && !
 	PortletURL portletURL = renderResponse.createRenderURL();
 
 	if (portletId.equals(PortletKeys.BLOGS_ADMIN)) {
-		portletURL.setParameter("struts_action", "/blogs_admin/view");
+		portletURL.setParameter("mvcRenderCommandName", "/blogs_admin/view");
 	}
 	else if (portletId.equals(PortletKeys.BLOGS_AGGREGATOR)) {
 		portletURL.setParameter("struts_action", "/blogs_aggregator/view");

portal-web/docroot/html/portlet/blogs_admin/toolbar.jsp
@@ -23,7 +23,7 @@ String toolbarItem = ParamUtil.getString(request, "toolbarItem");
 <aui:nav-bar>
 	<aui:nav cssClass="navbar-nav">
 		<portlet:renderURL var="viewEntriesURL">
-			<portlet:param name="struts_action" value="/blogs_admin/view" />
+			<portlet:param name="mvcRenderCommandName" value="/blogs_admin/view" />
 		</portlet:renderURL>
 
 		<c:if test="<%= BlogsPermission.contains(permissionChecker, scopeGroupId, ActionKeys.ADD_ENTRY) %>">

portal-web/docroot/html/portlet/blogs_admin/view.jsp
@@ -22,7 +22,7 @@ String assetTagName = ParamUtil.getString(request, "tag");
 
 PortletURL portletURL = renderResponse.createRenderURL();
 
-portletURL.setParameter("struts_action", "/blogs_admin/view");
+portletURL.setParameter("mvcRenderCommandName", "/blogs_admin/view");
 %>
 
 <portlet:actionURL var="restoreTrashEntriesURL">

commit fc93934b2c529a73c0fbd9c56da3d2e5b1e7d99b

LPS-56056 Duplicated attributes cause shutdown failure (caused by 9c5070569e0b4802cbd7f2042a9023a4637027f4)

portal-impl/src/com/liferay/portlet/blogs/action/ViewMVCRenderCommand.java
@@ -26,7 +26,7 @@ import javax.portlet.RenderResponse;
  */
 @OSGiBeanProperties(
 	property = {
-		"javax.portlet.name=" + PortletKeys.BLOGS, "mvc.command.name=",
+		"javax.portlet.name=" + PortletKeys.BLOGS,
 		"mvc.command.name=/blogs/view"
 	}
 )

portal-impl/src/com/liferay/portlet/blogsadmin/action/ViewMVCRenderCommand.java
@@ -26,7 +26,7 @@ import javax.portlet.RenderResponse;
  */
 @OSGiBeanProperties(
 	property = {
-		"javax.portlet.name=" + PortletKeys.BLOGS_ADMIN, "mvc.command.name=",
+		"javax.portlet.name=" + PortletKeys.BLOGS_ADMIN,
 		"mvc.command.name=/blogs_admin/view"
 	}
 )

portal-impl/src/com/liferay/portlet/blogsaggregator/action/ViewMVCRenderCommand.java
@@ -26,7 +26,7 @@ import javax.portlet.RenderResponse;
  */
 @OSGiBeanProperties(
 	property = {
-		"javax.portlet.name=" + PortletKeys.BLOGS_AGGREGATOR, "mvc.command.name=",
+		"javax.portlet.name=" + PortletKeys.BLOGS_AGGREGATOR,
 		"mvc.command.name=/blogs_aggregator/view"
 	}
 )