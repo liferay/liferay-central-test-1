sergiogonzalez/liferay-portal

LPS-56285 Require ClassedModel instead of using className/classPK pairs

portal-impl/src/com/liferay/portal/repository/social/FileEntrySocialActivityHandler.java
@@ -12,24 +12,37 @@
  * details.
  */
 
-package com.liferay.portlet.social.handler;
+package com.liferay.portal.repository.social;
 
 import com.liferay.portal.kernel.bean.BeanReference;
-import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
+import com.liferay.portlet.social.handler.BaseSocialActivityHandler;
+import com.liferay.portlet.social.handler.SocialActivityHandler;
 import com.liferay.portlet.social.service.SocialActivityLocalService;
 
 /**
  * @author Adolfo Pérez
  */
-public class DefaultSocialActivityHandler
-	extends BaseSocialActivityHandler<ClassedModel> {
+@OSGiBeanProperties(
+	property = "model.className=com.liferay.portal.kernel.repository.model.FileEntry",
+	service = SocialActivityHandler.class
+)
+public class FileEntrySocialActivityHandler
+	extends BaseSocialActivityHandler<FileEntry> {
+
+	@Override
+	protected String getClassName(FileEntry classedModel) {
+		return DLFileEntryConstants.getClassName();
+	}
 
 	@Override
 	protected SocialActivityLocalService getSocialActivityLocalService() {
-		return _socialActivityLocalService;
+		return socialActivityLocalService;
 	}
 
 	@BeanReference(type = SocialActivityLocalService.class)
-	protected SocialActivityLocalService _socialActivityLocalService;
+	protected SocialActivityLocalService socialActivityLocalService;
 
 }

portal-impl/src/com/liferay/portal/repository/social/FolderSocialActivityHandler.java
@@ -12,24 +12,37 @@
  * details.
  */
 
-package com.liferay.portlet.social.handler;
+package com.liferay.portal.repository.social;
 
 import com.liferay.portal.kernel.bean.BeanReference;
-import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.social.handler.BaseSocialActivityHandler;
+import com.liferay.portlet.social.handler.SocialActivityHandler;
 import com.liferay.portlet.social.service.SocialActivityLocalService;
 
 /**
  * @author Adolfo Pérez
  */
-public class DefaultSocialActivityHandler
-	extends BaseSocialActivityHandler<ClassedModel> {
+@OSGiBeanProperties(
+	property = "model.className=com.liferay.portal.kernel.repository.model.Folder",
+	service = SocialActivityHandler.class
+)
+public class FolderSocialActivityHandler
+	extends BaseSocialActivityHandler<Folder> {
+
+	@Override
+	protected String getClassName(Folder folder) {
+		return DLFolderConstants.getClassName();
+	}
 
 	@Override
 	protected SocialActivityLocalService getSocialActivityLocalService() {
-		return _socialActivityLocalService;
+		return socialActivityLocalService;
 	}
 
 	@BeanReference(type = SocialActivityLocalService.class)
-	protected SocialActivityLocalService _socialActivityLocalService;
+	protected SocialActivityLocalService socialActivityLocalService;
 
 }

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/repository/social/FileEntrySocialActivityHandler.java
@@ -12,24 +12,37 @@
  * details.
  */
 
-package com.liferay.portlet.social.handler;
+package com.liferay.portal.repository.social;
 
 import com.liferay.portal.kernel.bean.BeanReference;
-import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
+import com.liferay.portlet.social.handler.BaseSocialActivityHandler;
+import com.liferay.portlet.social.handler.SocialActivityHandler;
 import com.liferay.portlet.social.service.SocialActivityLocalService;
 
 /**
  * @author Adolfo Pérez
  */
-public class DefaultSocialActivityHandler
-	extends BaseSocialActivityHandler<ClassedModel> {
+@OSGiBeanProperties(
+	property = "model.className=com.liferay.portal.repository.liferayrepository.model.LiferayFileEntry",
+	service = SocialActivityHandler.class
+)
+public class FileEntrySocialActivityHandler
+	extends BaseSocialActivityHandler<FileEntry> {
+
+	@Override
+	protected String getClassName(FileEntry classedModel) {
+		return DLFileEntryConstants.getClassName();
+	}
 
 	@Override
 	protected SocialActivityLocalService getSocialActivityLocalService() {
-		return _socialActivityLocalService;
+		return socialActivityLocalService;
 	}
 
 	@BeanReference(type = SocialActivityLocalService.class)
-	protected SocialActivityLocalService _socialActivityLocalService;
+	protected SocialActivityLocalService socialActivityLocalService;
 
 }

portal-impl/src/com/liferay/portal/repository/social/FolderSocialActivityHandler.java
@@ -12,24 +12,37 @@
  * details.
  */
 
-package com.liferay.portlet.social.handler;
+package com.liferay.portal.repository.social;
 
 import com.liferay.portal.kernel.bean.BeanReference;
-import com.liferay.portal.model.ClassedModel;
+import com.liferay.portal.kernel.repository.model.Folder;
+import com.liferay.portal.kernel.spring.osgi.OSGiBeanProperties;
+import com.liferay.portlet.documentlibrary.model.DLFolderConstants;
+import com.liferay.portlet.social.handler.BaseSocialActivityHandler;
+import com.liferay.portlet.social.handler.SocialActivityHandler;
 import com.liferay.portlet.social.service.SocialActivityLocalService;
 
 /**
  * @author Adolfo Pérez
  */
-public class DefaultSocialActivityHandler
-	extends BaseSocialActivityHandler<ClassedModel> {
+@OSGiBeanProperties(
+	property = "model.className=com.liferay.portal.repository.liferayrepository.model.LiferayFolder",
+	service = SocialActivityHandler.class
+)
+public class FolderSocialActivityHandler
+	extends BaseSocialActivityHandler<Folder> {
+
+	@Override
+	protected String getClassName(Folder folder) {
+		return DLFolderConstants.getClassName();
+	}
 
 	@Override
 	protected SocialActivityLocalService getSocialActivityLocalService() {
-		return _socialActivityLocalService;
+		return socialActivityLocalService;
 	}
 
 	@BeanReference(type = SocialActivityLocalService.class)
-	protected SocialActivityLocalService _socialActivityLocalService;
+	protected SocialActivityLocalService socialActivityLocalService;
 
 }