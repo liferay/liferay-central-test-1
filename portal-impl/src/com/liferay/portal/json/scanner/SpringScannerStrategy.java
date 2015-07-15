brianchandotcom/liferay-portal

LPS-57084 Allow to plug registrators and scanners when registering JSONWS

portal-impl/src/com/liferay/portal/json/scanner/SpringScannerStrategy.java
@@ -1,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.json.scanner;
+
+import com.liferay.portal.kernel.json.JSONWebServiceScannerStrategy;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
+
+import java.lang.reflect.Method;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.springframework.aop.TargetSource;
+import org.springframework.aop.framework.AdvisedSupport;
+
+/**
+ * @author Miguel Pastor
+ */
+public class SpringScannerStrategy implements JSONWebServiceScannerStrategy {
+
+	@Override
+	public MethodDescriptor[] scan(Object service) {
+		Class<?> clazz = null;
+
+		try {
+			clazz = getTargetClass(service);
+		}
+		catch (Exception e) {
+			return new MethodDescriptor[0];
+		}
+
+		Method[] methods = clazz.getMethods();
+		List<MethodDescriptor> result = new ArrayList<>(methods.length);
+
+		for (Method method : methods) {
+			Class<?> declaringClass = method.getDeclaringClass();
+
+			if ((declaringClass != clazz) || !isInterfaceMethod(method)) {
+				continue;
+			}
+
+			result.add(new MethodDescriptor(method));
+		}
+
+		return result.toArray(new MethodDescriptor[result.size()]);
+	}
+
+	protected Class<?> getTargetClass(Object service) throws Exception {
+		if (ProxyUtil.isProxyClass(service.getClass())) {
+			AdvisedSupport advisedSupport =
+				ServiceBeanAopProxy.getAdvisedSupport(service);
+
+			TargetSource targetSource = advisedSupport.getTargetSource();
+
+			service = targetSource.getTarget();
+		}
+
+		return service.getClass();
+	}
+
+	protected boolean isInterfaceMethod(Method method) {
+		Class<?> declaringClass = method.getDeclaringClass();
+
+		if (declaringClass.isInterface()) {
+			return true;
+		}
+
+		Queue<Class<?>> queue = new LinkedList<>(
+			Arrays.asList(declaringClass.getInterfaces()));
+
+		Class<?> superClass = declaringClass.getSuperclass();
+
+		if (superClass != null) {
+			queue.add(superClass);
+		}
+
+		Class<?> clazz = null;
+
+		while ((clazz = queue.poll()) != null) {
+			if (clazz.isInterface()) {
+				try {
+					clazz.getMethod(
+						method.getName(), method.getParameterTypes());
+
+					return true;
+				}
+				catch (ReflectiveOperationException roe) {
+				}
+			}
+			else {
+				queue.addAll(Arrays.asList(clazz.getInterfaces()));
+
+				superClass = clazz.getSuperclass();
+
+				if (superClass != null) {
+					queue.add(superClass);
+				}
+			}
+		}
+
+		return false;
+	}
+
+}

portal-impl/src/com/liferay/portal/jsonwebservice/DefaultJSONWebServiceRegistrator.java
@@ -14,9 +14,11 @@
 
 package com.liferay.portal.jsonwebservice;
 
+import com.liferay.portal.json.scanner.SpringScannerStrategy;
 import com.liferay.portal.kernel.annotation.AnnotationLocator;
 import com.liferay.portal.kernel.bean.BeanLocator;
 import com.liferay.portal.kernel.bean.BeanLocatorException;
+import com.liferay.portal.kernel.json.JSONWebServiceScannerStrategy;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebService;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionsManagerUtil;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMappingResolver;
@@ -24,38 +26,35 @@ import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceNaming;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ProxyUtil;
-import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
 import com.liferay.portal.util.PropsValues;
 
 import java.lang.reflect.Method;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
-
-import org.springframework.aop.TargetSource;
-import org.springframework.aop.framework.AdvisedSupport;
 
 /**
  * @author Igor Spasic
  */
-public class JSONWebServiceRegistrator {
+public class DefaultJSONWebServiceRegistrator
+	implements com.liferay.portal.kernel.json.JSONWebServiceRegistrator {
 
-	public JSONWebServiceRegistrator() {
+	public DefaultJSONWebServiceRegistrator() {
 		_jsonWebServiceNaming =
 			JSONWebServiceActionsManagerUtil.getJSONWebServiceNaming();
 
 		_jsonWebServiceMappingResolver = new JSONWebServiceMappingResolver(
 			_jsonWebServiceNaming);
+
+		_jsonWebServiceScannerStrategy = new SpringScannerStrategy();
 	}
 
-	public JSONWebServiceRegistrator(
-		JSONWebServiceNaming jsonWebServiceNaming) {
+	public DefaultJSONWebServiceRegistrator(
+		JSONWebServiceNaming jsonWebServiceNaming,
+		JSONWebServiceScannerStrategy jsonWebServiceScannerStrategy) {
 
 		_jsonWebServiceNaming = jsonWebServiceNaming;
+		_jsonWebServiceScannerStrategy = jsonWebServiceScannerStrategy;
 
 		_jsonWebServiceMappingResolver = new JSONWebServiceMappingResolver(
 			_jsonWebServiceNaming);
@@ -110,6 +109,7 @@ public class JSONWebServiceRegistrator {
 		}
 	}
 
+	@Override
 	public void processBean(
 		String contextName, String contextPath, Object bean) {
 
@@ -135,62 +135,6 @@ public class JSONWebServiceRegistrator {
 
 	public void setWireViaUtil(boolean wireViaUtil) {
 		this._wireViaUtil = wireViaUtil;
-	}
-
-	protected Class<?> getTargetClass(Object service) throws Exception {
-		if (ProxyUtil.isProxyClass(service.getClass())) {
-			AdvisedSupport advisedSupport =
-				ServiceBeanAopProxy.getAdvisedSupport(service);
-
-			TargetSource targetSource = advisedSupport.getTargetSource();
-
-			service = targetSource.getTarget();
-		}
-
-		return service.getClass();
-	}
-
-	protected boolean isInterfaceMethod(Method method) {
-		Class<?> declaringClass = method.getDeclaringClass();
-
-		if (declaringClass.isInterface()) {
-			return true;
-		}
-
-		Queue<Class<?>> queue = new LinkedList<>(
-			Arrays.asList(declaringClass.getInterfaces()));
-
-		Class<?> superClass = declaringClass.getSuperclass();
-
-		if (superClass != null) {
-			queue.add(superClass);
-		}
-
-		Class<?> clazz = null;
-
-		while ((clazz = queue.poll()) != null) {
-			if (clazz.isInterface()) {
-				try {
-					clazz.getMethod(
-						method.getName(), method.getParameterTypes());
-
-					return true;
-				}
-				catch (ReflectiveOperationException roe) {
-				}
-			}
-			else {
-				queue.addAll(Arrays.asList(clazz.getInterfaces()));
-
-				superClass = clazz.getSuperclass();
-
-				if (superClass != null) {
-					queue.add(superClass);
-				}
-			}
-		}
-
-		return false;
 	}
 
 	protected Class<?> loadUtilClass(Class<?> implementationClass)
@@ -230,18 +174,13 @@ public class JSONWebServiceRegistrator {
 			jsonWebServiceMode = jsonWebService.mode();
 		}
 
-		Class<?> serviceBeanClass = getTargetClass(serviceBean);
+		JSONWebServiceScannerStrategy.MethodDescriptor[] methodDescriptors =
+			_jsonWebServiceScannerStrategy.scan(serviceBean);
 
-		Method[] methods = serviceBeanClass.getMethods();
+		for (JSONWebServiceScannerStrategy.MethodDescriptor methodDescriptor :
+				methodDescriptors) {
 
-		for (Method method : methods) {
-			Class<?> declaringClass = method.getDeclaringClass();
-
-			if ((declaringClass != serviceBeanClass) ||
-				!isInterfaceMethod(method)) {
-
-				continue;
-			}
+			final Method method = methodDescriptor.getMethod();
 
 			if (!_jsonWebServiceNaming.isIncludedMethod(method)) {
 				continue;
@@ -250,10 +189,12 @@ public class JSONWebServiceRegistrator {
 			JSONWebService methodJSONWebService = method.getAnnotation(
 				JSONWebService.class);
 
+			Class<?> declaringClass = methodDescriptor.getDeclaringClass();
+
 			if (jsonWebServiceMode.equals(JSONWebServiceMode.AUTO)) {
 				if (methodJSONWebService == null) {
 					registerJSONWebServiceAction(
-						contextName, contextPath, serviceBean, serviceBeanClass,
+						contextName, contextPath, serviceBean, declaringClass,
 						method);
 				}
 				else {
@@ -265,7 +206,7 @@ public class JSONWebServiceRegistrator {
 
 						registerJSONWebServiceAction(
 							contextName, contextPath, serviceBean,
-							serviceBeanClass, method);
+							declaringClass, method);
 					}
 				}
 			}
@@ -277,7 +218,7 @@ public class JSONWebServiceRegistrator {
 						JSONWebServiceMode.IGNORE)) {
 
 					registerJSONWebServiceAction(
-						contextName, contextPath, serviceBean, serviceBeanClass,
+						contextName, contextPath, serviceBean, declaringClass,
 						method);
 				}
 			}
@@ -328,10 +269,11 @@ public class JSONWebServiceRegistrator {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
-		JSONWebServiceRegistrator.class);
+		DefaultJSONWebServiceRegistrator.class);
 
 	private final JSONWebServiceMappingResolver _jsonWebServiceMappingResolver;
 	private final JSONWebServiceNaming _jsonWebServiceNaming;
+	private final JSONWebServiceScannerStrategy _jsonWebServiceScannerStrategy;
 	private Map<Class<?>, Class<?>> _utilClasses;
 	private boolean _wireViaUtil;
 

portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceActionsManagerImpl.java
@@ -17,6 +17,7 @@ package com.liferay.portal.jsonwebservice;
 import com.liferay.portal.kernel.bean.BeanLocator;
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
 import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
+import com.liferay.portal.kernel.json.JSONWebServiceRegistrator;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceAction;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionMapping;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionsManager;
@@ -276,7 +277,16 @@ public class JSONWebServiceActionsManagerImpl
 		String contextName, String contextPath, Object service) {
 
 		JSONWebServiceRegistrator jsonWebServiceRegistrator =
-			new JSONWebServiceRegistrator();
+			new DefaultJSONWebServiceRegistrator();
+
+		return registerService(
+			contextName, contextPath, service, jsonWebServiceRegistrator);
+	}
+
+	@Override
+	public int registerService(
+		String contextName, String contextPath, Object service,
+		JSONWebServiceRegistrator jsonWebServiceRegistrator) {
 
 		jsonWebServiceRegistrator.processBean(
 			contextName, contextPath, service);
@@ -315,10 +325,10 @@ public class JSONWebServiceActionsManagerImpl
 			return -1;
 		}
 
-		JSONWebServiceRegistrator jsonWebServiceRegistrator =
-			new JSONWebServiceRegistrator();
+		DefaultJSONWebServiceRegistrator defaultJsonWebServiceRegistrator =
+			new DefaultJSONWebServiceRegistrator();
 
-		jsonWebServiceRegistrator.processAllBeans(
+		defaultJsonWebServiceRegistrator.processAllBeans(
 			contextName, contextPath, beanLocator);
 
 		int count = getJSONWebServiceActionsCount(contextPath);

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/json/scanner/SpringScannerStrategy.java
@@ -1,0 +1,120 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.json.scanner;
+
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceScannerStrategy;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
+
+import java.lang.reflect.Method;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import org.springframework.aop.TargetSource;
+import org.springframework.aop.framework.AdvisedSupport;
+
+/**
+ * @author Miguel Pastor
+ */
+public class SpringScannerStrategy implements JSONWebServiceScannerStrategy {
+
+	@Override
+	public MethodDescriptor[] scan(Object service) {
+		Class<?> clazz = null;
+
+		try {
+			clazz = getTargetClass(service);
+		}
+		catch (Exception e) {
+			return new MethodDescriptor[0];
+		}
+
+		Method[] methods = clazz.getMethods();
+		List<MethodDescriptor> result = new ArrayList<>(methods.length);
+
+		for (Method method : methods) {
+			Class<?> declaringClass = method.getDeclaringClass();
+
+			if ((declaringClass != clazz) || !isInterfaceMethod(method)) {
+				continue;
+			}
+
+			result.add(new MethodDescriptor(method));
+		}
+
+		return result.toArray(new MethodDescriptor[result.size()]);
+	}
+
+	protected Class<?> getTargetClass(Object service) throws Exception {
+		if (ProxyUtil.isProxyClass(service.getClass())) {
+			AdvisedSupport advisedSupport =
+				ServiceBeanAopProxy.getAdvisedSupport(service);
+
+			TargetSource targetSource = advisedSupport.getTargetSource();
+
+			service = targetSource.getTarget();
+		}
+
+		return service.getClass();
+	}
+
+	protected boolean isInterfaceMethod(Method method) {
+		Class<?> declaringClass = method.getDeclaringClass();
+
+		if (declaringClass.isInterface()) {
+			return true;
+		}
+
+		Queue<Class<?>> queue = new LinkedList<>(
+			Arrays.asList(declaringClass.getInterfaces()));
+
+		Class<?> superClass = declaringClass.getSuperclass();
+
+		if (superClass != null) {
+			queue.add(superClass);
+		}
+
+		Class<?> clazz = null;
+
+		while ((clazz = queue.poll()) != null) {
+			if (clazz.isInterface()) {
+				try {
+					clazz.getMethod(
+						method.getName(), method.getParameterTypes());
+
+					return true;
+				}
+				catch (ReflectiveOperationException roe) {
+				}
+			}
+			else {
+				queue.addAll(Arrays.asList(clazz.getInterfaces()));
+
+				superClass = clazz.getSuperclass();
+
+				if (superClass != null) {
+					queue.add(superClass);
+				}
+			}
+		}
+
+		return false;
+	}
+
+}

portal-impl/src/com/liferay/portal/jsonwebservice/DefaultJSONWebServiceRegistrator.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.jsonwebservice;
 
+import com.liferay.portal.json.scanner.SpringScannerStrategy;
 import com.liferay.portal.kernel.annotation.AnnotationLocator;
 import com.liferay.portal.kernel.bean.BeanLocator;
 import com.liferay.portal.kernel.bean.BeanLocatorException;
@@ -22,40 +23,39 @@ import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionsManagerUtil
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMappingResolver;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceMode;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceNaming;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceRegistrator;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceScannerStrategy;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.util.ProxyUtil;
-import com.liferay.portal.spring.aop.ServiceBeanAopProxy;
 import com.liferay.portal.util.PropsValues;
 
 import java.lang.reflect.Method;
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
-import java.util.Queue;
-
-import org.springframework.aop.TargetSource;
-import org.springframework.aop.framework.AdvisedSupport;
 
 /**
  * @author Igor Spasic
  */
-public class JSONWebServiceRegistrator {
+public class DefaultJSONWebServiceRegistrator
+	implements JSONWebServiceRegistrator {
 
-	public JSONWebServiceRegistrator() {
+	public DefaultJSONWebServiceRegistrator() {
 		_jsonWebServiceNaming =
 			JSONWebServiceActionsManagerUtil.getJSONWebServiceNaming();
 
 		_jsonWebServiceMappingResolver = new JSONWebServiceMappingResolver(
 			_jsonWebServiceNaming);
+
+		_jsonWebServiceScannerStrategy = new SpringScannerStrategy();
 	}
 
-	public JSONWebServiceRegistrator(
-		JSONWebServiceNaming jsonWebServiceNaming) {
+	public DefaultJSONWebServiceRegistrator(
+		JSONWebServiceNaming jsonWebServiceNaming,
+		JSONWebServiceScannerStrategy jsonWebServiceScannerStrategy) {
 
 		_jsonWebServiceNaming = jsonWebServiceNaming;
+		_jsonWebServiceScannerStrategy = jsonWebServiceScannerStrategy;
 
 		_jsonWebServiceMappingResolver = new JSONWebServiceMappingResolver(
 			_jsonWebServiceNaming);
@@ -110,6 +110,7 @@ public class JSONWebServiceRegistrator {
 		}
 	}
 
+	@Override
 	public void processBean(
 		String contextName, String contextPath, Object bean) {
 
@@ -135,62 +136,6 @@ public class JSONWebServiceRegistrator {
 
 	public void setWireViaUtil(boolean wireViaUtil) {
 		this._wireViaUtil = wireViaUtil;
-	}
-
-	protected Class<?> getTargetClass(Object service) throws Exception {
-		if (ProxyUtil.isProxyClass(service.getClass())) {
-			AdvisedSupport advisedSupport =
-				ServiceBeanAopProxy.getAdvisedSupport(service);
-
-			TargetSource targetSource = advisedSupport.getTargetSource();
-
-			service = targetSource.getTarget();
-		}
-
-		return service.getClass();
-	}
-
-	protected boolean isInterfaceMethod(Method method) {
-		Class<?> declaringClass = method.getDeclaringClass();
-
-		if (declaringClass.isInterface()) {
-			return true;
-		}
-
-		Queue<Class<?>> queue = new LinkedList<>(
-			Arrays.asList(declaringClass.getInterfaces()));
-
-		Class<?> superClass = declaringClass.getSuperclass();
-
-		if (superClass != null) {
-			queue.add(superClass);
-		}
-
-		Class<?> clazz = null;
-
-		while ((clazz = queue.poll()) != null) {
-			if (clazz.isInterface()) {
-				try {
-					clazz.getMethod(
-						method.getName(), method.getParameterTypes());
-
-					return true;
-				}
-				catch (ReflectiveOperationException roe) {
-				}
-			}
-			else {
-				queue.addAll(Arrays.asList(clazz.getInterfaces()));
-
-				superClass = clazz.getSuperclass();
-
-				if (superClass != null) {
-					queue.add(superClass);
-				}
-			}
-		}
-
-		return false;
 	}
 
 	protected Class<?> loadUtilClass(Class<?> implementationClass)
@@ -230,18 +175,13 @@ public class JSONWebServiceRegistrator {
 			jsonWebServiceMode = jsonWebService.mode();
 		}
 
-		Class<?> serviceBeanClass = getTargetClass(serviceBean);
+		JSONWebServiceScannerStrategy.MethodDescriptor[] methodDescriptors =
+			_jsonWebServiceScannerStrategy.scan(serviceBean);
 
-		Method[] methods = serviceBeanClass.getMethods();
+		for (JSONWebServiceScannerStrategy.MethodDescriptor methodDescriptor :
+				methodDescriptors) {
 
-		for (Method method : methods) {
-			Class<?> declaringClass = method.getDeclaringClass();
-
-			if ((declaringClass != serviceBeanClass) ||
-				!isInterfaceMethod(method)) {
-
-				continue;
-			}
+			final Method method = methodDescriptor.getMethod();
 
 			if (!_jsonWebServiceNaming.isIncludedMethod(method)) {
 				continue;
@@ -250,10 +190,12 @@ public class JSONWebServiceRegistrator {
 			JSONWebService methodJSONWebService = method.getAnnotation(
 				JSONWebService.class);
 
+			Class<?> declaringClass = methodDescriptor.getDeclaringClass();
+
 			if (jsonWebServiceMode.equals(JSONWebServiceMode.AUTO)) {
 				if (methodJSONWebService == null) {
 					registerJSONWebServiceAction(
-						contextName, contextPath, serviceBean, serviceBeanClass,
+						contextName, contextPath, serviceBean, declaringClass,
 						method);
 				}
 				else {
@@ -265,7 +207,7 @@ public class JSONWebServiceRegistrator {
 
 						registerJSONWebServiceAction(
 							contextName, contextPath, serviceBean,
-							serviceBeanClass, method);
+							declaringClass, method);
 					}
 				}
 			}
@@ -277,7 +219,7 @@ public class JSONWebServiceRegistrator {
 						JSONWebServiceMode.IGNORE)) {
 
 					registerJSONWebServiceAction(
-						contextName, contextPath, serviceBean, serviceBeanClass,
+						contextName, contextPath, serviceBean, declaringClass,
 						method);
 				}
 			}
@@ -328,10 +270,11 @@ public class JSONWebServiceRegistrator {
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
-		JSONWebServiceRegistrator.class);
+		DefaultJSONWebServiceRegistrator.class);
 
 	private final JSONWebServiceMappingResolver _jsonWebServiceMappingResolver;
 	private final JSONWebServiceNaming _jsonWebServiceNaming;
+	private final JSONWebServiceScannerStrategy _jsonWebServiceScannerStrategy;
 	private Map<Class<?>, Class<?>> _utilClasses;
 	private boolean _wireViaUtil;
 

portal-impl/src/com/liferay/portal/jsonwebservice/JSONWebServiceActionsManagerImpl.java
@@ -21,6 +21,7 @@ import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceAction;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionMapping;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceActionsManager;
 import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceNaming;
+import com.liferay.portal.kernel.jsonwebservice.JSONWebServiceRegistrator;
 import com.liferay.portal.kernel.jsonwebservice.NoSuchJSONWebServiceException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -276,7 +277,16 @@ public class JSONWebServiceActionsManagerImpl
 		String contextName, String contextPath, Object service) {
 
 		JSONWebServiceRegistrator jsonWebServiceRegistrator =
-			new JSONWebServiceRegistrator();
+			new DefaultJSONWebServiceRegistrator();
+
+		return registerService(
+			contextName, contextPath, service, jsonWebServiceRegistrator);
+	}
+
+	@Override
+	public int registerService(
+		String contextName, String contextPath, Object service,
+		JSONWebServiceRegistrator jsonWebServiceRegistrator) {
 
 		jsonWebServiceRegistrator.processBean(
 			contextName, contextPath, service);
@@ -315,10 +325,10 @@ public class JSONWebServiceActionsManagerImpl
 			return -1;
 		}
 
-		JSONWebServiceRegistrator jsonWebServiceRegistrator =
-			new JSONWebServiceRegistrator();
+		DefaultJSONWebServiceRegistrator defaultJsonWebServiceRegistrator =
+			new DefaultJSONWebServiceRegistrator();
 
-		jsonWebServiceRegistrator.processAllBeans(
+		defaultJsonWebServiceRegistrator.processAllBeans(
 			contextName, contextPath, beanLocator);
 
 		int count = getJSONWebServiceActionsCount(contextPath);