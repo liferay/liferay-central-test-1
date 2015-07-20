JorgeFerrer/liferay-portal

LPS-55583 init.jsp

modules/apps/user-groups-admin/user-groups-admin-web/src/META-INF/resources/init.jsp
@@ -14,16 +14,77 @@
  */
 --%>
 
-<%@ include file="/html/portlet/init.jsp" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<%@ taglib uri="http://liferay.com/tld/security" prefix="liferay-security" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
+<%@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
 <%@ page import="com.liferay.portal.DuplicateUserGroupException" %><%@
+page import="com.liferay.portal.kernel.dao.search.SearchContainer" %><%@
+page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
+page import="com.liferay.portal.kernel.language.UnicodeLanguageUtil" %><%@
+page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
+page import="com.liferay.portal.kernel.search.BaseModelSearchResult" %><%@
+page import="com.liferay.portal.kernel.search.Sort" %><%@
+page import="com.liferay.portal.kernel.search.SortFactoryUtil" %><%@
+page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
+page import="com.liferay.portal.kernel.util.ParamUtil" %><%@
+page import="com.liferay.portal.kernel.util.StringPool" %><%@
+page import="com.liferay.portal.kernel.util.Validator" %><%@
+page import="com.liferay.portal.kernel.workflow.WorkflowConstants" %><%@
+page import="com.liferay.portal.model.Group" %><%@
+page import="com.liferay.portal.model.LayoutConstants" %><%@
+page import="com.liferay.portal.model.LayoutSet" %><%@
+page import="com.liferay.portal.model.LayoutSetPrototype" %><%@
+page import="com.liferay.portal.model.Organization" %><%@
+page import="com.liferay.portal.model.User" %><%@
+page import="com.liferay.portal.model.UserGroup" %><%@
 page import="com.liferay.portal.NoSuchUserGroupException" %><%@
 page import="com.liferay.portal.RequiredUserGroupException" %><%@
-page import="com.liferay.portal.UserGroupNameException" %><%@
 page import="com.liferay.portal.security.membershippolicy.UserGroupMembershipPolicyUtil" %><%@
+page import="com.liferay.portal.security.permission.ActionKeys" %><%@
+page import="com.liferay.portal.service.LayoutLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetPrototypeLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetPrototypeServiceUtil" %><%@
+page import="com.liferay.portal.service.UserLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.permission.GroupPermissionUtil" %><%@
+page import="com.liferay.portal.service.permission.PortalPermissionUtil" %><%@
 page import="com.liferay.portal.service.permission.UserGroupPermissionUtil" %><%@
+page import="com.liferay.portal.service.UserGroupLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.UserGroupServiceUtil" %><%@
+page import="com.liferay.portal.UserGroupNameException" %><%@
+page import="com.liferay.portal.util.PortalUtil" %><%@
+page import="com.liferay.portal.util.PortletKeys" %><%@
+page import="com.liferay.portal.util.PropsValues" %><%@
+page import="com.liferay.portal.util.WebKeys" %><%@
 page import="com.liferay.portlet.usergroupsadmin.search.UserGroupChecker" %><%@
+page import="com.liferay.portlet.usergroupsadmin.search.UserGroupDisplayTerms" %><%@
+page import="com.liferay.portlet.usergroupsadmin.search.UserGroupSearch" %><%@
+page import="com.liferay.portlet.usersadmin.search.UserSearch" %><%@
+page import="com.liferay.portlet.usersadmin.search.UserSearchTerms" %><%@
+page import="com.liferay.taglib.search.ResultRow" %><%@
+page import="com.liferay.user.groups.admin.web.constants.UserGroupsAdminPortletKeys" %><%@
 page import="com.liferay.portlet.usergroupsadmin.search.UserUserGroupChecker" %>
+
+<%@ page import="java.util.LinkedHashMap" %><%@
+page import="java.util.List" %>
+
+<%@ page import="javax.portlet.PortletURL" %><%@
+page import="javax.portlet.WindowState" %>
+
+<liferay-theme:defineObjects />
+<portlet:defineObjects />
+
+<%
+String currentURL = PortalUtil.getCurrentURL(request);
+WindowState windowState = liferayPortletRequest.getWindowState();
+%>
 
 <%
 boolean filterManageableOrganizations = true;

liferay/liferay-central-test-1

modules/apps/user-groups-admin/user-groups-admin-web/src/META-INF/resources/init.jsp
@@ -14,16 +14,76 @@
  */
 --%>
 
-<%@ include file="/html/portlet/init.jsp" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %>
+<%@ taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %>
+<%@ taglib uri="http://liferay.com/tld/portlet" prefix="liferay-portlet" %>
+<%@ taglib uri="http://liferay.com/tld/security" prefix="liferay-security" %>
+<%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %>
 
 <%@ page import="com.liferay.portal.DuplicateUserGroupException" %><%@
 page import="com.liferay.portal.NoSuchUserGroupException" %><%@
 page import="com.liferay.portal.RequiredUserGroupException" %><%@
 page import="com.liferay.portal.UserGroupNameException" %><%@
+page import="com.liferay.portal.kernel.dao.search.SearchContainer" %><%@
+page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
+page import="com.liferay.portal.kernel.language.UnicodeLanguageUtil" %><%@
+page import="com.liferay.portal.kernel.portlet.LiferayPortletURL" %><%@
+page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
+page import="com.liferay.portal.kernel.portlet.PortletProvider" %><%@
+page import="com.liferay.portal.kernel.portlet.PortletProviderUtil" %><%@
+page import="com.liferay.portal.kernel.util.HtmlUtil" %><%@
+page import="com.liferay.portal.kernel.util.ParamUtil" %><%@
+page import="com.liferay.portal.kernel.util.StringPool" %><%@
+page import="com.liferay.portal.kernel.util.Validator" %><%@
+page import="com.liferay.portal.kernel.workflow.WorkflowConstants" %><%@
+page import="com.liferay.portal.model.Group" %><%@
+page import="com.liferay.portal.model.Layout" %><%@
+page import="com.liferay.portal.model.LayoutConstants" %><%@
+page import="com.liferay.portal.model.LayoutSet" %><%@
+page import="com.liferay.portal.model.LayoutSetPrototype" %><%@
+page import="com.liferay.portal.model.Organization" %><%@
+page import="com.liferay.portal.model.User" %><%@
+page import="com.liferay.portal.model.UserGroup" %><%@
 page import="com.liferay.portal.security.membershippolicy.UserGroupMembershipPolicyUtil" %><%@
+page import="com.liferay.portal.security.permission.ActionKeys" %><%@
+page import="com.liferay.portal.service.LayoutLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetPrototypeLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.LayoutSetPrototypeServiceUtil" %><%@
+page import="com.liferay.portal.service.UserGroupLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.UserGroupServiceUtil" %><%@
+page import="com.liferay.portal.service.UserLocalServiceUtil" %><%@
+page import="com.liferay.portal.service.permission.GroupPermissionUtil" %><%@
+page import="com.liferay.portal.service.permission.PortalPermissionUtil" %><%@
 page import="com.liferay.portal.service.permission.UserGroupPermissionUtil" %><%@
+page import="com.liferay.portal.util.PortalUtil" %><%@
+page import="com.liferay.portal.util.PortletKeys" %><%@
+page import="com.liferay.portal.util.PropsValues" %><%@
+page import="com.liferay.portal.util.WebKeys" %><%@
 page import="com.liferay.portlet.usergroupsadmin.search.UserGroupChecker" %><%@
-page import="com.liferay.portlet.usergroupsadmin.search.UserUserGroupChecker" %>
+page import="com.liferay.portlet.usergroupsadmin.search.UserGroupDisplayTerms" %><%@
+page import="com.liferay.portlet.usergroupsadmin.search.UserGroupSearch" %><%@
+page import="com.liferay.portlet.usergroupsadmin.search.UserUserGroupChecker" %><%@
+page import="com.liferay.portlet.usersadmin.search.UserSearch" %><%@
+page import="com.liferay.portlet.usersadmin.search.UserSearchTerms" %><%@
+page import="com.liferay.taglib.search.ResultRow" %>
+
+<%@ page import="java.util.LinkedHashMap" %><%@
+page import="java.util.List" %>
+
+<%@ page import="javax.portlet.PortletURL" %><%@
+page import="javax.portlet.WindowState" %>
+
+<liferay-theme:defineObjects />
+<portlet:defineObjects />
+
+<%
+String currentURL = PortalUtil.getCurrentURL(request);
+WindowState windowState = liferayPortletRequest.getWindowState();
+%>
 
 <%
 boolean filterManageableOrganizations = true;