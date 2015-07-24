sergiogonzalez/liferay-portal

LPS-56817 auto SF

portal-impl/src/com/liferay/portal/util/FileImpl.java
@@ -87,6 +87,23 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	}
 
 	@Override
+	public String appendSuffix(String fileName, String suffix) {
+		String extension = getExtension(fileName);
+		String fileNameWithoutExtension = stripExtension(fileName);
+
+		StringBundler sb = new StringBundler(6);
+
+		sb.append(fileNameWithoutExtension);
+		sb.append(StringPool.OPEN_PARENTHESIS);
+		sb.append(suffix);
+		sb.append(StringPool.CLOSE_PARENTHESIS);
+		sb.append(StringPool.PERIOD);
+		sb.append(extension);
+
+		return sb.toString();
+	}
+
+	@Override
 	public void copyDirectory(File source, File destination)
 		throws IOException {
 
@@ -909,23 +926,6 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	@Override
 	public void unzip(File source, File destination) {
 		ExpandTask.expand(source, destination);
-	}
-
-	@Override
-	public String appendSuffix(String fileName, String suffix) {
-		String extension = getExtension(fileName);
-		String fileNameWithoutExtension = stripExtension(fileName);
-
-		StringBundler sb = new StringBundler(6);
-
-		sb.append(fileNameWithoutExtension);
-		sb.append(StringPool.OPEN_PARENTHESIS);
-		sb.append(suffix);
-		sb.append(StringPool.CLOSE_PARENTHESIS);
-		sb.append(StringPool.PERIOD);
-		sb.append(extension);
-
-		return sb.toString();
 	}
 
 	@Override

portal-service/src/com/liferay/portal/kernel/util/File.java
@@ -27,6 +27,8 @@ import java.util.Properties;
  */
 public interface File {
 
+	public String appendSuffix(String fileName, String suffix);
+
 	public void copyDirectory(java.io.File source, java.io.File destination)
 		throws IOException;
 
@@ -155,8 +157,6 @@ public interface File {
 	public void touch(String fileName) throws IOException;
 
 	public void unzip(java.io.File source, java.io.File destination);
-
-	public String appendSuffix(String fileName, String suffix);
 
 	public void write(java.io.File file, byte[] bytes) throws IOException;
 

portal-service/test/unit/com/liferay/portal/kernel/settings/MockFile.java
@@ -30,6 +30,11 @@ import java.util.Properties;
 public class MockFile implements com.liferay.portal.kernel.util.File {
 
 	@Override
+	public String appendSuffix(String fileName, String suffix) {
+		return null;
+	}
+
+	@Override
 	public void copyDirectory(File source, File destination) {
 	}
 
@@ -318,11 +323,6 @@ public class MockFile implements com.liferay.portal.kernel.util.File {
 
 	@Override
 	public void unzip(File source, File destination) {
-	}
-
-	@Override
-	public String appendSuffix(String fileName, String suffix) {
-		return null;
 	}
 
 	@Override

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/util/FileImpl.java
@@ -87,6 +87,22 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	}
 
 	@Override
+	public String appendParentheticalSuffix(String fileName, String suffix) {
+		String extension = getExtension(fileName);
+		String fileNameWithoutExtension = stripExtension(fileName);
+
+		StringBundler sb = new StringBundler(3);
+
+		sb.append(
+			StringUtil.appendParentheticalSuffix(
+				fileNameWithoutExtension, suffix, false));
+		sb.append(StringPool.PERIOD);
+		sb.append(extension);
+
+		return sb.toString();
+	}
+
+	@Override
 	public void copyDirectory(File source, File destination)
 		throws IOException {
 
@@ -909,22 +925,6 @@ public class FileImpl implements com.liferay.portal.kernel.util.File {
 	@Override
 	public void unzip(File source, File destination) {
 		ExpandTask.expand(source, destination);
-	}
-
-	@Override
-	public String appendParentheticalSuffix(String fileName, String suffix) {
-		String extension = getExtension(fileName);
-		String fileNameWithoutExtension = stripExtension(fileName);
-
-		StringBundler sb = new StringBundler(3);
-
-		sb.append(
-			StringUtil.appendParentheticalSuffix(
-				fileNameWithoutExtension, suffix, false));
-		sb.append(StringPool.PERIOD);
-		sb.append(extension);
-
-		return sb.toString();
 	}
 
 	@Override

portal-service/src/com/liferay/portal/kernel/util/File.java
@@ -27,6 +27,8 @@ import java.util.Properties;
  */
 public interface File {
 
+	public String appendParentheticalSuffix(String fileName, String suffix);
+
 	public void copyDirectory(java.io.File source, java.io.File destination)
 		throws IOException;
 
@@ -155,8 +157,6 @@ public interface File {
 	public void touch(String fileName) throws IOException;
 
 	public void unzip(java.io.File source, java.io.File destination);
-
-	public String appendParentheticalSuffix(String fileName, String suffix);
 
 	public void write(java.io.File file, byte[] bytes) throws IOException;
 

portal-service/test/unit/com/liferay/portal/kernel/settings/MockFile.java
@@ -30,6 +30,11 @@ import java.util.Properties;
 public class MockFile implements com.liferay.portal.kernel.util.File {
 
 	@Override
+	public String appendParentheticalSuffix(String fileName, String suffix) {
+		return null;
+	}
+
+	@Override
 	public void copyDirectory(File source, File destination) {
 	}
 
@@ -318,11 +323,6 @@ public class MockFile implements com.liferay.portal.kernel.util.File {
 
 	@Override
 	public void unzip(File source, File destination) {
-	}
-
-	@Override
-	public String appendParentheticalSuffix(String fileName, String suffix) {
-		return null;
 	}
 
 	@Override