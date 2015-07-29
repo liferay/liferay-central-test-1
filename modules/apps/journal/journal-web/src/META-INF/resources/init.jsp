juliocamarero/liferay-portal

LPS-56702 Use new settings framework instead of the preferences in the configuration view

modules/apps/journal/journal-web/src/META-INF/resources/init.jsp
@@ -30,8 +30,8 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 page import="com.liferay.item.selector.ItemSelectorReturnType" %><%@
 page import="com.liferay.item.selector.criteria.UUIDItemSelectorReturnType" %><%@
 page import="com.liferay.item.selector.criteria.layout.criterion.LayoutItemSelectorCriterion" %><%@
-page import="com.liferay.journal.configuration.JournalServiceConfigurationUtil" %><%@
 page import="com.liferay.journal.configuration.JournalServiceConfigurationValues" %><%@
+page import="com.liferay.journal.constants.JournalConstants" %><%@
 page import="com.liferay.journal.exception.ArticleContentException" %><%@
 page import="com.liferay.journal.exception.ArticleContentSizeException" %><%@
 page import="com.liferay.journal.exception.ArticleDisplayDateException" %><%@
@@ -71,6 +71,7 @@ page import="com.liferay.journal.service.permission.JournalArticlePermission" %>
 page import="com.liferay.journal.service.permission.JournalFeedPermission" %><%@
 page import="com.liferay.journal.service.permission.JournalFolderPermission" %><%@
 page import="com.liferay.journal.service.permission.JournalPermission" %><%@
+page import="com.liferay.journal.settings.JournalGroupServiceSettings" %><%@
 page import="com.liferay.journal.util.JournalContentUtil" %><%@
 page import="com.liferay.journal.util.comparator.ArticleVersionComparator" %><%@
 page import="com.liferay.journal.util.impl.JournalUtil" %><%@
@@ -79,6 +80,7 @@ page import="com.liferay.journal.web.configuration.JournalWebConfigurationValues
 page import="com.liferay.journal.web.constants.JournalPortletKeys" %><%@
 page import="com.liferay.journal.web.constants.JournalWebKeys" %><%@
 page import="com.liferay.journal.web.context.JournalDisplayContext" %><%@
+page import="com.liferay.journal.web.context.util.JournalWebRequestHelper" %><%@
 page import="com.liferay.journal.web.portlet.JournalPortlet" %><%@
 page import="com.liferay.journal.web.portlet.action.ActionUtil" %><%@
 page import="com.liferay.journal.web.search.ArticleDisplayTerms" %><%@

liferay/liferay-central-test-1

modules/apps/journal/journal-web/src/META-INF/resources/init.jsp
@@ -30,8 +30,9 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 page import="com.liferay.item.selector.ItemSelectorReturnType" %><%@
 page import="com.liferay.item.selector.criteria.UUIDItemSelectorReturnType" %><%@
 page import="com.liferay.item.selector.criteria.layout.criterion.LayoutItemSelectorCriterion" %><%@
-page import="com.liferay.journal.configuration.JournalServiceConfigurationUtil" %><%@
+page import="com.liferay.journal.configuration.JournalGroupServiceConfiguration" %><%@
 page import="com.liferay.journal.configuration.JournalServiceConfigurationValues" %><%@
+page import="com.liferay.journal.constants.JournalConstants" %><%@
 page import="com.liferay.journal.constants.JournalWebKeys" %><%@
 page import="com.liferay.journal.exception.ArticleContentException" %><%@
 page import="com.liferay.journal.exception.ArticleContentSizeException" %><%@
@@ -79,6 +80,7 @@ page import="com.liferay.journal.web.asset.JournalArticleAssetRenderer" %><%@
 page import="com.liferay.journal.web.configuration.JournalWebConfigurationValues" %><%@
 page import="com.liferay.journal.web.constants.JournalPortletKeys" %><%@
 page import="com.liferay.journal.web.context.JournalDisplayContext" %><%@
+page import="com.liferay.journal.web.context.util.JournalWebRequestHelper" %><%@
 page import="com.liferay.journal.web.portlet.JournalPortlet" %><%@
 page import="com.liferay.journal.web.portlet.action.ActionUtil" %><%@
 page import="com.liferay.journal.web.search.ArticleDisplayTerms" %><%@