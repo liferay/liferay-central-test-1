sergiogonzalez/liferay-portal

LPS-57357 Move properties to portletClass

modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/portlet/SocialActivityPortlet.java
@@ -15,6 +15,7 @@
 package com.liferay.social.activity.web.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.social.activity.web.constants.SocialActivityPortletKeys;
 import com.liferay.social.activity.web.upgrade.SocialActivityWebUpgrade;
 
 import javax.portlet.Portlet;
@@ -25,7 +26,32 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Roberto Díaz
  */
-@Component(immediate = true, service = Portlet.class)
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.control-panel-entry-category=site_administration.configuration",
+		"com.liferay.portlet.control-panel-entry-class=com.liferay.portlet.socialactivity.SocialActivityControlPanelEntry",
+		"com.liferay.portlet.control-panel-entry-weight=3.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-social-activity",
+		"com.liferay.portlet.display-category=category.social",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.icon=/icons/social_activity.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Social Activity",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SocialActivityPortletKeys.SOCIAL_ACTIVITY,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SocialActivityPortlet extends MVCPortlet {
 
 	@Reference(unbind = "-")

liferay/liferay-central-test-1

modules/apps/social/social-activity-web/src/com/liferay/social/activity/web/portlet/SocialActivityPortlet.java
@@ -15,6 +15,7 @@
 package com.liferay.social.activity.web.portlet;
 
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.social.activity.web.constants.SocialActivityPortletKeys;
 import com.liferay.social.activity.web.upgrade.SocialActivityWebUpgrade;
 
 import javax.portlet.Portlet;
@@ -25,7 +26,32 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Roberto Díaz
  */
-@Component(immediate = true, service = Portlet.class)
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.control-panel-entry-category=site_administration.configuration",
+		"com.liferay.portlet.control-panel-entry-class=com.liferay.portlet.socialactivity.SocialActivityControlPanelEntry",
+		"com.liferay.portlet.control-panel-entry-weight=3.0",
+		"com.liferay.portlet.css-class-wrapper=portlet-social-activity",
+		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.header-portlet-javascript=/js/main.js",
+		"com.liferay.portlet.icon=/icons/social_activity.png",
+		"com.liferay.portlet.preferences-owned-by-group=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Social Activity",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + SocialActivityPortletKeys.SOCIAL_ACTIVITY,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SocialActivityPortlet extends MVCPortlet {
 
 	@Reference(unbind = "-")