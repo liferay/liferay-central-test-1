sergiogonzalez/liferay-portal

LPS-56703 Move ConfigurationAction and create a ValidateRootFolderConfigurationAction so they can reuse common logic regarding validation

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLAdminConfigurationAction.java
@@ -12,9 +12,11 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibraryadmin.action;
+package com.liferay.document.library.web.portlet.action;
 
+import com.liferay.document.library.web.constants.DLPortletKeys;
 import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -23,12 +25,27 @@ import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
- * @author Jorge Ferrer
  * @author Sergio González
  */
-public class ConfigurationActionImpl
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_ADMIN},
+	service = ConfigurationAction.class
+)
+public class DLAdminConfigurationAction
 	extends BaseJSPSettingsConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library_admin/configuration.jsp";
+	}
 
 	@Override
 	public void processAction(
@@ -43,6 +60,15 @@ public class ConfigurationActionImpl
 		}
 
 		super.processAction(portletConfig, actionRequest, actionResponse);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
 	}
 
 	protected void validate(ActionRequest actionRequest) throws Exception {

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLConfigurationAction.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.ActionRequest;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY},
+	service = ConfigurationAction.class
+)
+public class DLConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	protected void validate(ActionRequest actionRequest) throws Exception {
+		validateDisplayStyleViews(actionRequest);
+
+		super.validate(actionRequest);
+	}
+
+	protected void validateDisplayStyleViews(ActionRequest actionRequest) {
+		String displayViews = GetterUtil.getString(
+			getParameter(actionRequest, "displayViews"));
+
+		if (Validator.isNull(displayViews)) {
+			SessionErrors.add(actionRequest, "displayViewsInvalid");
+		}
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLDisplayConfigurationAction.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.DOCUMENT_LIBRARY_DISPLAY},
+	service = ConfigurationAction.class
+)
+public class DLDisplayConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library_display/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/IGDisplayConfigurationAction.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.document.library.web.constants.DLPortletKeys;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + DLPortletKeys.MEDIA_GALLERY_DISPLAY},
+	service = ConfigurationAction.class
+)
+public class IGDisplayConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/image_gallery_display/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}

liferay/liferay-central-test-1

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLAdminConfigurationAction.java
@@ -12,23 +12,41 @@
  * details.
  */
 
-package com.liferay.portlet.documentlibraryadmin.action;
+package com.liferay.document.library.web.portlet.action;
 
 import com.liferay.portal.kernel.portlet.BaseJSPSettingsConfigurationAction;
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortletKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletConfig;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Jorge Ferrer
  * @author Sergio González
  */
-public class ConfigurationActionImpl
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_ADMIN},
+	service = ConfigurationAction.class
+)
+public class DLAdminConfigurationAction
 	extends BaseJSPSettingsConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library_admin/configuration.jsp";
+	}
 
 	@Override
 	public void processAction(
@@ -43,6 +61,15 @@ public class ConfigurationActionImpl
 		}
 
 		super.processAction(portletConfig, actionRequest, actionResponse);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
 	}
 
 	protected void validate(ActionRequest actionRequest) throws Exception {

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLConfigurationAction.java
@@ -1,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.portlet.ActionRequest;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY},
+	service = ConfigurationAction.class
+)
+public class DLConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+	protected void validate(ActionRequest actionRequest) throws Exception {
+		validateDisplayStyleViews(actionRequest);
+
+		super.validate(actionRequest);
+	}
+
+	protected void validateDisplayStyleViews(ActionRequest actionRequest) {
+		String displayViews = GetterUtil.getString(
+			getParameter(actionRequest, "displayViews"));
+
+		if (Validator.isNull(displayViews)) {
+			SessionErrors.add(actionRequest, "displayViewsInvalid");
+		}
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/DLDisplayConfigurationAction.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.DOCUMENT_LIBRARY_DISPLAY},
+	service = ConfigurationAction.class
+)
+public class DLDisplayConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/document_library_display/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}

modules/apps/document-library/document-library-web/src/com/liferay/document/library/web/portlet/action/IGDisplayConfigurationAction.java
@@ -1,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.web.portlet.action;
+
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.util.PortletKeys;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Sergio González
+ */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PortletKeys.MEDIA_GALLERY_DISPLAY},
+	service = ConfigurationAction.class
+)
+public class IGDisplayConfigurationAction
+	extends ValidateRootFolderConfigurationAction {
+
+	@Override
+	public String getJspPath(HttpServletRequest httpServletRequest) {
+		return "/image_gallery_display/configuration.jsp";
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.document.library.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}