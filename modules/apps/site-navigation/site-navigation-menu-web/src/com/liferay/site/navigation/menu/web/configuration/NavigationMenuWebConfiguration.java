brianchandotcom/liferay-portal

LPS-27113 Add default ddmTemplateKey to the modules configuration

modules/apps/site-navigation/site-navigation-menu-web/src/com/liferay/site/navigation/menu/web/configuration/NavigationMenuWebConfiguration.java
@@ -24,6 +24,9 @@ import aQute.bnd.annotation.metatype.Meta;
 )
 public interface NavigationMenuWebConfiguration {
 
+	@Meta.AD(deflt = "default-theme-navigation-ftl", required = false)
+	public String ddmTemplateKeyDefault();
+
 	@Meta.AD(deflt = "dots", required = false)
 	public String defaultBulletStyle();
 

liferay/liferay-central-test-1

modules/apps/site-navigation/site-navigation-menu-web/src/com/liferay/site/navigation/menu/web/configuration/NavigationMenuWebConfiguration.java
@@ -24,6 +24,9 @@ import aQute.bnd.annotation.metatype.Meta;
 )
 public interface NavigationMenuWebConfiguration {
 
+	@Meta.AD(deflt = "relative-with-breadcrumb-ftl", required = false)
+	public String ddmTemplateKeyDefault();
+
 	@Meta.AD(deflt = "dots", required = false)
 	public String defaultBulletStyle();
 