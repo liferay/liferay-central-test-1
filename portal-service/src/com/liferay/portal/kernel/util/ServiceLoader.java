liferay/liferay-central-test-1

commit 5c909aeda871005bed01d69db91e000afd75be88

LPS-56072 Removed JBoss version specifics from the source

portal-service/src/com/liferay/portal/kernel/util/ServiceLoader.java
@@ -133,10 +133,6 @@ public class ServiceLoader {
 			List<S> services, ClassLoader classLoader, Class<S> clazz, URL url)
 		throws Exception {
 
-		if (ServerDetector.isJBoss5()) {
-			url = normalizeURL(url);
-		}
-
 		try (InputStream inputStream = url.openStream()) {
 			BufferedReader bufferedReader = new BufferedReader(
 				new InputStreamReader(inputStream, StringPool.UTF8));

modules/core/portal-security-pacl/src/com/liferay/portal/security/pacl/checker/FileChecker.java
@@ -417,15 +417,6 @@ public class FileChecker extends BaseChecker {
 						fileName = fileName.substring(0, pos + 1);
 					}
 
-					if (ServerDetector.isJBoss7()) {
-						String jBossHomeDir = System.getProperty(
-							"jboss.home.dir");
-
-						if (fileName.startsWith(jBossHomeDir)) {
-							continue;
-						}
-					}
-
 					if (ServerDetector.isJetty()) {
 						String jettyHome = System.getProperty("jetty.home");
 

portal-impl/src/com/liferay/portal/deploy/DeployUtil.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.deploy;
 
-import com.liferay.portal.kernel.configuration.Filter;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.CharPool;
@@ -30,7 +29,6 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.spring.context.PortalContextLoaderListener;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.PrefsPropsUtil;
-import com.liferay.portal.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 import com.liferay.util.ant.CopyTask;
 import com.liferay.util.ant.DeleteTask;
@@ -42,8 +40,6 @@ import java.io.InputStream;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.portlet.PortletPreferences;
 
 import org.apache.commons.io.FileUtils;
 
@@ -85,19 +81,7 @@ public class DeployUtil {
 
 		String serverId = GetterUtil.getString(ServerDetector.getServerId());
 
-		if (serverId.equals(ServerDetector.JBOSS_ID) &&
-			ServerDetector.isJBoss5()) {
-
-			String name = "auto.deploy." + serverId + ".dest.dir";
-
-			PortletPreferences portletPreferences =
-				PrefsPropsUtil.getPreferences(true);
-
-			String value = PropsUtil.get(name, new Filter("5"));
-
-			destDir = portletPreferences.getValue(name, value);
-		}
-		else if (serverId.equals(ServerDetector.TOMCAT_ID)) {
+		if (serverId.equals(ServerDetector.TOMCAT_ID)) {
 			destDir = PrefsPropsUtil.getString(
 				PropsKeys.AUTO_DEPLOY_TOMCAT_DEST_DIR,
 				PropsValues.AUTO_DEPLOY_TOMCAT_DEST_DIR);

portal-impl/src/com/liferay/portal/server/capabilities/JBossServerCapabilities.java
@@ -14,7 +14,6 @@
 
 package com.liferay.portal.server.capabilities;
 
-import com.liferay.portal.kernel.util.ServerDetector;
 import com.liferay.portal.server.DeepNamedValueScanner;
 
 import javax.servlet.ServletContext;
@@ -37,33 +36,28 @@ public class JBossServerCapabilities implements ServerCapabilities {
 	protected void determineSupportsHotDeploy(ServletContext servletContext)
 		throws Exception {
 
-		if (ServerDetector.isJBoss5()) {
-			_supportsHotDeploy = true;
+		DeepNamedValueScanner deepNamedValueScanner =
+			new DeepNamedValueScanner("scanEnabled", true);
+
+		deepNamedValueScanner.setExcludedClassNames(
+			"ChainedInterceptorFactory", "TagAttributeInfo", ".jandex.",
+			".vfs.");
+		deepNamedValueScanner.setExcludedNames("serialversion");
+		deepNamedValueScanner.setIncludedClassNames(
+			"org.apache.", "org.jboss.");
+		deepNamedValueScanner.setVisitArrays(true);
+		deepNamedValueScanner.setVisitMaps(true);
+
+		deepNamedValueScanner.scan(servletContext);
+
+		Boolean scanEnabledValue =
+			(Boolean)deepNamedValueScanner.getMatchedValue();
+
+		if (scanEnabledValue == null) {
+			_supportsHotDeploy = false;
 		}
 		else {
-			DeepNamedValueScanner deepNamedValueScanner =
-				new DeepNamedValueScanner("scanEnabled", true);
-
-			deepNamedValueScanner.setExcludedClassNames(
-				"ChainedInterceptorFactory", "TagAttributeInfo", ".jandex.",
-				".vfs.");
-			deepNamedValueScanner.setExcludedNames("serialversion");
-			deepNamedValueScanner.setIncludedClassNames(
-				"org.apache.", "org.jboss.");
-			deepNamedValueScanner.setVisitArrays(true);
-			deepNamedValueScanner.setVisitMaps(true);
-
-			deepNamedValueScanner.scan(servletContext);
-
-			Boolean scanEnabledValue =
-				(Boolean)deepNamedValueScanner.getMatchedValue();
-
-			if (scanEnabledValue == null) {
-				_supportsHotDeploy = false;
-			}
-			else {
-				_supportsHotDeploy = scanEnabledValue.booleanValue();
-			}
+			_supportsHotDeploy = scanEnabledValue.booleanValue();
 		}
 	}
 

portal-impl/src/com/liferay/portal/tools/deploy/BaseDeployer.java
@@ -578,13 +578,8 @@ public class BaseDeployer implements AutoDeployer, Deployer {
 		throws Exception {
 
 		if (appServerType.equals(ServerDetector.JBOSS_ID)) {
-			if (ServerDetector.isJBoss5()) {
-				copyDependencyXml("jboss-web.xml", srcFile + "/WEB-INF");
-			}
-			else {
-				copyDependencyXml(
-					"jboss-deployment-structure.xml", srcFile + "/WEB-INF");
-			}
+			copyDependencyXml(
+				"jboss-deployment-structure.xml", srcFile + "/WEB-INF");
 		}
 		else if (appServerType.equals(ServerDetector.WEBLOGIC_ID)) {
 			copyDependencyXml("weblogic.xml", srcFile + "/WEB-INF");

portal-service/src/com/liferay/portal/kernel/util/ServerDetector.java
@@ -105,14 +105,6 @@ public class ServerDetector {
 		return getInstance()._jBoss;
 	}
 
-	public static boolean isJBoss5() {
-		return getInstance()._jBoss5;
-	}
-
-	public static boolean isJBoss7() {
-		return getInstance()._jBoss7;
-	}
-
 	public static boolean isJetty() {
 		return getInstance()._jetty;
 	}
@@ -201,13 +193,6 @@ public class ServerDetector {
 		else if (_isJBoss()) {
 			_serverId = JBOSS_ID;
 			_jBoss = true;
-
-			if (_isJBoss5()) {
-				_jBoss5 = true;
-			}
-			else {
-				_jBoss7 = true;
-			}
 		}
 		else if (_isJOnAS()) {
 			_serverId = JONAS_ID;
@@ -265,36 +250,6 @@ public class ServerDetector {
 		return _hasSystemProperty("jboss.home.dir");
 	}
 
-	private boolean _isJBoss5() {
-		try {
-			for (MBeanServer mBeanServer :
-					MBeanServerFactory.findMBeanServer(null)) {
-
-				String defaultDomain = GetterUtil.getString(
-					mBeanServer.getDefaultDomain(), "jboss");
-
-				if (defaultDomain.equals("jboss")) {
-					ObjectName objectName = new ObjectName(
-						"jboss.system:type=Server");
-
-					String version = (String)mBeanServer.getAttribute(
-						objectName, "VersionNumber");
-
-					if (version.startsWith("5")) {
-						return true;
-					}
-
-					return false;
-				}
-			}
-		}
-		catch (Exception e) {
-			_log.error(e, e);
-		}
-
-		return false;
-	}
-
 	private boolean _isJetty() {
 		return _hasSystemProperty("jetty.home");
 	}
@@ -331,8 +286,6 @@ public class ServerDetector {
 
 	private boolean _glassfish;
 	private boolean _jBoss;
-	private boolean _jBoss5;
-	private boolean _jBoss7;
 	private boolean _jetty;
 	private boolean _jonas;
 	private boolean _oc4j;

util-java/src/com/liferay/util/log4j/Log4JUtil.java
@@ -72,22 +72,6 @@ public class Log4JUtil {
 				java.util.logging.Level.WARNING,
 				"Unable to load portal-log4j-ext.xml", ioe);
 		}
-
-		if (ServerDetector.isJBoss5()) {
-			Logger rootLogger = LogManager.getRootLogger();
-
-			Enumeration<Appender> enu = rootLogger.getAllAppenders();
-
-			while (enu.hasMoreElements()) {
-				Appender appender = enu.nextElement();
-
-				if (appender instanceof WriterAppender) {
-					WriterAppender writerAppender = (WriterAppender)appender;
-
-					writerAppender.activateOptions();
-				}
-			}
-		}
 	}
 
 	public static void configureLog4J(URL url) {

commit 63be1717fb3bc2850f49a366c3e575d5fd3afb19

LPS-56072 Remove leftover unused code. Caused by 5c909aeda871005bed01d69db91e000afd75be88

portal-service/src/com/liferay/portal/kernel/util/ServiceLoader.java
@@ -23,7 +23,6 @@ import java.io.InputStreamReader;
 
 import java.lang.reflect.Constructor;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.util.ArrayList;
@@ -85,48 +84,6 @@ public class ServiceLoader {
 		}
 
 		return services;
-	}
-
-	/**
-	 * @see com.liferay.portal.kernel.process.ClassPathUtil#_buildClassPath(
-	 *      ClassLoader, String)
-	 */
-	public static URL normalizeURL(URL url) throws MalformedURLException {
-		String urlString = url.toString();
-
-		if (urlString.startsWith("vfsfile:")) {
-			urlString = StringUtil.replaceFirst(urlString, "vfsfile:", "file:");
-		}
-		else if (urlString.startsWith("vfsjar:")) {
-			urlString = StringUtil.replaceFirst(urlString, "vfsjar:", "file:");
-		}
-		else if (urlString.startsWith("vfszip:")) {
-			urlString = StringUtil.replaceFirst(urlString, "vfszip:", "file:");
-		}
-
-		if (urlString.contains(".jar/")) {
-			urlString = StringUtil.replaceFirst(urlString, ".jar/", ".jar!/");
-
-			if (urlString.startsWith("file:")) {
-				urlString = "jar:" + urlString;
-			}
-		}
-
-		urlString = urlString.replace('\\', '/');
-
-		int index = urlString.indexOf("file:");
-
-		if (index != -1) {
-			index += 5;
-
-			if (urlString.charAt(index) != '/') {
-				urlString =
-					urlString.substring(0, index) + '/' +
-						urlString.substring(index);
-			}
-		}
-
-		return new URL(urlString);
 	}
 
 	private static <S> void _load(