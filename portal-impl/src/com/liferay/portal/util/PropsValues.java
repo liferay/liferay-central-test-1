wesleygong/liferay-portal

LPS-56801 Add option to sync LDAP users through uuid

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -888,6 +888,8 @@ public class PropsValues {
 
 	public static final boolean LDAP_IMPORT_USER_PASSWORD_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_PASSWORD_ENABLED));
 
+	public static final boolean LDAP_IMPORT_USER_SYNC_UUID = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_SYNC_UUID));
+
 	public static final int LDAP_PAGE_SIZE = GetterUtil.getInteger(PropsUtil.get(PropsKeys.LDAP_PAGE_SIZE));
 
 	public static final boolean LDAP_PASSWORD_POLICY_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_PASSWORD_POLICY_ENABLED));

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1293,6 +1293,8 @@ public interface PropsKeys {
 
 	public static final String LDAP_IMPORT_USER_SEARCH_FILTER = "ldap.import.user.search.filter";
 
+	public static final String LDAP_IMPORT_USER_SYNC_UUID = "ldap.import.user.sync.uuid";
+
 	public static final String LDAP_PAGE_SIZE = "ldap.page.size";
 
 	public static final String LDAP_PASSWORD_POLICY_ENABLED = "ldap.password.policy.enabled";

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -842,6 +842,8 @@ public class PropsValues {
 
 	public static final boolean LDAP_IMPORT_USER_PASSWORD_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_PASSWORD_ENABLED));
 
+	public static final String LDAP_IMPORT_USER_SYNC_STRATEGY = GetterUtil.getString(PropsUtil.get(PropsKeys.LDAP_IMPORT_USER_SYNC_STRATEGY));
+
 	public static final int LDAP_PAGE_SIZE = GetterUtil.getInteger(PropsUtil.get(PropsKeys.LDAP_PAGE_SIZE));
 
 	public static final boolean LDAP_PASSWORD_POLICY_ENABLED = GetterUtil.getBoolean(PropsUtil.get(PropsKeys.LDAP_PASSWORD_POLICY_ENABLED));

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1211,6 +1211,8 @@ public interface PropsKeys {
 
 	public static final String LDAP_IMPORT_USER_SEARCH_FILTER = "ldap.import.user.search.filter";
 
+	public static final String LDAP_IMPORT_USER_SYNC_STRATEGY = "ldap.import.user.sync.strategy";
+
 	public static final String LDAP_PAGE_SIZE = "ldap.page.size";
 
 	public static final String LDAP_PASSWORD_POLICY_ENABLED = "ldap.password.policy.enabled";