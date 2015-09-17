mikakoivisto/liferay-portal

LPS-54590 regen

sql/indexes.sql
@@ -450,6 +450,7 @@ create unique index IX_D63D20BB on ResourceBlockPermission (resourceBlockId, rol
 create index IX_20A2E3D9 on ResourceBlockPermission (roleId);
 
 create unique index IX_8D83D0CE on ResourcePermission (companyId, name, scope, primKey, roleId);
+create index IX_80147751 on ResourcePermission (companyId, name, scope, primKeyId, roleId, viewPermission);
 create index IX_26284944 on ResourcePermission (companyId, primKey);
 create index IX_A37A0588 on ResourcePermission (roleId);
 create index IX_F4555981 on ResourcePermission (scope);

liferay/liferay-central-test-1

sql/indexes.sql
@@ -377,6 +377,7 @@ create unique index IX_D63D20BB on ResourceBlockPermission (resourceBlockId, rol
 create index IX_20A2E3D9 on ResourceBlockPermission (roleId);
 
 create unique index IX_8D83D0CE on ResourcePermission (companyId, name, scope, primKey, roleId);
+create index IX_49AEC6F3 on ResourcePermission (companyId, name, scope, primKeyId, roleId, viewActionId);
 create index IX_26284944 on ResourcePermission (companyId, primKey);
 create index IX_A37A0588 on ResourcePermission (roleId);
 create index IX_F4555981 on ResourcePermission (scope);