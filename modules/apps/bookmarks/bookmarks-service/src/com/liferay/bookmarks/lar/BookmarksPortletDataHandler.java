brianchandotcom/liferay-portal

LPS-53475 Convert PDH's from bundles to OSGi components

modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/lar/BookmarksPortletDataHandler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.bookmarks.lar;
 
+import com.liferay.bookmarks.constants.BookmarksPortletKeys;
 import com.liferay.bookmarks.model.BookmarksEntry;
 import com.liferay.bookmarks.model.BookmarksFolder;
 import com.liferay.bookmarks.model.BookmarksFolderConstants;
@@ -26,6 +27,7 @@ import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.ExportImportPathUtil;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
 import com.liferay.portal.kernel.lar.StagedModelType;
@@ -41,6 +43,12 @@ import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Jorge Ferrer
  * @author Bruno Farache
@@ -49,11 +57,20 @@ import javax.portlet.PortletPreferences;
  * @author Mate Thurzo
  * @author Daniel Kocsis
  */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + BookmarksPortletKeys.BOOKMARKS,
+		"javax.portlet.name=" + BookmarksPortletKeys.BOOKMARKS_ADMIN
+	},
+	service = PortletDataHandler.class
+)
 public class BookmarksPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "bookmarks";
 
-	public BookmarksPortletDataHandler() {
+	@Activate
+	protected void activate() {
 		setDataPortletPreferences("rootFolderId");
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(BookmarksEntry.class),
@@ -238,6 +255,10 @@ public class BookmarksPortletDataHandler extends BasePortletDataHandler {
 		}
 
 		return portletPreferences;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }

modules/apps/polls/polls-service/src/com/liferay/polls/lar/PollsPortletDataHandler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.polls.lar;
 
+import com.liferay.polls.constants.PollsPortletKeys;
 import com.liferay.polls.model.PollsChoice;
 import com.liferay.polls.model.PollsQuestion;
 import com.liferay.polls.model.PollsVote;
@@ -27,6 +28,7 @@ import com.liferay.polls.service.permission.PollsPermission;
 import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.lar.PortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
@@ -38,16 +40,28 @@ import java.util.List;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Bruno Farache
  * @author Marcellus Tavares
  * @author Mate Thurzo
  */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PollsPortletKeys.POLLS},
+	service = PortletDataHandler.class
+)
 public class PollsPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "polls";
 
-	public PollsPortletDataHandler() {
+	@Activate
+	protected void activate() {
 		setDataLocalized(true);
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(PollsQuestion.class));
@@ -199,6 +213,10 @@ public class PollsPortletDataHandler extends BasePortletDataHandler {
 				portletDataContext);
 
 		voteActionableDynamicQuery.performCount();
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }

modules/apps/wiki/wiki-service/src/com/liferay/wiki/lar/WikiPortletDataHandler.java
@@ -18,6 +18,7 @@ import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.lar.BasePortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataContext;
 import com.liferay.portal.kernel.lar.PortletDataException;
+import com.liferay.portal.kernel.lar.PortletDataHandler;
 import com.liferay.portal.kernel.lar.PortletDataHandlerBoolean;
 import com.liferay.portal.kernel.lar.PortletDataHandlerControl;
 import com.liferay.portal.kernel.lar.StagedModelDataHandlerUtil;
@@ -26,6 +27,7 @@ import com.liferay.portal.kernel.lar.xstream.XStreamAliasRegistryUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.wiki.constants.WikiConstants;
+import com.liferay.wiki.constants.WikiPortletKeys;
 import com.liferay.wiki.model.WikiNode;
 import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.model.impl.WikiNodeImpl;
@@ -41,6 +43,12 @@ import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Bruno Farache
  * @author Jorge Ferrer
@@ -49,28 +57,17 @@ import javax.portlet.PortletPreferences;
  * @author Zsolt Berentey
  * @author Mate Thurzo
  */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + WikiPortletKeys.WIKI,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN
+	},
+	service = PortletDataHandler.class
+)
 public class WikiPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "wiki";
-
-	public WikiPortletDataHandler() {
-		setDataPortletPreferences("hiddenNodes, visibleNodes");
-		setDeletionSystemEventStagedModelTypes(
-			new StagedModelType(WikiNode.class),
-			new StagedModelType(WikiPage.class));
-		setExportControls(
-			new PortletDataHandlerBoolean(
-				NAMESPACE, "wiki-pages", true, false,
-				new PortletDataHandlerControl[] {
-					new PortletDataHandlerBoolean(
-						NAMESPACE, "referenced-content")
-				},
-				WikiPage.class.getName()));
-		setImportControls(getExportControls());
-
-		XStreamAliasRegistryUtil.register(WikiNodeImpl.class, "WikiNode");
-		XStreamAliasRegistryUtil.register(WikiPageImpl.class, "WikiPage");
-	}
 
 	@Override
 	public String getServiceName() {
@@ -92,6 +89,26 @@ public class WikiPortletDataHandler extends BasePortletDataHandler {
 		finally {
 			WikiCacheThreadLocal.setClearCache(true);
 		}
+	}
+
+	@Activate
+	protected void activate() {
+		setDataPortletPreferences("hiddenNodes, visibleNodes");
+		setDeletionSystemEventStagedModelTypes(
+			new StagedModelType(WikiNode.class),
+			new StagedModelType(WikiPage.class));
+		setExportControls(
+			new PortletDataHandlerBoolean(
+				NAMESPACE, "wiki-pages", true, false,
+				new PortletDataHandlerControl[] {
+					new PortletDataHandlerBoolean(
+						NAMESPACE, "referenced-content")
+				},
+				WikiPage.class.getName()));
+		setImportControls(getExportControls());
+
+		XStreamAliasRegistryUtil.register(WikiNodeImpl.class, "WikiNode");
+		XStreamAliasRegistryUtil.register(WikiPageImpl.class, "WikiPage");
 	}
 
 	@Override
@@ -248,6 +265,10 @@ public class WikiPortletDataHandler extends BasePortletDataHandler {
 			portletDataContext, WikiNode.class);
 
 		return portletPreferences;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }

liferay/liferay-central-test-1

modules/apps/bookmarks/bookmarks-service/src/com/liferay/bookmarks/lar/BookmarksPortletDataHandler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.bookmarks.lar;
 
+import com.liferay.bookmarks.constants.BookmarksPortletKeys;
 import com.liferay.bookmarks.model.BookmarksEntry;
 import com.liferay.bookmarks.model.BookmarksFolder;
 import com.liferay.bookmarks.model.BookmarksFolderConstants;
@@ -31,6 +32,7 @@ import com.liferay.portal.service.PortletLocalServiceUtil;
 import com.liferay.portlet.exportimport.lar.BasePortletDataHandler;
 import com.liferay.portlet.exportimport.lar.ExportImportPathUtil;
 import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataHandler;
 import com.liferay.portlet.exportimport.lar.PortletDataHandlerBoolean;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
 import com.liferay.portlet.exportimport.lar.StagedModelType;
@@ -41,6 +43,12 @@ import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Jorge Ferrer
  * @author Bruno Farache
@@ -49,11 +57,20 @@ import javax.portlet.PortletPreferences;
  * @author Mate Thurzo
  * @author Daniel Kocsis
  */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + BookmarksPortletKeys.BOOKMARKS,
+		"javax.portlet.name=" + BookmarksPortletKeys.BOOKMARKS_ADMIN
+	},
+	service = PortletDataHandler.class
+)
 public class BookmarksPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "bookmarks";
 
-	public BookmarksPortletDataHandler() {
+	@Activate
+	protected void activate() {
 		setDataPortletPreferences("rootFolderId");
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(BookmarksEntry.class),
@@ -238,6 +255,10 @@ public class BookmarksPortletDataHandler extends BasePortletDataHandler {
 		}
 
 		return portletPreferences;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }

modules/apps/polls/polls-service/src/com/liferay/polls/lar/PollsPortletDataHandler.java
@@ -14,6 +14,7 @@
 
 package com.liferay.polls.lar;
 
+import com.liferay.polls.constants.PollsPortletKeys;
 import com.liferay.polls.model.PollsChoice;
 import com.liferay.polls.model.PollsQuestion;
 import com.liferay.polls.model.PollsVote;
@@ -28,6 +29,7 @@ import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
 import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portlet.exportimport.lar.BasePortletDataHandler;
 import com.liferay.portlet.exportimport.lar.PortletDataContext;
+import com.liferay.portlet.exportimport.lar.PortletDataHandler;
 import com.liferay.portlet.exportimport.lar.PortletDataHandlerBoolean;
 import com.liferay.portlet.exportimport.lar.PortletDataHandlerControl;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
@@ -38,16 +40,28 @@ import java.util.List;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Bruno Farache
  * @author Marcellus Tavares
  * @author Mate Thurzo
  */
+@Component(
+	immediate = true,
+	property = {"javax.portlet.name=" + PollsPortletKeys.POLLS},
+	service = PortletDataHandler.class
+)
 public class PollsPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "polls";
 
-	public PollsPortletDataHandler() {
+	@Activate
+	protected void activate() {
 		setDataLocalized(true);
 		setDeletionSystemEventStagedModelTypes(
 			new StagedModelType(PollsQuestion.class));
@@ -200,6 +214,10 @@ public class PollsPortletDataHandler extends BasePortletDataHandler {
 				portletDataContext);
 
 		voteActionableDynamicQuery.performCount();
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }

modules/apps/wiki/wiki-service/src/com/liferay/wiki/lar/WikiPortletDataHandler.java
@@ -20,12 +20,14 @@ import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portlet.exportimport.lar.BasePortletDataHandler;
 import com.liferay.portlet.exportimport.lar.PortletDataContext;
 import com.liferay.portlet.exportimport.lar.PortletDataException;
+import com.liferay.portlet.exportimport.lar.PortletDataHandler;
 import com.liferay.portlet.exportimport.lar.PortletDataHandlerBoolean;
 import com.liferay.portlet.exportimport.lar.PortletDataHandlerControl;
 import com.liferay.portlet.exportimport.lar.StagedModelDataHandlerUtil;
 import com.liferay.portlet.exportimport.lar.StagedModelType;
 import com.liferay.portlet.exportimport.xstream.XStreamAliasRegistryUtil;
 import com.liferay.wiki.constants.WikiConstants;
+import com.liferay.wiki.constants.WikiPortletKeys;
 import com.liferay.wiki.model.WikiNode;
 import com.liferay.wiki.model.WikiPage;
 import com.liferay.wiki.model.impl.WikiNodeImpl;
@@ -41,6 +43,12 @@ import java.util.Map;
 
 import javax.portlet.PortletPreferences;
 
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Bruno Farache
  * @author Jorge Ferrer
@@ -49,28 +57,17 @@ import javax.portlet.PortletPreferences;
  * @author Zsolt Berentey
  * @author Mate Thurzo
  */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + WikiPortletKeys.WIKI,
+		"javax.portlet.name=" + WikiPortletKeys.WIKI_ADMIN
+	},
+	service = PortletDataHandler.class
+)
 public class WikiPortletDataHandler extends BasePortletDataHandler {
 
 	public static final String NAMESPACE = "wiki";
-
-	public WikiPortletDataHandler() {
-		setDataPortletPreferences("hiddenNodes, visibleNodes");
-		setDeletionSystemEventStagedModelTypes(
-			new StagedModelType(WikiNode.class),
-			new StagedModelType(WikiPage.class));
-		setExportControls(
-			new PortletDataHandlerBoolean(
-				NAMESPACE, "wiki-pages", true, false,
-				new PortletDataHandlerControl[] {
-					new PortletDataHandlerBoolean(
-						NAMESPACE, "referenced-content")
-				},
-				WikiPage.class.getName()));
-		setImportControls(getExportControls());
-
-		XStreamAliasRegistryUtil.register(WikiNodeImpl.class, "WikiNode");
-		XStreamAliasRegistryUtil.register(WikiPageImpl.class, "WikiPage");
-	}
 
 	@Override
 	public String getServiceName() {
@@ -92,6 +89,26 @@ public class WikiPortletDataHandler extends BasePortletDataHandler {
 		finally {
 			WikiCacheThreadLocal.setClearCache(true);
 		}
+	}
+
+	@Activate
+	protected void activate() {
+		setDataPortletPreferences("hiddenNodes, visibleNodes");
+		setDeletionSystemEventStagedModelTypes(
+			new StagedModelType(WikiNode.class),
+			new StagedModelType(WikiPage.class));
+		setExportControls(
+			new PortletDataHandlerBoolean(
+				NAMESPACE, "wiki-pages", true, false,
+				new PortletDataHandlerControl[] {
+					new PortletDataHandlerBoolean(
+						NAMESPACE, "referenced-content")
+				},
+				WikiPage.class.getName()));
+		setImportControls(getExportControls());
+
+		XStreamAliasRegistryUtil.register(WikiNodeImpl.class, "WikiNode");
+		XStreamAliasRegistryUtil.register(WikiPageImpl.class, "WikiPage");
 	}
 
 	@Override
@@ -249,6 +266,10 @@ public class WikiPortletDataHandler extends BasePortletDataHandler {
 			portletDataContext, WikiNode.class);
 
 		return portletPreferences;
+	}
+
+	@Reference(target = "(original.bean=*)", unbind = "-")
+	protected void setServletContext(ServletContext servletContext) {
 	}
 
 }