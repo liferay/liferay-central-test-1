JorgeFerrer/liferay-portal

LPS-57379 Move from settings to configuration package (and standardize package and class names) to conform with naming of API.

modules/apps/asset/asset-categories-navigation-web/src/com/liferay/asset/categories/navigation/web/configuration/internal/AssetCategoriesNavigationPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.navigation.web.configuration.internal;
+
+import com.liferay.asset.categories.navigation.web.configuration.AssetCategoriesNavigationPortletInstanceConfiguration;
+import com.liferay.asset.categories.navigation.web.constants.AssetCategoriesNavigationPortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class AssetCategoriesNavigationPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return AssetCategoriesNavigationPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return AssetCategoriesNavigationPortletKeys.ASSET_CATEGORIES_NAVIGATION;
+	}
+
+}

modules/apps/bookmarks/bookmarks-api/src/com/liferay/bookmarks/configuration/internal/BookmarksGroupServiceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.configuration.internal;
+
+import com.liferay.bookmarks.configuration.BookmarksGroupServiceConfiguration;
+import com.liferay.bookmarks.constants.BookmarksConstants;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Iván Zaera
+ */
+@Component
+public class BookmarksGroupServiceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return BookmarksGroupServiceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return BookmarksConstants.SERVICE_NAME;
+	}
+
+}

modules/apps/iframe/iframe-web/src/com/liferay/iframe/web/configuration/internal/IFramePortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.iframe.web.configuration.internal;
+
+import com.liferay.iframe.web.configuration.IFramePortletInstanceConfiguration;
+import com.liferay.iframe.web.constants.IFramePortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class IFramePortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return IFramePortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return IFramePortletKeys.IFRAME;
+	}
+
+}

modules/apps/journal/journal-content-search-web/src/com/liferay/journal/content/search/web/configuration/internal/JournalContentSearchPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.search.web.configuration.internal;
+
+import com.liferay.journal.content.search.web.configuration.JournalContentSearchPortletInstanceConfiguration;
+import com.liferay.journal.content.search.web.constants.JournalContentSearchPortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class JournalContentSearchPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return JournalContentSearchPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return JournalContentSearchPortletKeys.JOURNAL_CONTENT_SEARCH;
+	}
+
+}

modules/apps/wiki/wiki-web/docroot/WEB-INF/src/com/liferay/wiki/web/configuration/internal/WikiAdminPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.configuration.internal;
+
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.web.configuration.WikiPortletInstanceConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Iván Zaera
+ */
+@Component
+public class WikiAdminPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return WikiPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return WikiPortletKeys.WIKI_ADMIN;
+	}
+
+}

liferay/liferay-central-test-1

modules/apps/asset/asset-categories-navigation-web/src/com/liferay/asset/categories/navigation/web/configuration/definition/AssetCategoriesNavigationPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.navigation.web.configuration.definition;
+
+import com.liferay.asset.categories.navigation.web.configuration.AssetCategoriesNavigationPortletInstanceConfiguration;
+import com.liferay.asset.categories.navigation.web.constants.AssetCategoriesNavigationPortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class AssetCategoriesNavigationPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return AssetCategoriesNavigationPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return AssetCategoriesNavigationPortletKeys.ASSET_CATEGORIES_NAVIGATION;
+	}
+
+}

modules/apps/bookmarks/bookmarks-api/src/com/liferay/bookmarks/configuration/definition/BookmarksGroupServiceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.bookmarks.configuration.definition;
+
+import com.liferay.bookmarks.configuration.BookmarksGroupServiceConfiguration;
+import com.liferay.bookmarks.constants.BookmarksConstants;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Iván Zaera
+ */
+@Component
+public class BookmarksGroupServiceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return BookmarksGroupServiceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return BookmarksConstants.SERVICE_NAME;
+	}
+
+}

modules/apps/iframe/iframe-web/src/com/liferay/iframe/web/configuration/definition/IFramePortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.iframe.web.configuration.definition;
+
+import com.liferay.iframe.web.configuration.IFramePortletInstanceConfiguration;
+import com.liferay.iframe.web.constants.IFramePortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class IFramePortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return IFramePortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return IFramePortletKeys.IFRAME;
+	}
+
+}

modules/apps/journal/journal-content-search-web/src/com/liferay/journal/content/search/web/configuration/definition/JournalContentSearchPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.search.web.configuration.definition;
+
+import com.liferay.journal.content.search.web.configuration.JournalContentSearchPortletInstanceConfiguration;
+import com.liferay.journal.content.search.web.constants.JournalContentSearchPortletKeys;
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Juergen Kappler
+ */
+@Component
+public class JournalContentSearchPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return JournalContentSearchPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return JournalContentSearchPortletKeys.JOURNAL_CONTENT_SEARCH;
+	}
+
+}

modules/apps/wiki/wiki-web/docroot/WEB-INF/src/com/liferay/wiki/web/configuration/definition/WikiAdminPortletInstanceConfigurationPidMapping.java
@@ -1,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.web.configuration.definition;
+
+import com.liferay.portal.kernel.settings.definition.ConfigurationPidMapping;
+import com.liferay.wiki.constants.WikiPortletKeys;
+import com.liferay.wiki.web.configuration.WikiPortletInstanceConfiguration;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Iván Zaera
+ */
+@Component
+public class WikiAdminPortletInstanceConfigurationPidMapping
+	implements ConfigurationPidMapping {
+
+	@Override
+	public Class<?> getConfigurationBeanClass() {
+		return WikiPortletInstanceConfiguration.class;
+	}
+
+	@Override
+	public String getConfigurationPid() {
+		return WikiPortletKeys.WIKI_ADMIN;
+	}
+
+}