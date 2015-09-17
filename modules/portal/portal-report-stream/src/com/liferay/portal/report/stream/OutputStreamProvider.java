brianchandotcom/liferay-portal

LPS-58134 OutputStreamProviders and trackers to provide report facilities

modules/portal/portal-upgrade/src/com/liferay/portal/upgrade/api/OutputStreamProvider.java
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.api;
+
+import java.io.OutputStream;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface OutputStreamProvider {
+
+	public OutputStreamInformation create(String hint);
+
+	public interface OutputStreamInformation {
+
+		public String getDescription();
+
+		public OutputStream getOutputStream();
+
+	}
+
+}

modules/portal/portal-upgrade/src/com/liferay/portal/upgrade/api/OutputStreamProviderTracker.java
@@ -1,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.api;
+
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+
+import java.util.Set;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, service = OutputStreamProviderTracker.class)
+public class OutputStreamProviderTracker {
+
+	public OutputStreamProvider getDefaultOutputStreamProvider() {
+		return _outputStreamProvider;
+	}
+
+	public OutputStreamProvider getOutputStreamProvider(
+		String outputStreamProviderName) {
+
+		OutputStreamProvider outputStreamProvider =
+			_outputStreamProviders.getService(outputStreamProviderName);
+
+		if (outputStreamProvider == null) {
+			throw new IllegalArgumentException(
+				"Report provider with name " + outputStreamProviderName +
+					" is not registered");
+		}
+
+		return outputStreamProvider;
+	}
+
+	public Set<String> getOutputStreamProviderNames() {
+		return _outputStreamProviders.keySet();
+	}
+
+	@Reference
+	public void setOutputStreamProvider(
+		OutputStreamProvider outputStreamProvider) {
+
+		_outputStreamProvider = outputStreamProvider;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		try {
+			_outputStreamProviders = ServiceTrackerMapFactory.singleValueMap(
+				bundleContext, OutputStreamProvider.class, "name");
+		}
+		catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+
+		_outputStreamProviders.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_outputStreamProviders.close();
+	}
+
+	private OutputStreamProvider _outputStreamProvider;
+	private ServiceTrackerMap<String, OutputStreamProvider>
+		_outputStreamProviders;
+
+}

modules/portal/portal-upgrade/src/com/liferay/portal/upgrade/internal/streamprovider/SystemOutputStreamProvider.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.internal.streamprovider;
+
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.upgrade.api.OutputStreamProvider;
+
+import java.io.OutputStream;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, property = {"name=console"})
+public class SystemOutputStreamProvider implements OutputStreamProvider {
+
+	@Override
+	public OutputStreamInformation create(String hint) {
+		return new OutputStreamInformation() {
+
+			@Override
+			public String getDescription() {
+				return "the console";
+			}
+
+			@Override
+			public OutputStream getOutputStream() {
+				return StreamUtil.uncloseable(System.out);
+			}
+
+		};
+	}
+
+}

modules/portal/portal-upgrade/src/com/liferay/portal/upgrade/internal/streamprovider/TempFileOutputStreamProvider.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.internal.streamprovider;
+
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.upgrade.api.OutputStreamProvider;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, property = {"name=tempfile"})
+public class TempFileOutputStreamProvider implements OutputStreamProvider {
+
+	@Override
+	public OutputStreamInformation create(String hint) {
+		try {
+			Path tempDirectory = Files.createTempDirectory("tempDir");
+
+			final Path tempFile = Files.createTempFile(
+				tempDirectory, hint, ".log");
+
+			return new OutputStreamInformation() {
+
+				@Override
+				public String getDescription() {
+					return tempFile.toAbsolutePath().toString();
+				}
+
+				@Override
+				public OutputStream getOutputStream() {
+					try {
+						return StreamUtil.uncloseable(
+							new FileOutputStream(tempFile.toFile()));
+					}
+					catch (FileNotFoundException e) {
+						throw new RuntimeException(e);
+					}
+				}
+
+			};
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}

liferay/liferay-central-test-1

modules/portal/portal-report-stream/src/com/liferay/portal/report/stream/OutputStreamProvider.java
@@ -1,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.report.stream;
+
+import java.io.OutputStream;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+public interface OutputStreamProvider {
+
+	public OutputStreamInformation create(String hint);
+
+	public interface OutputStreamInformation {
+
+		public String getDescription();
+
+		public OutputStream getOutputStream();
+
+	}
+
+}

modules/portal/portal-report-stream/src/com/liferay/portal/report/stream/OutputStreamProviderTracker.java
@@ -1,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.report.stream;
+
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+
+import java.util.Set;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, service = OutputStreamProviderTracker.class)
+public class OutputStreamProviderTracker {
+
+	public OutputStreamProvider getDefaultOutputStreamProvider() {
+		return _outputStreamProvider;
+	}
+
+	public OutputStreamProvider getOutputStreamProvider(
+		String outputStreamProviderName) {
+
+		OutputStreamProvider outputStreamProvider =
+			_outputStreamProviders.getService(outputStreamProviderName);
+
+		if (outputStreamProvider == null) {
+			throw new IllegalArgumentException(
+				"Report provider with name " + outputStreamProviderName +
+					" is not registered");
+		}
+
+		return outputStreamProvider;
+	}
+
+	public Set<String> getOutputStreamProviderNames() {
+		return _outputStreamProviders.keySet();
+	}
+
+	@Reference
+	public void setOutputStreamProvider(
+		OutputStreamProvider outputStreamProvider) {
+
+		_outputStreamProvider = outputStreamProvider;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		try {
+			_outputStreamProviders = ServiceTrackerMapFactory.singleValueMap(
+				bundleContext, OutputStreamProvider.class, "name");
+		}
+		catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(e);
+		}
+
+		_outputStreamProviders.open();
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_outputStreamProviders.close();
+	}
+
+	private OutputStreamProvider _outputStreamProvider;
+	private ServiceTrackerMap<String, OutputStreamProvider>
+		_outputStreamProviders;
+
+}

modules/portal/portal-report-stream/src/com/liferay/portal/report/stream/internal/SystemOutputStreamProvider.java
@@ -1,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.report.stream.internal;
+
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.report.stream.OutputStreamProvider;
+
+import java.io.OutputStream;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, property = {"name=console"})
+public class SystemOutputStreamProvider implements OutputStreamProvider {
+
+	@Override
+	public OutputStreamInformation create(String hint) {
+		return new OutputStreamInformation() {
+
+			@Override
+			public String getDescription() {
+				return "the console";
+			}
+
+			@Override
+			public OutputStream getOutputStream() {
+				return StreamUtil.uncloseable(System.out);
+			}
+
+		};
+	}
+
+}

modules/portal/portal-report-stream/src/com/liferay/portal/report/stream/internal/TempFileOutputStreamProvider.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.report.stream.internal;
+
+import com.liferay.portal.kernel.util.StreamUtil;
+import com.liferay.portal.report.stream.OutputStreamProvider;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carlos Sierra Andrés
+ */
+@Component(immediate = true, property = {"name=tempfile"})
+public class TempFileOutputStreamProvider implements OutputStreamProvider {
+
+	@Override
+	public OutputStreamProvider.OutputStreamInformation create(String hint) {
+		try {
+			Path tempDirectory = Files.createTempDirectory("tempDir");
+
+			final Path tempFile = Files.createTempFile(
+				tempDirectory, hint, ".log");
+
+			return new OutputStreamInformation() {
+
+				@Override
+				public String getDescription() {
+					return tempFile.toAbsolutePath().toString();
+				}
+
+				@Override
+				public OutputStream getOutputStream() {
+					try {
+						return StreamUtil.uncloseable(
+							new FileOutputStream(tempFile.toFile()));
+					}
+					catch (FileNotFoundException e) {
+						throw new RuntimeException(e);
+					}
+				}
+
+			};
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}