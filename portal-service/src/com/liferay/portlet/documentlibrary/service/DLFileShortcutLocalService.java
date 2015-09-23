shuyangzhou/liferay-portal

LPS-58784 Build Service

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileShortcutLocalService.java
@@ -339,6 +339,8 @@ public interface DLFileShortcutLocalService extends BaseLocalService,
 	*/
 	public void setBeanIdentifier(java.lang.String beanIdentifier);
 
+	public void setFileShortcutsEnabled(long toFileEntryId, boolean enabled);
+
 	public void setTreePaths(long folderId, java.lang.String treePath)
 		throws PortalException;
 

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileShortcutLocalServiceUtil.java
@@ -412,6 +412,11 @@ public class DLFileShortcutLocalServiceUtil {
 		getService().setBeanIdentifier(beanIdentifier);
 	}
 
+	public static void setFileShortcutsEnabled(long toFileEntryId,
+		boolean enabled) {
+		getService().setFileShortcutsEnabled(toFileEntryId, enabled);
+	}
+
 	public static void setTreePaths(long folderId, java.lang.String treePath)
 		throws com.liferay.portal.kernel.exception.PortalException {
 		getService().setTreePaths(folderId, treePath);

liferay/liferay-central-test-1

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileShortcutLocalService.java
@@ -365,6 +365,8 @@ public interface DLFileShortcutLocalService extends BaseLocalService,
 
 	public void updateFileShortcuts(long oldToFileEntryId, long newToFileEntryId);
 
+	public void updateFileShortcutsActive(long toFileEntryId, boolean active);
+
 	public void updateStatus(long userId, long fileShortcutId, int status,
 		com.liferay.portal.service.ServiceContext serviceContext)
 		throws PortalException;

portal-service/src/com/liferay/portlet/documentlibrary/service/DLFileShortcutLocalServiceUtil.java
@@ -451,6 +451,11 @@ public class DLFileShortcutLocalServiceUtil {
 		getService().updateFileShortcuts(oldToFileEntryId, newToFileEntryId);
 	}
 
+	public static void updateFileShortcutsActive(long toFileEntryId,
+		boolean active) {
+		getService().updateFileShortcutsActive(toFileEntryId, active);
+	}
+
 	public static void updateStatus(long userId, long fileShortcutId,
 		int status, com.liferay.portal.service.ServiceContext serviceContext)
 		throws com.liferay.portal.kernel.exception.PortalException {