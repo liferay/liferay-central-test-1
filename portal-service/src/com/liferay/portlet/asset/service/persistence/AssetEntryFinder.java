brianchandotcom/liferay-portal

LPS-58877 Build Service

portal-service/src/com/liferay/portlet/asset/service/persistence/AssetEntryFinder.java
@@ -27,4 +27,10 @@ public interface AssetEntryFinder {
 
 	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> findEntries(
 		com.liferay.portlet.asset.service.persistence.AssetEntryQuery entryQuery);
+
+	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> joinDLF_ByAssetEntry(
+		long classNameId, java.lang.String treePath);
+
+	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> joinDLFE_ByAssetEntry(
+		long classNameId, java.lang.String treePath);
 }

portal-service/src/com/liferay/portlet/asset/service/persistence/AssetEntryFinderUtil.java
@@ -35,6 +35,16 @@ public class AssetEntryFinderUtil {
 		return getFinder().findEntries(entryQuery);
 	}
 
+	public static java.util.List<com.liferay.portlet.asset.model.AssetEntry> joinDLF_ByAssetEntry(
+		long classNameId, java.lang.String treePath) {
+		return getFinder().joinDLF_ByAssetEntry(classNameId, treePath);
+	}
+
+	public static java.util.List<com.liferay.portlet.asset.model.AssetEntry> joinDLFE_ByAssetEntry(
+		long classNameId, java.lang.String treePath) {
+		return getFinder().joinDLFE_ByAssetEntry(classNameId, treePath);
+	}
+
 	public static AssetEntryFinder getFinder() {
 		if (_finder == null) {
 			_finder = (AssetEntryFinder)PortalBeanLocatorUtil.locate(AssetEntryFinder.class.getName());

liferay/liferay-central-test-1

portal-service/src/com/liferay/portlet/asset/service/persistence/AssetEntryFinder.java
@@ -25,6 +25,12 @@ public interface AssetEntryFinder {
 	public int countEntries(
 		com.liferay.portlet.asset.service.persistence.AssetEntryQuery entryQuery);
 
+	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> findByDLFileEntryC_T(
+		long classNameId, java.lang.String treePath);
+
+	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> findByDLFolderC_T(
+		long classNameId, java.lang.String treePath);
+
 	public java.util.List<com.liferay.portlet.asset.model.AssetEntry> findEntries(
 		com.liferay.portlet.asset.service.persistence.AssetEntryQuery entryQuery);
 }

portal-service/src/com/liferay/portlet/asset/service/persistence/AssetEntryFinderUtil.java
@@ -30,6 +30,16 @@ public class AssetEntryFinderUtil {
 		return getFinder().countEntries(entryQuery);
 	}
 
+	public static java.util.List<com.liferay.portlet.asset.model.AssetEntry> findByDLFileEntryC_T(
+		long classNameId, java.lang.String treePath) {
+		return getFinder().findByDLFileEntryC_T(classNameId, treePath);
+	}
+
+	public static java.util.List<com.liferay.portlet.asset.model.AssetEntry> findByDLFolderC_T(
+		long classNameId, java.lang.String treePath) {
+		return getFinder().findByDLFolderC_T(classNameId, treePath);
+	}
+
 	public static java.util.List<com.liferay.portlet.asset.model.AssetEntry> findEntries(
 		com.liferay.portlet.asset.service.persistence.AssetEntryQuery entryQuery) {
 		return getFinder().findEntries(entryQuery);