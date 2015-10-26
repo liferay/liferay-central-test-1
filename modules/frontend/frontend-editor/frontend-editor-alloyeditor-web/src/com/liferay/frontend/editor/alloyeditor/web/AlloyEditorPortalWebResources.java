peterfellwock/liferay-portal

LPS-56537 - Create editor PortalWebResources classes and constants

modules/frontend/frontend-editors/frontend-alloyeditor-web/src/com/liferay/frontend/editors/alloyeditor/web/AlloyEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.alloyeditor.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class AlloyEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_ALLOYEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editors.alloyeditor.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editors/frontend-bbcode-web/src/com/liferay/frontend/editors/bbcode/web/BBCodeEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.bbcode.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class BBCodeEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_BBCODEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editors.bbcode.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editors/frontend-ckeditor-web/src/com/liferay/frontend/editors/ckeditor/web/CKEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.ckeditor.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class CKEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_CKEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editors.ckeditor.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editors/frontend-simple-web/src/com/liferay/frontend/editors/simple/web/SimpleEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.simple.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class SimpleEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_SIMPLEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editors.simple.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editors/frontend-tinymce-web/src/com/liferay/frontend/editors/tinymce/web/TinyMCEEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editors.tinymce.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class TinyMCEEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_TINYMCEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editors.tinymce.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

liferay/liferay-central-test-1

modules/frontend/frontend-editor/frontend-editor-alloyeditor-web/src/com/liferay/frontend/editor/alloyeditor/web/AlloyEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.alloyeditor.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class AlloyEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_EDITOR_ALLOYEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.alloyeditor.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editor/frontend-editor-bbcode-web/src/com/liferay/frontend/editor/bbcode/web/BBCodeEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.bbcode.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class BBCodeEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_EDITOR_BBCODEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.bbcode.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editor/frontend-editor-ckeditor-web/src/com/liferay/frontend/editor/ckeditor/web/CKEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.ckeditor.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class CKEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_EDITOR_CKEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.ckeditor.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editor/frontend-editor-simple-web/src/com/liferay/frontend/editor/simple/web/SimpleEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.simple.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class SimpleEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_EDITOR_SIMPLEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.simple.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}

modules/frontend/frontend-editor/frontend-editor-tinymce-web/src/com/liferay/frontend/editor/tinymce/web/TinyMCEEditorPortalWebResources.java
@@ -1,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.frontend.editor.tinymce.web;
+
+import com.liferay.portal.kernel.servlet.PortalWebResourceConstants;
+import com.liferay.portal.kernel.servlet.PortalWebResources;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Michael Bradford
+ */
+@Component(immediate = true, service = PortalWebResources.class)
+public class TinyMCEEditorPortalWebResources implements PortalWebResources {
+
+	@Override
+	public String getContextPath() {
+		return _servletContext.getContextPath();
+	}
+
+	@Override
+	public long getLastModified() {
+		return _bundle.getLastModified();
+	}
+
+	@Override
+	public String getResourceType() {
+		return PortalWebResourceConstants.RESOURCE_TYPE_EDITOR_TINYMCEEDITOR;
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundle = bundleContext.getBundle();
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.frontend.editor.tinymce.web)"
+	)
+	protected void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private Bundle _bundle;
+	private ServletContext _servletContext;
+
+}