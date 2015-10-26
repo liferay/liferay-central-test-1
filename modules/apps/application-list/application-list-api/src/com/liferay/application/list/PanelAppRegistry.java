brianchandotcom/liferay-portal

LPS-59069 add the category when adding the panel app

modules/apps/application-list/application-list-api/src/com/liferay/application/list/PanelAppRegistry.java
@@ -18,13 +18,16 @@ import com.liferay.application.list.util.PanelCategoryServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PredicateFilter;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.PortletLocalService;
 
 import java.util.Collections;
 import java.util.List;
@@ -35,6 +38,7 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
@@ -114,7 +118,9 @@ public class PanelAppRegistry {
 		_serviceTrackerMap = ServiceTrackerMapFactory.multiValueMap(
 			bundleContext, PanelApp.class, "(panel.category.key=*)",
 			new PanelCategoryServiceReferenceMapper(),
-			new ServiceRankingPropertyServiceReferenceComparator());
+			new ServiceRankingPropertyServiceReferenceComparator(),
+			new StringPanelAppListServiceTrackerMapListener()
+		);
 
 		_serviceTrackerMap.open();
 	}
@@ -123,6 +129,15 @@ public class PanelAppRegistry {
 	protected void deactivate() {
 		_serviceTrackerMap.close();
 	}
+
+	@Reference(unbind = "-")
+	protected void setPortletLocalService(
+		PortletLocalService portletLocalService) {
+
+		this.portletLocalService = portletLocalService;
+	}
+
+	protected PortletLocalService portletLocalService;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		PanelAppRegistry.class);
@@ -142,6 +157,27 @@ public class PanelAppRegistry {
 			ServiceReference<PanelApp> serviceReference2) {
 
 			return -(super.compare(serviceReference1, serviceReference2));
+		}
+
+	}
+
+	private class StringPanelAppListServiceTrackerMapListener
+		implements ServiceTrackerMapListener<String, PanelApp, List<PanelApp>> {
+
+		@Override
+		public void keyEmitted(
+			ServiceTrackerMap<String, List<PanelApp>> serviceTrackerMap,
+			String panelCategoryKey, PanelApp panelApp,
+			List<PanelApp> panelApps) {
+
+			Portlet portlet = panelApp.getPortlet();
+
+			if (portlet != null) {
+				portlet.setControlPanelEntryCategory(panelCategoryKey);
+			}
+			else {
+				_log.error("Unable to get portlet " + panelApp.getPortletId());
+			}
 		}
 
 	}

liferay/liferay-central-test-1

modules/apps/application-list/application-list-api/src/com/liferay/application/list/PanelAppRegistry.java
@@ -18,13 +18,16 @@ import com.liferay.application.list.util.PanelCategoryServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.map.PropertyServiceReferenceComparator;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapListener;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PredicateFilter;
 import com.liferay.portal.model.Group;
+import com.liferay.portal.model.Portlet;
 import com.liferay.portal.security.permission.PermissionChecker;
+import com.liferay.portal.service.PortletLocalService;
 
 import java.util.Collections;
 import java.util.List;
@@ -35,6 +38,7 @@ import org.osgi.framework.ServiceReference;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Adolfo Pérez
@@ -114,7 +118,9 @@ public class PanelAppRegistry {
 		_serviceTrackerMap = ServiceTrackerMapFactory.multiValueMap(
 			bundleContext, PanelApp.class, "(panel.category.key=*)",
 			new PanelCategoryServiceReferenceMapper(),
-			new ServiceRankingPropertyServiceReferenceComparator());
+			new ServiceRankingPropertyServiceReferenceComparator(),
+			new StringPanelAppListServiceTrackerMapListener()
+		);
 
 		_serviceTrackerMap.open();
 	}
@@ -123,6 +129,15 @@ public class PanelAppRegistry {
 	protected void deactivate() {
 		_serviceTrackerMap.close();
 	}
+
+	@Reference(unbind = "-")
+	protected void setPortletLocalService(
+		PortletLocalService portletLocalService) {
+
+		this.portletLocalService = portletLocalService;
+	}
+
+	protected PortletLocalService portletLocalService;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		PanelAppRegistry.class);
@@ -142,6 +157,27 @@ public class PanelAppRegistry {
 			ServiceReference<PanelApp> serviceReference2) {
 
 			return -(super.compare(serviceReference1, serviceReference2));
+		}
+
+	}
+
+	private class StringPanelAppListServiceTrackerMapListener
+		implements ServiceTrackerMapListener<String, PanelApp, List<PanelApp>> {
+
+		@Override
+		public void keyEmitted(
+			ServiceTrackerMap<String, List<PanelApp>> serviceTrackerMap,
+			String panelCategoryKey, PanelApp panelApp,
+			List<PanelApp> panelApps) {
+
+			Portlet portlet = panelApp.getPortlet();
+
+			if (portlet != null) {
+				portlet.setControlPanelEntryCategory(panelCategoryKey);
+			}
+			else if (_log.isDebugEnabled()) {
+				_log.debug("Unable to get portlet " + panelApp.getPortletId());
+			}
 		}
 
 	}