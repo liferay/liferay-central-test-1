liferay/liferay-central-test-1

commit e5d93e06c8a2d58bf4cee68d889e63526019c312

LPS-60076 SF

modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormPortlet.java
@@ -196,7 +196,7 @@ public class DDLFormPortlet extends MVCPortlet {
 
 		if (requireCaptcha) {
 			DDMFormField captchaDDMFormField = new DDMFormField(
-				_CAPTCHA_FIELD_NAME, "captcha");
+				_DDM_FORM_FIELD_NAME_CAPTCHA, "captcha");
 
 			captchaDDMFormField.setProperty(
 				"url", createCaptchaResourceURL(renderResponse));
@@ -217,10 +217,8 @@ public class DDLFormPortlet extends MVCPortlet {
 
 		DDMStructure ddmStructure = recordSet.getDDMStructure();
 
-		boolean requireCaptcha = isCaptchaRequired(recordSet);
-
 		DDMForm ddmForm = getDDMForm(
-			renderResponse, ddmStructure, requireCaptcha);
+			renderResponse, ddmStructure, isCaptchaRequired(recordSet));
 
 		DDMFormLayout ddmFormLayout = getDDMFormLayout(
 			ddmStructure, requireCaptcha);
@@ -240,7 +238,7 @@ public class DDLFormPortlet extends MVCPortlet {
 				ddmFormLayout);
 
 			DDMFormLayoutRow ddmFormLayoutRow =
-				createFullColumnDDMFormLayoutRow(_CAPTCHA_FIELD_NAME);
+				createFullColumnDDMFormLayoutRow(_DDM_FORM_FIELD_NAME_CAPTCHA);
 
 			lastDDMFormLayoutPage.addDDMFormLayoutRow(ddmFormLayoutRow);
 		}
@@ -316,7 +314,7 @@ public class DDLFormPortlet extends MVCPortlet {
 			DDMWebKeys.DYNAMIC_DATA_MAPPING_FORM_HTML, ddmFormHTML);
 	}
 
-	private static final String _CAPTCHA_FIELD_NAME = "_CAPTCHA_";
+	private static final String _DDM_FORM_FIELD_NAME_CAPTCHA = "_CAPTCHA_";
 
 	private static final Log _log = LogFactoryUtil.getLog(DDLFormPortlet.class);
 

modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/action/UpdateRecordSetSettingsMVCActionCommand.java
@@ -78,10 +78,12 @@ public class UpdateRecordSetSettingsMVCActionCommand
 		UnicodeProperties settingsProperties = new UnicodeProperties(true);
 
 		String redirectURL = ParamUtil.getString(actionRequest, "redirectURL");
+
+		settingsProperties.setProperty("redirectURL", redirectURL);
+
 		boolean requireCaptcha = ParamUtil.getBoolean(
 			actionRequest, "requireCaptcha");
 
-		settingsProperties.setProperty("redirectURL", redirectURL);
 		settingsProperties.setProperty(
 			"requireCaptcha", String.valueOf(requireCaptcha));
 

commit 7bf8bc7683aa704c2fdc097d0afee327b613224a

LPS-60076 Fix compile, caused by e5d93e06c8a2d58bf4cee68d889e63526019c312

modules/apps/dynamic-data-lists/dynamic-data-lists-form-web/src/main/java/com/liferay/dynamic/data/lists/form/web/portlet/DDLFormPortlet.java
@@ -217,8 +217,10 @@ public class DDLFormPortlet extends MVCPortlet {
 
 		DDMStructure ddmStructure = recordSet.getDDMStructure();
 
+		boolean requireCaptcha = isCaptchaRequired(recordSet);
+
 		DDMForm ddmForm = getDDMForm(
-			renderResponse, ddmStructure, isCaptchaRequired(recordSet));
+			renderResponse, ddmStructure, requireCaptcha);
 
 		DDMFormLayout ddmFormLayout = getDDMFormLayout(
 			ddmStructure, requireCaptcha);