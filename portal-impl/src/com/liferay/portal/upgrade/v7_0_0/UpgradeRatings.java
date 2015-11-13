liferay/liferay-central-test-1

commit 728bf4b961e1cf1bb6f3c9fb1c8ff95053add69e

LPS-49049 Upgrade

portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeRatings.java
@@ -1,0 +1,199 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.upgrade.v7_0_0;
+
+import com.liferay.portal.kernel.configuration.Filter;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portal.util.PropsUtil;
+import com.liferay.portal.util.PropsValues;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Sergio González
+ */
+public class UpgradeRatings extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		upgradeRatingsEntry();
+		upgradeRatingsStats();
+	}
+
+	protected void upgradeRatingsEntry() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"select distinct classNameId from RatingsEntry");
+
+			rs = ps.executeQuery();
+
+			while (rs.next()) {
+				upgradeRatingsEntry(rs.getLong("classNameId"));
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+	protected void upgradeRatingsEntry(long classNameId) throws Exception {
+		String className = PortalUtil.getClassName(classNameId);
+
+		if (ArrayUtil.contains(
+				PropsValues.RATINGS_UPGRADE_THUMBS_CLASS_NAMES, className)) {
+
+			upgradeRatingsEntryThumbs(classNameId);
+		}
+		else {
+			int defaultRatingsStarsNormalizationFactor = GetterUtil.getInteger(
+				PropsUtil.get(
+					PropsKeys.RATINGS_UPGRADE_STARS_NORMALIZATION_FACTOR,
+					new Filter("default")),
+				5);
+
+			int ratingsStarsNormalizationFactor = GetterUtil.getInteger(
+				PropsUtil.get(
+					PropsKeys.RATINGS_UPGRADE_STARS_NORMALIZATION_FACTOR,
+					new Filter(className)),
+				defaultRatingsStarsNormalizationFactor);
+
+			upgradeRatingsEntryStars(
+				classNameId, ratingsStarsNormalizationFactor);
+		}
+	}
+
+	protected void upgradeRatingsEntryStars(
+			long classNameId, int normalizationFactor)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"update RatingsEntry set score = score / ? " +
+					"where classNameId = ?");
+
+			ps.setInt(1, normalizationFactor);
+			ps.setLong(2, classNameId);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+	}
+
+	protected void upgradeRatingsEntryThumbs(long classNameId)
+		throws Exception {
+
+		Connection con = null;
+		PreparedStatement ps = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			ps = con.prepareStatement(
+				"update RatingsEntry set score = ? where score = ? and " +
+					"classNameId = ?");
+
+			ps.setDouble(1, 0);
+			ps.setDouble(2, -1);
+			ps.setLong(3, classNameId);
+
+			ps.executeUpdate();
+		}
+		finally {
+			DataAccess.cleanUp(con, ps);
+		}
+	}
+
+	protected void upgradeRatingsStats() throws Exception {
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		try {
+			con = DataAccess.getUpgradeOptimizedConnection();
+
+			DatabaseMetaData databaseMetaData = con.getMetaData();
+
+			boolean supportsBatchUpdates =
+				databaseMetaData.supportsBatchUpdates();
+
+			ps = con.prepareStatement(
+				"select classNameId, classPK, count(1) as totalEntries, " +
+					"sum(re.score) as totalScore, sum(re.score) / count(1) " +
+						"as averageScore from RatingsEntry re group by " +
+							"classNameId, classPK");
+
+			rs = ps.executeQuery();
+
+			ps = con.prepareStatement(
+				"update RatingsStats set totalEntries = ?, totalScore = ?, " +
+					"averageScore = ? where classNameId = ? and classPK = ?");
+
+			int count = 0;
+
+			while (rs.next()) {
+				ps.setInt(1, rs.getInt("totalEntries"));
+				ps.setDouble(2, rs.getDouble("totalScore"));
+				ps.setDouble(3, rs.getDouble("averageScore"));
+				ps.setLong(4, rs.getLong("classNameId"));
+				ps.setLong(5, rs.getLong("classPK"));
+
+				if (supportsBatchUpdates) {
+					ps.addBatch();
+
+					if (count == PropsValues.HIBERNATE_JDBC_BATCH_SIZE) {
+						ps.executeBatch();
+
+						count = 0;
+					}
+					else {
+						count++;
+					}
+				}
+				else {
+					ps.executeUpdate();
+				}
+			}
+
+			if (supportsBatchUpdates && (count > 0)) {
+				ps.executeBatch();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(con, ps, rs);
+		}
+	}
+
+}

portal-impl/src/com/liferay/portal/upgrade/UpgradeProcess_7_0_0.java
@@ -32,6 +32,7 @@ import com.liferay.portal.upgrade.v7_0_0.UpgradeLanguagePreferences;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeLock;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeMessageBoards;
 import com.liferay.portal.upgrade.v7_0_0.UpgradePortletSettings;
+import com.liferay.portal.upgrade.v7_0_0.UpgradeRatings;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeRepositoryEntry;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeSchema;
 import com.liferay.portal.upgrade.v7_0_0.UpgradeShopping;
@@ -67,6 +68,7 @@ public class UpgradeProcess_7_0_0 extends UpgradeProcess {
 		upgrade(UpgradeJournalArticleType.class);
 		upgrade(UpgradeLock.class);
 		upgrade(UpgradeMessageBoards.class);
+		upgrade(UpgradeRatings.class);
 		upgrade(UpgradeRepositoryEntry.class);
 		upgrade(UpgradeShopping.class);
 		upgrade(UpgradeShoppingPreferences.class);

portal-impl/src/com/liferay/portal/util/PropsValues.java
@@ -1546,6 +1546,8 @@ public class PropsValues {
 
 	public static final int RATINGS_DEFAULT_NUMBER_OF_STARS = GetterUtil.getInteger(PropsUtil.get(PropsKeys.RATINGS_DEFAULT_NUMBER_OF_STARS));
 
+	public static final String[] RATINGS_UPGRADE_THUMBS_CLASS_NAMES = PropsUtil.getArray(PropsKeys.RATINGS_UPGRADE_THUMBS_CLASS_NAMES);
+
 	public static final int RECENT_CONTENT_MAX_DISPLAY_ITEMS = GetterUtil.getInteger(PropsUtil.get(PropsKeys.RECENT_CONTENT_MAX_DISPLAY_ITEMS));
 
 	public static final String[] REDIRECT_URL_DOMAINS_ALLOWED = PropsUtil.getArray(PropsKeys.REDIRECT_URL_DOMAINS_ALLOWED);

portal-impl/src/portal.properties
@@ -10698,6 +10698,26 @@
     #
     ratings.default.number.of.stars=5
 
+    #
+    # In versions prior to 7.0, the ratings stored in database were not
+    # normalized. Since 7.0 we will be using normalize values between [0, 1].
+    #
+    # The following properties are used by the upgrade process to normalize the
+    # existing values.
+    #
+    # Set this property to specify the normalize factor that will be used during
+    # the upgrade process for the different class names that uses stars as the
+    # ratings mechanism.
+    #
+    # The normalize factor is the maximun number of stars for the class name.
+    #
+    ratings.upgrade.stars.normalization.factor[default]=5
+    #ratings.upgrade.stars.normalization.factor[com.liferay.portlet.blogs.model.BlogsEntry]=10
+    #
+    # Set this property to specify what class names uses thumbs as ratings
+    #
+    ratings.upgrade.thumbs.class.names=com.liferay.portlet.messageboards.model.MBDiscussion,com.liferay.portlet.messageboards.model.MBMessage
+
 ##
 ## Recent Content Portlet
 ##

portal-service/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2155,6 +2155,10 @@ public interface PropsKeys {
 
 	public static final String RATINGS_DEFAULT_NUMBER_OF_STARS = "ratings.default.number.of.stars";
 
+	public static final String RATINGS_UPGRADE_STARS_NORMALIZATION_FACTOR = "ratings.upgrade.stars.normalization.factor";
+
+	public static final String RATINGS_UPGRADE_THUMBS_CLASS_NAMES = "ratings.upgrade.thumbs.class.names";
+
 	public static final String RECENT_CONTENT_MAX_DISPLAY_ITEMS = "recent.content.max.display.items";
 
 	public static final String REDIRECT_URL_DOMAINS_ALLOWED = "redirect.url.domains.allowed";

commit 1d3c3d14bba4edfccfee06efe3150e0fdbba3537

LPS-60473 The first ps got overwritten by the second ps, leaving the first one never closed. Caused by 728bf4b961e1cf1bb6f3c9fb1c8ff95053add69e. Rename the second ps to ps2, preapre to fix the unclosed statement issue, no logic change.

portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeRatings.java
@@ -161,24 +161,24 @@ public class UpgradeRatings extends UpgradeProcess {
 
 			rs = ps.executeQuery();
 
-			ps = con.prepareStatement(
+			PreparedStatement ps2 = con.prepareStatement(
 				"update RatingsStats set totalEntries = ?, totalScore = ?, " +
 					"averageScore = ? where classNameId = ? and classPK = ?");
 
 			int count = 0;
 
 			while (rs.next()) {
-				ps.setInt(1, rs.getInt("totalEntries"));
-				ps.setDouble(2, rs.getDouble("totalScore"));
-				ps.setDouble(3, rs.getDouble("averageScore"));
-				ps.setLong(4, rs.getLong("classNameId"));
-				ps.setLong(5, rs.getLong("classPK"));
+				ps2.setInt(1, rs.getInt("totalEntries"));
+				ps2.setDouble(2, rs.getDouble("totalScore"));
+				ps2.setDouble(3, rs.getDouble("averageScore"));
+				ps2.setLong(4, rs.getLong("classNameId"));
+				ps2.setLong(5, rs.getLong("classPK"));
 
 				if (supportsBatchUpdates) {
-					ps.addBatch();
+					ps2.addBatch();
 
 					if (count == PropsValues.HIBERNATE_JDBC_BATCH_SIZE) {
-						ps.executeBatch();
+						ps2.executeBatch();
 
 						count = 0;
 					}
@@ -187,12 +187,12 @@ public class UpgradeRatings extends UpgradeProcess {
 					}
 				}
 				else {
-					ps.executeUpdate();
+					ps2.executeUpdate();
 				}
 			}
 
 			if (supportsBatchUpdates && (count > 0)) {
-				ps.executeBatch();
+				ps2.executeBatch();
 			}
 		}
 		finally {