liferay/liferay-central-test-1

commit 0905a887f2a6d2edb9a1a24e97d21019e9a2ec52

LPS-60039 Add batching to improve performance

portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeResourcePermission.java
@@ -17,9 +17,10 @@ package com.liferay.portal.upgrade.v7_0_0;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.StringBundler;
+import com.liferay.portal.util.PropsValues;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -36,6 +37,11 @@ public class UpgradeResourcePermission extends UpgradeProcess {
 
 		try {
 			con = DataAccess.getUpgradeOptimizedConnection();
+
+			DatabaseMetaData databaseMetaData = con.getMetaData();
+
+			boolean supportsBatchUpdates =
+				databaseMetaData.supportsBatchUpdates();
 
 			ps = con.prepareStatement(
 				"select resourcePermissionId, primKey, primKeyId, actionIds, " +
@@ -58,23 +64,49 @@ public class UpgradeResourcePermission extends UpgradeProcess {
 					continue;
 				}
 
-				StringBundler sb = new StringBundler(6);
+				PreparedStatement ps2 = null;
 
-				sb.append("update ResourcePermission set primKeyId = ");
-				sb.append(newPrimKeyId);
-				sb.append(", viewActionId = ");
+				try {
+					ps2 = con.prepareStatement(
+						"update ResourcePermission set primKeyId = ?," +
+							"viewActionId = ?  where resourcePermissionId = ?");
 
-				if (newViewActionId) {
-					sb.append("[$TRUE$]");
+					ps2.setLong(1, newPrimKeyId);
+
+					if (newViewActionId) {
+						ps2.setBoolean(2, true);
+					}
+					else {
+						ps2.setBoolean(2, false);
+					}
+
+					ps2.setLong(3, resourcePermissionId);
+
+					int count = 0;
+
+					if (supportsBatchUpdates) {
+						ps2.addBatch();
+
+						if (count == PropsValues.HIBERNATE_JDBC_BATCH_SIZE) {
+							ps2.executeBatch();
+
+							count = 0;
+						}
+						else {
+							count++;
+						}
+					}
+					else {
+						ps2.executeUpdate();
+					}
+
+					if (supportsBatchUpdates && (count > 0)) {
+						ps2.executeBatch();
+					}
 				}
-				else {
-					sb.append("[$FALSE$]");
+				finally {
+					DataAccess.cleanUp(ps2);
 				}
-
-				sb.append(" where resourcePermissionId = ");
-				sb.append(resourcePermissionId);
-
-				runSQL(sb.toString());
 			}
 		}
 		finally {

commit a8fd411c89a8e825f0adf4f8c25dabfc4c462752

LPS-60473 Fix the broken batching logic, the count should be done for the while loop, not inside it. After the loop if there is any left over batched parameters, we must execute them. Caused by 0905a887f2a6d2edb9a1a24e97d21019e9a2ec52

portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeResourcePermission.java
@@ -56,6 +56,8 @@ public class UpgradeResourcePermission extends UpgradeProcess {
 					"update ResourcePermission set primKeyId = ?," +
 						"viewActionId = ?  where resourcePermissionId = ?");
 
+				int count = 0;
+
 				while (rs.next()) {
 					long resourcePermissionId = rs.getLong(
 						"resourcePermissionId");
@@ -84,8 +86,6 @@ public class UpgradeResourcePermission extends UpgradeProcess {
 
 					ps2.setLong(3, resourcePermissionId);
 
-					int count = 0;
-
 					if (supportsBatchUpdates) {
 						ps2.addBatch();
 
@@ -101,6 +101,10 @@ public class UpgradeResourcePermission extends UpgradeProcess {
 					else {
 						ps2.executeUpdate();
 					}
+				}
+
+				if (supportsBatchUpdates && (count > 0)) {
+					ps2.executeBatch();
 				}
 			}
 			finally {