peterfellwock/liferay-portal

LPS-50917 Regen

portal-service/src/com/liferay/portal/service/TeamLocalService.java
@@ -301,6 +301,10 @@ public interface TeamLocalService extends BaseLocalService,
 	public java.util.List<com.liferay.portal.model.Team> getTeams(int start,
 		int end);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.Team> getTeamsByUserOrUserGroups(
+		long groupId, long userId);
+
 	/**
 	* Returns all the teams matching the UUID and company.
 	*

portal-service/src/com/liferay/portal/service/TeamLocalServiceUtil.java
@@ -377,6 +377,11 @@ public class TeamLocalServiceUtil {
 		return getService().getTeams(start, end);
 	}
 
+	public static java.util.List<com.liferay.portal.model.Team> getTeamsByUserOrUserGroups(
+		long groupId, long userId) {
+		return getService().getTeamsByUserOrUserGroups(groupId, userId);
+	}
+
 	/**
 	* Returns all the teams matching the UUID and company.
 	*

portal-service/src/com/liferay/portal/service/TeamLocalServiceWrapper.java
@@ -408,6 +408,12 @@ public class TeamLocalServiceWrapper implements TeamLocalService,
 		return _teamLocalService.getTeams(start, end);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portal.model.Team> getTeamsByUserOrUserGroups(
+		long groupId, long userId) {
+		return _teamLocalService.getTeamsByUserOrUserGroups(groupId, userId);
+	}
+
 	/**
 	* Returns all the teams matching the UUID and company.
 	*

liferay/liferay-central-test-1

portal-service/src/com/liferay/portal/service/TeamLocalService.java
@@ -360,6 +360,10 @@ public interface TeamLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getUserGroupTeamsCount(long userGroupId);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public java.util.List<com.liferay.portal.model.Team> getUserOrUserGroupTeams(
+		long groupId, long userId);
+
 	/**
 	* Returns the userIds of the users associated with the team.
 	*

portal-service/src/com/liferay/portal/service/TeamLocalServiceUtil.java
@@ -447,6 +447,11 @@ public class TeamLocalServiceUtil {
 		return getService().getUserGroupTeamsCount(userGroupId);
 	}
 
+	public static java.util.List<com.liferay.portal.model.Team> getUserOrUserGroupTeams(
+		long groupId, long userId) {
+		return getService().getUserOrUserGroupTeams(groupId, userId);
+	}
+
 	/**
 	* Returns the userIds of the users associated with the team.
 	*

portal-service/src/com/liferay/portal/service/TeamLocalServiceWrapper.java
@@ -485,6 +485,12 @@ public class TeamLocalServiceWrapper implements TeamLocalService,
 		return _teamLocalService.getUserGroupTeamsCount(userGroupId);
 	}
 
+	@Override
+	public java.util.List<com.liferay.portal.model.Team> getUserOrUserGroupTeams(
+		long groupId, long userId) {
+		return _teamLocalService.getUserOrUserGroupTeams(groupId, userId);
+	}
+
 	/**
 	* Returns the userIds of the users associated with the team.
 	*