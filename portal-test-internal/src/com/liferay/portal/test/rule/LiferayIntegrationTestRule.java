liferay/liferay-central-test-1

commit e263026634e5f0c6ada4ac8fd3e76740b0236ad1

LPS-59238 Execute the verifiers at the required point. Allow the user to disable even the first execution

modules/portal/portal-verify-extender/src/com/liferay/portal/verify/extender/internal/VerifyProcessTracker.java
@@ -14,27 +14,35 @@
 
 package com.liferay.portal.verify.extender.internal;
 
+import aQute.bnd.annotation.metatype.Configurable;
+
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.map.ServiceTrackerMapFactory;
+import com.liferay.osgi.service.tracker.map.ServiceTrackerMapListener;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.RunnableUtil;
+import com.liferay.portal.model.Release;
 import com.liferay.portal.output.stream.container.OutputStreamContainer;
 import com.liferay.portal.output.stream.container.OutputStreamContainerFactory;
 import com.liferay.portal.output.stream.container.OutputStreamContainerFactoryTracker;
+import com.liferay.portal.service.ReleaseLocalService;
 import com.liferay.portal.verify.VerifyException;
 import com.liferay.portal.verify.VerifyProcess;
+import com.liferay.portal.verify.extender.internal.configuration.VerifyProcessTrackerConfiguration;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import java.util.Map;
 import java.util.Set;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
@@ -44,7 +52,8 @@ import org.osgi.service.component.annotations.Reference;
  * @author Carlos Sierra Andrés
  */
 @Component(
-	immediate = true,
+	configurationPid = "com.liferay.portal.verify.extender.internal.configuration.VerifyProcessTrackerConfiguration",
+	configurationPolicy = ConfigurationPolicy.OPTIONAL, immediate = true,
 	property = {
 		"osgi.command.function=execute", "osgi.command.function=executeAll",
 		"osgi.command.function=list", "osgi.command.function=show",
@@ -119,10 +128,29 @@ public class VerifyProcessTracker {
 	}
 
 	@Activate
-	protected void activate(BundleContext bundleContext) {
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		_verifyProcessTrackerConfiguration = Configurable.createConfigurable(
+			VerifyProcessTrackerConfiguration.class, properties);
+
 		try {
+			_releases = ServiceTrackerMapFactory.singleValueMap(
+				bundleContext, Release.class, "release.bundle.symbolic.name");
+
+			_releases.open();
+
+			VerifyServiceTrackerMapListener verifyServiceTrackerMapListener =
+				null;
+
+			if (_verifyProcessTrackerConfiguration.autoVerify()) {
+				verifyServiceTrackerMapListener =
+					new VerifyServiceTrackerMapListener();
+			}
+
 			_verifyProcesses = ServiceTrackerMapFactory.singleValueMap(
-				bundleContext, VerifyProcess.class, "verify.process.name");
+				bundleContext, VerifyProcess.class, "verify.process.name",
+				verifyServiceTrackerMapListener);
 
 			_verifyProcesses.open();
 		}
@@ -142,6 +170,8 @@ public class VerifyProcessTracker {
 
 	@Deactivate
 	protected void deactivate() {
+		_releases.close();
+
 		_verifyProcesses.close();
 	}
 
@@ -220,11 +250,46 @@ public class VerifyProcessTracker {
 			outputStreamContainerFactoryTracker;
 	}
 
+	@Reference
+	protected void setReleaseLocalService(
+		ReleaseLocalService releaseLocalService) {
+
+		_releaseLocalService = releaseLocalService;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		VerifyProcessTracker.class);
 
 	private OutputStreamContainerFactoryTracker
 		_outputStreamContainerFactoryTracker;
+	private ReleaseLocalService _releaseLocalService;
+	private ServiceTrackerMap<String, Release> _releases;
 	private ServiceTrackerMap<String, VerifyProcess> _verifyProcesses;
+	private VerifyProcessTrackerConfiguration
+		_verifyProcessTrackerConfiguration;
+
+	private class VerifyServiceTrackerMapListener
+		implements ServiceTrackerMapListener
+			<String, VerifyProcess, VerifyProcess> {
+
+		@Override
+		public void keyEmitted(
+			ServiceTrackerMap<String, VerifyProcess> serviceTrackerMap,
+			String key, VerifyProcess service, VerifyProcess content) {
+
+			Release release = _releases.getService(key);
+
+			if ((release == null) || release.isVerified()) {
+				return;
+			}
+
+			execute(key);
+
+			release.setVerified(true);
+
+			_releaseLocalService.updateRelease(release);
+		}
+
+	};
 
 }

modules/portal/portal-verify-extender/src/com/liferay/portal/verify/extender/internal/configuration/VerifyProcessTrackerConfiguration.java
@@ -1,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.verify.extender.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Miguel Pastor
+ */
+@Meta.OCD(
+	id = "com.liferay.portal.verify.extender.internal.configuration.VerifyProcessTrackerConfiguration"
+)
+public interface VerifyProcessTrackerConfiguration {
+
+	@Meta.AD(deflt = "true", required = false)
+	public boolean autoVerify();
+
+}

commit 6baa37cd2e68bbdfb3e5aa29564dd3f923a71aa1

LPS-59238 Make sure spring init logic is guarded by log assertor, this is catching the regression caused by e263026634e5f0c6ada4ac8fd3e76740b0236ad1

portal-test-internal/src/com/liferay/portal/test/rule/LiferayIntegrationTestRule.java
@@ -18,14 +18,19 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.BaseTestRule;
 import com.liferay.portal.kernel.test.rule.BaseTestRule.StatementWrapper;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRunTestRule;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.ServerDetector;
+import com.liferay.portal.kernel.util.SystemProperties;
 import com.liferay.portal.test.rule.callback.ClearThreadLocalTestCallback;
+import com.liferay.portal.test.rule.callback.LogAssertionTestCallback;
 import com.liferay.portal.test.rule.callback.UniqueStringRandomizerBumperTestCallback;
 import com.liferay.portal.util.InitUtil;
 import com.liferay.portal.util.PropsUtil;
+import com.liferay.util.log4j.Log4JUtil;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.rules.TestRule;
@@ -66,7 +71,28 @@ public class LiferayIntegrationTestRule extends AggregateTestRule {
 							List<String> configLocations = ListUtil.fromArray(
 								PropsUtil.getArray(PropsKeys.SPRING_CONFIGS));
 
+							boolean configureLog4j = false;
+
+							if (GetterUtil.getBoolean(
+									SystemProperties.get(
+										"log4j.configure.on.startup"),
+									true)) {
+
+								SystemProperties.set(
+									"log4j.configure.on.startup", "false");
+
+								configureLog4j = true;
+							}
+
 							InitUtil.initWithSpring(configLocations, true);
+
+							if (configureLog4j) {
+								Log4JUtil.configureLog4J(
+									InitUtil.class.getClassLoader());
+
+								LogAssertionTestCallback.startAssert(
+									Collections.<ExpectedLogs>emptyList());
+							}
 
 							if (System.getProperty("external-properties") ==
 									null) {