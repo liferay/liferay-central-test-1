brianchandotcom/liferay-portal

LPS-61501 use nicer popovers in the site admin menu

modules/apps/product-navigation/product-navigation-site-administration/src/main/resources/META-INF/resources/sites/site_administration_header.jsp
@@ -24,9 +24,15 @@ PanelCategory panelCategory = siteAdministrationPanelCategoryDisplayContext.getP
 ResourceBundle resourceBundle = ResourceBundleUtil.getBundle("content.Language", locale, getClass());
 %>
 
-<aui:a cssClass="icon-sites" href="javascript:;" id="manageSitesLink" title='<%= LanguageUtil.get(resourceBundle, "go-to-other-site") %>'>
-	<aui:icon image="sites" markupView="lexicon" />
-</aui:a>
+<liferay-ui:icon
+	cssClass="icon-sites"
+	icon="sites"
+	id="manageSitesLink"
+	label="<%= false %>"
+	markupView="lexicon"
+	message='<%= LanguageUtil.get(resourceBundle, "go-to-other-site") %>'
+	url="javascrip:;"
+/>
 
 <div class="hide">
 	<div id="<portlet:namespace/>siteSelectorContent">

liferay/liferay-central-test-1

modules/apps/product-navigation/product-navigation-site-administration/src/main/resources/META-INF/resources/sites/site_administration_header.jsp
@@ -24,9 +24,15 @@ PanelCategory panelCategory = siteAdministrationPanelCategoryDisplayContext.getP
 ResourceBundle resourceBundle = ResourceBundleUtil.getBundle("content.Language", locale, getClass());
 %>
 
-<aui:a cssClass="icon-sites" href="javascript:;" id="manageSitesLink" title='<%= LanguageUtil.get(resourceBundle, "go-to-other-site") %>'>
-	<aui:icon image="sites" markupView="lexicon" />
-</aui:a>
+<liferay-ui:icon
+	cssClass="icon-sites"
+	icon="sites"
+	id="manageSitesLink"
+	label="<%= false %>"
+	markupView="lexicon"
+	message='<%= LanguageUtil.get(resourceBundle, "go-to-other-site") %>'
+	url="javascript:;"
+/>
 
 <div class="hide">
 	<div id="<portlet:namespace/>siteSelectorContent">