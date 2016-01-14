sergiogonzalez/liferay-portal

LPS-61560 Create the item selector urls and pass them to the taglib in blogs

modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/action/EditEntryMVCRenderCommand.java
@@ -15,12 +15,23 @@
 package com.liferay.blogs.web.portlet.action;
 
 import com.liferay.blogs.web.constants.BlogsPortletKeys;
+import com.liferay.item.selector.ItemSelectorHelper;
+import com.liferay.item.selector.ItemSelectorWebKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Sergio González
+ * @author Roberto Díaz
  */
 @Component(
 	immediate = true,
@@ -35,8 +46,32 @@ import org.osgi.service.component.annotations.Component;
 public class EditEntryMVCRenderCommand extends GetEntryMVCRenderCommand {
 
 	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			renderRequest);
+
+		request.setAttribute(
+			ItemSelectorWebKeys.ITEM_SELECTOR_HELPER, _itemSelectorHelper);
+
+		return super.render(renderRequest, renderResponse);
+	}
+
+	@Reference(
+		target = "(javax.portlet.name=" + BlogsPortletKeys.BLOGS + ")",
+		unbind = "-"
+	)
+	public void setItemSelectorHelper(ItemSelectorHelper itemSelectorHelper) {
+		_itemSelectorHelper = itemSelectorHelper;
+	}
+
+	@Override
 	protected String getPath() {
 		return "/blogs/edit_entry.jsp";
 	}
+
+	private volatile ItemSelectorHelper _itemSelectorHelper;
 
 }

modules/apps/blogs/blogs-web/src/main/resources/META-INF/resources/init.jsp
@@ -32,6 +32,7 @@ taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 page import="com.liferay.blogs.web.configuration.BlogsPortletInstanceConfiguration" %><%@
 page import="com.liferay.blogs.web.constants.BlogsPortletKeys" %><%@
 page import="com.liferay.blogs.web.display.context.BlogsPortletInstanceSettingsHelper" %><%@
+page import="com.liferay.item.selector.ItemSelectorWebKeys" %><%@
 page import="com.liferay.portal.kernel.bean.BeanParamUtil" %><%@
 page import="com.liferay.portal.kernel.comment.CommentManagerUtil" %><%@
 page import="com.liferay.portal.kernel.comment.Discussion" %><%@
@@ -127,7 +128,7 @@ page import="com.liferay.portlet.documentlibrary.FileSizeException" %><%@
 page import="com.liferay.portlet.documentlibrary.service.DLAppLocalServiceUtil" %><%@
 page import="com.liferay.portlet.documentlibrary.util.DLUtil" %><%@
 page import="com.liferay.portlet.trash.util.TrashUtil" %><%@
-page import="com.liferay.taglib.search.ResultRow" %>
+page import="com.liferay.taglib.search.ResultRow" %><%@ page import="com.liferay.item.selector.ItemSelectorHelper" %>
 
 <%@ page import="java.text.Format" %>
 

liferay/liferay-central-test-1

modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/portlet/action/EditEntryMVCRenderCommand.java
@@ -14,13 +14,24 @@
 
 package com.liferay.blogs.web.portlet.action;
 
+import com.liferay.blogs.web.BlogsItemSelectorHelper;
 import com.liferay.blogs.web.constants.BlogsPortletKeys;
+import com.liferay.blogs.web.constants.BlogsWebKeys;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.util.PortalUtil;
+
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Sergio González
+ * @author Roberto Díaz
  */
 @Component(
 	immediate = true,
@@ -35,8 +46,31 @@ import org.osgi.service.component.annotations.Component;
 public class EditEntryMVCRenderCommand extends GetEntryMVCRenderCommand {
 
 	@Override
+	public String render(
+			RenderRequest renderRequest, RenderResponse renderResponse)
+		throws PortletException {
+
+		HttpServletRequest request = PortalUtil.getHttpServletRequest(
+			renderRequest);
+
+		request.setAttribute(
+			BlogsWebKeys.BLOGS_ITEM_SELECTOR_HELPER, _blogsItemSelectorHelper);
+
+		return super.render(renderRequest, renderResponse);
+	}
+
+	@Reference(unbind = "-")
+	public void setItemSelectorHelper(
+		BlogsItemSelectorHelper blogsItemSelectorHelper) {
+
+		_blogsItemSelectorHelper = blogsItemSelectorHelper;
+	}
+
+	@Override
 	protected String getPath() {
 		return "/blogs/edit_entry.jsp";
 	}
+
+	private volatile BlogsItemSelectorHelper _blogsItemSelectorHelper;
 
 }

modules/apps/blogs/blogs-web/src/main/resources/META-INF/resources/init.jsp
@@ -29,8 +29,10 @@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
 taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
 <%@ page import="com.liferay.blogs.configuration.BlogsGroupServiceOverriddenConfiguration" %><%@
+page import="com.liferay.blogs.web.BlogsItemSelectorHelper" %><%@
 page import="com.liferay.blogs.web.configuration.BlogsPortletInstanceConfiguration" %><%@
 page import="com.liferay.blogs.web.constants.BlogsPortletKeys" %><%@
+page import="com.liferay.blogs.web.constants.BlogsWebKeys" %><%@
 page import="com.liferay.blogs.web.display.context.BlogsPortletInstanceSettingsHelper" %><%@
 page import="com.liferay.portal.kernel.bean.BeanParamUtil" %><%@
 page import="com.liferay.portal.kernel.comment.CommentManagerUtil" %><%@