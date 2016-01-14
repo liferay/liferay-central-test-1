tomwang2011/liferay-portal

LPS-61864 Use long[] instead of models in UserBagImpl to save memory. Add new methods to avoid fetching models when we only need to check if it's in the UserBag or only need the IDs.

portal-impl/src/com/liferay/portal/security/permission/UserBagImpl.java
@@ -14,52 +14,69 @@
 
 package com.liferay.portal.security.permission;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.Role;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 
-import java.util.Collections;
+import java.lang.reflect.Array;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author László Csontos
+ * @author Preston Crary
  */
 public class UserBagImpl implements UserBag {
 
 	public UserBagImpl(
-		long userId, Set<Group> userGroups, Set<Organization> userOrgs,
-		Set<Group> userOrgGroups, Set<Role> userRoles) {
+		long userId, Collection<Group> userGroups,
+		Collection<Organization> userOrgs, Collection<Group> userOrgGroups,
+		Collection<Role> userRoles) {
 
 		_userId = userId;
-		_userGroups = Collections.unmodifiableSet(userGroups);
-		_userOrgs = Collections.unmodifiableSet(userOrgs);
-		_userOrgGroups = Collections.unmodifiableSet(userOrgGroups);
-		_userRoles = Collections.unmodifiableSet(userRoles);
+		_userGroupIds = _toUniqueSortedLongArray(userGroups);
+		_userOrgIds = _toUniqueSortedLongArray(userOrgs);
+		_userOrgGroupIds = _toUniqueSortedLongArray(userOrgGroups);
+		_userRoleIds = _toUniqueSortedLongArray(userRoles);
 	}
 
 	@Override
-	public Set<Group> getGroups() {
-		if (_groups == null) {
-			_groups = new HashSet<>();
+	public Set<Group> getGroups() throws PortalException {
+		Set<Group> groups = new HashSet<>(getUserGroups());
 
-			_groups.addAll(_userGroups);
-			_groups.addAll(_userOrgGroups);
+		groups.addAll(getUserOrgGroups());
 
-			_groups = Collections.unmodifiableSet(_groups);
-		}
-
-		return _groups;
+		return groups;
 	}
 
 	@Override
-	public Set<Role> getRoles() {
-		return _userRoles;
+	public long[] getRoleIds() {
+		return _userRoleIds.clone();
 	}
 
 	@Override
-	public Set<Group> getUserGroups() {
-		return _userGroups;
+	public List<Role> getRoles() throws PortalException {
+		return RoleLocalServiceUtil.getRoles(_userRoleIds);
+	}
+
+	@Override
+	public long[] getUserGroupIds() {
+		return _userGroupIds.clone();
+	}
+
+	@Override
+	public List<Group> getUserGroups() throws PortalException {
+		return GroupLocalServiceUtil.getGroups(_userGroupIds);
 	}
 
 	@Override
@@ -68,25 +85,81 @@ public class UserBagImpl implements UserBag {
 	}
 
 	@Override
-	public Set<Group> getUserOrgGroups() {
-		return _userOrgGroups;
+	public long[] getUserOrgGroupIds() {
+		return _userOrgGroupIds.clone();
 	}
 
 	@Override
-	public Set<Organization> getUserOrgs() {
-		return _userOrgs;
+	public List<Group> getUserOrgGroups() throws PortalException {
+		return GroupLocalServiceUtil.getGroups(_userOrgGroupIds);
+	}
+
+	@Override
+	public long[] getUserOrgIds() {
+		return _userOrgIds.clone();
+	}
+
+	@Override
+	public List<Organization> getUserOrgs() throws PortalException {
+		return OrganizationLocalServiceUtil.getOrganizations(_userOrgIds);
 	}
 
 	@Override
 	public boolean hasRole(Role role) {
-		return _userRoles.contains(role);
+		return _search(_userRoleIds, role.getRoleId());
 	}
 
-	private Set<Group> _groups;
-	private final Set<Group> _userGroups;
+	@Override
+	public boolean hasUserGroup(Group group) {
+		return _search(_userGroupIds, group.getGroupId());
+	}
+
+	@Override
+	public boolean hasUserOrg(Organization organization) {
+		return _search(_userOrgIds, organization.getOrganizationId());
+	}
+
+	@Override
+	public boolean hasUserOrgGroup(Group group) {
+		return _search(_userOrgGroupIds, group.getGroupId());
+	}
+
+	private boolean _search(long[] ids, long id) {
+		if (Arrays.binarySearch(ids, id) > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private long[] _toUniqueSortedLongArray(
+		Collection<? extends BaseModel> collection) {
+
+		if ((collection == null) || collection.isEmpty()) {
+			return (long[])Array.newInstance(long.class, 0);
+		}
+
+		long[] array = (long[])Array.newInstance(long.class, collection.size());
+
+		int index = 0;
+
+		for (BaseModel baseModel : collection) {
+			array[index] = (long)baseModel.getPrimaryKeyObj();
+
+			index++;
+		}
+
+		array = ArrayUtil.unique(array);
+
+		Arrays.sort(array);
+
+		return array;
+	}
+
+	private final long[] _userGroupIds;
 	private final long _userId;
-	private final Set<Group> _userOrgGroups;
-	private final Set<Organization> _userOrgs;
-	private final Set<Role> _userRoles;
+	private final long[] _userOrgGroupIds;
+	private final long[] _userOrgIds;
+	private final long[] _userRoleIds;
 
 }

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portal/security/permission/UserBagImpl.java
@@ -14,52 +14,69 @@
 
 package com.liferay.portal.security.permission;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.model.BaseModel;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Organization;
 import com.liferay.portal.model.Role;
+import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.OrganizationLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 
-import java.util.Collections;
+import java.lang.reflect.Array;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author László Csontos
+ * @author Preston Crary
  */
 public class UserBagImpl implements UserBag {
 
 	public UserBagImpl(
-		long userId, Set<Group> userGroups, Set<Organization> userOrgs,
-		Set<Group> userOrgGroups, Set<Role> userRoles) {
+		long userId, Collection<Group> userGroups,
+		Collection<Organization> userOrgs, Collection<Group> userOrgGroups,
+		Collection<Role> userRoles) {
 
 		_userId = userId;
-		_userGroups = Collections.unmodifiableSet(userGroups);
-		_userOrgs = Collections.unmodifiableSet(userOrgs);
-		_userOrgGroups = Collections.unmodifiableSet(userOrgGroups);
-		_userRoles = Collections.unmodifiableSet(userRoles);
+		_userGroupIds = _toUniqueSortedLongArray(userGroups);
+		_userOrgIds = _toUniqueSortedLongArray(userOrgs);
+		_userOrgGroupIds = _toUniqueSortedLongArray(userOrgGroups);
+		_userRoleIds = _toUniqueSortedLongArray(userRoles);
 	}
 
 	@Override
-	public Set<Group> getGroups() {
-		if (_groups == null) {
-			_groups = new HashSet<>();
+	public Set<Group> getGroups() throws PortalException {
+		Set<Group> groups = new HashSet<>(getUserGroups());
 
-			_groups.addAll(_userGroups);
-			_groups.addAll(_userOrgGroups);
+		groups.addAll(getUserOrgGroups());
 
-			_groups = Collections.unmodifiableSet(_groups);
-		}
-
-		return _groups;
+		return groups;
 	}
 
 	@Override
-	public Set<Role> getRoles() {
-		return _userRoles;
+	public long[] getRoleIds() {
+		return _userRoleIds.clone();
 	}
 
 	@Override
-	public Set<Group> getUserGroups() {
-		return _userGroups;
+	public List<Role> getRoles() throws PortalException {
+		return RoleLocalServiceUtil.getRoles(_userRoleIds);
+	}
+
+	@Override
+	public long[] getUserGroupIds() {
+		return _userGroupIds.clone();
+	}
+
+	@Override
+	public List<Group> getUserGroups() throws PortalException {
+		return GroupLocalServiceUtil.getGroups(_userGroupIds);
 	}
 
 	@Override
@@ -68,25 +85,81 @@ public class UserBagImpl implements UserBag {
 	}
 
 	@Override
-	public Set<Group> getUserOrgGroups() {
-		return _userOrgGroups;
+	public long[] getUserOrgGroupIds() {
+		return _userOrgGroupIds.clone();
 	}
 
 	@Override
-	public Set<Organization> getUserOrgs() {
-		return _userOrgs;
+	public List<Group> getUserOrgGroups() throws PortalException {
+		return GroupLocalServiceUtil.getGroups(_userOrgGroupIds);
+	}
+
+	@Override
+	public long[] getUserOrgIds() {
+		return _userOrgIds.clone();
+	}
+
+	@Override
+	public List<Organization> getUserOrgs() throws PortalException {
+		return OrganizationLocalServiceUtil.getOrganizations(_userOrgIds);
 	}
 
 	@Override
 	public boolean hasRole(Role role) {
-		return _userRoles.contains(role);
+		return _search(_userRoleIds, role.getRoleId());
 	}
 
-	private Set<Group> _groups;
-	private final Set<Group> _userGroups;
+	@Override
+	public boolean hasUserGroup(Group group) {
+		return _search(_userGroupIds, group.getGroupId());
+	}
+
+	@Override
+	public boolean hasUserOrg(Organization organization) {
+		return _search(_userOrgIds, organization.getOrganizationId());
+	}
+
+	@Override
+	public boolean hasUserOrgGroup(Group group) {
+		return _search(_userOrgGroupIds, group.getGroupId());
+	}
+
+	private boolean _search(long[] ids, long id) {
+		if (Arrays.binarySearch(ids, id) >= 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private long[] _toUniqueSortedLongArray(
+		Collection<? extends BaseModel> baseModels) {
+
+		if ((baseModels == null) || baseModels.isEmpty()) {
+			return (long[])Array.newInstance(long.class, 0);
+		}
+
+		long[] array = (long[])Array.newInstance(long.class, baseModels.size());
+
+		int index = 0;
+
+		for (BaseModel baseModel : baseModels) {
+			array[index] = (long)baseModel.getPrimaryKeyObj();
+
+			index++;
+		}
+
+		array = ArrayUtil.unique(array);
+
+		Arrays.sort(array);
+
+		return array;
+	}
+
+	private final long[] _userGroupIds;
 	private final long _userId;
-	private final Set<Group> _userOrgGroups;
-	private final Set<Organization> _userOrgs;
-	private final Set<Role> _userRoles;
+	private final long[] _userOrgGroupIds;
+	private final long[] _userOrgIds;
+	private final long[] _userRoleIds;
 
 }