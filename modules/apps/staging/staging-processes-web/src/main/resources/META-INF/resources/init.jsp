brianchandotcom/liferay-portal

LPS-61222 Apply changes

modules/apps/staging/staging-processes-web/src/main/resources/META-INF/resources/init.jsp
@@ -29,7 +29,8 @@ taglib uri="http://liferay.com/tld/trash" prefix="liferay-trash" %><%@
 taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
 taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
-<%@ page import="com.liferay.portal.LayoutPrototypeException" %><%@
+<%@ page import="com.liferay.exportimport.util.ExportImportConfigurationNameComparator" %><%@
+page import="com.liferay.portal.LayoutPrototypeException" %><%@
 page import="com.liferay.portal.NoSuchGroupException" %><%@
 page import="com.liferay.portal.NoSuchLayoutException" %><%@
 page import="com.liferay.portal.NoSuchRoleException" %><%@
@@ -50,9 +51,6 @@ page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
 page import="com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil" %><%@
 page import="com.liferay.portal.kernel.scheduler.StorageType" %><%@
 page import="com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse" %><%@
-page import="com.liferay.portal.kernel.search.BaseModelSearchResult" %><%@
-page import="com.liferay.portal.kernel.search.Sort" %><%@
-page import="com.liferay.portal.kernel.search.SortFactoryUtil" %><%@
 page import="com.liferay.portal.kernel.servlet.SessionMessages" %><%@
 page import="com.liferay.portal.kernel.util.ArrayUtil" %><%@
 page import="com.liferay.portal.kernel.util.Constants" %><%@

liferay/liferay-central-test-1

modules/apps/staging/staging-processes-web/src/main/resources/META-INF/resources/init.jsp
@@ -29,7 +29,8 @@ taglib uri="http://liferay.com/tld/trash" prefix="liferay-trash" %><%@
 taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
 taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
-<%@ page import="com.liferay.portal.RemoteOptionsException" %><%@
+<%@ page import="com.liferay.exportimport.util.ExportImportConfigurationNameComparator" %><%@
+page import="com.liferay.portal.RemoteOptionsException" %><%@
 page import="com.liferay.portal.exception.LayoutPrototypeException" %><%@
 page import="com.liferay.portal.exception.NoSuchGroupException" %><%@
 page import="com.liferay.portal.exception.NoSuchLayoutException" %><%@
@@ -50,9 +51,6 @@ page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
 page import="com.liferay.portal.kernel.scheduler.SchedulerEngineHelperUtil" %><%@
 page import="com.liferay.portal.kernel.scheduler.StorageType" %><%@
 page import="com.liferay.portal.kernel.scheduler.messaging.SchedulerResponse" %><%@
-page import="com.liferay.portal.kernel.search.BaseModelSearchResult" %><%@
-page import="com.liferay.portal.kernel.search.Sort" %><%@
-page import="com.liferay.portal.kernel.search.SortFactoryUtil" %><%@
 page import="com.liferay.portal.kernel.servlet.SessionMessages" %><%@
 page import="com.liferay.portal.kernel.util.ArrayUtil" %><%@
 page import="com.liferay.portal.kernel.util.Constants" %><%@