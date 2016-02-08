juliocamarero/liferay-portal

LPS-62765 create view for recent sites

modules/apps/site/site-item-selector-web/src/main/java/com/liferay/site/item/selector/web/RecentSitesItemSelectorView.java
@@ -1,0 +1,147 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.item.selector.web;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.site.item.selector.criterion.SiteItemSelectorCriterion;
+import com.liferay.site.item.selector.web.constants.SitesItemSelectorWebKeys;
+import com.liferay.site.item.selector.web.display.context.RecentSitesItemSelectorViewDisplayContext;
+import com.liferay.site.util.GroupURLProvider;
+import com.liferay.site.util.RecentGroupManager;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(property = {"service.ranking:Integer=300"}, service = ItemSelectorView.class)
+public class RecentSitesItemSelectorView
+	implements ItemSelectorView<SiteItemSelectorCriterion> {
+
+	@Override
+	public Class<SiteItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return SiteItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = PortalUtil.getResourceBundle(locale);
+
+		return ResourceBundleUtil.getString(resourceBundle, "recent");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return false;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			SiteItemSelectorCriterion siteItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		request.setAttribute(
+			SitesItemSelectorWebKeys.SITES_ITEM_SELECTOR_GROUP_URL_PROVIDER,
+			_groupURLProvider);
+
+		RecentSitesItemSelectorViewDisplayContext
+			siteItemSelectorViewDisplayContext =
+				new RecentSitesItemSelectorViewDisplayContext(
+					(HttpServletRequest)request, siteItemSelectorCriterion,
+					itemSelectedEventName, _recentGroupManager);
+
+		request.setAttribute(
+			SitesItemSelectorWebKeys.SITES_ITEM_SELECTOR_DISPLAY_CONTEXT,
+			siteItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/recent_sites.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	@Reference(unbind = "-")
+	public void setGroupURLProvider(GroupURLProvider groupURLProvider) {
+		_groupURLProvider = groupURLProvider;
+	}
+
+	@Reference(unbind = "-")
+	public void setRecentGroupManager(RecentGroupManager recentGroupManager) {
+		_recentGroupManager = recentGroupManager;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.item.selector.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new URLItemSelectorReturnType(),
+					new UUIDItemSelectorReturnType()
+				}));
+
+	private GroupURLProvider _groupURLProvider;
+	private RecentGroupManager _recentGroupManager;
+	private ServletContext _servletContext;
+
+}

liferay/liferay-central-test-1

modules/apps/site/site-item-selector-web/src/main/java/com/liferay/site/item/selector/web/RecentSitesItemSelectorView.java
@@ -1,0 +1,148 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.item.selector.web;
+
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.site.constants.SiteWebKeys;
+import com.liferay.site.item.selector.criterion.SiteItemSelectorCriterion;
+import com.liferay.site.item.selector.web.constants.SitesItemSelectorWebKeys;
+import com.liferay.site.item.selector.web.display.context.RecentSitesItemSelectorViewDisplayContext;
+import com.liferay.site.util.GroupURLProvider;
+import com.liferay.site.util.RecentGroupManager;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Julio Camarero
+ */
+@Component(
+	property = {"service.ranking:Integer=100"}, service = ItemSelectorView.class
+)
+public class RecentSitesItemSelectorView
+	implements ItemSelectorView<SiteItemSelectorCriterion> {
+
+	@Override
+	public Class<SiteItemSelectorCriterion> getItemSelectorCriterionClass() {
+		return SiteItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		ResourceBundle resourceBundle = PortalUtil.getResourceBundle(locale);
+
+		return ResourceBundleUtil.getString(resourceBundle, "recent");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return false;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest request, ServletResponse response,
+			SiteItemSelectorCriterion siteItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		request.setAttribute(SiteWebKeys.GROUP_URL_PROVIDER, _groupURLProvider);
+
+		RecentSitesItemSelectorViewDisplayContext
+			siteItemSelectorViewDisplayContext =
+				new RecentSitesItemSelectorViewDisplayContext(
+					(HttpServletRequest)request, siteItemSelectorCriterion,
+					itemSelectedEventName, _recentGroupManager);
+
+		request.setAttribute(
+			SitesItemSelectorWebKeys.SITES_ITEM_SELECTOR_DISPLAY_CONTEXT,
+			siteItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher("/view_sites.jsp");
+
+		requestDispatcher.include(request, response);
+	}
+
+	@Reference(unbind = "-")
+	public void setGroupURLProvider(GroupURLProvider groupURLProvider) {
+		_groupURLProvider = groupURLProvider;
+	}
+
+	@Reference(unbind = "-")
+	public void setRecentGroupManager(RecentGroupManager recentGroupManager) {
+		_recentGroupManager = recentGroupManager;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.site.item.selector.web)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		_servletContext = servletContext;
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new URLItemSelectorReturnType(),
+					new UUIDItemSelectorReturnType()
+				}));
+
+	private GroupURLProvider _groupURLProvider;
+	private RecentGroupManager _recentGroupManager;
+	private ServletContext _servletContext;
+
+}