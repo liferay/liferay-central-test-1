cdmsantos/liferay-portal

LPS-62488 Add placeholder property to some ddm fields

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/com/liferay/dynamic/data/mapping/dependencies/alloy/textarea.ftl
@@ -2,7 +2,7 @@
 
 <@aui["field-wrapper"] data=data>
 	<div class="form-group">
-		<@aui.input cssClass=cssClass dir=requestedLanguageDir helpMessage=escape(fieldStructure.tip) label=escape(label) name=namespacedFieldName required=required type="textarea" value=fieldValue>
+		<@aui.input cssClass=cssClass dir=requestedLanguageDir helpMessage=escape(fieldStructure.tip) label=escape(label) name=namespacedFieldName placeholder=fieldName required=required type="textarea" value=fieldValue>
 			<#if required>
 				<@aui.validator name="required" />
 			</#if>

liferay/liferay-central-test-1

modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/resources/com/liferay/dynamic/data/mapping/dependencies/alloy/textarea.ftl
@@ -2,7 +2,7 @@
 
 <@liferay_aui["field-wrapper"] data=data>
 	<div class="form-group">
-		<@liferay_aui.input cssClass=cssClass dir=requestedLanguageDir helpMessage=escape(fieldStructure.tip) label=escape(label) name=namespacedFieldName required=required type="textarea" value=fieldValue>
+		<@liferay_aui.input cssClass=cssClass dir=requestedLanguageDir helpMessage=escape(fieldStructure.tip) label=escape(label) name=namespacedFieldName placeholder=fieldName required=required type="textarea" value=fieldValue>
 			<#if required>
 				<@liferay_aui.validator name="required" />
 			</#if>