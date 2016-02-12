mjbradford89/liferay-portal

LPS-60967 - deprecate liferay-ui:flags

readme/7.0/BREAKING_CHANGES.markdown
@@ -2980,3 +2980,29 @@ You should update your code to invoke:
 
 As part of the modularization efforts it has been considered that that this
 logic belongs to wiki-web module.
+
+---------------------------------------
+
+### The liferay-ui:flags has been deprecated and replaced with liferay-frontend:flags
+- **Date:** 2015-Dec-2
+- **JIRA Ticket:** LPS-60967
+
+#### What changed?
+
+The `liferay-ui:flags` taglib has been deprecated and replaced with
+`liferay-frontend:flags` taglib.
+
+#### Who is affected?
+
+Plugins or templates that are using the `liferay-ui:flags` tag need to update
+their usage of the tag.
+
+#### How should I update my code?
+
+You should import the `liferay-frontend` tag library if it isn't already and
+update the tag namespace from `liferay-ui:flags` to `liferay-frontend:flags`.
+
+#### Why was this change made?
+
+This change was made as a part of the ongoing strategy to modularize Liferay
+Portal by means of an OSGi container.

liferay/liferay-central-test-1

readme/7.0/BREAKING_CHANGES.markdown
@@ -3757,6 +3757,8 @@ tag.
 
 #### Why was this change made?
 
+#### Why was this change made?
+
 This change was made as a part of the ongoing strategy to remove deprecated
 tags.
 
@@ -3804,3 +3806,29 @@ There is no direct replacement. You should remove all usages of the `liferay-sec
 #### Why was this change made?
 
 This change was made as a part of the ongoing strategy to deprecate unused tags.
+
+---------------------------------------
+
+### The liferay-ui:flags has been deprecated and replaced with liferay-flags:flags
+- **Date:** 2015-Dec-2
+- **JIRA Ticket:** LPS-60967
+
+#### What changed?
+
+The `liferay-ui:flags` taglib has been deprecated and replaced with
+`liferay-flags:flags` taglib.
+
+#### Who is affected?
+
+Plugins or templates that are using the `liferay-ui:flags` tag need to update
+their usage of the tag.
+
+#### How should I update my code?
+
+You should import the `liferay-flags` tag library if it isn't already and
+update the tag namespace from `liferay-ui:flags` to `liferay-flags:flags`.
+
+#### Why was this change made?
+
+This change was made as a part of the ongoing strategy to modularize Liferay
+Portal by means of an OSGi container.