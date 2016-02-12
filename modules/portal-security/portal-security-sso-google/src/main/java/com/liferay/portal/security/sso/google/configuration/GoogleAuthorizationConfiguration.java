stian-sigvartsen/liferay-portal

LPS-62396 GoogeAuthorizationConfiguration added

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/configuration/GoogleAuthorizationConfiguration.java
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.google.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * Defines the configuration property keys and sensible default values.
+ *
+ * <p>
+ * This class also defines the identity of the configuration schema which, among
+ * other things, defines the filename (minus the <code>.cfg</code> extension)
+ * for setting values via a file.
+ * </p>
+ *
+ * @author Stian Sigvartsen
+ */
+@ExtendedObjectClassDefinition(category = "platform")
+@Meta.OCD(
+	id = "com.liferay.portal.security.sso.google.configuration.GoogleAuthorizationConfiguration",
+	localization = "content/Language", name = "%google.authorization.configuration.name"
+)
+public interface GoogleAuthorizationConfiguration {
+
+	@Meta.AD(
+		deflt = "true",
+		description = "Set this to true to enable Google authentication",
+		required = false
+	)
+	public boolean enabled();
+
+	@Meta.AD(
+		deflt = "",
+		description = "Set this to client ID that Google provided for your registered application",
+		required = false
+	)
+	public String googleClientId();
+
+	@Meta.AD(
+		deflt = "",
+		description = "Set this to client secret that Google provided for your registered application",
+		required = false
+	)
+	public String googleClientSecret();
+
+}

liferay/liferay-central-test-1

modules/portal-security/portal-security-sso-google/src/main/java/com/liferay/portal/security/sso/google/configuration/GoogleAuthorizationConfiguration.java
@@ -1,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.google.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * Defines the configuration property keys and sensible default values.
+ *
+ * <p>
+ * This class also defines the identity of the configuration schema which, among
+ * other things, defines the filename (minus the <code>.cfg</code> extension)
+ * for setting values via a file.
+ * </p>
+ *
+ * @author Stian Sigvartsen
+ */
+@ExtendedObjectClassDefinition(category = "platform")
+@Meta.OCD(
+	id = "com.liferay.portal.security.sso.google.configuration.GoogleAuthorizationConfiguration",
+	localization = "content/Language", name = "%google.authorization.configuration.name"
+)
+public interface GoogleAuthorizationConfiguration {
+
+	@Meta.AD(
+		deflt = "",
+		description = "Set this to client ID that Google provided for your registered application",
+		required = false
+	)
+	public String clientId();
+
+	@Meta.AD(
+		deflt = "",
+		description = "Set this to client secret that Google provided for your registered application",
+		required = false
+	)
+	public String clientSecret();
+
+	@Meta.AD(
+		deflt = "true",
+		description = "Set this to true to enable Google authentication",
+		required = false
+	)
+	public boolean enabled();
+
+}