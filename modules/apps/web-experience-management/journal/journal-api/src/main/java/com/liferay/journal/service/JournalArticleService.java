shuyangzhou/liferay-portal

LPS-62450 Build Service

modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleService.java
@@ -740,6 +740,9 @@ public interface JournalArticleService extends BaseService {
 	public int getArticlesCountByStructureId(long groupId,
 		java.lang.String ddmStructureKey, int status);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<JournalArticle> getArticlesWithLayouts(long groupId);
+
 	/**
 	* Returns the web content article matching the URL title that is currently
 	* displayed or next to be displayed if no article is currently displayed.

modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleServiceUtil.java
@@ -827,6 +827,11 @@ public class JournalArticleServiceUtil {
 			status);
 	}
 
+	public static java.util.List<com.liferay.journal.model.JournalArticle> getArticlesWithLayouts(
+		long groupId) {
+		return getService().getArticlesWithLayouts(groupId);
+	}
+
 	/**
 	* Returns the web content article matching the URL title that is currently
 	* displayed or next to be displayed if no article is currently displayed.

modules/apps/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleServiceWrapper.java
@@ -837,6 +837,12 @@ public class JournalArticleServiceWrapper implements JournalArticleService,
 			ddmStructureKey, status);
 	}
 
+	@Override
+	public java.util.List<com.liferay.journal.model.JournalArticle> getArticlesWithLayouts(
+		long groupId) {
+		return _journalArticleService.getArticlesWithLayouts(groupId);
+	}
+
 	/**
 	* Returns the web content article matching the URL title that is currently
 	* displayed or next to be displayed if no article is currently displayed.

modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/http/JournalArticleServiceSoap.java
@@ -899,6 +899,21 @@ public class JournalArticleServiceSoap {
 		}
 	}
 
+	public static com.liferay.journal.model.JournalArticleSoap[] getArticlesWithLayouts(
+		long groupId) throws RemoteException {
+		try {
+			java.util.List<com.liferay.journal.model.JournalArticle> returnValue =
+				JournalArticleServiceUtil.getArticlesWithLayouts(groupId);
+
+			return com.liferay.journal.model.JournalArticleSoap.toSoapModels(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
 	/**
 	* Returns the web content article matching the URL title that is currently
 	* displayed or next to be displayed if no article is currently displayed.

liferay/liferay-central-test-1

modules/apps/web-experience-management/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleService.java
@@ -958,6 +958,9 @@ public interface JournalArticleService extends BaseService {
 	public JournalArticle getLatestArticle(long resourcePrimKey)
 		throws PortalException;
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<JournalArticle> getLayoutArticles(long groupId);
+
 	/**
 	* Returns the OSGi service identifier.
 	*

modules/apps/web-experience-management/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleServiceUtil.java
@@ -1073,6 +1073,11 @@ public class JournalArticleServiceUtil {
 		return getService().getLatestArticle(resourcePrimKey);
 	}
 
+	public static java.util.List<com.liferay.journal.model.JournalArticle> getLayoutArticles(
+		long groupId) {
+		return getService().getLayoutArticles(groupId);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*

modules/apps/web-experience-management/journal/journal-api/src/main/java/com/liferay/journal/service/JournalArticleServiceWrapper.java
@@ -1095,6 +1095,12 @@ public class JournalArticleServiceWrapper implements JournalArticleService,
 		return _journalArticleService.getLatestArticle(resourcePrimKey);
 	}
 
+	@Override
+	public java.util.List<com.liferay.journal.model.JournalArticle> getLayoutArticles(
+		long groupId) {
+		return _journalArticleService.getLayoutArticles(groupId);
+	}
+
 	/**
 	* Returns the OSGi service identifier.
 	*

modules/apps/web-experience-management/journal/journal-service/src/main/java/com/liferay/journal/service/http/JournalArticleServiceSoap.java
@@ -1245,6 +1245,21 @@ public class JournalArticleServiceSoap {
 		}
 	}
 
+	public static com.liferay.journal.model.JournalArticleSoap[] getLayoutArticles(
+		long groupId) throws RemoteException {
+		try {
+			java.util.List<com.liferay.journal.model.JournalArticle> returnValue =
+				JournalArticleServiceUtil.getLayoutArticles(groupId);
+
+			return com.liferay.journal.model.JournalArticleSoap.toSoapModels(returnValue);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			throw new RemoteException(e.getMessage());
+		}
+	}
+
 	/**
 	* Moves all versions of the web content article matching the group and
 	* article ID to the folder.