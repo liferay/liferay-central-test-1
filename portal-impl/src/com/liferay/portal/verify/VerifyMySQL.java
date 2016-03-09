liferay/liferay-central-test-1

commit cced4854a028124f0670411c01ceb361b541fc44

LPS-59598 Use built in connection unless calling doVerify(Collection<? extends ThrowableAwareRunnable>)

portal-impl/src/com/liferay/portal/verify/VerifyMySQL.java
@@ -16,7 +16,6 @@ package com.liferay.portal.verify;
 
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -24,7 +23,6 @@ import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.util.PropsValues;
 
-import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -47,13 +45,11 @@ public class VerifyMySQL extends VerifyProcess {
 			return;
 		}
 
-		try (Connection connection = DataAccess.getUpgradeOptimizedConnection();
-			Statement statement = connection.createStatement()) {
+		Statement statement = connection.createStatement();
 
-			verifyTableEngine(statement);
+		verifyTableEngine(statement);
 
-			verifyDatetimePrecision(connection.getMetaData(), statement);
-		}
+		verifyDatetimePrecision(connection.getMetaData(), statement);
 	}
 
 	protected String getActualColumnType(

portal-impl/src/com/liferay/portal/verify/VerifyAsset.java
@@ -23,7 +23,6 @@ import com.liferay.portlet.documentlibrary.model.DLFileEntry;
 import com.liferay.portlet.documentlibrary.model.DLFileEntryConstants;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -33,15 +32,12 @@ import java.sql.ResultSet;
 public class VerifyAsset extends VerifyProcess {
 
 	protected void deleteOrphanedAssetEntries() throws Exception {
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
 			long classNameId = PortalUtil.getClassNameId(
 				DLFileEntryConstants.getClassName());
-
-			con = DataAccess.getUpgradeOptimizedConnection();
 
 			StringBundler sb = new StringBundler(5);
 
@@ -51,7 +47,7 @@ public class VerifyAsset extends VerifyProcess {
 			sb.append(" and classPK not in (select fileVersionId from ");
 			sb.append("DLFileVersion)");
 
-			ps = con.prepareStatement(sb.toString());
+			ps = connection.prepareStatement(sb.toString());
 
 			rs = ps.executeQuery();
 
@@ -68,7 +64,7 @@ public class VerifyAsset extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 
@@ -79,14 +75,11 @@ public class VerifyAsset extends VerifyProcess {
 	}
 
 	protected void rebuildTree() throws Exception {
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"select distinct groupId from AssetCategory where " +
 					"(leftCategoryId is null) or (rightCategoryId is null)");
 
@@ -99,7 +92,7 @@ public class VerifyAsset extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyAuditedModel.java
@@ -101,16 +101,13 @@ public class VerifyAuditedModel extends VerifyProcess {
 	}
 
 	protected Object[] getAuditedModelArray(
-			String tableName, String pkColumnName, long primKey)
+			Connection con, String tableName, String pkColumnName, long primKey)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
 			ps = con.prepareStatement(
 				"select companyId, userId, createDate, modifiedDate from " +
 					tableName + " where " + pkColumnName + " = ?");
@@ -126,7 +123,7 @@ public class VerifyAuditedModel extends VerifyProcess {
 				Timestamp modifiedDate = rs.getTimestamp("modifiedDate");
 
 				return new Object[] {
-					companyId, userId, getUserName(userId), createDate,
+					companyId, userId, getUserName(con, userId), createDate,
 					modifiedDate
 				};
 			}
@@ -138,7 +135,7 @@ public class VerifyAuditedModel extends VerifyProcess {
 			return null;
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 
@@ -185,14 +182,11 @@ public class VerifyAuditedModel extends VerifyProcess {
 		}
 	}
 
-	protected String getUserName(long userId) throws Exception {
-		Connection con = null;
+	protected String getUserName(Connection con, long userId) throws Exception {
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
 			ps = con.prepareStatement(
 				"select firstName, middleName, lastName from User_ where " +
 					"userId = ?");
@@ -216,21 +210,18 @@ public class VerifyAuditedModel extends VerifyProcess {
 			return StringPool.BLANK;
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 
 	protected void verifyAuditedModel(
-			String tableName, String primaryKeyColumnName, long primKey,
-			Object[] auditedModelArray, boolean updateDates)
+			Connection con, String tableName, String primaryKeyColumnName,
+			long primKey, Object[] auditedModelArray, boolean updateDates)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
 			long companyId = (Long)auditedModelArray[0];
 
 			if (auditedModelArray[2] == null) {
@@ -283,7 +274,7 @@ public class VerifyAuditedModel extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps);
+			DataAccess.cleanUp(ps);
 		}
 	}
 
@@ -291,13 +282,10 @@ public class VerifyAuditedModel extends VerifyProcess {
 			VerifiableAuditedModel verifiableAuditedModel)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
-		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
+		try (Connection con = DataAccess.getUpgradeOptimizedConnection()) {
 			StringBundler sb = new StringBundler(8);
 
 			sb.append("select ");
@@ -331,7 +319,7 @@ public class VerifyAuditedModel extends VerifyProcess {
 						verifiableAuditedModel.getJoinByTableName());
 
 					auditedModelArray = getAuditedModelArray(
-						verifiableAuditedModel.getRelatedModelName(),
+						con, verifiableAuditedModel.getRelatedModelName(),
 						verifiableAuditedModel.getRelatedPKColumnName(),
 						relatedPrimKey);
 				}
@@ -346,13 +334,13 @@ public class VerifyAuditedModel extends VerifyProcess {
 				}
 
 				verifyAuditedModel(
-					verifiableAuditedModel.getTableName(),
+					con, verifiableAuditedModel.getTableName(),
 					verifiableAuditedModel.getPrimaryKeyColumnName(), primKey,
 					auditedModelArray, verifiableAuditedModel.isUpdateDates());
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyCalendar.java
@@ -21,7 +21,6 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -46,13 +45,10 @@ public class VerifyCalendar extends VerifyProcess {
 	protected void updateEvent(long eventId, String recurrence)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"update CalEvent set recurrence = ? where eventId = ?");
 
 			ps.setString(1, recurrence);
@@ -61,7 +57,7 @@ public class VerifyCalendar extends VerifyProcess {
 			ps.executeUpdate();
 		}
 		finally {
-			DataAccess.cleanUp(con, ps);
+			DataAccess.cleanUp(ps);
 		}
 	}
 
@@ -106,14 +102,11 @@ public class VerifyCalendar extends VerifyProcess {
 
 		jsonSerializer.registerDefaultSerializers();
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"select eventId, recurrence from CalEvent where (CAST_TEXT(" +
 					"recurrence) != '') and recurrence not like " +
 						"'%serializable%'");
@@ -137,7 +130,7 @@ public class VerifyCalendar extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyDB2.java
@@ -19,7 +19,6 @@ import com.liferay.portal.kernel.dao.db.DBFactoryUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.util.StringBundler;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -38,13 +37,10 @@ public class VerifyDB2 extends VerifyProcess {
 			return;
 		}
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
 			StringBundler sb = new StringBundler(4);
 
 			sb.append("select tbname, name, coltype, length from ");
@@ -52,7 +48,7 @@ public class VerifyDB2 extends VerifyProcess {
 			sb.append("current schema from sysibm.sysschemata) AND coltype = ");
 			sb.append("'VARCHAR' and length = 500");
 
-			ps = con.prepareStatement(sb.toString());
+			ps = connection.prepareStatement(sb.toString());
 
 			rs = ps.executeQuery();
 
@@ -71,7 +67,7 @@ public class VerifyDB2 extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyGroup.java
@@ -40,7 +40,6 @@ import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PortalUtil;
 import com.liferay.portal.util.RobotsUtil;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -84,13 +83,10 @@ public class VerifyGroup extends VerifyProcess {
 	}
 
 	protected void updateName(long groupId, String name) throws Exception {
-		Connection con = null;
 		PreparedStatement ps = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"update Group_ set name = ? where groupId= " + groupId);
 
 			ps.setString(1, name);
@@ -98,7 +94,7 @@ public class VerifyGroup extends VerifyProcess {
 			ps.executeUpdate();
 		}
 		finally {
-			DataAccess.cleanUp(con, ps);
+			DataAccess.cleanUp(ps);
 		}
 	}
 
@@ -161,13 +157,10 @@ public class VerifyGroup extends VerifyProcess {
 	}
 
 	protected void verifyOrganizationNames() throws Exception {
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
 			StringBundler sb = new StringBundler(5);
 
 			sb.append("select groupId, name from Group_ where name like '%");
@@ -176,7 +169,7 @@ public class VerifyGroup extends VerifyProcess {
 			sb.append(GroupLocalServiceImpl.ORGANIZATION_NAME_SUFFIX);
 			sb.append("'");
 
-			ps = con.prepareStatement(sb.toString());
+			ps = connection.prepareStatement(sb.toString());
 
 			rs = ps.executeQuery();
 
@@ -205,7 +198,7 @@ public class VerifyGroup extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyGroupedModel.java
@@ -100,14 +100,11 @@ public class VerifyGroupedModel extends VerifyProcess {
 			String tableName, String primaryKeColumnName, long primKey)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"select groupId from " + tableName + " where " +
 					primaryKeColumnName + " = ?");
 
@@ -126,7 +123,7 @@ public class VerifyGroupedModel extends VerifyProcess {
 			return 0;
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 
@@ -134,13 +131,10 @@ public class VerifyGroupedModel extends VerifyProcess {
 			VerifiableGroupedModel verifiableGroupedModel)
 		throws Exception {
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
-		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
+		try (Connection con = DataAccess.getUpgradeOptimizedConnection()) {
 			StringBundler sb = new StringBundler(7);
 
 			sb.append("select ");
@@ -181,11 +175,11 @@ public class VerifyGroupedModel extends VerifyProcess {
 				sb.append(" = ");
 				sb.append(primKey);
 
-				runSQL(sb.toString());
+				runSQL(con, sb.toString());
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

portal-impl/src/com/liferay/portal/verify/VerifyOracle.java
@@ -23,7 +23,6 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ReleaseInfo;
 import com.liferay.portal.kernel.util.StringBundler;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -37,14 +36,11 @@ public class VerifyOracle extends VerifyProcess {
 	protected void alterVarchar2Columns() throws Exception {
 		int buildNumber = getBuildNumber();
 
-		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 
 		try {
-			con = DataAccess.getUpgradeOptimizedConnection();
-
-			ps = con.prepareStatement(
+			ps = connection.prepareStatement(
 				"select table_name, column_name, data_length from " +
 					"user_tab_columns where data_type = 'VARCHAR2' and " +
 						"char_used = 'B'");
@@ -104,7 +100,7 @@ public class VerifyOracle extends VerifyProcess {
 			}
 		}
 		finally {
-			DataAccess.cleanUp(con, ps, rs);
+			DataAccess.cleanUp(null, ps, rs);
 		}
 	}
 

commit 5b9bcebaa5c71bd31b89bb2ddbe216cf23adc471

LPS-63617 Statement must be closed, caused by cced4854a028124f0670411c01ceb361b541fc44

portal-impl/src/com/liferay/portal/verify/VerifyMySQL.java
@@ -74,9 +74,9 @@ public class VerifyMySQL extends VerifyProcess {
 
 	protected void verifyDatetimePrecision() throws Exception {
 		DatabaseMetaData databaseMetaData = connection.getMetaData();
-		Statement statement = connection.createStatement();
 
 		try (LoggingTimer loggingTimer = new LoggingTimer();
+			Statement statement = connection.createStatement();
 			ResultSet rs = databaseMetaData.getTables(
 				null, null, null, null)) {
 
@@ -132,9 +132,8 @@ public class VerifyMySQL extends VerifyProcess {
 	}
 
 	protected void verifyTableEngine() throws Exception {
-		Statement statement = connection.createStatement();
-
 		try (LoggingTimer loggingTimer = new LoggingTimer();
+			Statement statement = connection.createStatement();
 			ResultSet rs = statement.executeQuery("show table status")) {
 
 			while (rs.next()) {