brianchandotcom/liferay-portal

LPS-57200 This feels safer

portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
@@ -265,7 +265,8 @@ public class AssetUtil {
 		if (groupId > 0) {
 			Group group = GroupLocalServiceUtil.fetchGroup(groupId);
 
-			liferayPortletRequest.setAttribute(WebKeys.GROUP, group);
+			liferayPortletRequest.setAttribute(
+				WebKeys.ASSET_RENDERER_GROUP, group);
 		}
 
 		PortletURL addPortletURL = assetRendererFactory.getURLAdd(

portal-kernel/src/com/liferay/asset/kernel/model/BaseAssetRendererFactory.java
@@ -18,6 +18,7 @@ import com.liferay.asset.kernel.NoSuchClassTypeException;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
@@ -29,6 +30,7 @@ import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -330,6 +332,11 @@ public abstract class BaseAssetRendererFactory<T>
 		throws PortalException {
 
 		return PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
+	}
+
+	protected Group getGroup(LiferayPortletRequest liferayPortletRequest) {
+		return (Group)liferayPortletRequest.getAttribute(
+			WebKeys.ASSET_RENDERER_GROUP);
 	}
 
 	protected void setCategorizable(boolean categorizable) {

portal-kernel/src/com/liferay/portal/kernel/util/WebKeys.java
@@ -43,6 +43,8 @@ public interface WebKeys {
 
 	public static final String ASSET_RENDERER_FACTORY_CLASS_TYPE_ID = "ASSET_RENDERER_FACTORY_CLASS_TYPE_ID";
 
+	public static final String ASSET_RENDERER_GROUP = "ASSET_RENDERER_GROUP";
+
 	public static final String ASSET_TAG = "ASSET_TAG";
 
 	public static final String ASSET_VOCABULARIES = "ASSET_VOCABULARIES";

liferay/liferay-central-test-1

portal-impl/src/com/liferay/portlet/asset/util/AssetUtil.java
@@ -265,7 +265,8 @@ public class AssetUtil {
 		if (groupId > 0) {
 			Group group = GroupLocalServiceUtil.fetchGroup(groupId);
 
-			liferayPortletRequest.setAttribute(WebKeys.GROUP, group);
+			liferayPortletRequest.setAttribute(
+				WebKeys.ASSET_RENDERER_FACTORY_GROUP, group);
 		}
 
 		PortletURL addPortletURL = assetRendererFactory.getURLAdd(

portal-kernel/src/com/liferay/asset/kernel/model/BaseAssetRendererFactory.java
@@ -18,6 +18,7 @@ import com.liferay.asset.kernel.NoSuchClassTypeException;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
@@ -29,6 +30,7 @@ import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.Tuple;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -330,6 +332,11 @@ public abstract class BaseAssetRendererFactory<T>
 		throws PortalException {
 
 		return PortalUtil.getControlPanelPlid(themeDisplay.getCompanyId());
+	}
+
+	protected Group getGroup(LiferayPortletRequest liferayPortletRequest) {
+		return (Group)liferayPortletRequest.getAttribute(
+			WebKeys.ASSET_RENDERER_FACTORY_GROUP);
 	}
 
 	protected void setCategorizable(boolean categorizable) {

portal-kernel/src/com/liferay/portal/kernel/util/WebKeys.java
@@ -43,6 +43,8 @@ public interface WebKeys {
 
 	public static final String ASSET_RENDERER_FACTORY_CLASS_TYPE_ID = "ASSET_RENDERER_FACTORY_CLASS_TYPE_ID";
 
+	public static final String ASSET_RENDERER_FACTORY_GROUP = "ASSET_RENDERER_FACTORY_GROUP";
+
 	public static final String ASSET_TAG = "ASSET_TAG";
 
 	public static final String ASSET_VOCABULARIES = "ASSET_VOCABULARIES";