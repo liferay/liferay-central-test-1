brianchandotcom/liferay-portal

LPS-61420 Non-private methods should not start with underscore

modules/apps/foundation/portal-configuration/portal-configuration-settings/src/main/java/com/liferay/portal/configuration/settings/ConfigurationBeanManagedService.java
@@ -86,11 +86,11 @@ public class ConfigurationBeanManagedService implements ManagedService {
 				new UpdatePrivilegedAction(properties));
 		}
 		else {
-			_updated(properties);
+			updated(properties);
 		}
 	}
 
-	protected void _updated(Dictionary<String, ?> properties) {
+	protected void updated(Dictionary<String, ?> properties) {
 		if (properties == null) {
 			properties = new HashMapDictionary<>();
 		}
@@ -115,7 +115,7 @@ public class ConfigurationBeanManagedService implements ManagedService {
 
 		@Override
 		public Void run() {
-			_updated(_properties);
+			updated(_properties);
 
 			return null;
 		}

liferay/liferay-central-test-1

modules/apps/foundation/portal-configuration/portal-configuration-settings/src/main/java/com/liferay/portal/configuration/settings/ConfigurationBeanManagedService.java
@@ -86,11 +86,11 @@ public class ConfigurationBeanManagedService implements ManagedService {
 				new UpdatePrivilegedAction(properties));
 		}
 		else {
-			_updated(properties);
+			doUdated(properties);
 		}
 	}
 
-	protected void _updated(Dictionary<String, ?> properties) {
+	protected void doUdated(Dictionary<String, ?> properties) {
 		if (properties == null) {
 			properties = new HashMapDictionary<>();
 		}
@@ -115,7 +115,7 @@ public class ConfigurationBeanManagedService implements ManagedService {
 
 		@Override
 		public Void run() {
-			_updated(_properties);
+			doUdated(_properties);
 
 			return null;
 		}