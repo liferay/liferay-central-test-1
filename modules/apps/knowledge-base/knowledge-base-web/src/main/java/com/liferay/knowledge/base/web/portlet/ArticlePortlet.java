sergiogonzalez/liferay-portal

LPS-64277 Make portlets DS components

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/ArticlePortlet.java
@@ -15,10 +15,11 @@
 package com.liferay.knowledge.base.web.portlet;
 
 import com.liferay.knowledge.base.constants.ActionKeys;
+import com.liferay.knowledge.base.constants.PortletKeys;
 import com.liferay.knowledge.base.exception.NoSuchArticleException;
 import com.liferay.knowledge.base.exception.NoSuchCommentException;
 import com.liferay.knowledge.base.model.KBArticle;
-import com.liferay.knowledge.base.service.KBArticleServiceUtil;
+import com.liferay.knowledge.base.service.KBArticleService;
 import com.liferay.knowledge.base.service.permission.KBArticlePermission;
 import com.liferay.knowledge.base.web.constants.WebKeys;
 import com.liferay.portal.kernel.exception.NoSuchSubscriptionException;
@@ -29,22 +30,52 @@ import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Peter Shin
  * @author Brian Wing Shun Chan
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=knowledge-base-portlet knowledge-base-portlet-article",
+		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.header-portlet-css=/admin/css/common.css,/article/css/main.css",
+		"com.liferay.portlet.instanciable=true",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Knowledge Base Article",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-send-redirect=true",
+		"javax.portlet.init-param.config-template=/article/configuration.jsp",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/article/",
+		"javax.portlet.init-param.view-template=/article/view.jsp",
+		"javax.portlet.name=" + PortletKeys.KNOWLEDGE_BASE_ARTICLE,
+		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-article-portlet-preferences.xml",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supported-public-render-parameter=categoryId",
+		"javax.portlet.supported-public-render-parameter=tag",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class ArticlePortlet extends BaseKBPortlet {
 
 	@Override
@@ -62,7 +93,7 @@ public class ArticlePortlet extends BaseKBPortlet {
 			long resourcePrimKey = getResourcePrimKey(renderRequest);
 
 			if (resourcePrimKey > 0) {
-				kbArticle = KBArticleServiceUtil.getLatestKBArticle(
+				kbArticle = _kbArticleService.getLatestKBArticle(
 					resourcePrimKey, status);
 			}
 
@@ -77,7 +108,7 @@ public class ArticlePortlet extends BaseKBPortlet {
 
 				SessionMessages.add(
 					renderRequest,
-					PortalUtil.getPortletId(renderRequest) +
+					_portal.getPortletId(renderRequest) +
 						SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
 			}
 			else {
@@ -194,5 +225,18 @@ public class ArticlePortlet extends BaseKBPortlet {
 
 		return WorkflowConstants.STATUS_APPROVED;
 	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleService(KBArticleService kbArticleService) {
+		_kbArticleService = kbArticleService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		_portal = portal;
+	}
+
+	private KBArticleService _kbArticleService;
+	private Portal _portal;
 
 }

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/SectionPortlet.java
@@ -16,11 +16,12 @@ package com.liferay.knowledge.base.web.portlet;
 
 import com.liferay.knowledge.base.constants.ActionKeys;
 import com.liferay.knowledge.base.constants.KBFolderConstants;
+import com.liferay.knowledge.base.constants.PortletKeys;
 import com.liferay.knowledge.base.exception.NoSuchArticleException;
 import com.liferay.knowledge.base.exception.NoSuchCommentException;
 import com.liferay.knowledge.base.model.KBArticle;
-import com.liferay.knowledge.base.service.KBArticleLocalServiceUtil;
-import com.liferay.knowledge.base.service.KBArticleServiceUtil;
+import com.liferay.knowledge.base.service.KBArticleLocalService;
+import com.liferay.knowledge.base.service.KBArticleService;
 import com.liferay.knowledge.base.service.permission.KBArticlePermission;
 import com.liferay.knowledge.base.web.constants.WebKeys;
 import com.liferay.portal.kernel.exception.NoSuchSubscriptionException;
@@ -31,20 +32,50 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.IOException;
 
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Peter Shin
  * @author Brian Wing Shun Chan
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=knowledge-base-portlet knowledge-base-portlet-section",
+		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.header-portlet-css=/admin/css/common.css,/section/css/main.css",
+		"com.liferay.portlet.icon=/icons/section.png",
+		"com.liferay.portlet.instanciable=true",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Knowledge Base Section",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-send-redirect=true",
+		"javax.portlet.init-param.config-template=/section/configuration.jsp",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/section/",
+		"javax.portlet.init-param.view-template=/section/view.jsp",
+		"javax.portlet.name=" + PortletKeys.KNOWLEDGE_BASE_SECTION,
+		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-section-portlet-preferences.xml",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supported-public-render-parameter=categoryId",
+		"javax.portlet.supported-public-render-parameter=tag",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SectionPortlet extends BaseKBPortlet {
 
 	@Override
@@ -104,7 +135,7 @@ public class SectionPortlet extends BaseKBPortlet {
 			renderRequest, "resourcePrimKey");
 
 		if (resourcePrimKey > 0) {
-			return KBArticleServiceUtil.getLatestKBArticle(
+			return _kbArticleService.getLatestKBArticle(
 				resourcePrimKey, status);
 		}
 
@@ -114,17 +145,17 @@ public class SectionPortlet extends BaseKBPortlet {
 			return null;
 		}
 
-		long groupId = PortalUtil.getScopeGroupId(renderRequest);
+		long groupId = _portal.getScopeGroupId(renderRequest);
 
 		String kbFolderUrlTitle = ParamUtil.getString(
 			renderRequest, "kbFolderUrlTitle");
 
 		if (Validator.isNotNull(kbFolderUrlTitle)) {
-			return KBArticleLocalServiceUtil.getKBArticleByUrlTitle(
+			return _kbArticleLocalService.getKBArticleByUrlTitle(
 				groupId, kbFolderUrlTitle, urlTitle);
 		}
 
-		return KBArticleLocalServiceUtil.getKBArticleByUrlTitle(
+		return _kbArticleLocalService.getKBArticleByUrlTitle(
 			groupId, KBFolderConstants.DEFAULT_PARENT_FOLDER_ID, urlTitle);
 	}
 
@@ -162,5 +193,26 @@ public class SectionPortlet extends BaseKBPortlet {
 
 		return WorkflowConstants.STATUS_APPROVED;
 	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleLocalService(
+		KBArticleLocalService kbArticleLocalService) {
+
+		_kbArticleLocalService = kbArticleLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleService(KBArticleService kbArticleService) {
+		_kbArticleService = kbArticleService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		_portal = portal;
+	}
+
+	private KBArticleLocalService _kbArticleLocalService;
+	private KBArticleService _kbArticleService;
+	private Portal _portal;
 
 }

liferay/liferay-central-test-1

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/ArticlePortlet.java
@@ -15,10 +15,11 @@
 package com.liferay.knowledge.base.web.portlet;
 
 import com.liferay.knowledge.base.constants.ActionKeys;
+import com.liferay.knowledge.base.constants.PortletKeys;
 import com.liferay.knowledge.base.exception.NoSuchArticleException;
 import com.liferay.knowledge.base.exception.NoSuchCommentException;
 import com.liferay.knowledge.base.model.KBArticle;
-import com.liferay.knowledge.base.service.KBArticleServiceUtil;
+import com.liferay.knowledge.base.service.KBArticleService;
 import com.liferay.knowledge.base.service.permission.KBArticlePermission;
 import com.liferay.knowledge.base.web.constants.WebKeys;
 import com.liferay.portal.kernel.exception.NoSuchSubscriptionException;
@@ -29,22 +30,53 @@ import com.liferay.portal.kernel.servlet.SessionMessages;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.IOException;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletPreferences;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
 /**
  * @author Peter Shin
  * @author Brian Wing Shun Chan
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.css-class-wrapper=knowledge-base-portlet knowledge-base-portlet-article",
+		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.header-portlet-css=/admin/css/common.css,/article/css/main.css",
+		"com.liferay.portlet.icon=/icons/article.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Knowledge Base Article",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-send-redirect=true",
+		"javax.portlet.init-param.config-template=/article/configuration.jsp",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/article/",
+		"javax.portlet.init-param.view-template=/article/view.jsp",
+		"javax.portlet.name=" + PortletKeys.KNOWLEDGE_BASE_ARTICLE,
+		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-article-portlet-preferences.xml",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supported-public-render-parameter=categoryId",
+		"javax.portlet.supported-public-render-parameter=tag",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class ArticlePortlet extends BaseKBPortlet {
 
 	@Override
@@ -62,7 +94,7 @@ public class ArticlePortlet extends BaseKBPortlet {
 			long resourcePrimKey = getResourcePrimKey(renderRequest);
 
 			if (resourcePrimKey > 0) {
-				kbArticle = KBArticleServiceUtil.getLatestKBArticle(
+				kbArticle = _kbArticleService.getLatestKBArticle(
 					resourcePrimKey, status);
 			}
 
@@ -77,7 +109,7 @@ public class ArticlePortlet extends BaseKBPortlet {
 
 				SessionMessages.add(
 					renderRequest,
-					PortalUtil.getPortletId(renderRequest) +
+					_portal.getPortletId(renderRequest) +
 						SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
 			}
 			else {
@@ -194,5 +226,18 @@ public class ArticlePortlet extends BaseKBPortlet {
 
 		return WorkflowConstants.STATUS_APPROVED;
 	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleService(KBArticleService kbArticleService) {
+		_kbArticleService = kbArticleService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		_portal = portal;
+	}
+
+	private KBArticleService _kbArticleService;
+	private Portal _portal;
 
 }

modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/portlet/SectionPortlet.java
@@ -16,11 +16,12 @@ package com.liferay.knowledge.base.web.portlet;
 
 import com.liferay.knowledge.base.constants.ActionKeys;
 import com.liferay.knowledge.base.constants.KBFolderConstants;
+import com.liferay.knowledge.base.constants.PortletKeys;
 import com.liferay.knowledge.base.exception.NoSuchArticleException;
 import com.liferay.knowledge.base.exception.NoSuchCommentException;
 import com.liferay.knowledge.base.model.KBArticle;
-import com.liferay.knowledge.base.service.KBArticleLocalServiceUtil;
-import com.liferay.knowledge.base.service.KBArticleServiceUtil;
+import com.liferay.knowledge.base.service.KBArticleLocalService;
+import com.liferay.knowledge.base.service.KBArticleService;
 import com.liferay.knowledge.base.service.permission.KBArticlePermission;
 import com.liferay.knowledge.base.web.constants.WebKeys;
 import com.liferay.portal.kernel.exception.NoSuchSubscriptionException;
@@ -31,20 +32,50 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.io.IOException;
 
+import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Peter Shin
  * @author Brian Wing Shun Chan
  */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.css-class-wrapper=knowledge-base-portlet knowledge-base-portlet-section",
+		"com.liferay.portlet.display-category=category.cms",
+		"com.liferay.portlet.header-portlet-css=/admin/css/common.css,/section/css/main.css",
+		"com.liferay.portlet.icon=/icons/section.png",
+		"com.liferay.portlet.instanceable=true",
+		"com.liferay.portlet.scopeable=true",
+		"javax.portlet.display-name=Knowledge Base Section",
+		"javax.portlet.expiration-cache=0",
+		"javax.portlet.init-param.always-send-redirect=true",
+		"javax.portlet.init-param.config-template=/section/configuration.jsp",
+		"javax.portlet.init-param.copy-request-parameters=true",
+		"javax.portlet.init-param.template-path=/section/",
+		"javax.portlet.init-param.view-template=/section/view.jsp",
+		"javax.portlet.name=" + PortletKeys.KNOWLEDGE_BASE_SECTION,
+		"javax.portlet.preferences=classpath:/META-INF/portlet-preferences/default-section-portlet-preferences.xml",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=administrator,guest,power-user,user",
+		"javax.portlet.supported-public-render-parameter=categoryId",
+		"javax.portlet.supported-public-render-parameter=tag",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
 public class SectionPortlet extends BaseKBPortlet {
 
 	@Override
@@ -104,7 +135,7 @@ public class SectionPortlet extends BaseKBPortlet {
 			renderRequest, "resourcePrimKey");
 
 		if (resourcePrimKey > 0) {
-			return KBArticleServiceUtil.getLatestKBArticle(
+			return _kbArticleService.getLatestKBArticle(
 				resourcePrimKey, status);
 		}
 
@@ -114,17 +145,17 @@ public class SectionPortlet extends BaseKBPortlet {
 			return null;
 		}
 
-		long groupId = PortalUtil.getScopeGroupId(renderRequest);
+		long groupId = _portal.getScopeGroupId(renderRequest);
 
 		String kbFolderUrlTitle = ParamUtil.getString(
 			renderRequest, "kbFolderUrlTitle");
 
 		if (Validator.isNotNull(kbFolderUrlTitle)) {
-			return KBArticleLocalServiceUtil.getKBArticleByUrlTitle(
+			return _kbArticleLocalService.getKBArticleByUrlTitle(
 				groupId, kbFolderUrlTitle, urlTitle);
 		}
 
-		return KBArticleLocalServiceUtil.getKBArticleByUrlTitle(
+		return _kbArticleLocalService.getKBArticleByUrlTitle(
 			groupId, KBFolderConstants.DEFAULT_PARENT_FOLDER_ID, urlTitle);
 	}
 
@@ -162,5 +193,26 @@ public class SectionPortlet extends BaseKBPortlet {
 
 		return WorkflowConstants.STATUS_APPROVED;
 	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleLocalService(
+		KBArticleLocalService kbArticleLocalService) {
+
+		_kbArticleLocalService = kbArticleLocalService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setKBArticleService(KBArticleService kbArticleService) {
+		_kbArticleService = kbArticleService;
+	}
+
+	@Reference(unbind = "-")
+	protected void setPortal(Portal portal) {
+		_portal = portal;
+	}
+
+	private KBArticleLocalService _kbArticleLocalService;
+	private KBArticleService _kbArticleService;
+	private Portal _portal;
 
 }