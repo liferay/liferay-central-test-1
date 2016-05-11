mjbradford89/liferay-portal

LPS-65551 - if the dom is still loading, attach handler to init SPA on DOMContentLoaded

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/init.es.js
@@ -8,71 +8,82 @@ import RenderURLScreen from './screen/RenderURLScreen.es';
 import Uri from 'metal-uri/src/Uri';
 import utils from 'senna/src/utils/utils';
 
-let app = new App();
+var initSPA = function() {
+	let app = new App();
 
-app.addRoutes(
-	[
-		{
-			handler: ActionURLScreen,
-			path: function(url) {
-				var uri = new Uri(url);
+	app.addRoutes(
+		[
+			{
+				handler: ActionURLScreen,
+				path: function(url) {
+					var uri = new Uri(url);
 
-				var loginRedirect = new Uri(Liferay.SPA.loginRedirect);
+					var loginRedirect = new Uri(Liferay.SPA.loginRedirect);
 
-				var hostname = loginRedirect.getHostname() || window.location.hostname;
+					var hostname = loginRedirect.getHostname() || window.location.hostname;
 
-				if (!app.isLinkSameOrigin_(hostname)) {
-					return false;
+					if (!app.isLinkSameOrigin_(hostname)) {
+						return false;
+					}
+
+					return uri.getParameterValue('p_p_lifecycle') === '1';
 				}
+			},
+			{
+				handler: RenderURLScreen,
+				path: function(url) {
+					if (url.indexOf(themeDisplay.getPathMain()) === 0) {
+						return false;
+					}
 
-				return uri.getParameterValue('p_p_lifecycle') === '1';
-			}
-		},
-		{
-			handler: RenderURLScreen,
-			path: function(url) {
-				if (url.indexOf(themeDisplay.getPathMain()) === 0) {
-					return false;
+					var excluded = Liferay.SPA.excludedPaths.find((excludedPath) => url.indexOf(excludedPath) === 0);
+
+					if (excluded !== undefined) {
+						return false;
+					}
+
+					var uri = new Uri(url);
+
+					var lifecycle = uri.getParameterValue('p_p_lifecycle');
+
+					return lifecycle === '0' || !lifecycle;
 				}
-
-				var excluded = Liferay.SPA.excludedPaths.find((excludedPath) => url.indexOf(excludedPath) === 0);
-
-				if (excluded !== undefined) {
-					return false;
-				}
-
-				var uri = new Uri(url);
-
-				var lifecycle = uri.getParameterValue('p_p_lifecycle');
-
-				return lifecycle === '0' || !lifecycle;
 			}
-		}
-	]
-);
-
-Liferay.Util.submitForm = function(form) {
-	async.nextTick(
-		() => {
-			let formElement = form.getDOM();
-			let url = formElement.action;
-
-			if (app.canNavigate(url) && formElement.method !== 'get') {
-				Liferay.Util._submitLocked = false;
-
-				globals.capturedFormElement = formElement;
-
-				app.navigate(utils.getUrlPath(url));
-			}
-			else {
-				formElement.submit();
-			}
-		}
+		]
 	);
+
+	Liferay.Util.submitForm = function(form) {
+		async.nextTick(
+			() => {
+				let formElement = form.getDOM();
+				let url = formElement.action;
+
+				if (app.canNavigate(url) && formElement.method !== 'get') {
+					Liferay.Util._submitLocked = false;
+
+					globals.capturedFormElement = formElement;
+
+					app.navigate(utils.getUrlPath(url));
+				}
+				else {
+					formElement.submit();
+				}
+			}
+		);
+	};
+
+	Liferay.SPA.app = app;
+
+	Liferay.fire('SPAReady');
 };
 
-Liferay.SPA = Liferay.SPA || {};
+if (globals.document.readyState == 'loading') {
+	globals.document.addEventListener('DOMContentLoaded', initSPA);
+}
+else {
+	initSPA();
+}
 
-Liferay.SPA.app = app;
+Liferay.SPA = Liferay.SPA || {};
 
 export default Liferay.SPA;

liferay/liferay-central-test-1

modules/apps/foundation/frontend-js/frontend-js-spa-web/src/main/resources/META-INF/resources/liferay/init.es.js
@@ -8,71 +8,82 @@ import RenderURLScreen from './screen/RenderURLScreen.es';
 import Uri from 'metal-uri/src/Uri';
 import utils from 'senna/src/utils/utils';
 
-let app = new App();
+var initSPA = function() {
+	let app = new App();
 
-app.addRoutes(
-	[
-		{
-			handler: ActionURLScreen,
-			path: function(url) {
-				var uri = new Uri(url);
+	app.addRoutes(
+		[
+			{
+				handler: ActionURLScreen,
+				path: function(url) {
+					var uri = new Uri(url);
 
-				var loginRedirect = new Uri(Liferay.SPA.loginRedirect);
+					var loginRedirect = new Uri(Liferay.SPA.loginRedirect);
 
-				var hostname = loginRedirect.getHostname() || window.location.hostname;
+					var hostname = loginRedirect.getHostname() || window.location.hostname;
 
-				if (!app.isLinkSameOrigin_(hostname)) {
-					return false;
+					if (!app.isLinkSameOrigin_(hostname)) {
+						return false;
+					}
+
+					return uri.getParameterValue('p_p_lifecycle') === '1';
 				}
+			},
+			{
+				handler: RenderURLScreen,
+				path: function(url) {
+					if (url.indexOf(themeDisplay.getPathMain()) === 0) {
+						return false;
+					}
 
-				return uri.getParameterValue('p_p_lifecycle') === '1';
-			}
-		},
-		{
-			handler: RenderURLScreen,
-			path: function(url) {
-				if (url.indexOf(themeDisplay.getPathMain()) === 0) {
-					return false;
+					var excluded = Liferay.SPA.excludedPaths.some((excludedPath) => url.indexOf(excludedPath) === 0);
+
+					if (excluded) {
+						return false;
+					}
+
+					var uri = new Uri(url);
+
+					var lifecycle = uri.getParameterValue('p_p_lifecycle');
+
+					return lifecycle === '0' || !lifecycle;
 				}
-
-				var excluded = Liferay.SPA.excludedPaths.some((excludedPath) => url.indexOf(excludedPath) === 0);
-
-				if (excluded) {
-					return false;
-				}
-
-				var uri = new Uri(url);
-
-				var lifecycle = uri.getParameterValue('p_p_lifecycle');
-
-				return lifecycle === '0' || !lifecycle;
 			}
-		}
-	]
-);
-
-Liferay.Util.submitForm = function(form) {
-	async.nextTick(
-		() => {
-			let formElement = form.getDOM();
-			let url = formElement.action;
-
-			if (app.canNavigate(url) && formElement.method !== 'get') {
-				Liferay.Util._submitLocked = false;
-
-				globals.capturedFormElement = formElement;
-
-				app.navigate(utils.getUrlPath(url));
-			}
-			else {
-				formElement.submit();
-			}
-		}
+		]
 	);
+
+	Liferay.Util.submitForm = function(form) {
+		async.nextTick(
+			() => {
+				let formElement = form.getDOM();
+				let url = formElement.action;
+
+				if (app.canNavigate(url) && formElement.method !== 'get') {
+					Liferay.Util._submitLocked = false;
+
+					globals.capturedFormElement = formElement;
+
+					app.navigate(utils.getUrlPath(url));
+				}
+				else {
+					formElement.submit();
+				}
+			}
+		);
+	};
+
+	Liferay.SPA.app = app;
+
+	Liferay.fire('SPAReady');
 };
 
-Liferay.SPA = Liferay.SPA || {};
+if (globals.document.readyState == 'loading') {
+	globals.document.addEventListener('DOMContentLoaded', initSPA);
+}
+else {
+	initSPA();
+}
 
-Liferay.SPA.app = app;
+Liferay.SPA = Liferay.SPA || {};
 
 export default Liferay.SPA;