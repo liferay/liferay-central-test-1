liferay/liferay-central-test-1

commit 16ac20770d02af475df5bcb6ee98cf8f42bebe02

LPS-60027 DestinationFactory might not be always present

modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/internal/ResourcesImporterBundleActivator.java
@@ -37,6 +37,10 @@ public class ResourcesImporterBundleActivator implements BundleActivator {
 		ServiceReference<DestinationFactory> serviceReference =
 			bundleContext.getServiceReference(DestinationFactory.class);
 
+		if (serviceReference == null) {
+			return;
+		}
+
 		try {
 			DestinationFactory destinationFactory = bundleContext.getService(
 				serviceReference);

commit 7e1df4dbf994aae215111b0614b41e407672bcde

LPS-60027 Fix NPE caused by 16ac20770d02af475df5bcb6ee98cf8f42bebe02, but this logic is still very fishy. Carlos, are you sure we should just shortcut on missing DestinationFactory in start()? I think we should use ServiceTracker to track DestinationFactory in order to create the destination when DestinationFactory shows up.

modules/apps/web-experience/export-import/export-import-resources-importer/src/main/java/com/liferay/exportimport/resources/importer/internal/ResourcesImporterBundleActivator.java
@@ -67,6 +67,10 @@ public class ResourcesImporterBundleActivator implements BundleActivator {
 
 	@Override
 	public void stop(BundleContext bundleContext) {
+		if (_serviceRegistration == null) {
+			return;
+		}
+
 		Destination destination = bundleContext.getService(
 			_serviceRegistration.getReference());
 