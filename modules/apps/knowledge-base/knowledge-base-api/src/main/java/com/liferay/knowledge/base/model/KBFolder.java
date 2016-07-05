sergiogonzalez/liferay-portal

LPS-66685 Autogenerated

modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/model/KBFolder.java
@@ -54,11 +54,19 @@ public interface KBFolder extends KBFolderModel, PersistedModel {
 			}
 		};
 
+	public java.util.List<java.lang.Long> getAncestorFolderIds()
+		throws com.liferay.portal.kernel.exception.PortalException;
+
 	public long getClassNameId();
+
+	public KBFolder getParentKBFolder()
+		throws com.liferay.portal.kernel.exception.PortalException;
 
 	public java.lang.String getParentTitle(java.util.Locale locale)
 		throws com.liferay.portal.kernel.exception.PortalException;
 
 	public boolean isEmpty()
 		throws com.liferay.portal.kernel.exception.PortalException;
+
+	public boolean isRoot();
 }

modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/model/KBFolderWrapper.java
@@ -189,8 +189,19 @@ public class KBFolderWrapper implements KBFolder, ModelWrapper<KBFolder> {
 	}
 
 	@Override
+	public boolean isRoot() {
+		return _kbFolder.isRoot();
+	}
+
+	@Override
 	public ExpandoBridge getExpandoBridge() {
 		return _kbFolder.getExpandoBridge();
+	}
+
+	@Override
+	public KBFolder getParentKBFolder()
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _kbFolder.getParentKBFolder();
 	}
 
 	@Override
@@ -322,6 +333,12 @@ public class KBFolderWrapper implements KBFolder, ModelWrapper<KBFolder> {
 	@Override
 	public Date getModifiedDate() {
 		return _kbFolder.getModifiedDate();
+	}
+
+	@Override
+	public java.util.List<java.lang.Long> getAncestorFolderIds()
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _kbFolder.getAncestorFolderIds();
 	}
 
 	@Override

liferay/liferay-central-test-1

modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/model/KBFolder.java
@@ -54,11 +54,19 @@ public interface KBFolder extends KBFolderModel, PersistedModel {
 			}
 		};
 
+	public java.util.List<java.lang.Long> getAncestorKBFolderIds()
+		throws com.liferay.portal.kernel.exception.PortalException;
+
 	public long getClassNameId();
+
+	public KBFolder getParentKBFolder()
+		throws com.liferay.portal.kernel.exception.PortalException;
 
 	public java.lang.String getParentTitle(java.util.Locale locale)
 		throws com.liferay.portal.kernel.exception.PortalException;
 
 	public boolean isEmpty()
 		throws com.liferay.portal.kernel.exception.PortalException;
+
+	public boolean isRoot();
 }

modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/model/KBFolderWrapper.java
@@ -189,8 +189,19 @@ public class KBFolderWrapper implements KBFolder, ModelWrapper<KBFolder> {
 	}
 
 	@Override
+	public boolean isRoot() {
+		return _kbFolder.isRoot();
+	}
+
+	@Override
 	public ExpandoBridge getExpandoBridge() {
 		return _kbFolder.getExpandoBridge();
+	}
+
+	@Override
+	public KBFolder getParentKBFolder()
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _kbFolder.getParentKBFolder();
 	}
 
 	@Override
@@ -322,6 +333,12 @@ public class KBFolderWrapper implements KBFolder, ModelWrapper<KBFolder> {
 	@Override
 	public Date getModifiedDate() {
 		return _kbFolder.getModifiedDate();
+	}
+
+	@Override
+	public java.util.List<java.lang.Long> getAncestorKBFolderIds()
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _kbFolder.getAncestorKBFolderIds();
 	}
 
 	@Override