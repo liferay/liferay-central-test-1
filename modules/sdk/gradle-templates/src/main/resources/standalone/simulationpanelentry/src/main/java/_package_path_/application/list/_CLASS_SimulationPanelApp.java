juliocamarero/liferay-portal

LPS-65474 create template for simulation panel entry

modules/sdk/gradle-templates/src/main/resources/standalone/simulationpanelentry/src/main/java/_package_path_/application/list/_CLASS_SimulationPanelApp.java
@@ -1,0 +1,82 @@
+package _package_.application.list;
+
+import com.liferay.application.list.BaseJSPPanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.product.navigation.simulation.application.list.SimulationPanelCategory;
+import com.liferay.product.navigation.simulation.web.constants.ProductNavigationSimulationPortletKeys;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=500",
+		"panel.category.key=" + SimulationPanelCategory.SIMULATION
+	},
+	service = PanelApp.class
+)
+public class _CLASS_SimulationPanelApp extends BaseJSPPanelApp {
+
+	@Override
+	public String getJspPath() {
+		return "/simulation_sample.jsp";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "simulation-sample");
+	}
+
+	@Override
+	public String getPortletId() {
+		return ProductNavigationSimulationPortletKeys.
+			PRODUCT_NAVIGATION_SIMULATION;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (group.isControlPanel()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/*@Override
+	@Reference(
+		target = "(javax.portlet.name=" + SiteNavigationLanguagePortletKeys.SITE_NAVIGATION_LANGUAGE + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}*/
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=_name_)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}

liferay/liferay-central-test-1

modules/sdk/gradle-templates/src/main/resources/standalone/simulationpanelentry/src/main/java/_package_path_/application/list/_CLASS_SimulationPanelApp.java
@@ -1,0 +1,80 @@
+package _package_.application.list;
+
+import com.liferay.application.list.BaseJSPPanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.product.navigation.simulation.application.list.SimulationPanelCategory;
+import com.liferay.product.navigation.simulation.web.constants.ProductNavigationSimulationPortletKeys;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=500",
+		"panel.category.key=" + SimulationPanelCategory.SIMULATION
+	},
+	service = PanelApp.class
+)
+public class _CLASS_SimulationPanelApp extends BaseJSPPanelApp {
+
+	@Override
+	public String getJspPath() {
+		return "/simulation_sample.jsp";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "simulation-sample");
+	}
+
+	@Override
+	public String getPortletId() {
+		return ProductNavigationSimulationPortletKeys.
+			PRODUCT_NAVIGATION_SIMULATION;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (group.isControlPanel()) {
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + ProductNavigationSimulationPortletKeys.PRODUCT_NAVIGATION_SIMULATION + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+	@Override
+	@Reference(
+		target = "(osgi.web.symbolicname=_name_)",
+		unbind = "-"
+	)
+	public void setServletContext(ServletContext servletContext) {
+		super.setServletContext(servletContext);
+	}
+
+}