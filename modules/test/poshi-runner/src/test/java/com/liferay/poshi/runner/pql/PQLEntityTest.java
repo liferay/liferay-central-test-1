pyoo47/liferay-portal

LRQA-26691 Add unit tests to test 'PQLModifier' queries

modules/test/poshi-runner/src/test/java/com/liferay/poshi/runner/pql/PQLEntityTest.java
@@ -37,6 +37,16 @@ public class PQLEntityTest extends TestCase {
 		_compare(PQLEntity.fixQuery("( (test) OR (test))"), "(test) OR (test)");
 	}
 
+	@Test
+	public void testRemoveModifierFromQuery() throws Exception {
+		_compare(PQLEntity.removeModifierFromQuery("test"), "test");
+		_compare(PQLEntity.removeModifierFromQuery("NOT test"), "test");
+		_compare(PQLEntity.removeModifierFromQuery(" NOT test"), "test");
+		_compare(PQLEntity.removeModifierFromQuery(" test NOT"), "test NOT");
+		_compare(PQLEntity.removeModifierFromQuery("OR test"), "OR test");
+		_compare(PQLEntity.removeModifierFromQuery(" OR test"), "OR test");
+	}
+
 	private void _compare(String actualValue, String expectedValue)
 		throws Exception {
 

liferay/liferay-central-test-1

modules/test/poshi-runner/src/test/java/com/liferay/poshi/runner/pql/PQLEntityTest.java
@@ -40,6 +40,17 @@ public class PQLEntityTest extends TestCase {
 			PQLEntity.fixPQL(" ( (( test ( test ))) "), "( (( test ( test )))");
 	}
 
+	@Test
+	public void testRemoveModifierFromPQL() throws Exception {
+		_compareString(PQLEntity.removeModifierFromPQL("test"), "test");
+		_compareString(PQLEntity.removeModifierFromPQL("NOT test"), "test");
+		_compareString(PQLEntity.removeModifierFromPQL(" NOT test"), "test");
+		_compareString(
+			PQLEntity.removeModifierFromPQL(" test NOT"), "test NOT");
+		_compareString(PQLEntity.removeModifierFromPQL("OR test"), "OR test");
+		_compareString(PQLEntity.removeModifierFromPQL(" OR test"), "OR test");
+	}
+
 	private void _compareString(String actualString, String expectedString)
 		throws Exception {
 