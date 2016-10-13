jorgediaz-lr/liferay-portal

LPS-66570 Regen

portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalService.java
@@ -115,6 +115,9 @@ public interface GroupLocalService extends BaseLocalService,
 	public boolean hasUserGroups(long userId);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public boolean isActive(Group group);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public ActionableDynamicQuery getActionableDynamicQuery();
 
 	public DynamicQuery dynamicQuery();

liferay/liferay-central-test-1

portal-kernel/src/com/liferay/portal/kernel/service/GroupLocalService.java
@@ -115,6 +115,9 @@ public interface GroupLocalService extends BaseLocalService,
 	public boolean hasUserGroups(long userId);
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public boolean isLiveGroupActive(Group group);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public ActionableDynamicQuery getActionableDynamicQuery();
 
 	public DynamicQuery dynamicQuery();