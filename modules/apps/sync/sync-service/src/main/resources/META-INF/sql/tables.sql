shinnlok/liferay-portal

SYNC-1622 Connector - Regen

modules/apps/sync/sync-service/src/main/resources/META-INF/sql/tables.sql
@@ -29,6 +29,7 @@ create table SyncDLObject (
 	size_ LONG,
 	checksum VARCHAR(75) null,
 	event VARCHAR(75) null,
+	key_ VARCHAR(75) null,
 	lastPermissionChangeDate DATE null,
 	lockExpirationDate DATE null,
 	lockUserId LONG,

modules/apps/sync/sync-test/src/testIntegration/java/com/liferay/sync/service/persistence/test/SyncDLObjectPersistenceTest.java
@@ -162,6 +162,8 @@ public class SyncDLObjectPersistenceTest {
 
 		newSyncDLObject.setEvent(RandomTestUtil.randomString());
 
+		newSyncDLObject.setKey(RandomTestUtil.randomString());
+
 		newSyncDLObject.setLastPermissionChangeDate(RandomTestUtil.nextDate());
 
 		newSyncDLObject.setLockExpirationDate(RandomTestUtil.nextDate());
@@ -220,6 +222,8 @@ public class SyncDLObjectPersistenceTest {
 			newSyncDLObject.getChecksum());
 		Assert.assertEquals(existingSyncDLObject.getEvent(),
 			newSyncDLObject.getEvent());
+		Assert.assertEquals(existingSyncDLObject.getKey(),
+			newSyncDLObject.getKey());
 		Assert.assertEquals(Time.getShortTimestamp(
 				existingSyncDLObject.getLastPermissionChangeDate()),
 			Time.getShortTimestamp(
@@ -378,7 +382,7 @@ public class SyncDLObjectPersistenceTest {
 			"repositoryId", true, "parentFolderId", true, "treePath", true,
 			"name", true, "extension", true, "mimeType", true, "description",
 			true, "changeLog", true, "version", true, "versionId", true,
-			"size", true, "checksum", true, "event", true,
+			"size", true, "checksum", true, "event", true, "key", true,
 			"lastPermissionChangeDate", true, "lockExpirationDate", true,
 			"lockUserId", true, "lockUserName", true, "type", true, "typePK",
 			true, "typeUuid", true);
@@ -636,6 +640,8 @@ public class SyncDLObjectPersistenceTest {
 		syncDLObject.setChecksum(RandomTestUtil.randomString());
 
 		syncDLObject.setEvent(RandomTestUtil.randomString());
+
+		syncDLObject.setKey(RandomTestUtil.randomString());
 
 		syncDLObject.setLastPermissionChangeDate(RandomTestUtil.nextDate());
 

liferay/liferay-central-test-1

modules/apps/sync/sync-service/src/main/resources/META-INF/sql/tables.sql
@@ -29,6 +29,7 @@ create table SyncDLObject (
 	size_ LONG,
 	checksum VARCHAR(75) null,
 	event VARCHAR(75) null,
+	lanTokenKey VARCHAR(75) null,
 	lastPermissionChangeDate DATE null,
 	lockExpirationDate DATE null,
 	lockUserId LONG,

modules/apps/sync/sync-test/src/testIntegration/java/com/liferay/sync/service/persistence/test/SyncDLObjectPersistenceTest.java
@@ -162,6 +162,8 @@ public class SyncDLObjectPersistenceTest {
 
 		newSyncDLObject.setEvent(RandomTestUtil.randomString());
 
+		newSyncDLObject.setLanTokenKey(RandomTestUtil.randomString());
+
 		newSyncDLObject.setLastPermissionChangeDate(RandomTestUtil.nextDate());
 
 		newSyncDLObject.setLockExpirationDate(RandomTestUtil.nextDate());
@@ -220,6 +222,8 @@ public class SyncDLObjectPersistenceTest {
 			newSyncDLObject.getChecksum());
 		Assert.assertEquals(existingSyncDLObject.getEvent(),
 			newSyncDLObject.getEvent());
+		Assert.assertEquals(existingSyncDLObject.getLanTokenKey(),
+			newSyncDLObject.getLanTokenKey());
 		Assert.assertEquals(Time.getShortTimestamp(
 				existingSyncDLObject.getLastPermissionChangeDate()),
 			Time.getShortTimestamp(
@@ -378,7 +382,7 @@ public class SyncDLObjectPersistenceTest {
 			"repositoryId", true, "parentFolderId", true, "treePath", true,
 			"name", true, "extension", true, "mimeType", true, "description",
 			true, "changeLog", true, "version", true, "versionId", true,
-			"size", true, "checksum", true, "event", true,
+			"size", true, "checksum", true, "event", true, "lanTokenKey", true,
 			"lastPermissionChangeDate", true, "lockExpirationDate", true,
 			"lockUserId", true, "lockUserName", true, "type", true, "typePK",
 			true, "typeUuid", true);
@@ -636,6 +640,8 @@ public class SyncDLObjectPersistenceTest {
 		syncDLObject.setChecksum(RandomTestUtil.randomString());
 
 		syncDLObject.setEvent(RandomTestUtil.randomString());
+
+		syncDLObject.setLanTokenKey(RandomTestUtil.randomString());
 
 		syncDLObject.setLastPermissionChangeDate(RandomTestUtil.nextDate());
 