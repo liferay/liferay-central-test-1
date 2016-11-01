JorgeFerrer/liferay-portal

LPS-66238 Add checkbox to result rows

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/init.jsp
@@ -178,6 +178,7 @@ page import="com.liferay.users.admin.kernel.util.UsersAdmin" %><%@
 page import="com.liferay.users.admin.kernel.util.UsersAdminUtil" %><%@
 page import="com.liferay.users.admin.web.constants.UsersAdminWebKeys" %><%@
 page import="com.liferay.users.admin.web.search.OrganizationChecker" %><%@
+page import="com.liferay.users.admin.web.search.OrganizationMemberChecker" %><%@
 page import="com.liferay.users.admin.web.search.OrganizationResultRowSplitter" %>
 
 <%@ page import="java.text.Format" %>

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/view_tree.jsp
@@ -77,6 +77,7 @@ if (organization != null) {
 	<c:when test="<%= showList %>">
 		<liferay-frontend:management-bar
 			includeCheckBox="<%= true %>"
+			searchContainerId="organizationMembers"
 		>
 			<liferay-frontend:management-bar-filters>
 				<liferay-frontend:management-bar-navigation
@@ -257,7 +258,9 @@ if (organization != null) {
 				emptyResultsMessage="no-results-were-found"
 				emptyResultsMessageCssClass="taglib-empty-result-message-header-has-plus-btn"
 				headerNames="name,type"
+				id="organizationMembers"
 				iteratorURL="<%= currentURLObj %>"
+				rowChecker="<%= new OrganizationMemberChecker(renderResponse) %>"
 				total="<%= organizationSearch.getTotal() + userSearch.getTotal() %>"
 				var="membersSearchContainer"
 			>

liferay/liferay-central-test-1

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/init.jsp
@@ -178,7 +178,8 @@ page import="com.liferay.users.admin.kernel.util.UsersAdmin" %><%@
 page import="com.liferay.users.admin.kernel.util.UsersAdminUtil" %><%@
 page import="com.liferay.users.admin.web.constants.UsersAdminWebKeys" %><%@
 page import="com.liferay.users.admin.web.search.OrganizationChecker" %><%@
-page import="com.liferay.users.admin.web.search.OrganizationResultRowSplitter" %>
+page import="com.liferay.users.admin.web.search.OrganizationResultRowSplitter" %><%@
+page import="com.liferay.users.admin.web.search.OrganizationUserChecker" %>
 
 <%@ page import="java.text.Format" %>
 

modules/apps/foundation/users-admin/users-admin-web/src/main/resources/META-INF/resources/view_tree.jsp
@@ -77,6 +77,7 @@ if (organization != null) {
 	<c:when test="<%= showList %>">
 		<liferay-frontend:management-bar
 			includeCheckBox="<%= true %>"
+			searchContainerId="organizationUsers"
 		>
 			<liferay-frontend:management-bar-filters>
 				<liferay-frontend:management-bar-navigation
@@ -257,7 +258,9 @@ if (organization != null) {
 				emptyResultsMessage="no-results-were-found"
 				emptyResultsMessageCssClass="taglib-empty-result-message-header-has-plus-btn"
 				headerNames="name,type"
+				id="organizationUsers"
 				iteratorURL="<%= currentURLObj %>"
+				rowChecker="<%= new OrganizationUserChecker(renderResponse) %>"
 				total="<%= organizationSearch.getTotal() + userSearch.getTotal() %>"
 				var="membersSearchContainer"
 			>