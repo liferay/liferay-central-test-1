marcellustavares/liferay-portal

LPS-69400 update references for Bookmarks

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/asset/folder_full_content.jsp
@@ -67,14 +67,14 @@ BookmarksFolder folder = (BookmarksFolder)request.getAttribute(BookmarksWebKeys.
 				</div>
 			</div>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= BookmarksFolder.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= BookmarksFolder.class.getName() %>"
 					classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 		</aui:col>
 	</aui:row>
 </c:if>

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/asset/full_content.jsp
@@ -26,11 +26,11 @@ int status = ParamUtil.getInteger(request, "status", WorkflowConstants.STATUS_AN
 
 <p class="asset-description"><%= HtmlUtil.escape(entry.getDescription()) %></p>
 
-<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
-	<liferay-ui:custom-attribute-list
+<liferay-custom-field-ui:custom-attribute-available className="<%= BookmarksEntry.class.getName() %>">
+	<liferay-custom-field-ui:custom-attribute-list
 		className="<%= BookmarksEntry.class.getName() %>"
 		classPK="<%= (entry != null) ? entry.getEntryId() : 0 %>"
 		editable="<%= false %>"
 		label="<%= true %>"
 	/>
-</liferay-ui:custom-attributes-available>
+</liferay-custom-field-ui:custom-attribute-available>

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/edit_entry.jsp
@@ -156,16 +156,16 @@ if (portletTitleBasedNavigation) {
 					<aui:input name="description" />
 				</aui:fieldset>
 
-				<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-available className="<%= BookmarksEntry.class.getName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= BookmarksEntry.class.getName() %>"
 							classPK="<%= entryId %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attribute-available>
 
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="categorization">
 					<liferay-asset:asset-categories-selector className="<%= BookmarksEntry.class.getName() %>" classPK="<%= entryId %>" />

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/edit_folder.jsp
@@ -159,16 +159,16 @@ if (portletTitleBasedNavigation) {
 				</aui:fieldset>
 			</c:if>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
+			<liferay-custom-field-ui:custom-attribute-available className="<%= BookmarksFolder.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= BookmarksFolder.class.getName() %>"
 						classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 
 			<c:if test="<%= folder == null %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/info_panel.jsp
@@ -173,14 +173,14 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(entries)) {
 				classPK="<%= entry.getEntryId() %>"
 			/>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= BookmarksEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= BookmarksEntry.class.getName() %>"
 					classPK="<%= entry.getEntryId() %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 
 			<c:if test="<%= bookmarksGroupServiceOverriddenConfiguration.enableRelatedAssets() %>">
 

liferay/liferay-central-test-1

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/asset/folder_full_content.jsp
@@ -67,14 +67,14 @@ BookmarksFolder folder = (BookmarksFolder)request.getAttribute(BookmarksWebKeys.
 				</div>
 			</div>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= BookmarksFolder.class.getName() %>"
 					classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 		</aui:col>
 	</aui:row>
 </c:if>

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/asset/full_content.jsp
@@ -26,11 +26,11 @@ int status = ParamUtil.getInteger(request, "status", WorkflowConstants.STATUS_AN
 
 <p class="asset-description"><%= HtmlUtil.escape(entry.getDescription()) %></p>
 
-<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
-	<liferay-ui:custom-attribute-list
+<liferay-custom-field-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
+	<liferay-custom-field-ui:custom-attribute-list
 		className="<%= BookmarksEntry.class.getName() %>"
 		classPK="<%= (entry != null) ? entry.getEntryId() : 0 %>"
 		editable="<%= false %>"
 		label="<%= true %>"
 	/>
-</liferay-ui:custom-attributes-available>
+</liferay-custom-field-ui:custom-attributes-available>

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/edit_entry.jsp
@@ -156,16 +156,16 @@ if (portletTitleBasedNavigation) {
 					<aui:input name="description" />
 				</aui:fieldset>
 
-				<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
 					<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-						<liferay-ui:custom-attribute-list
+						<liferay-custom-field-ui:custom-attribute-list
 							className="<%= BookmarksEntry.class.getName() %>"
 							classPK="<%= entryId %>"
 							editable="<%= true %>"
 							label="<%= true %>"
 						/>
 					</aui:fieldset>
-				</liferay-ui:custom-attributes-available>
+				</liferay-custom-field-ui:custom-attributes-available>
 
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="categorization">
 					<liferay-asset:asset-categories-selector className="<%= BookmarksEntry.class.getName() %>" classPK="<%= entryId %>" />

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/edit_folder.jsp
@@ -159,16 +159,16 @@ if (portletTitleBasedNavigation) {
 				</aui:fieldset>
 			</c:if>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
+			<liferay-custom-field-ui:custom-attributes-available className="<%= BookmarksFolder.class.getName() %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="custom-fields">
-					<liferay-ui:custom-attribute-list
+					<liferay-custom-field-ui:custom-attribute-list
 						className="<%= BookmarksFolder.class.getName() %>"
 						classPK="<%= (folder != null) ? folder.getFolderId() : 0 %>"
 						editable="<%= true %>"
 						label="<%= true %>"
 					/>
 				</aui:fieldset>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 
 			<c:if test="<%= folder == null %>">
 				<aui:fieldset collapsed="<%= true %>" collapsible="<%= true %>" label="permissions">

modules/apps/collaboration/bookmarks/bookmarks-web/src/main/resources/META-INF/resources/bookmarks/info_panel.jsp
@@ -173,14 +173,14 @@ if (ListUtil.isEmpty(folders) && ListUtil.isEmpty(entries)) {
 				classPK="<%= entry.getEntryId() %>"
 			/>
 
-			<liferay-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= BookmarksEntry.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= BookmarksEntry.class.getName() %>"
 					classPK="<%= entry.getEntryId() %>"
 					editable="<%= false %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 
 			<c:if test="<%= bookmarksGroupServiceOverriddenConfiguration.enableRelatedAssets() %>">
 