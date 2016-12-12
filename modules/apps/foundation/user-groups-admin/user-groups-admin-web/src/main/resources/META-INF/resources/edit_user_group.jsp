marcellustavares/liferay-portal

LPS-69400 update references for User Groups Admin

modules/apps/foundation/user-groups-admin/user-groups-admin-web/src/main/resources/META-INF/resources/edit_user_group.jsp
@@ -65,14 +65,14 @@ renderResponse.setTitle((userGroup == null) ? LanguageUtil.get(request, "new-use
 
 			<aui:input disabled="<%= !hasUserGroupUpdatePermission %>" name="description" />
 
-			<liferay-ui:custom-attributes-available className="<%= UserGroup.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attribute-available className="<%= UserGroup.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= UserGroup.class.getName() %>"
 					classPK="<%= userGroupId %>"
 					editable="<%= true %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attribute-available>
 		</aui:fieldset>
 
 		<%

liferay/liferay-central-test-1

modules/apps/foundation/user-groups-admin/user-groups-admin-web/src/main/resources/META-INF/resources/edit_user_group.jsp
@@ -65,14 +65,14 @@ renderResponse.setTitle((userGroup == null) ? LanguageUtil.get(request, "new-use
 
 			<aui:input disabled="<%= !hasUserGroupUpdatePermission %>" name="description" />
 
-			<liferay-ui:custom-attributes-available className="<%= UserGroup.class.getName() %>">
-				<liferay-ui:custom-attribute-list
+			<liferay-custom-field-ui:custom-attributes-available className="<%= UserGroup.class.getName() %>">
+				<liferay-custom-field-ui:custom-attribute-list
 					className="<%= UserGroup.class.getName() %>"
 					classPK="<%= userGroupId %>"
 					editable="<%= true %>"
 					label="<%= true %>"
 				/>
-			</liferay-ui:custom-attributes-available>
+			</liferay-custom-field-ui:custom-attributes-available>
 		</aui:fieldset>
 
 		<%