liferay/liferay-central-test-1

commit d891ee7f588e9c86a3e177363ee03e9ad3f29dd0

LPS-65864 Refresh fragment hosts after dynamic install modules are installed

modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -90,6 +90,7 @@ import org.osgi.framework.launch.FrameworkFactory;
 import org.osgi.framework.startlevel.BundleStartLevel;
 import org.osgi.framework.startlevel.FrameworkStartLevel;
 import org.osgi.framework.wiring.BundleRevision;
+import org.osgi.framework.wiring.FrameworkWiring;
 import org.osgi.util.tracker.BundleTracker;
 
 import org.springframework.beans.factory.BeanIsAbstractException;
@@ -1113,6 +1114,47 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		if (_log.isDebugEnabled()) {
 			_log.debug("Started initial bundles");
 		}
+
+		BundleContext bundleContext = _framework.getBundleContext();
+
+		Bundle[] installedBundles = bundleContext.getBundles();
+
+		List<String> fragmentHosts = new ArrayList<>();
+
+		for (Bundle bundle : installedBundles) {
+			BundleStartLevel bundleStartLevel = bundle.adapt(
+				BundleStartLevel.class);
+
+			if (bundleStartLevel.getStartLevel() !=
+					PropsValues.MODULE_FRAMEWORK_DYNAMIC_INSTALL_START_LEVEL) {
+
+				continue;
+			}
+
+			Dictionary<String, String> headers = bundle.getHeaders();
+
+			String fragmentHost = headers.get(Constants.FRAGMENT_HOST);
+
+			if (fragmentHost == null) {
+				continue;
+			}
+
+			fragmentHosts.add(fragmentHost);
+		}
+
+		List<Bundle> refreshBundles = new ArrayList<>();
+
+		for (Bundle bundle : installedBundles) {
+			if (fragmentHosts.contains(bundle.getSymbolicName())) {
+				refreshBundles.add(bundle);
+
+				break;
+			}
+		}
+		FrameworkWiring frameworkWiring = _framework.adapt(
+			FrameworkWiring.class);
+
+		frameworkWiring.refreshBundles(refreshBundles);
 	}
 
 	private void _setUpPrerequisiteFrameworkServices(

commit 3cd9b2dae9a6c0919ae412dfebc747005ca4aefc

LPS-69957 Search for Fragment Host bundles from all bundles, not only the dynamic ones, as we do have fragment usage for static bundles(org.apache.felix.dependencymanager and com.liferay.portal.remote.dependency.manager.tccl). Regression from d891ee7f588e9c86a3e177363ee03e9ad3f29dd0

modules/core/portal-bootstrap/src/main/java/com/liferay/portal/bootstrap/ModuleFrameworkImpl.java
@@ -1379,15 +1379,6 @@ public class ModuleFrameworkImpl implements ModuleFramework {
 		List<String> hostBundleSymbolicNames = new ArrayList<>();
 
 		for (Bundle bundle : installedBundles) {
-			BundleStartLevel bundleStartLevel = bundle.adapt(
-				BundleStartLevel.class);
-
-			if (bundleStartLevel.getStartLevel() !=
-					PropsValues.MODULE_FRAMEWORK_DYNAMIC_INSTALL_START_LEVEL) {
-
-				continue;
-			}
-
 			Dictionary<String, String> headers = bundle.getHeaders();
 
 			String fragmentHost = headers.get(Constants.FRAGMENT_HOST);