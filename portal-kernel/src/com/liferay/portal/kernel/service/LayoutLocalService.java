brianchandotcom/liferay-portal

LPS-70169 Regenerate

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalService.java
@@ -461,6 +461,9 @@ public interface LayoutLocalService extends BaseLocalService,
 	public Layout fetchLayoutByUuidAndGroupId(java.lang.String uuid,
 		long groupId, boolean privateLayout);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Layout getDefaultLayout(long groupId, boolean privateLayout);
+
 	/**
 	* Returns the layout for the friendly URL
 	*

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalServiceUtil.java
@@ -546,6 +546,11 @@ public class LayoutLocalServiceUtil {
 				   .fetchLayoutByUuidAndGroupId(uuid, groupId, privateLayout);
 	}
 
+	public static com.liferay.portal.kernel.model.Layout getDefaultLayout(
+		long groupId, boolean privateLayout) {
+		return getService().getDefaultLayout(groupId, privateLayout);
+	}
+
 	/**
 	* Returns the layout for the friendly URL
 	*

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalServiceWrapper.java
@@ -562,6 +562,12 @@ public class LayoutLocalServiceWrapper implements LayoutLocalService,
 			privateLayout);
 	}
 
+	@Override
+	public com.liferay.portal.kernel.model.Layout getDefaultLayout(
+		long groupId, boolean privateLayout) {
+		return _layoutLocalService.getDefaultLayout(groupId, privateLayout);
+	}
+
 	/**
 	* Returns the layout for the friendly URL
 	*

liferay/liferay-central-test-1

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalService.java
@@ -428,6 +428,9 @@ public interface LayoutLocalService extends BaseLocalService,
 	public Layout deleteLayout(long plid) throws PortalException;
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Layout fetchDefaultLayout(long groupId, boolean privateLayout);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Layout fetchFirstLayout(long groupId, boolean privateLayout,
 		long parentLayoutId);
 

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalServiceUtil.java
@@ -500,6 +500,11 @@ public class LayoutLocalServiceUtil {
 		return getService().deleteLayout(plid);
 	}
 
+	public static com.liferay.portal.kernel.model.Layout fetchDefaultLayout(
+		long groupId, boolean privateLayout) {
+		return getService().fetchDefaultLayout(groupId, privateLayout);
+	}
+
 	public static com.liferay.portal.kernel.model.Layout fetchFirstLayout(
 		long groupId, boolean privateLayout, long parentLayoutId) {
 		return getService()

portal-kernel/src/com/liferay/portal/kernel/service/LayoutLocalServiceWrapper.java
@@ -509,6 +509,12 @@ public class LayoutLocalServiceWrapper implements LayoutLocalService,
 	}
 
 	@Override
+	public com.liferay.portal.kernel.model.Layout fetchDefaultLayout(
+		long groupId, boolean privateLayout) {
+		return _layoutLocalService.fetchDefaultLayout(groupId, privateLayout);
+	}
+
+	@Override
 	public com.liferay.portal.kernel.model.Layout fetchFirstLayout(
 		long groupId, boolean privateLayout, long parentLayoutId) {
 		return _layoutLocalService.fetchFirstLayout(groupId, privateLayout,