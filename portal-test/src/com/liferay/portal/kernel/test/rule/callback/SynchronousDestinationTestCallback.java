liferay/liferay-central-test-1

commit 349bb01abb9ddd9b2cfe07df48c8e16f4927bcd0

LPS-48724 Disable schedulers in tests

portal-impl/test/portal-test.properties
@@ -27,3 +27,5 @@ module.framework.base.dir=../bundles/osgi
 
 journal.transformer.regex.pattern.0=beta.sample.com
 journal.transformer.regex.replacement.0=production.sample.com
+
+scheduler.enabled=false

commit e94dac65672c9b6a62fe1a5897f1d16ca28b903a

LPS-69532 Only sync destination when scheduler is enabled. Kaleo components won't active without scheduler, rooted from KaleoTimerInstanceTokenLocalServiceImpl. This is an issue introduced from 349bb01abb9ddd9b2cfe07df48c8e16f4927bcd0 . Although we should turn on scheduler for portal-impl integration tests, it is too risky to do so right now, we may bring up many randome failures. I would defer this until next CI cycle. For now, simply don't sync liferay/kaleo_graph_walker, unless scheduler is in place.

portal-test/src/com/liferay/portal/kernel/test/rule/callback/SynchronousDestinationTestCallback.java
@@ -32,6 +32,9 @@ import com.liferay.portal.kernel.test.rule.callback.SynchronousDestinationTestCa
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.registry.Filter;
 import com.liferay.registry.Registry;
 import com.liferay.registry.RegistryUtil;
@@ -158,8 +161,6 @@ public class SynchronousDestinationTestCallback
 				DestinationNames.BACKGROUND_TASK);
 			Filter backgroundTaskStatusFilter = _registerDestinationFilter(
 				DestinationNames.BACKGROUND_TASK_STATUS);
-			Filter kaleoGraphWalkerFilter = _registerDestinationFilter(
-				"liferay/kaleo_graph_walker");
 			Filter mailFilter = _registerDestinationFilter(
 				DestinationNames.MAIL);
 			Filter pdfProcessorFilter = _registerDestinationFilter(
@@ -171,8 +172,19 @@ public class SynchronousDestinationTestCallback
 
 			serviceDependencyManager.registerDependencies(
 				asyncFilter, backgroundTaskFilter, backgroundTaskStatusFilter,
-				kaleoGraphWalkerFilter, mailFilter, pdfProcessorFilter,
-				rawMetaDataProcessorFilter, subscrpitionSenderFilter);
+				mailFilter, pdfProcessorFilter, rawMetaDataProcessorFilter,
+				subscrpitionSenderFilter);
+
+			boolean schedulerEnabled = GetterUtil.getBoolean(
+				PropsUtil.get(PropsKeys.SCHEDULER_ENABLED));
+
+			if (schedulerEnabled) {
+				Filter kaleoGraphWalkerFilter = _registerDestinationFilter(
+					"liferay/kaleo_graph_walker");
+
+				serviceDependencyManager.registerDependencies(
+					kaleoGraphWalkerFilter);
+			}
 
 			serviceDependencyManager.waitForDependencies();
 
@@ -186,10 +198,13 @@ public class SynchronousDestinationTestCallback
 				DestinationNames.DOCUMENT_LIBRARY_RAW_METADATA_PROCESSOR);
 			replaceDestination(
 				DestinationNames.DOCUMENT_LIBRARY_SYNC_EVENT_PROCESSOR);
-			replaceDestination("liferay/kaleo_graph_walker");
 			replaceDestination(DestinationNames.MAIL);
 			replaceDestination(DestinationNames.SCHEDULER_ENGINE);
 			replaceDestination(DestinationNames.SUBSCRIPTION_SENDER);
+
+			if (schedulerEnabled) {
+				replaceDestination("liferay/kaleo_graph_walker");
+			}
 
 			for (String searchEngineId :
 					SearchEngineHelperUtil.getSearchEngineIds()) {