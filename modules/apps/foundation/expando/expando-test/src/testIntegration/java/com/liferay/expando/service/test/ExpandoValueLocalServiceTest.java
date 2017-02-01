cgoncas/liferay-portal

LPS-37417 Test that reproduce the bug

modules/apps/foundation/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
@@ -23,6 +23,7 @@ import com.liferay.expando.kernel.model.ExpandoColumnConstants;
 import com.liferay.expando.kernel.model.ExpandoTable;
 import com.liferay.expando.kernel.model.ExpandoValue;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalServiceUtil;
+import com.liferay.expando.kernel.service.ExpandoRowLocalServiceUtil;
 import com.liferay.expando.kernel.service.ExpandoValueLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -158,6 +159,43 @@ public class ExpandoValueLocalServiceTest {
 		}
 		catch (ValueDataException vde) {
 		}
+	}
+
+	@Test
+	public void testDeleteAllColumns() throws Exception {
+		ExpandoColumn column = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column", ExpandoColumnConstants.STRING);
+
+		long classPk = CounterLocalServiceUtil.increment();
+
+		ExpandoValue value = ExpandoTestUtil.addValue(
+			_expandoTable, column, classPk, "value");
+
+		ExpandoValueLocalServiceUtil.deleteColumnValues(column.getColumnId());
+
+		Assert.assertNull(
+			ExpandoRowLocalServiceUtil.fetchExpandoRow(value.getRowId()));
+	}
+
+	@Test
+	public void testDeleteOnlyOneColumn() throws Exception {
+		ExpandoColumn column1 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column 1", ExpandoColumnConstants.STRING);
+
+		ExpandoColumn column2 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column 2", ExpandoColumnConstants.STRING);
+
+		long classPk = CounterLocalServiceUtil.increment();
+
+		ExpandoValue value = ExpandoTestUtil.addValue(
+			_expandoTable, column1, classPk, "value");
+
+		ExpandoTestUtil.addValue(_expandoTable, column2, classPk, "value");
+
+		ExpandoValueLocalServiceUtil.deleteColumnValues(column1.getColumnId());
+
+		Assert.assertNotNull(
+			ExpandoRowLocalServiceUtil.getRow(value.getRowId()));
 	}
 
 	@Test

liferay/liferay-central-test-1

modules/apps/foundation/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
@@ -23,6 +23,7 @@ import com.liferay.expando.kernel.model.ExpandoColumnConstants;
 import com.liferay.expando.kernel.model.ExpandoTable;
 import com.liferay.expando.kernel.model.ExpandoValue;
 import com.liferay.expando.kernel.service.ExpandoColumnLocalServiceUtil;
+import com.liferay.expando.kernel.service.ExpandoRowLocalServiceUtil;
 import com.liferay.expando.kernel.service.ExpandoValueLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
@@ -158,6 +159,43 @@ public class ExpandoValueLocalServiceTest {
 		}
 		catch (ValueDataException vde) {
 		}
+	}
+
+	@Test
+	public void testDeleteAllColumns() throws Exception {
+		ExpandoColumn column = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column", ExpandoColumnConstants.STRING);
+
+		long classPK = CounterLocalServiceUtil.increment();
+
+		ExpandoValue value = ExpandoTestUtil.addValue(
+			_expandoTable, column, classPK, "value");
+
+		ExpandoValueLocalServiceUtil.deleteColumnValues(column.getColumnId());
+
+		Assert.assertNull(
+			ExpandoRowLocalServiceUtil.fetchExpandoRow(value.getRowId()));
+	}
+
+	@Test
+	public void testDeleteOnlyOneColumn() throws Exception {
+		ExpandoColumn column1 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column 1", ExpandoColumnConstants.STRING);
+
+		ExpandoColumn column2 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column 2", ExpandoColumnConstants.STRING);
+
+		long classPK = CounterLocalServiceUtil.increment();
+
+		ExpandoValue value = ExpandoTestUtil.addValue(
+			_expandoTable, column1, classPK, "value");
+
+		ExpandoTestUtil.addValue(_expandoTable, column2, classPK, "value");
+
+		ExpandoValueLocalServiceUtil.deleteColumnValues(column1.getColumnId());
+
+		Assert.assertNotNull(
+			ExpandoRowLocalServiceUtil.getRow(value.getRowId()));
 	}
 
 	@Test