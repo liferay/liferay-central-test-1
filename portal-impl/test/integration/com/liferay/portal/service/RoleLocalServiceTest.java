drewbrokke/liferay-portal

LPS-69535 Add test for RoleLocalServiceUtil.getGroupRolesAndTeamRoles

portal-impl/test/integration/com/liferay/portal/service/RoleLocalServiceTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.service;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.NoSuchGroupException;
 import com.liferay.portal.kernel.exception.RoleNameException;
 import com.liferay.portal.kernel.model.Group;
@@ -42,6 +43,7 @@ import com.liferay.portal.kernel.test.util.RoleTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserGroupTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.comparator.RoleNameComparator;
 import com.liferay.portal.kernel.util.comparator.RoleRoleIdComparator;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.util.test.LayoutTestUtil;
@@ -180,6 +182,67 @@ public class RoleLocalServiceTest {
 
 		Collections.sort(actualRoles, roleIdComparator);
 		Collections.sort(expectedRoles, roleIdComparator);
+
+		Assert.assertEquals(expectedRoles, actualRoles);
+	}
+
+	@Test
+	public void testGetGroupRolesAndTeamRoles() throws Exception {
+		Object[] organizationAndTeam = getOrganizationAndTeam();
+
+		Organization organization = (Organization)organizationAndTeam[0];
+
+		long companyId = organization.getCompanyId();
+		long groupId = organization.getGroupId();
+
+		int[] roleTypes = RoleConstants.TYPES_ORGANIZATION_AND_REGULAR;
+
+		List<String> excludedRoleNames = new ArrayList<>();
+
+		excludedRoleNames.add(RoleConstants.ADMINISTRATOR);
+		excludedRoleNames.add(RoleConstants.GUEST);
+
+		int count = RoleLocalServiceUtil.getGroupRolesAndTeamRolesCount(
+			companyId, null, excludedRoleNames, roleTypes, 0, groupId);
+
+		List<Role> actualRoles = RoleLocalServiceUtil.getGroupRolesAndTeamRoles(
+			companyId, null, excludedRoleNames, roleTypes, 0, groupId,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		List<Role> allRoles = RoleLocalServiceUtil.getRoles(companyId);
+
+		List<Role> expectedRoles = new ArrayList<>();
+
+		for (Role role : allRoles) {
+			String name = role.getName();
+
+			if (name.equals(RoleConstants.ADMINISTRATOR) ||
+				name.equals(RoleConstants.GUEST)) {
+
+				continue;
+			}
+
+			int type = role.getType();
+
+			if ((type == RoleConstants.TYPE_REGULAR) ||
+				(type == RoleConstants.TYPE_ORGANIZATION)) {
+
+				expectedRoles.add(role);
+			}
+			else if ((type == RoleConstants.TYPE_PROVIDER) && role.isTeam()) {
+				Team team = TeamLocalServiceUtil.getTeam(role.getClassPK());
+
+				if (team.getGroupId() == groupId) {
+					expectedRoles.add(role);
+				}
+			}
+		}
+
+		Assert.assertEquals(expectedRoles.size(), count);
+
+		Comparator roleNameComparator = new RoleNameComparator(true);
+
+		Collections.sort(expectedRoles, roleNameComparator);
 
 		Assert.assertEquals(expectedRoles, actualRoles);
 	}

liferay/liferay-central-test-1

portal-impl/test/integration/com/liferay/portal/service/RoleLocalServiceTest.java
@@ -14,6 +14,7 @@
 
 package com.liferay.portal.service;
 
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.NoSuchGroupException;
 import com.liferay.portal.kernel.exception.RoleNameException;
 import com.liferay.portal.kernel.model.Group;
@@ -114,6 +115,70 @@ public class RoleLocalServiceTest {
 				}
 			}
 		}
+
+		Comparator roleIdComparator = new RoleRoleIdComparator();
+
+		Collections.sort(actualRoles, roleIdComparator);
+		Collections.sort(expectedRoles, roleIdComparator);
+
+		Assert.assertEquals(expectedRoles, actualRoles);
+	}
+
+	@Test
+	public void testGetGroupRolesAndTeamRoles() throws Exception {
+		Object[] organizationAndTeam = getOrganizationAndTeam();
+
+		Organization organization = (Organization)organizationAndTeam[0];
+
+		long companyId = organization.getCompanyId();
+		long groupId = organization.getGroupId();
+
+		int[] roleTypes = RoleConstants.TYPES_ORGANIZATION_AND_REGULAR;
+
+		List<String> excludedRoleNames = new ArrayList<>();
+
+		excludedRoleNames.add(RoleConstants.ADMINISTRATOR);
+		excludedRoleNames.add(RoleConstants.GUEST);
+
+		int count = RoleLocalServiceUtil.getGroupRolesAndTeamRolesCount(
+			companyId, null, excludedRoleNames, roleTypes, 0, groupId);
+
+		List<Role> actualRoles = RoleLocalServiceUtil.getGroupRolesAndTeamRoles(
+			companyId, null, excludedRoleNames, roleTypes, 0, groupId,
+			QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		List<Role> allRoles = RoleLocalServiceUtil.getRoles(companyId);
+
+		List<Role> expectedRoles = new ArrayList<>();
+
+		for (Role role : allRoles) {
+			String name = role.getName();
+
+			if (name.equals(RoleConstants.ADMINISTRATOR) ||
+				name.equals(RoleConstants.GUEST)) {
+
+				continue;
+			}
+
+			int type = role.getType();
+
+			if ((type == RoleConstants.TYPE_REGULAR) ||
+				(type == RoleConstants.TYPE_ORGANIZATION)) {
+
+				expectedRoles.add(role);
+			}
+			else if ((type == RoleConstants.TYPE_PROVIDER) && role.isTeam()) {
+				Team team = TeamLocalServiceUtil.getTeam(role.getClassPK());
+
+				if (team.getGroupId() == groupId) {
+					expectedRoles.add(role);
+				}
+			}
+		}
+
+		Assert.assertEquals(expectedRoles.size(), count);
+
+		actualRoles = new ArrayList(actualRoles);
 
 		Comparator roleIdComparator = new RoleRoleIdComparator();
 