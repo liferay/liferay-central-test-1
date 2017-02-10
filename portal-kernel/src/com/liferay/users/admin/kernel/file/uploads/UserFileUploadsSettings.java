pei-jung/liferay-portal

LPS-69211 portal-kernel - creates UserFileUploadsSettings.java

portal-kernel/src/com/liferay/users/admin/kernel/settings/UserFileUploadsSettings.java
@@ -1,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.kernel.settings;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Drew Brokke
+ */
+@ProviderType
+public interface UserFileUploadsSettings {
+
+	public boolean getImageCheckToken();
+
+	public int getImageMaxHeight();
+
+	public long getImageMaxSize();
+
+	public int getImageMaxWidth();
+
+}

liferay/liferay-central-test-1

portal-kernel/src/com/liferay/users/admin/kernel/file/uploads/UserFileUploadsSettings.java
@@ -1,0 +1,33 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.kernel.file.uploads;
+
+import aQute.bnd.annotation.ProviderType;
+
+/**
+ * @author Drew Brokke
+ */
+@ProviderType
+public interface UserFileUploadsSettings {
+
+	public int getImageMaxHeight();
+
+	public long getImageMaxSize();
+
+	public int getImageMaxWidth();
+
+	public boolean isImageCheckToken();
+
+}