liferay/liferay-central-test-1

commit 0b91d390694c21461ffbe9a6ae3b52d663fd8368

LPS-54433 Even though the user hasn't signed in, we should still populate thread locals

portal-impl/src/com/liferay/portal/servlet/filters/secure/SecureFilter.java
@@ -192,17 +192,28 @@ public class SecureFilter extends BasePortalFilter {
 
 		User user = (User)session.getAttribute(WebKeys.USER);
 
+		initThreadLocals(user);
+
+		PrincipalThreadLocal.setPassword(PortalUtil.getUserPassword(request));
+	}
+
+	protected void initThreadLocals(User user) throws Exception {
 		CompanyThreadLocal.setCompanyId(user.getCompanyId());
 
 		PrincipalThreadLocal.setName(user.getUserId());
-		PrincipalThreadLocal.setPassword(PortalUtil.getUserPassword(request));
 
 		if (!_usePermissionChecker) {
 			return;
 		}
 
 		PermissionChecker permissionChecker =
-			PermissionCheckerFactoryUtil.create(user);
+			PermissionThreadLocal.getPermissionChecker();
+
+		if (permissionChecker != null) {
+			return;
+		}
+
+		permissionChecker = PermissionCheckerFactoryUtil.create(user);
 
 		PermissionThreadLocal.setPermissionChecker(permissionChecker);
 	}
@@ -276,7 +287,13 @@ public class SecureFilter extends BasePortalFilter {
 
 			User user = PortalUtil.getUser(request);
 
-			if ((user != null) && !user.isDefaultUser()) {
+			if (user == null) {
+				user = PortalUtil.initUser(request);
+			}
+
+			initThreadLocals(user);
+
+			if (!user.isDefaultUser()) {
 				request = setCredentials(
 					request, request.getSession(), user.getUserId(), null);
 			}

commit f6cd13f8474cc399246675076859bfd711b5d9b5

LPS-70786 Avoid duplicated threadlocals setup (Regression caused by 0b91d390694c21461ffbe9a6ae3b52d663fd8368)

portal-impl/src/com/liferay/portal/servlet/filters/secure/SecureFilter.java
@@ -141,7 +141,8 @@ public class SecureFilter extends BasePortalFilter {
 				request, String.valueOf(user.getUserId()),
 				HttpServletRequest.BASIC_AUTH);
 
-			initThreadLocals(request);
+			PrincipalThreadLocal.setPassword(
+				PortalUtil.getUserPassword(request));
 		}
 
 		return request;
@@ -186,12 +187,17 @@ public class SecureFilter extends BasePortalFilter {
 				request, String.valueOf(user.getUserId()),
 				HttpServletRequest.DIGEST_AUTH);
 
-			initThreadLocals(request);
+			PrincipalThreadLocal.setPassword(
+				PortalUtil.getUserPassword(request));
 		}
 
 		return request;
 	}
 
+	/**
+	 * @deprecated As of 7.0.0, with no direct replacement
+	 */
+	@Deprecated
 	protected void initThreadLocals(HttpServletRequest request)
 		throws Exception {
 
@@ -358,7 +364,7 @@ public class SecureFilter extends BasePortalFilter {
 
 		session.setAttribute(WebKeys.USER, user);
 
-		initThreadLocals(request);
+		PrincipalThreadLocal.setPassword(PortalUtil.getUserPassword(request));
 
 		return request;
 	}