sergiogonzalez/com-liferay-adaptive-media

LPS-70625 Optiona is present

adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/configuration/AdaptiveMediaImageConfigurationHelperImpl.java
@@ -207,10 +207,8 @@ public class AdaptiveMediaImageConfigurationHelperImpl
 			return;
 		}
 
-		configurationEntryOptional.ifPresent(
-			configurationEntry ->
-				_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
-					companyId, configurationEntry));
+		_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
+			companyId, configurationEntry.get());
 
 		Collection<AdaptiveMediaImageConfigurationEntry> configurationEntries =
 			getAdaptiveMediaImageConfigurationEntries(

liferay/liferay-central-test-1

modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/configuration/AdaptiveMediaImageConfigurationHelperImpl.java
@@ -207,10 +207,8 @@ public class AdaptiveMediaImageConfigurationHelperImpl
 			return;
 		}
 
-		configurationEntryOptional.ifPresent(
-			configurationEntry ->
-				_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
-					companyId, configurationEntry));
+		_imageEntryLocalService.deleteAdaptiveMediaImageEntries(
+			companyId, configurationEntryOptional.get());
 
 		Collection<AdaptiveMediaImageConfigurationEntry> configurationEntries =
 			getAdaptiveMediaImageConfigurationEntries(