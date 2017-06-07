ealonso/liferay-portal

LPS-72921 Return vocabulary id in case when user chooses vocabulary in the asset categories selector

modules/apps/web-experience/asset/asset-categories-selector-web/src/main/resources/META-INF/resources/js/SelectCategory.es.js
@@ -85,8 +85,9 @@ class SelectCategory extends PortletBase {
 			newVal.forEach(
 				(node) => {
 					data[node.name] = {
-						categoryId: node.id,
-						value: node.name
+						categoryId: node.isVocabulary ? 0 : node.id,
+						value: node.name,
+						vocabularyId: node.isVocabulary ? node.id : 0
 					};
 				}
 			);
@@ -95,8 +96,9 @@ class SelectCategory extends PortletBase {
 				(node) => {
 					if (newVal.indexOf(node) === -1) {
 						data[node.name] = {
-							categoryId: node.id,
+							categoryId: node.isVocabulary ? 0 : node.id,
 							value: node.name,
+							vocabularyId: node.isVocabulary ? node.id : 0,
 							unchecked: true
 						};
 					}

liferay/liferay-central-test-1

modules/apps/web-experience/asset/asset-categories-selector-web/src/main/resources/META-INF/resources/js/SelectCategory.es.js
@@ -85,8 +85,9 @@ class SelectCategory extends PortletBase {
 			newVal.forEach(
 				(node) => {
 					data[node.name] = {
-						categoryId: node.id,
-						value: node.name
+						categoryId: node.vocabulary ? 0 : node.id,
+						value: node.name,
+						vocabularyId: node.vocabulary ? node.id : 0
 					};
 				}
 			);
@@ -95,8 +96,9 @@ class SelectCategory extends PortletBase {
 				(node) => {
 					if (newVal.indexOf(node) === -1) {
 						data[node.name] = {
-							categoryId: node.id,
+							categoryId: node.vocabulary ? 0 : node.id,
 							value: node.name,
+							vocabularyId: node.vocabulary ? node.id : 0,
 							unchecked: true
 						};
 					}