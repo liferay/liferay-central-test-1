JorgeFerrer/liferay-portal

LPS-71381 portal-configuration-module-configuration - ConfigurationProviderImpl - implements new interface method

modules/apps/foundation/portal-configuration/portal-configuration-module-configuration/src/main/java/com/liferay/portal/configuration/module/configuration/internal/ConfigurationProviderImpl.java
@@ -22,6 +22,7 @@ import com.liferay.portal.kernel.model.PortletInstance;
 import com.liferay.portal.kernel.module.configuration.ConfigurationException;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.GlobalServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
 import com.liferay.portal.kernel.settings.PortletInstanceSettingsLocator;
 import com.liferay.portal.kernel.settings.Settings;
@@ -138,6 +139,16 @@ public class ConfigurationProviderImpl implements ConfigurationProvider {
 			clazz,
 			new PortletInstanceSettingsLocator(
 				layout, portletId, configurationPid));
+	}
+
+	@Override
+	public <T> T getSystemConfiguration(Class<T> clazz)
+		throws ConfigurationException {
+
+		String configurationPid = _getConfigurationPid(clazz);
+
+		return getConfiguration(
+			clazz, new GlobalServiceSettingsLocator(configurationPid));
 	}
 
 	protected <T> Class<?> getOverrideClass(Class<T> clazz) {

liferay/liferay-central-test-1

modules/apps/foundation/portal-configuration/portal-configuration-module-configuration/src/main/java/com/liferay/portal/configuration/module/configuration/internal/ConfigurationProviderImpl.java
@@ -28,6 +28,7 @@ import com.liferay.portal.kernel.settings.Settings;
 import com.liferay.portal.kernel.settings.SettingsException;
 import com.liferay.portal.kernel.settings.SettingsFactory;
 import com.liferay.portal.kernel.settings.SettingsLocator;
+import com.liferay.portal.kernel.settings.SystemSettingsLocator;
 import com.liferay.portal.kernel.settings.TypedSettings;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -138,6 +139,16 @@ public class ConfigurationProviderImpl implements ConfigurationProvider {
 			clazz,
 			new PortletInstanceSettingsLocator(
 				layout, portletId, configurationPid));
+	}
+
+	@Override
+	public <T> T getSystemConfiguration(Class<T> clazz)
+		throws ConfigurationException {
+
+		String configurationPid = _getConfigurationPid(clazz);
+
+		return getConfiguration(
+			clazz, new SystemSettingsLocator(configurationPid));
 	}
 
 	protected <T> Class<?> getOverrideClass(Class<T> clazz) {